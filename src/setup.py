53a53a1506f27433e098c80f3871c95ab65c2d56
rohi3999 <rsingh@esri.com>
2016-03-22 07:45:37 -0700

    Update license
    Changing to proprietary license till a decision on open sourcing is
    made.

--- src/setup.py
+++ src/setup.py
@@ -37,7 +37,7 @@ setup(
     author_email='python@esri.com',
 
     # Choose your license
-    license='Apache 2.0',
+    license='Proprietary License',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -51,7 +51,7 @@ setup(
         'Intended Audience :: Developers',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache License',
+        'License :: Other/Proprietary License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
