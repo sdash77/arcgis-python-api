2ee3207d343d0ff371230969de8f1ae980439943
rohitgeo <rsingh@esri.com>
2016-06-04 07:37:56 -0700

    Use Sphinx-bootstrap + Calcite-Bootstrap for Sphinx API Docs
    theme to make the API documentation use Esri styles pages. Thanks for
    the suggestion, @ajturner

--- docs/apidoc/conf.py
+++ docs/apidoc/conf.py
@@ -15,7 +15,7 @@
 
 import sys
 import os
-
+import sphinx_bootstrap_theme
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -31,7 +31,7 @@ import os
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
+#    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -102,8 +102,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["."]
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_theme = 'sphinx_rtd_theme'
+#html_theme_path = ["."]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
