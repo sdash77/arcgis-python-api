2e358f6a61811d8139dee916c5df52b1be8f36aa
rohitgeo <rsingh@esri.com>
2016-06-15 10:30:20 -0700

    minor setup.py changes
    version and naming

--- src/setup.py
+++ src/setup.py
@@ -24,9 +24,9 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='0.2',
 
-    description='ArcGIS Python',
+    description='ArcGIS Python API',
     long_description='Python API to your Web GIS',
 
     # The project's main homepage.
