caf253df2f7a4dbad33bd695e723198eb26a232f
rohitgeo <rsingh@esri.com>
2016-07-07 13:06:45 -0700

    add missing '/' in thumbnail path
    for displaying user thumbnails

--- src/arcgis/gis.py
+++ src/arcgis/gis.py
@@ -1748,7 +1748,7 @@ class User(dict):
         if thumbnail_file is None:
             return 'http://www.arcgis.com/home/js/arcgisonline/css/images/no-user-thumb.jpg'
         else:
-            thumbnail_url_path = self._portal.con.baseurl + 'community/users/' + self.username + '/info/' + thumbnail_file
+            thumbnail_url_path = self._portal.con.baseurl + '/community/users/' + self.username + '/info/' + thumbnail_file
             return thumbnail_url_path
 
     def _repr_html_(self):
