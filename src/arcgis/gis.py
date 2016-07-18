26e950dfdadd3eab1a97207ca21aa153fd1f06f0
rohitgeo <rsingh@esri.com>
2016-07-18 11:47:59 -0700

    Typo in comment

--- src/arcgis/gis.py
+++ src/arcgis/gis.py
@@ -2522,7 +2522,7 @@ class Item(dict):
     def get_data(self, try_json=True):
         """Returns the data for the item.
         If the data is a file, it's downloaded and the path to the downloaded file is returned.
-        If if try_json is True, the method tries to convert it to a Python dict and returns it.
+        Else if try_json is True, the method tries to convert it to a Python dict and returns it.
         To convert this dict to string using json.dumps(data). 
         Else, returns the data as a byte array, that can be converted to string using data.decode('utf-8')"""
         return self._portal.get_item_data(self.itemid, try_json)
