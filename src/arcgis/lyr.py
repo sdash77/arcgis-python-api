2b50552c13bd98959df579d1e3995ef61051872e
rohitgeo <rsingh@esri.com>
2016-08-06 23:18:39 -0700

    Add support for schematics and all other layers and services
    using the invoke method on service/layer

--- src/arcgis/lyr.py
+++ src/arcgis/lyr.py
@@ -54,6 +54,17 @@ class Layer(object):
     @property
     def _js_lyr(self):
         return { 'type' : type(self).__name__, 'url' : self.url }
+
+    def invoke(self, method, **kwargs):
+        """Invokes the specified method on this service passing in parameters from the kwargs name-value pairs"""
+        url = self._url + "/" + method
+        params = { "f" : "json"}
+        if len(kwargs) > 0:
+            for k,v in kwargs.items():
+                params[k] = v
+                del k,v
+        return self._con.post(path=url, postdata=params)
+
 class GISService(object):
     """ a GIS service
     """
@@ -89,6 +100,16 @@ class GISService(object):
     
     def __repr__(self):
         return '<%s url:"%s">' % (type(self).__name__, self.url)
+
+    def invoke(self, method, **kwargs):
+        """Invokes the specified method on this service passing in parameters from the kwargs name-value pairs"""
+        url = self._url + "/" + method
+        params = { "f" : "json"}
+        if len(kwargs) > 0:
+            for k,v in kwargs.items():
+                params[k] = v
+                del k,v
+        return self._con.post(path=url, postdata=params)
 
 class VectorTileLayer(Layer):
     def __init__(self, url, gis=None, dictdata=None):
@@ -606,6 +627,109 @@ class NetworkService(GISService):
             self.init()
         self._load_layers()
         return self._closestFacilityLayers
+
+class SchematicsService(GISService):
+    def __init__(self, url, gis=None, dictdata=None):
+        super(SchematicsService, self).__init__(url, gis, dictdata)
+
+    @property
+    def diagrams(self):
+        """
+        The Schematic Diagrams resource represents all the schematic diagrams
+        under a schematic service. It is returned as an array of Schematic
+        Diagram resource by the REST API.
+        """
+        params = {"f" : "json"}
+        exportURL = self._url + "/diagrams"
+        return self._con.get(path=exportURL,
+                             params=params)
+    #----------------------------------------------------------------------
+    @property
+    def folders(self):
+        """
+        The Schematic Folders resource represents the set of schematic folders
+        in the schematic dataset(s) related to the schematic layers under a
+        schematic service. It is returned as an array of <Schematic Folder Object>
+        by the REST API.
+        """
+        params = {"f" : "json"}
+        exportURL = self._url + "/folders"
+        return self._con.get(path=exportURL,
+                         params=params)
+    #----------------------------------------------------------------------
+    @property
+    def schematic_layers(self):
+        """
+        The Schematic Layers resource represents all the schematic layers
+        under a schematic service published by ArcGIS Server. It is returned
+        as an array of Schematic Layer resources by the REST API.
+        """
+        params = {"f" : "json"}
+        exportURL = self._url + "/schematicLayers"
+        return self._con.get(path=exportURL,
+                         params=params)
+    #----------------------------------------------------------------------
+    @property
+    def templates(self):
+        """
+        The Schematic Diagram Templates represents all the schematic diagram
+        templates related to the published schematic layers under a schematic
+        service. It is returned as an array of Schematic Diagram Template
+        resources by the REST API.
+        """
+        params = {"f" : "json"}
+        exportURL = self._url + "/templates"
+        return self._con.get(path=exportURL,
+                                 params=params)
+    #----------------------------------------------------------------------
+    def search_diagrams(self,whereClause=None,relatedObjects=None,
+                       relatedSchematicObjects=None):
+        """
+        The Schematic Search Diagrams operation is performed on the schematic
+        service resource. The result of this operation is an array of Schematic
+        Diagram Information Object.
+
+        It is used to search diagrams in the schematic service by criteria;
+        that is, diagrams filtered out via a where clause on any schematic
+        diagram class table field, diagrams that contain schematic features
+        associated with a specific set of GIS features/objects, or diagrams
+        that contain schematic features associated with the same GIS features/
+        objects related to another set of schematic features.
+
+        Inputs:
+            whereClause - A where clause for the query filter. Any legal SQL
+                          where clause operating on the fields in the schematic
+                          diagram class table is allowed. See the Schematic
+                          diagram class table fields section below to know the
+                          exact list of field names that can be used in this
+                          where clause.
+            relatedObjects - An array containing the list of the GIS features/
+                             objects IDs per feature class/table name that are in
+                             relation with schematic features in the resulting
+                             queried diagrams. Each GIS feature/object ID
+                             corresponds to a value of the OBJECTID field in the
+                             GIS feature class/table.
+            relatedSchematicObjects - An array containing the list of the
+                                      schematic feature names per schematic
+                                      feature class ID that have the same
+                                      associated GIS features/objects with
+                                      schematic features in the resulting
+                                      queried diagrams. Each schematic feature
+                                      name corresponds to a value of the
+                                      SCHEMATICTID field in the schematic
+                                      feature class.
+        """
+        params = {"f" : "json"}
+        if whereClause:
+            params["where"] = whereClause
+        if relatedObjects:
+            params["relatedObjects"] = relatedObjects
+        if relatedSchematicObjects:
+            params["relatedSchematicObjects"] = relatedSchematicObjects
+
+        exportURL = self._url + "/searchDiagrams"
+        return self._con.get(path=exportURL,
+                             params=params)
 
 class NetworkLayer(Layer):
     """
