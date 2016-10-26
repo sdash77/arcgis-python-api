4bb1e815289d54361785756241470cb86247d07e
rohitgeo <rsingh@esri.com>
2016-10-26 00:28:27 -0700

    fix IWA support for GET requests for #220

--- src/arcgis/_impl/connection.py
+++ src/arcgis/_impl/connection.py
@@ -683,10 +683,15 @@ class _ArcGISConnection(object):
 
             handlers = self.get_handlers()
             opener = request.build_opener(*handlers)
+
             opener.addheaders = headers
+
+            resp = opener.open(url)
+
             #request.install_opener(opener)
-            req = request.Request(url)
-            resp = request.urlopen(req)
+            #req = request.Request(url)
+            #resp = request.urlopen(req)
+
             resp_data, is_file = self._process_response(resp,
                                                out_folder=out_folder,
                                                file_name=file_name,
