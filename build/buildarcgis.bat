ea4608991ef9865dc7ea907f9adc1ef1a450a49c
Rohit Singh <rsingh@esri.com>
2016-12-14 10:25:10 -0800

    build update

--- build/buildarcgis.bat
+++ build/buildarcgis.bat
@@ -4,5 +4,5 @@ anaconda remove -f geonuma/arcgis/1.0/win-64/arcgis-1.0-py35_1.tar.bz2
 mkdir ___output
 conda build arcgis
 conda build --py 3.4 arcgis
-conda convert -f -p win-32 C:\Users\rohit\Anaconda3\conda-bld\win-64\arcgis-1.0-py34_1.tar.bz2 -o ___output
-conda convert -f -p win-32 C:\Users\rohit\Anaconda3\conda-bld\win-64\arcgis-1.0-py35_1.tar.bz2 -o ___output
+conda convert -f -p win-32 C:\Users\rohit\Anaconda3\conda-bld\win-64\arcgis-1.0-py34_3.tar.bz2 -o ___output
+conda convert -f -p win-32 C:\Users\rohit\Anaconda3\conda-bld\win-64\arcgis-1.0-py35_3.tar.bz2 -o ___output
