e5b7c361c90219ae878e59776dd7f0aa3994af1e
Michael Gayheart <mgayheart@esri.com>
2017-02-01 17:50:19 -0800

    Issue #282 (#285)
    Issue #282

--- src/arcgis/_impl/portalpy.py
+++ src/arcgis/_impl/portalpy.py
@@ -383,7 +383,9 @@ class Portal(object):
                        description = "",
                        copyright_text = "",
                        wkid=102100,
-                       service_type="imageService", create_params=None, owner=None, folder=None):
+                       service_type="imageService",
+                       create_params=None,
+                       owner=None, folder=None, common_params=None):
         """ Creates service.
          #"Create,Delete,Query,Update,Editing",
         :return:
@@ -443,6 +445,12 @@ class Portal(object):
             postdata['createParameters'] = json.dumps(createParameters)
 
         postdata['outputType'] = service_type
+
+        # If common_params dictionary provided, add each key/value pair to postdata.
+        if common_params is not None:
+            for key in common_params:
+                if key not in postdata:
+                    postdata[key] = common_params[key]
 
         resp = self.con.post(path, postdata)
         if resp and resp.get('success'):
