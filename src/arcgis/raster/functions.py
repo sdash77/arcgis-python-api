8b39938b88dc639210e7e092d33c60daf4238b0b
Rohit Singh <rsingh@esri.com>
2017-04-17 13:46:31 -0700

    fix slope and hillshade functions by setting DEM variable

--- src/arcgis/raster/functions.py
+++ src/arcgis/raster/functions.py
@@ -858,9 +858,9 @@ def hillshade(dem, azimuth=215.0, altitude=75.0, z_factor=0.3, slope_type=1, ps_
     template_dict = {
         "rasterFunction": "Hillshade",
         "rasterFunctionArguments": {
-            "Raster": raster
+            "DEM": raster
         },
-        "variableName": "Raster"
+        "variableName": "DEM"
     }
 
     if astype is not None:
@@ -2504,7 +2504,6 @@ def slope(dem, z_factor=None, slope_type=None, ps_power=None, psz_factor=None, r
     :param ps_power: new at 10.2. double, used together with SCALED slope type
     :param psz_factor: new at 10.2. double, used together with SCALED slope type
     :param remove_edge_effect: new at 10.2. boolean, true of false
-    :param dem: optional, default is the image service
     :param astype: output pixel type
     :return: the output raster
 
@@ -2516,9 +2515,9 @@ def slope(dem, z_factor=None, slope_type=None, ps_power=None, psz_factor=None, r
     template_dict = {
         "rasterFunction": "Slope",
         "rasterFunctionArguments": {
-            "Raster": raster
+            "DEM": raster
         },
-        "variableName": "Raster"
+        "variableName": "DEM"
     }
 
     if astype is not None:
@@ -2534,8 +2533,8 @@ def slope(dem, z_factor=None, slope_type=None, ps_power=None, psz_factor=None, r
         template_dict["rasterFunctionArguments"]["PSZFactor"] = psz_factor
     if remove_edge_effect is not None:
         template_dict["rasterFunctionArguments"]["RemoveEdgeEffect"] = remove_edge_effect
-    if dem is not None:
-        template_dict["rasterFunctionArguments"]["DEM"] = dem
+    # if dem is not None:
+    #     template_dict["rasterFunctionArguments"]["DEM"] = raster
 
     return _clone_layer(layer, template_dict)
 
