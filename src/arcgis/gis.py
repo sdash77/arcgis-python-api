c0eafdf0d3d676bb34bf6b76c9c1d35d6d2cb8d8
Rohit Singh <rsingh@esri.com>
2017-04-17 13:24:50 -0700

    Revert token handling logic in _GISResource
    1) current logic leads to geocoding error:
    AttributeError: 'Geocoder' object has no attribute '_lazy_properties'
    
    2) prevents leakage of GIS token as it always attempts to get server
    token instead of using GIS token - using _con.token does not attempt to
    fetch server token in all cases leading to leakage of GIS token
    
    cc: @achapkowski

--- src/arcgis/gis.py
+++ src/arcgis/gis.py
@@ -4622,7 +4622,7 @@ class _GISResource(object):
 
     def _refresh(self):
         params = {"f": "json"}
-        dictdata = self._con.post(self.url, params, token=self._con.token)
+        dictdata = self._con.post(self.url, params, token=self._lazy_token)
         self._lazy_properties = PropertyMap(dictdata)
 
     @property
@@ -4649,7 +4649,7 @@ class _GISResource(object):
                 if self._con._token is None:
                     self._lazy_token = None
                 else:
-                    self._lazy_token = self._con.token
+                    self._lazy_token = self._con.generate_portal_server_token(self._url)
 
                 self._refresh()
 
