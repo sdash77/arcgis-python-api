09b5522fab11401d338f23c2e80861cf6e354992
achapkowski <achapkowski@esri.com>
2017-04-21 14:23:40 -0400

    add pyshp support for reading shapefiles if arcpy is not present.

--- src/arcgis/data/geodataset/io/fileops.py
+++ src/arcgis/data/geodataset/io/fileops.py
@@ -19,7 +19,7 @@ from ....geometry import types
 try:
     import arcpy
     from arcpy import da
-    HASARCPY = False#TODO change back to True
+    HASARCPY = True
 except:
     # warn(message="ArcPy not found.")
     HASARCPY = False
@@ -41,8 +41,8 @@ def from_featureclass(filename, **kwargs):
      sr: spatial reference object
      fields: list of fields to extract from the table
     """
+    from .. import SpatialDataFrame
     if HASARCPY:
-        from .. import SpatialDataFrame
         sql_clause = kwargs.pop('sql_clause', (None,None))
         where_clause = kwargs.pop('where_clause', None)
         sr = kwargs.pop('sr', None)
@@ -86,9 +86,25 @@ def from_featureclass(filename, **kwargs):
         else:
             sdf.sr = sdf.geometry[0].spatialReference
         return sdf
-    elif HASARCPY == False and HASPYSHP:
-
-        return
+    elif HASARCPY == False and \
+         HASPYSHP == True and\
+         filename.lower().find('.shp') > -1:
+        geoms = []
+        records = []
+        reader = shapefile.Reader(filename)
+        fields = [field[0] for field in reader.fields if field[0] != 'DeletionFlag']
+        for r in reader.shapeRecords():
+            atr = dict(zip(fields, r.record))
+            geom = types.Geometry(r.shape.__geo_interface__)
+            atr['SHAPE'] = geom
+            records.append(atr)
+            del atr
+            del r
+            del geom
+        sdf = SpatialDataFrame(records)
+        sdf.set_geometry(col='SHAPE')
+        sdf.reset_index(inplace=True)
+        return sdf
     return
 #--------------------------------------------------------------------------
 def to_featureclass(df, out_name, out_location=None,
