d76a2b87e5b5a7c521fb68c575aafcdd9de21196
Rohit Singh <rsingh@esri.com>
2017-05-14 23:01:41 -0700

    reduce default capabilities on create_service for #381

--- src/arcgis/gis.py
+++ src/arcgis/gis.py
@@ -2300,8 +2300,8 @@ class ContentManager(object):
             max_record_count        optional int, ,maximum number of records in query operations
             supported_query_formats optional string, formats in which query results are returned
             capabilities            optional string, Specify service capabilities.
-                                    If left unspecified, 'Image,Catalog,Metadata,Download,Pixels,Edit,Mensuration,Uploads'
-                                    are used for image services, and 'Create,Delete,Query,Update,Editing'
+                                    If left unspecified, 'Image,Catalog,Metadata,Download,Pixels'
+                                    are used for image services, and 'Query'
                                     are used for feature services, and 'Query' otherwise
             description             optional string, a user-friendly description for the published dataset.
             copyright_text          optional string, copyright information associated with the dataset.
@@ -2356,9 +2356,9 @@ class ContentManager(object):
         """
         if capabilities is None:
             if service_type == 'imageService':
-                capabilities = 'Image,Catalog,Metadata,Download,Pixels,Edit,Mensuration,Uploads'
+                capabilities = 'Image,Catalog,Metadata,Download,Pixels'
             elif service_type == 'featureService':
-                capabilities = 'Create,Delete,Query,Update,Editing'
+                capabilities = 'Query'
             else:
                 capabilities = 'Query'
 
