22e78ac7e9e999b285cf0e65a465bd0af5fcb352
Rohit Singh <rsingh@esri.com>
2017-05-22 08:08:07 -0700

    doc update for gis.content.import_data and add

--- src/arcgis/gis.py
+++ src/arcgis/gis.py
@@ -2159,7 +2159,7 @@ class ContentManager(object):
         self._portal = gis._portal
 
     def add(self, item_properties, data=None, thumbnail=None, metadata=None, owner=None, folder=None):
-        """ Adds content to a Portal by creating an item.
+        """ Adds content to the GIS by creating an item.
 
 
             .. note::
@@ -2203,6 +2203,8 @@ class ContentManager(object):
             description        optional string.  Description of the item.
             -----------------  ----------------------------------------------------------------------------
             title              optional string.  Name of the item.
+            -----------------  ----------------------------------------------------------------------------
+            text               optional string.  For text based items such as Feature Collections & WebMaps
             -----------------  ----------------------------------------------------------------------------
             url                optional string.  URL to item that are based on URLs.
             -----------------  ----------------------------------------------------------------------------
@@ -2553,26 +2555,29 @@ class ContentManager(object):
 
     def import_data(self, df, address_fields=None, **kwargs):
         """
-        Imports a Pandas data frame, that has an address column,
-        to a feature collection
+        Imports a Pandas data frame, that has an address column, or an arcgis spatial dataframe
+        into the GIS.
 
+        Spatial dataframes are imported into the GIS and published as feature layers.
+        Pandas dataframes that have an address column are imported as an in memory feature collection.
+        Note: By default, there is a limit of 1,000 rows/features for Pandas dataframes. This limit isn't there for spatial dataframes.
 
         df : pandas dataframe or arcgis.SpatialDataFrame
         address_fields : dict containing mapping of df columns to address fields, eg: { "CountryCode" : "Country"} or { "Address" : "Address" }
         title: optional title of the item. This is used for spatial dataframe objects.
-        tags: optional tags when publishing a spatial dataframe to Portal/AGOL
+        tags: optional tags when publishing a spatial dataframe to the the GIS
         Returns feature collection, that can be used for analysis, visualization or published to the GIS as an item
         """
         from .features import FeatureCollection
         from . import SpatialDataFrame
         from ._impl.common._utils import zipws
-        import zipfile
+
         import shutil
         from uuid import uuid4
         import pandas as pd
         try:
             import arcpy
-            has_arcpy = False#True
+            has_arcpy = True
         except ImportError:
             has_arcpy = False
         try:
