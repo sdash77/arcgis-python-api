a14859a1b98685745a7bc4d5da29f27718bef7ba
achapkowski <achapkowski@esri.com>
2017-06-05 10:41:00 -0400

    fixed issue with portaladmin causing errors when portal/home given for a site URL.  Changed from using self._url to self._portal.url instead.

--- src/arcgis/gis.py
+++ src/arcgis/gis.py
@@ -170,10 +170,10 @@ class GIS(object):
         self._tools = _Tools(self)
         if set_active:
             arcgis.env.active_gis = self
-        # if self.properties.isPortal:
-        #     from ._impl.portaladmin.portaladmin import PortalAdminManager
-        #     self.portaladmin = PortalAdminManager(url="%s/portaladmin" % self._url,
-        #                                           gis=self)
+        if self.properties.isPortal:
+            from ._impl.portaladmin.portaladmin import PortalAdminManager
+            self.portaladmin = PortalAdminManager(url="%s/portaladmin" % self._portal.url,
+                                                  gis=self)
     @property
     def servers(self):
         """
