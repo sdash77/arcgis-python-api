435ec6fafbbc8e70458088642b4592a06aeaebd9
achapkowski <achapkowski@esri.com>
2017-06-12 13:52:27 -0400

    Fixed portaladmin import issue

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -173,10 +173,10 @@ class GIS(object):
         if self.properties.isPortal and \
            hasattr(self.users.me, 'role') and \
            self.users.me.role == "org_admin":
-            from ._impl.portaladmin.portaladmin import PortalAdminManager
-            from ._impl.oauth import OAuth
-            self.oauth = OAuth(url="%s/oauth2" % self._portal.url,
-                               gis=self)
+            from .._impl.portaladmin.portaladmin import PortalAdminManager
+            #from ._impl.oauth import OAuth
+            #self.oauth = OAuth(url="%s/oauth2" % self._portal.url,
+            #                   gis=self)
             self.admin = PortalAdminManager(url="%s/portaladmin" % self._portal.url,
                                             gis=self)
     @property
