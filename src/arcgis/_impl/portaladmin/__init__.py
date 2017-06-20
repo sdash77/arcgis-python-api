5a240a54ca8f3b6721535efeb332ad1482976ead
Andrew <achapkowski@users.noreply.github.com>
2017-06-20 08:37:33 -0400

    Portal Admin Manager changes. (#462)
    portaladmin

--- src/arcgis/_impl/portaladmin/__init__.py
+++ src/arcgis/_impl/portaladmin/__init__.py
@@ -1,5 +1,5 @@
 """
-initializer for portaladmin package
+initializer for portaladmin sub-package
 """
 from .portaladmin import PortalAdminManager
 __all__ = ['PortalAdminManager']
--- src/arcgis/_impl/portaladmin/portaladmin.py
+++ src/arcgis/_impl/portaladmin/portaladmin.py
@@ -1,7 +1,10 @@
+"""
+Entry point to working with local enterprise GIS functions
+"""
 from ..connection import _ArcGISConnection
 from ...gis import GIS
 from ._base import BasePortalAdmin
-
+########################################################################
 class PortalAdminManager(BasePortalAdmin):
     """
     This is the root resource for administering your portal. Starting from
@@ -11,14 +14,22 @@ class PortalAdminManager(BasePortalAdmin):
     configured using the Create Site operation. Once initialized, the
     portal environment is available through System and Security resources.
 
-
+    Parameter:
+    :param url: web address to portaladmin API
+    :param gis: GIS object containing Administrative credentials
+    :param initialize: (optional) if True, properties of REST endpoint are
+    loaded on creation of object. False (default) means they are loaded
+    when needed.
     """
     _logs = None
     _federation = None
     _system = None
     _security = None
+    _machines = None
+    _site = None
     _url = None
     _gis = None
+    #----------------------------------------------------------------------
     def __init__(self, url, gis=None, **kwargs):
         """initializer"""
         super(PortalAdminManager, self).__init__(url=url,
@@ -36,12 +47,6 @@ class PortalAdminManager(BasePortalAdmin):
         if initialize:
             self._init(self._gis)
     #----------------------------------------------------------------------
-    #def __str__(self):
-    #    return '<%s at %s>' % (type(self).__name__, self._url)
-    #----------------------------------------------------------------------
-    #def __repr__(self):
-    #    return '<%s at %s>' % (type(self).__name__, self._url)
-    #----------------------------------------------------------------------
     @property
     def machines(self):
         """
@@ -49,10 +54,11 @@ class PortalAdminManager(BasePortalAdmin):
         machine has a status that indicates whether the machine is ready
         to accept requests.
         """
-        params = {'f' : 'json'}
-        url = "%s/%s" % (self._url, "machines")
-        return self._con.get(path=url,
-                             params=params)
+        if self._machines is None:
+            from ._machines import Machines
+            url = "%s/machines" % self._url
+            self._machines = Machines(url=url, gis=self._gis, portaladmin=self)
+        return self._machines
     #----------------------------------------------------------------------
     @property
     def security(self):
@@ -65,6 +71,17 @@ class PortalAdminManager(BasePortalAdmin):
             url = "%s/security" % self._url
             self._security = Security(url=url, gis=self._gis)
         return self._security
+    #----------------------------------------------------------------------
+    @property
+    def site(self):
+        """
+        Site is the root resources used after a local GIS is installed. Here
+        administrators can create, export, import, and join sites.
+        """
+        if self._site is None:
+            from ._site import Site
+            self._site = Site(url=self._url, portaladmin=self)
+        return self._site
     #----------------------------------------------------------------------
     @property
     def logs(self):
@@ -91,185 +108,13 @@ class PortalAdminManager(BasePortalAdmin):
     @property
     def system(self):
         """
-        The importSite operation lets you restore your site from a backup
-        site configuration file that you created using the exportSite
-        operation. It imports the site configuration file into the
-        currently running portal site.
-        The importSite operation will replace all site configurations with
-        information included in the backup site configuration file. See the
-        exportSite operation documentation for details on what the backup
-        file includes. The importSite operation also updates the portal
-        content index.
+        This resource provides access to the ArcGIS Web Adaptor
+        configuration, portal directories, database management server,
+        indexing capabilities, license information, and the properties of
+        your portal.
         """
         if self._system is None:
             from ._system import System
             url = "%s/system" % self._url
             self._system = System(url=url, gis=self._gis)
         return self._system
-    #----------------------------------------------------------------------
-    def machine_status(self, machine_name):
-        """
-        This operation checks whether a portal machine is ready to receive
-        requests.
-        """
-        url = "%s/machines/status/%s" % (self._url, machine_name)
-        params = {"f": "json"}
-        res = self._con.get(path=url,
-                            params=params)
-        if 'status' in res:
-            return res['status'] == 'success'
-        return False
-    #----------------------------------------------------------------------
-    def create(self,
-               username,
-               password,
-               full_name,
-               email,
-               content_store,
-               description="",
-               question_idx=None,
-               question_ans=None):
-        """
-        The create site operation initializes and configures Portal for
-        ArcGIS for use. It must be the first operation invoked after
-        installation. Creating a new site involves:
-          - Creating the initial administrator account
-          - Creating a new database administrator account (which is same as
-            the initial administrator account)
-          - Creating token shared keys
-          - Registering directories
-        This operation is time consuming, as the database is initialized
-        and populated with default templates and content. If the database
-        directory is not empty, this operation attempts to migrate the
-        database to the current version while keeping its data intact. At
-        the end of this operation, the web server that hosts the API is
-        restarted.
-
-        Parameters:
-         :username: initial admin account name
-         :password: password for initial admin account
-         :full_name: full name of the admin account
-         :email: account email address
-         :content_store: JSON string including the path to the location of
-          the site's content.
-         :description: optional descript for the account
-         :question_idx: index of the secret question to retrieve a
-          forgotten password
-         :question_ans: answer to the secret question
-        """
-        url = "%s/createNewSite" % self._url
-        params = {"f": "json",
-                  "username" : username,
-                  "password" : password,
-                  "fullName" : full_name,
-                  "email" : email,
-                  "description" : description,
-                  "contentStore" : content_store}
-        if question_idx and question_ans:
-            params['securityQuestionIdx'] = question_idx
-            params['securityQuestionAns'] = question_ans
-        return self._con.post(path=url, postdata=params)
-    #----------------------------------------------------------------------
-    def export_site(self, location):
-        """
-        This operation exports the portal site configuration to a location
-        you specify. The exported file includes the following information:
-          Content directory - the content directory contains the data
-           associated with every item in the portal
-          Database dump file - a plain-text file that contains the SQL
-           commands required to reconstruct the portal database
-          Configuration store connection file - a JSON file that contains
-           the database connection information
-
-        Parameters:
-         :location: path to the folder accessible to the portal where the
-          exported site configuration will be written.
-        """
-        url = "%s/exportSite" % self._url
-        params = {'f' : 'json',
-                  'location' : location}
-        return self._con.post(path=url,
-                              postdata=params)
-    #----------------------------------------------------------------------
-    def import_site(self, location):
-        """
-        The importSite operation lets you restore your site from a backup
-        site configuration file that you created using the exportSite
-        operation. It imports the site configuration file into the
-        currently running portal site.
-        The importSite operation will replace all site configurations with
-        information included in the backup site configuration file. See the
-        export_site operation documentation for details on what the backup
-        file includes. The importSite operation also updates the portal
-        content index.
-
-        Parameters:
-         :location: A file path to an exported configuration.
-
-        """
-        url = "%s/importSite" % self._url
-        if url.find(":7443") == -1:
-            raise ValueError(
-                "You must access portal not using the web adaptor (port 7443)"
-            )
-        params = {'f' : 'json',
-                      'location' : location}
-        res =  self._con.post(path=url,
-                              postdata=params)
-        if 'status' in res:
-            return res['status'] == 'success'
-        return False
-    #----------------------------------------------------------------------
-    def join(self, admin_url, username, password):
-        """
-        The joinSite operation connects a portal machine to an existing
-        site. You must provide an account with administrative privileges to
-        the site for the operation to be successful.
-        When an attempt is made to join a site, the site validates the
-        administrative credentials, then returns connection information
-        about its configuration store back to the portal machine. The portal
-        machine then uses the connection information to work with the
-        configuration store.
-        If this is the first portal machine in your site, use the Create
-        Site operation instead.
-        The joinSite operation:
-         - Registers a machine to an existing site (active machine)
-         - Creates a snapshot of the database of the active machine
-         - Updates the token shared key
-         - Updates Web Adaptor configurations
-        Sets up replication to keep the database of both machines in sync
-        The operation is time-consuming as the database is configured on
-        the machine and all configurations are applied from the active
-        machine. After the operation is complete, the web server that hosts
-        the API will be restarted.
-
-        Parameters:
-         :admin_url: The admin URL of the existing portal site to which a
-          machine will be joined
-         :username: username for the initial administrator account of the
-          existing portal site.
-         :password:  password for the initial administrator account of the
-          existing portal site.
-
-        """
-        url = "%s/joinSite" % self._url
-        params = {'f' : 'json',
-                  'machineAdminUrl' : admin_url,
-                  'username' : username,
-                  'password' : password}
-        return self._con.post(path=url,
-                                  postdata=params)
-    #----------------------------------------------------------------------
-    def unregister_machine(self, machine_name):
-        """
-        This operation unregisters a portal machine from a portal site. The
-        operation can only performed when there are two machines
-        participating in a portal site.
-
-        Parameters:
-         :machine_name: name of the machine to be unregistered
-        """
-        params = {"f" : "json",
-                  "machineName" : machine_name}
-        url = "%s/machines/unregister" % self._url
-        return self._con.post(path=url, postdata=params)
