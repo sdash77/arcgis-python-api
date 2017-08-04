5800c932dc13a6267fa50245407232985a9a852a
achapkowski <achapkowski@esri.com>
2017-08-04 08:38:15 -0400

    - resolve issue #521 - updated _log.py doctstrings.

--- src/arcgis/gis/admin/_logs.py
+++ src/arcgis/gis/admin/_logs.py
@@ -8,6 +8,15 @@ class Logs(BasePortalAdmin):
     """
     Logs are records written by various components of the portal. You can
     query the logs, clean the logs, and edit log settings.
+
+    ================  ===============================================================
+    **Argument**      **Description**
+    ----------------  ---------------------------------------------------------------
+    gis               required GIS, portal connection object
+    ----------------  ---------------------------------------------------------------
+    url               required string, web address of the log resource
+    ================  ===============================================================
+
     """
     _gis = None
     _url = None
@@ -51,6 +60,15 @@ class Logs(BasePortalAdmin):
     def settings(self, value):
         """
         Reads/writes the current log settings for the portal.
+
+        ================  ===============================================================
+        **Argument**      **Description**
+        ----------------  ---------------------------------------------------------------
+        value             required dictionary, the dictionary of the log settings
+        ================  ===============================================================
+
+        :returns:
+           None
         """
         url = "%s/settings/edit" % self._url
         params = {'f' : 'json'}
@@ -61,68 +79,88 @@ class Logs(BasePortalAdmin):
             raise ValueError("Value must be a dictionary")
         return self._con.post(path=url, postdata=params)
     #----------------------------------------------------------------------
-    def query(self, start_time, end_time=None,
-              level="WARNING", query_filter="*",
+    def query(self,
+              start_time,
+              end_time=None,
+              level="WARNING",
+              query_filter="*",
               page_size=1000):
         """
         The query operation allows you to aggregate, filter, and page
         through logs written by the portal.
 
-        Parameters:
-         :start_time: The most recent time to query. If the hasMore member
-          of the response object is true, then to get the next set of
-          records, pass the endTime member as the startTime parameter for
-          the next request. This parameter is optional.
-          Time can be specified as a portal timestamp (format in
-          yyyy-mm-ddThh:mm:ss) or in milliseconds since UNIX epoch. For
-          example:
-          Timestamp: { "startTime": "2015-08-01T15:17:20,123", ... }
-          Milliseconds: { "startTime": 1312237040123, ... }
-          Default: now
-         :end_time: The oldest time to include in the result set. You can
-          use this to limit the query to the last number of minutes, hours,
-          days, months, and years as needed. If since PortalStart is true,
-          then the default is all logs since the portal was started. This
-          parameter is optional.
-         :level: Can be one of [OFF, SEVERE, WARNING, INFO, FINE, VERBOSE,
-          DEBUG]. Returns only records with a log level at or more severe
-          than the level specified. This parameter is required.
-          Default: WARNING
-         :query_filter: Filtering is allowed by any combination of codes,
-          users, and source components. The filter accepts a comma
-          delimited list of filter definitions. If any definition is
-          omitted, it defaults to all. This parameter is required.
-          Example:
-          {"codes":[204000-205999,212015,219114], "users":["admin","jcho"],
-           "source": ["PORTAL ADMIN"]}
-          The source of logged events are generated from the sharing,
-          administrative, and portal components of the software.
-          For example:
-           - Events related to publishing and users are categorized under
-             SHARING.
-           - Events related to security and indexing are categorized under
-             PORTAL ADMIN.
-           - Events related to installing the software are categorized
-             under PORTAL.
-         :page_size: The maximum number of log records to be returned by
-          this query. This parameter is optional.
-          Default: 1000
-        :Returns:
-         JSON response of the log messages
+        ================  ===============================================================
+        **Argument**      **Description**
+        ----------------  ---------------------------------------------------------------
+        start_time        required datetime/integer. The most recent time to query.
+                          Time can be specified as a portal timestamp (format in
+                          "%Y-%m-%dT%H:%M:%S") or in milliseconds since UNIX epoch. For
+                          :Examples:
+                          Datetime Object: datetime.datetime.now()
+                          Timestamp: "2015-08-01T15:17:20,123"
+                          Milliseconds: 1312237040123
+                          Default: datetime.datetime.now()
+        ----------------  ---------------------------------------------------------------
+        end_time          optional datetime/integer, The oldest time to include in the
+                          result set. You can use this to limit the query to the last
+                          number of minutes, hours, days, months, and years as needed.
+                          Datetime Object: datetime.datetime.now()
+                          Timestamp: "2015-08-01T15:17:20,123"
+                          Milliseconds: 1312237040123
+                          Default: datetime.datetime.now()
+        ----------------  ---------------------------------------------------------------
+        level             optional string, Can be one of [OFF, SEVERE, WARNING, INFO,
+                          FINE, VERBOSE, DEBUG]. Returns only records with a log level at
+                          or more severe than the level specified.
+                          Default: WARNING
+        ----------------  ---------------------------------------------------------------
+        query_filter      optional string, Filtering is allowed by any combination of
+                          codes, users, and source components. The filter accepts a comma
+                          delimited list of filter definitions. If any definition is
+                          omitted, it defaults to all ("*").
+                          :Example:
+                          {"codes":[204000-205999,212015,219114], "users":["admin","jcho"],
+                           "source": ["PORTAL ADMIN"]}
+                          The source of logged events are generated from the sharing,
+                          administrative, and portal components of the software.
+                          For example:
+                           - Events related to publishing and users are categorized under
+                             SHARING.
+                           - Events related to security and indexing are categorized under
+                             PORTAL ADMIN.
+                           - Events related to installing the software are categorized
+                             under PORTAL.
+        ----------------  ---------------------------------------------------------------
+        page_size         optional integer, the number of log records to return. The
+                          default is 1000
+        ================  ===============================================================
+
+        :returns:
+           dictionary of messages
         """
         from datetime import datetime
-        from six import integer_types
+        from six import integer_types, string_types
         url = "%s/query" % self._url
         if isinstance(start_time, datetime):
             start_time = start_time.strftime("%Y-%m-%dT%H:%M:%S")
-        elif not isinstance(end_time, integer_types):
-            raise ValueError("Invalid datetime, must be integer or datetime object")
+        elif isinstance(start_time, string_types):
+            try:
+                datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S")
+            except:
+                raise Exception("Invalid start_time string, must be in the format YYYY-MM-DDTHH:MM:SS")
+        elif isinstance(start_time, tuple(list(integer_types) + [float])):
+            start_time = datetime.utcfromtimestamp(start_time).strftime("%Y-%m-%dT%H:%M:%S")
         if end_time is None:
             end_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         elif isinstance(end_time, datetime):
             end_time = end_time.strftime("%Y-%m-%dT%H:%M:%S")
-        elif not isinstance(end_time, integer_types):
-            raise ValueError("Invalid datetime, must be integer or datetime object")
+        elif isinstance(end_time, string_types):
+            try:
+                datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%S")
+            except:
+                raise Exception("Invalid end_time string, must be in the format YYYY-MM-DDTHH:MM:SS")
+        elif isinstance(end_time, tuple(list(integer_types) + [float])):
+            end_time = datetime.utcfromtimestamp(end_time).strftime("%Y-%m-%dT%H:%M:%S")
         if query_filter == "*":
             query_filter = {"codes":[], "users":[], "source": "*"}
         params = {
