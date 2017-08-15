5179147b409e6e48794966ad1970318a4c2a6a36
Rohit Singh <rsingh@esri.com>
2017-08-15 12:14:17 -0700

    bump version number to 1.2.1
    cc @AtmaMani

--- build/upload.sh
+++ build/upload.sh
@@ -5,13 +5,13 @@
 # find . -type f -exec anaconda upload {} \;
 
 # hard coded upload
-anaconda upload ___output/linux-32/arcgis-1.2.0-py36_1.tar.bz2
-anaconda upload ___output/linux-32/arcgis-1.2.0-py35_1.tar.bz2
-anaconda upload ___output/linux-64/arcgis-1.2.0-py36_1.tar.bz2
-anaconda upload ___output/linux-64/arcgis-1.2.0-py35_1.tar.bz2
-anaconda upload ___output/win-32/arcgis-1.2.0-py36_1.tar.bz2
-anaconda upload ___output/win-32/arcgis-1.2.0-py35_1.tar.bz2
-anaconda upload ___output/win-64/arcgis-1.2.0-py36_1.tar.bz2
-anaconda upload ___output/win-64/arcgis-1.2.0-py35_1.tar.bz2
-anaconda upload ___output/osx-64/arcgis-1.2.0-py36_1.tar.bz2
-anaconda upload ___output/osx-64/arcgis-1.2.0-py35_1.tar.bz2
+anaconda upload ___output/linux-32/arcgis-1.2.1-py36_1.tar.bz2
+anaconda upload ___output/linux-32/arcgis-1.2.1-py35_1.tar.bz2
+anaconda upload ___output/linux-64/arcgis-1.2.1-py36_1.tar.bz2
+anaconda upload ___output/linux-64/arcgis-1.2.1-py35_1.tar.bz2
+anaconda upload ___output/win-32/arcgis-1.2.1-py36_1.tar.bz2
+anaconda upload ___output/win-32/arcgis-1.2.1-py35_1.tar.bz2
+anaconda upload ___output/win-64/arcgis-1.2.1-py36_1.tar.bz2
+anaconda upload ___output/win-64/arcgis-1.2.1-py35_1.tar.bz2
+anaconda upload ___output/osx-64/arcgis-1.2.1-py36_1.tar.bz2
+anaconda upload ___output/osx-64/arcgis-1.2.1-py35_1.tar.bz2
