df50a5862554c356f3a1280300c68502189dc18c
Andrew <achapkowski@users.noreply.github.com>
2017-08-30 12:55:51 -0400

    fix for _util.zipws (#571)
    * added direct access to resource manager off of gis.admin classes.
    
    * call create unique name (arcpy function) to ensure valid feature class name for unittest
    
    * added vector tile layer support
    
    * added function that will create tile services from Feature Services and Feature Service Views.
    
    * - moved _PortalResource manager from __init__.py to gis.admin._resources.py
    - made _PortalResourceManage to PortalResourceManager
    - added doc to .rst files for PortalResourceManager
    - changed resource_manager property to resources
    - update _ux.py code to use new imports
    
    * fix for issue #516.  Accepts User or string of username or None now.
    
    * updated the doctstring for Item.move()
    
    * - resolve issue #521
    - updated _log.py doctstrings.
    
    * Fixed FeatureSet issue where geometry_type doesn't set properly because the wrong variable is set.
    
    * updated geocoding function to return featureset on batch_geocode
    
    * update some docstrings relating to geocoding/_functions.py
    
    * Updated the geocoding/_functions.py docstrings to use the table format.
    
    * resolved issue #523 - Added ability to add either a file or text to Item's ResourceManager
    
    * - added find_outliers
    - added docstrings for find outliers
    
    * Enhancements for issue #524
    
    * trying out wordwrap for tables
    
    * rebuilt doc with new css - word wrap in tables
    
    * Revert "Word wrap enhancement for tables in HTML doc" (#534)
    
    * Added Living Atlas support
    
    * living atlas addition
    
    * Fixed issue #540.
    
    * reverted create removal
    
    * Added missing parameters to to GroupManager.create
    Added missing parameters to SyncManager.create where the optional parameters missing caused some formats to not download correctly.  Issue #539
    
    * resolves issue #542 - when adding to listing datastores it requeries the server for changes.
    
    * PKI connection fixes.
    
    * - Added support for add_users and remove_users on Group class to use both string usernames and User classes
    - fixed PKI error
    
    * HTML Page URL property added to Item class. resolves issue #468
    
    * Updated _utils.py _data_handler to handle numpy.int64 and numpy.int32.
    
    * Added additional docstrings to SpatialDataFrame init function.
    
    * - added proxy support for issue #556
    - add docstring to GIS object and example
    - added proxy support to Server object
    
    * requested changes for @rohitgeo
    
    * resolves issue #347
    
    * fix for issue #290
    
    * first commit for issue #517
    
    * Fixed issue with pyshp import_data where zipws compresses itself.
    Added two new geoanalytics tools (Beta 10.6) Issue #518, #517

--- src/arcgis/geoanalytics/find_locations.py
+++ src/arcgis/geoanalytics/find_locations.py
@@ -17,6 +17,137 @@ _log = _logging.getLogger(__name__)
 
 _use_async = True
 
+def detect_track_incidents(input_layer,
+                           track_fields,
+                           start_condition_expression,
+                           end_condition_expression,
+                           output_mode="AllFeatures",
+                           output_name=None,
+                           gis=None):
+    """
+    The Detect Incidents task works with a time-enabled layer of points,
+    lines, areas, or tables that represents an instant in time. Using
+    sequentially ordered features, called tracks, this tool determines
+    which features are incidents of interest. Incidents are determined by
+    conditions that you specify. First, the tool determines which features
+    belong to a track using one or more fields. Using the time at each
+    feature, the tracks are ordered sequentially and the incident condition
+    is applied. Features that meet the starting incident condition are
+    marked as an incident. You can optionally apply an ending incident
+    condition; when the end condition is true, the feature is no longer
+    an incident. The results will be returned with the original features
+    with new columns representing the incident name and indicate which
+    feature meets the incident condition. You can return all original
+    features, only the features that are incidents, or all of the features
+    within tracks where at least one incident occurred.
+
+    For example, suppose you have GPS measurements of hurricanes every 10
+    minutes. Each GPS measurement records the hurricane's name, location,
+    time of recording, and wind speed. Using these fields, you could create
+    an incident where any measurement with a wind speed greater than 208
+    km/h is an incident titled Catastrophic. By not setting an end
+    condition, the incident would end if the feature no longer meets the
+    start condition (wind speed slows down to less than 208).
+
+    Using another example, suppose you were monitoring concentrations of a
+    chemical in your local water supply using a field called
+    contanimateLevel. You know that the recommended levels are less than
+    0.01 mg/L, and dangerous levels are above 0.03 mg/L. To detect
+    incidents, where a value above 0.03mg/L is an incident, and remains an
+    incident until contamination levels are back to normal, you create an
+    incident using a start condition of contanimateLevel > 0.03 and an end
+    condition of contanimateLevel < 0.01. This will mark any sequence where
+    values exceed 0.03mg/L until they return to a value less than 0.01.
+
+    ================  ===============================================================
+    **Argument**      **Description**
+    ----------------  ---------------------------------------------------------------
+    input_layer       required FeatureSet, The table, point, line or polygon features
+                      containing potential incidents.
+    ----------------  ---------------------------------------------------------------
+    track_fields      required string, The fields used to identify distinct tracks.
+                      There can be multiple track_fields.
+    ----------------  ---------------------------------------------------------------
+    start_condition_expression   The condition used to identify incidents. If there
+                                 is no endConditionExpression specified, any feature
+                                 that meets this condition is an incident. If there
+                                 is an end condition, any feature that meets the
+                                 start_condition_expression and does not meet the
+                                 end_condition_expression is an incident.
+                                 The expressions are Arcade expressions.
+    ----------------  ---------------------------------------------------------------
+    end_condition_expression   The condition used to identify incidents. If there is
+                               no endConditionExpression specified, any feature that
+                               meets this condition is an incident. If there is an
+                               end condition, any feature that meets the
+                               start_condition_expression and does not meet the
+                               end_condition_expression is an incident. This is an
+                               Arcade expression.
+    ----------------  ---------------------------------------------------------------
+    output_mode       optional string, default value is AllFeatures.  Determines
+                      which features are returned. Two modes are available:
+
+                       - AllFeatures - All of the input features are returned.
+                       - Incidents - Only features that were found to be incidents
+                                     are returned.
+    ----------------  ---------------------------------------------------------------
+    output_name       optional string, The task will create a feature service of the
+                      results. You define the name of the service.
+    ----------------  ---------------------------------------------------------------
+    gis               optional GIS, the GIS on which this tool runs. If not
+                      specified, the active GIS is used.
+    ================  ===============================================================
+
+    :returns:
+       Service
+    """
+    kwargs = locals()
+    tool_name = "DetectIncidents"
+    gis = _arcgis.env.active_gis if gis is None else gis
+    url = gis.properties.helperServices.geoanalytics.url
+    params = {
+        "f" : "json"
+    }
+    for key, value in kwargs.items():
+        if value is not None:
+            params[key] = value
+
+    if output_name is None:
+        output_service_name = 'Detect_Incidents_' + _id_generator()
+        output_name = output_service_name.replace(' ', '_')
+    else:
+        output_service_name = output_name.replace(' ', '_')
+
+    output_service = _create_output_service(gis, output_name, output_service_name, 'Detect Track Incidents')
+
+    params['output_name'] = _json.dumps({
+        "serviceProperties": {"name" : output_name, "serviceUrl" : output_service.url},
+        "itemProperties": {"itemId" : output_service.itemid}})
+
+    _set_context(params)
+
+    param_db = {
+        "input_layer": (_FeatureSet, "inputLayer"),
+        "track_fields": (str, "trackFields"),
+        "start_condition_expression": (str, "startConditionExpression"),
+        "end_condition_expression": (str, "endConditionExpression"),
+        "output_mode": (str, "outputMode"),
+        "output_name": (str, "outputName"),
+        "output": (_FeatureSet, "output"),
+        "context": (str, "context")
+    }
+    return_values = [
+        {"name": "output", "display_name": "Output Features", "type": _FeatureSet},
+    ]
+    try:
+        _execute_gp_tool(gis, tool_name, params, param_db, return_values, _use_async, url, True)
+        return output_service
+    except:
+        output_service.delete()
+        raise
+
+    return
+
 def find_similar_locations(
     input_layer,
     search_layer,
@@ -58,7 +189,7 @@ def find_similar_locations(
 
    output_name: Output Features Name (str). Required parameter.
 
-   gis: Optional, the GIS on which this tool runs. If not specified, the active GIS is used. 
+   gis: Optional, the GIS on which this tool runs. If not specified, the active GIS is used.
 
 
 Returns:
@@ -66,11 +197,11 @@ Returns:
 
 
     """
-    kwargs = locals()    
+    kwargs = locals()
 
     gis = _arcgis.env.active_gis if gis is None else gis
     url = gis.properties.helperServices.geoanalytics.url
-    
+
     params = {}
     for key, value in kwargs.items():
         if value is not None:
@@ -83,13 +214,13 @@ Returns:
         output_service_name = output_name.replace(' ', '_')
 
     output_service = _create_output_service(gis, output_name, output_service_name, 'Find Similar Locations')
-    
+
     params['output_name'] = _json.dumps({
         "serviceProperties": {"name" : output_name, "serviceUrl" : output_service.url},
         "itemProperties": {"itemId" : output_service.itemid}})
-    
+
     _set_context(params)
-    
+
     param_db = {
         "input_layer": (_FeatureSet, "inputLayer"),
         "search_layer": (_FeatureSet, "searchLayer"),
@@ -117,5 +248,5 @@ find_similar_locations.__annotations__ = {
     'match_method': str,
     'number_of_results': int,
     'append_fields': str,
-    'output_name': str}    
+    'output_name': str}
 
