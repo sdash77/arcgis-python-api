b7a9cc533af2b16d18013c90506a8ff3bca1586d
achapkowski <achapkowski@esri.com>
2017-09-06 06:30:54 -0400

    moved update user level to User object instead of UserManager

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -1092,54 +1092,6 @@ class UserManager(object):
                     _log.error('Unable to update the thumbnail for  ' + username)
             return user
 
-    #----------------------------------------------------------------------
-    def update_user_level(self, user, level):
-        """
-        The Update User Level operation (POST only) allows administrators
-        of an organization to update the level of a user. Administrators can
-        leverage two levels of membership when assigning roles and
-        privileges to members, membership levels allow organizations to
-        control access to some ArcGIS capabilities for some members while
-        granting more complete access to other members. Level 1 membership
-        is designed for members who need privileges to view and interact
-        with existing content, while Level 2 membership is for those who
-        contribute, create, and share content and groups, in addition to
-        other tasks.
-        Maximum user quota of an organization at the given level is checked
-        before allowing the update.
-
-        Built-in roles including organization administrator, publisher, and
-        user are assigned as Level 2, members with custom roles can be
-        assigned as Level 1 or Level 2.
-
-        Level 1 membership allows for limited capabilities given through a
-        maximum of 8 privileges: portal:user:joinGroup,
-        portal:user:viewOrgGroups, portal:user:viewOrgItems,
-        portal:user:viewOrgUsers, premium:user:geocode,
-        premium:user:networkanalysis, premium:user:demographics, and
-        premium:user:elevation. If updating the role of a Level 1 user with
-        a custom role that has more privileges than the eight, additional
-        privileges will be disabled for the user to ensure restriction.
-
-        Level 1 users are not allowed to own any content or group which can
-        be reassigned to other users through the Reassign Item and Reassign
-        Group operations before downgrading them. The operation will also
-        fail if the user being updated has got licenses assigned to premium
-        apps that are not allowed at the targeting level.
-        """
-        url = "%s/portals/self/updateUserLevel" % self._portal.resturl
-        if isinstance(user, User):
-            user = user.username
-        params = {
-            'user' : user,
-            'level' : level,
-            'f' : 'json'
-        }
-        res = self._gis._con.post(url, params)
-        if 'success' in res:
-            return res['success']
-        return res
-
     def signup(self, username, password, fullname, email):
         """ Signs up users to an instance of Portal for ArcGIS.
 
@@ -3129,6 +3081,68 @@ class User(dict):
                     </div>
                 </div>
                 """
+
+    #----------------------------------------------------------------------
+    def update_level(self, level):
+        """
+        The Update User Level operation (Admin only) allows administrators
+        of an organization to update the level of a user. Administrators can
+        leverage two levels of membership when assigning roles and
+        privileges to members, membership levels allow organizations to
+        control access to some ArcGIS capabilities for some members while
+        granting more complete access to other members. Level 1 membership
+        is designed for members who need privileges to view and interact
+        with existing content, while Level 2 membership is for those who
+        contribute, create, and share content and groups, in addition to
+        other tasks.
+        Maximum user quota of an organization at the given level is checked
+        before allowing the update.
+
+        Built-in roles including organization administrator, publisher, and
+        user are assigned as Level 2, members with custom roles can be
+        assigned as Level 1 or Level 2.
+
+        Level 1 membership allows for limited capabilities given through a
+        maximum of 8 privileges: portal:user:joinGroup,
+        portal:user:viewOrgGroups, portal:user:viewOrgItems,
+        portal:user:viewOrgUsers, premium:user:geocode,
+        premium:user:networkanalysis, premium:user:demographics, and
+        premium:user:elevation. If updating the role of a Level 1 user with
+        a custom role that has more privileges than the eight, additional
+        privileges will be disabled for the user to ensure restriction.
+
+        Level 1 users are not allowed to own any content or group which can
+        be reassigned to other users through the Reassign Item and Reassign
+        Group operations before downgrading them. The operation will also
+        fail if the user being updated has got licenses assigned to premium
+        apps that are not allowed at the targeting level.
+
+        =====================  =========================================================
+        **Argument**           **Description**
+        ---------------------  ---------------------------------------------------------
+        level                  required integer, The values of 1 or 2. This is the user
+                               level for the given user.
+        =====================  =========================================================
+
+        :returns:
+           boolean
+        """
+        if not isinstance(level, int):
+            raise ValueError("level must be an integer with values 1 or 2")
+
+        if level < 1 or level > 2:
+            raise ValueError("level is an integers with values: 1 or 2")
+
+        url = "%s/portals/self/updateUserLevel" % self._portal.resturl
+        params = {
+            'user' : self.username,
+            'level' : level,
+            'f' : 'json'
+        }
+        res = self._gis._con.post(url, params)
+        if 'success' in res:
+            return res['success']
+        return res
 
     def reset(self, password, new_password=None, new_security_question=None, new_security_answer=None):
         """ Resets a user's password, security question, and/or security answer.
