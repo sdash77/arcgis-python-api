56b5d9a5b451903a35e4006dcc489bbd73ea24d1
Rohit Singh <rsingh@esri.com>
2017-09-28 23:18:24 -0700

    doc updates and renamed Imagery manager classes

--- src/arcgis/raster/__init__.py
+++ src/arcgis/raster/__init__.py
@@ -9,7 +9,7 @@ an elevation surface.
 Use arcgis.raster.analytics.is_supported(gis) to check if raster analysis is supported in your GIS.
 """
 
-from ._layer import ImageryLayer
+from ._layer import ImageryLayer, RasterManager, ImageryTileManager, RasterCatalogItem
 from .analytics import *
 from .functions import *
 
