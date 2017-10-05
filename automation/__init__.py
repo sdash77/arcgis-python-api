7638faed434358604274b2fd8c54a2f80c69f49f
David Vitale <DavidJVitale@users.noreply.github.com>
2017-10-05 16:30:30 -0700

    Added README, building documentation for unix

--- automation/__init__.py
+++ automation/__init__.py
@@ -3,3 +3,8 @@ import os
 GEOSAURUS_ROOT_DIR = os.path.abspath(os.path.join(
     os.path.dirname( __file__ ),
     '..'))
+
+STAGING_DIR = os.path.abspath(os.path.join(
+    GEOSAURUS_ROOT_DIR,
+    "automation",
+    "staging"))
--- automation/build_documentation.py
+++ automation/build_documentation.py
@@ -1,8 +1,16 @@
 import os 
 
-from __init__ import GEOSAURUS_ROOT_DIR
+from __init__ import GEOSAURUS_ROOT_DIR, STAGING_DIR
+
+DOCS_DIR = os.path.abspath(os.path.join(GEOSAURUS_ROOT_DIR, "docs"))
 
 def build_documentation():
+    """Detects what OS is being used, runs the appropriate 'make' mechanism
+    in the ../docs/ folder. The resulting compiled html is then located in
+    ./staging folder. (NOTE: Running this script multiple times will
+    overwrite the previous generated documentation. Please move all files
+    out of the ./staging folder (see the publish functions in this dir)"""
+    
     print("Starting to build the documentation...")
 
     if os.name == 'posix':
@@ -17,6 +25,14 @@ def build_documentation():
 
 def _build_doc_for_unix():
     print("Building for *nix system...")
+    make_command = "make singlehtml"
+    flag_to_force_output_to_staging_dir = "BUILDDIR={}".format(STAGING_DIR)
+    final_bash_command = "cd {} && {} {}".format(
+            DOCS_DIR,
+            make_command,
+            flag_to_force_output_to_staging_dir)
+    os.system(final_bash_command)
+
 
 def _build_doc_for_windows():
     print("Building for Windows system...")
