108318e78ed013a2db688e1521bd66684bc32834
David Vitale <DavidJVitale@users.noreply.github.com>
2017-10-05 16:36:36 -0700

    Potential Windows doc building

--- automation/build_documentation.py
+++ automation/build_documentation.py
@@ -33,6 +33,14 @@ def _build_doc_for_unix():
             flag_to_force_output_to_staging_dir)
     os.system(final_bash_command)
 
-
 def _build_doc_for_windows():
     print("Building for Windows system...")
+    bat_command_to_force_output_to_staging_dir = \
+        "set BUILDDIRGLOBALOVERRIDE={}".format(DOCS_DIR)
+    bat_make_command = os.path.join(DOCS_DIR, "make.bat")
+    bat_command_to_negate_forcing_output_to_staging_dir = \
+        "set BUILDDIRGLOBALOVERRIDE={}".format('""')
+
+    os.system(bat_command_to_force_output_to_staging_dir)
+    os.system(bat_make_command)
+    os.system(bat_command_to_negate_forcing_output_to_staging_dir)
