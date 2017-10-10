b136f38cc75a7c7d1b1d5be8754116f9ef0bd69e
David Vitale <DavidJVitale@users.noreply.github.com>
2017-10-10 15:01:32 -0700

    Added timeout to see what is happening with Jenkins hanging...

--- automation/run_unit_tests.py
+++ automation/run_unit_tests.py
@@ -4,7 +4,8 @@ import subprocess
 import os
 
 def run_unit_tests():
-    subprocess.check_call(['python',os.path.join(GEOSAURUS_ROOT_DIR, 'unittests', 'run_test_cases.py'), os.path.join(GEOSAURUS_ROOT_DIR, 'unittests')])
+    subprocess.check_call(['python',os.path.join(GEOSAURUS_ROOT_DIR, 'unittests', 'run_test_cases.py'), os.path.join(GEOSAURUS_ROOT_DIR, 'unittests')],
+            timeout=600)
     print("Running unit tests not supported through this interface yet")
 
 if __name__ == '__main__':
