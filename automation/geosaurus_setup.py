33277bd520e336514c2d7d2bb2fac812e5866967
David Vitale <DavidJVitale@users.noreply.github.com>
2017-10-10 11:59:31 -0700

    Added geosaurus_setup to build the source before running anything

---
+++ automation/geosaurus_setup.py
@@ -0,0 +1,18 @@
+import os 
+import subprocess
+
+from __init__ import GEOSAURUS_ROOT_DIR
+
+def geosaurus_setup():
+    """Installs correct packages, builds geosaurus source"""
+    
+    print("Setting up the geosaurus repository...")
+    _run_sys_command("conda install -c conda-forge ipywidgets -y")
+    _run_sys_command("conda install pandas -y")
+    _run_sys_command("pip install -e {}".format(
+        os.path.join(GEOSAURUS_ROOT_DIR,"src")))
+    print("Success! Dependencies installed and source built")
+
+def _run_sys_command(cmd):
+    print("About to run the following command: '{}'".format(cmd))
+    subprocess.check_call(cmd, shell=True)
--- automation/jenkins_entry_point.py
+++ automation/jenkins_entry_point.py
@@ -6,6 +6,7 @@ from __init__ import GEOSAURUS_ROOT_DIR
 sys.path.append(GEOSAURUS_ROOT_DIR)
 from src.arcgis import __version__ as _py_api_version
 
+from geosaurus_setup import geosaurus_setup
 from build_conda_package import build_conda_package
 from build_docker_image import build_docker_image
 from build_documentation import build_documentation
@@ -14,12 +15,14 @@ from publish_results import publish_results
 
 _master_regex = ".*master.*"
 _pull_request_regex = ".*pull.*request.*"
-_funcs_to_run_for_arg = [(_master_regex, [run_unit_tests,
+_funcs_to_run_for_arg = [(_master_regex, [geosaurus_setup,
+                                          run_unit_tests,
                                           build_documentation,
                                           build_conda_package,
                                           build_docker_image,
                                           publish_results]),
-                         (_pull_request_regex, [run_unit_tests,
+                         (_pull_request_regex, [geosaurus_setup,
+                                                run_unit_tests,
                                                 build_documentation,
                                                 publish_results])]
 _automation_type = ""
