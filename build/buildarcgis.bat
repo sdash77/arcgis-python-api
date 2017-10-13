23334b64a82026b99c26006d51f2160657d42868
David Vitale <DavidJVitale@users.noreply.github.com>
2017-10-13 14:39:58 -0700

    stupid batch script error

--- build/buildarcgis.bat
+++ build/buildarcgis.bat
@@ -9,17 +9,17 @@ conda build --py 3.6 arcgis --output-folder ___output
 conda build --py 3.5 arcgis --output-folder ___output
 
 REM for each file generated from the above commands (i.e. files that end in .tar.bz2)
-for /r %%file in (___output\win-64\arcgis-%ver%-*.tar.bz2) do (
+for /r %%i in (___output\win-64\arcgis-%ver%-*.tar.bz2) do (
     REM run the convert command on each of those files
-    conda convert -f -p win-32 %%file -o ___output
+    conda convert -f -p win-32 %%i -o ___output
 )
 
 REM for each file converted for win-64, upload it to conda
-for /r %%file in (___output\win-64\arcgis-%ver%-*.tar.bz2) do (
-    anaconda upload %%file
+for /r %%i in (___output\win-64\arcgis-%ver%-*.tar.bz2) do (
+    anaconda upload %%i
 )
 
 REM for each file converted for win-32, upload it to conda
-for /r %%file in (___output\win-32\arcgis-%ver%-*.tar.bz2) do (
-    anaconda upload %%file
+for /r %%i in (___output\win-32\arcgis-%ver%-*.tar.bz2) do (
+    anaconda upload %%i
 )
