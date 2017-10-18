e12999dd72621ed77b0b223c9b8492c7bcd92165
David Vitale <DavidJVitale@users.noreply.github.com>
2017-10-18 10:29:03 -0700

    Fixed error of trying to delete open log file, causing error

--- automation/automation_setup.py
+++ automation/automation_setup.py
@@ -1,7 +1,6 @@
 import os 
-import shutil
 import subprocess
-import glob
+import shutil
 import logging
 log = logging.getLogger()
 
@@ -10,18 +9,23 @@ from __init__ import *
 def automation_setup(*args, **kwargs):
     """Clears results Installs correct packages, builds geosaurus source"""
     log.info("Starting setup...")
-    _clear_staging_folder()
+    _clear_staging_folder([".gitignore", "log.log"])
     _install_correct_packages()
     _install_geosaurus_src()
     log.info("Setup complete!")
 
-def _clear_staging_folder():
-    shutil.move(os.path.join(STAGING_DIR, ".gitignore"),
-                os.path.join(STAGING_DIR, "..", ".gitignore"))
-    shutil.rmtree(STAGING_DIR)
-    os.makedirs(STAGING_DIR)
-    shutil.move(os.path.join(STAGING_DIR, "..", ".gitignore"),
-                os.path.join(STAGING_DIR, ".gitignore"))
+def _clear_staging_folder(files_to_ignore):
+    for full_path in _paths_in_dir_to_delete(STAGING_DIR, files_to_ignore):
+        if os.path.isdir(full_path):
+            shutil.rmtree(full_path)
+        else:
+            os.remove(full_path)
+
+def _paths_in_dir_to_delete(dir_, files_to_ignore):
+    return [
+        os.path.join(dir_, item)
+        for item in os.listdir(dir_)
+        if item not in files_to_ignore]
 
 def _install_correct_packages():
     run_shell_command("pip install xmlrunner")
