75f0b9a61b8917934fadd0068a5242680e65dda3
DavidJVitale <DavidJVitale@users.noreply.github.com>
2017-11-09 13:37:58 -0800

    Fixed error for current OS build

--- build/build_conda_package.py
+++ build/build_conda_package.py
@@ -111,7 +111,6 @@ def build_conda_packages_for_all_os_and_py():
 def build_conda_packages(os_build_targets = DEFAULT_OSES,
                          python_versions = DEFAULT_PYS,
                          clear_output_folder = True):
-    print("entered main func: {} \n {}".format(os_build_targets, python_versions))
     if clear_output_folder:
         _clear_output_folder()
     _check_edge_cases(os_build_targets,
@@ -142,12 +141,10 @@ def _split_oses_to_unix_and_win(os_build_targets):
     unix = []
     win = []
     for os in os_build_targets:
-        log.debug("osbuild => {}".format(os_build_targets))
         if is_unix(os):
             unix.append(os)
         elif is_windows(os):
             win.append(os)
-    log.debug("build targets split into {} and {}".format(unix, win))
     return unix, win
 
 def _clear_output_folder():
@@ -177,16 +174,13 @@ def _check_edge_cases(os_build_targets,
 
 
 def _setup_meta_yaml_file_for(os_folder_name):
-    """Copies a meta.yaml file to build/arcgis/ that is needed for the 
-    conda-build process. The meta.yaml file is assembled for the target OS
-    based off the base_meta.yaml files located in build/env"""
     os_specific_meta_file = os.path.join(
                                  BUILD_DIR,
                                  "meta",
                                  os_folder_name,
                                  "meta.yaml")
     shutil.copyfile(os_specific_meta_file, ACTIVE_META_YML_FILE)
-    log.info("using {} for build process.".format(os_specific_meta_file))
+    log.info("using {} meta.yaml file for build.".format(os_folder_name))
 
 def _run_conda_build_command(python_version,
                              output_dir,
@@ -202,7 +196,6 @@ def _find_conda_package(path):
                 return os.path.join(root, name)
 
 def _convert_conda_package(conda_package, os_build_targets, output_dir):
-    log.info("{}\n{}\n{}".format(conda_package, os_build_targets, output_dir))
     for os_build_target in os_build_targets:
         _run_conda_convert_command(conda_package = conda_package,
                                    os_build_target = os_build_target,
@@ -212,16 +205,24 @@ def _convert_conda_package(conda_package, os_build_targets, output_dir):
                              dst = os.path.join(output_dir, os_build_target))
 
 def _run_conda_convert_command(conda_package, os_build_target, output_dir):
-    _run_shell_cmd(BASE_CONVERT_CMD.format(conda_package = conda_package,
-                                           os_build_target = os_build_target,
-                                           output_dir = output_dir))
+    output = _run_shell_cmd(BASE_CONVERT_CMD.format(
+                                conda_package = conda_package,
+                                os_build_target = os_build_target,
+                                output_dir = output_dir))
+    if "Skipping conversion" in output:
+        target_folder = os.path.join(output_dir, os_build_target)
+        os.makedirs(target_folder)
+        shutil.copy(conda_package, target_folder)
+        log.debug("conda convert didn't complete: manually copied {} to "
+                  "{}".format(conda_package, target_folder))
 
 def _copy_repodata_files(src, dst):
     for root, dirs, files in os.walk(src):
         for name in files:
             if "repodata" in name:
-                shutil.copy(os.path.join(root, name),
-                            dst)
+                found_repodata_file = os.path.join(root, name)
+                log.debug("moving repodata {}".format(found_repodata_file))
+                shutil.copy(found_repodata_file, dst)
 
 def _run_shell_cmd(cmd):
     log.info("Currently running cmd '{}'. Output of cmd will be logged after "\
@@ -230,7 +231,9 @@ def _run_shell_cmd(cmd):
         byte_output = subprocess.check_output(cmd,
                                               stderr=subprocess.STDOUT,
                                               shell=True)
-        log.debug("cmd output => {}".format(byte_output.decode("utf-8")))
+        str_output = byte_output.decode("utf-8")
+        log.debug("cmd output => {}".format(str_output)
+        return str_output
     except subprocess.CalledProcessError as e:
         log.warn("cmd failed, returned non-zero code. Output:\n"\
                  "{}".format(e.output.decode("utf-8")))
