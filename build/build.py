0733ffad4bd0d268cee2a12e94e2bc21262962ed
DavidJVitale <DavidJVitale@users.noreply.github.com>
2017-11-15 14:55:32 -0800

    Removed meta.yaml logic, repodata manually transferring: added 'conda index' functionality

--- build/build.py
+++ build/build.py
@@ -17,11 +17,13 @@ BASE_BUILD_CMD = "cd {build_dir} && conda build arcgis --py {python_version} "\
                  "--output-folder {output_dir}"
 BASE_CONVERT_CMD = "conda convert -f -p {os_build_target} {conda_package} "\
                    "-o {output_dir}" 
+BASE_INDEX_CMD = "cd {output_dir} && conda index {os_build_target}"
 BASE_UPLOAD_CMD = "anaconda upload -f {conda_package}"
 
 SUPPORTED_WIN = ['win-32', 'win-64']
 SUPPORTED_LINUX = ['linux-32', 'linux-64']
 SUPPORTED_OSX = ['osx-64']
+SUPPORTED_UNIX = SUPPORTED_LINUX + SUPPORTED_OSX
 SUPPORTED_OSES = SUPPORTED_WIN +\
                  SUPPORTED_LINUX +\
                  SUPPORTED_OSX
@@ -38,21 +40,12 @@ BUILD_OUTPUT_DIR = os.path.abspath(os.path.join(
     BUILD_DIR,
     "output"))
 
-DEFAULT_META_YML_FILE = os.path.abspath(os.path.join(
-    BUILD_DIR,
-    "meta",
-    "default_meta.yaml"))
-ACTIVE_META_YML_FILE = os.path.abspath(os.path.join(
-    BUILD_DIR,
-    "arcgis",
-    "meta.yaml"))
-
 def _main():
     args = _parse_cmd_line_args()
     _setup_logging(args)
 
     if _no_target_specified(args):
-        build_conda_pkg_with_default_meta_yaml()
+        build_conda_pkg_default()
     elif _all_is_specified_anywhere(args):
         build_conda_packages_for_all_os_and_py()
     elif _only_python_is_specified(args):
@@ -73,16 +66,16 @@ def _main():
 
 def _parse_cmd_line_args():
     parser = argparse.ArgumentParser(description = "Builds arcgis conda "\
-        "packages for specified O.S. and python versions. Will use ./meta/"\
-        "default_meta.yaml if -o, -p, or -a aren't specified. Must have "\
+        "packages for specified O.S. and python versions. Must have "\
         "conda-build and anaconda-client installed on root conda enviroment. "\
-        "Can only build windows conda packages from a windows machine. "\
+        "Can only build windows conda packages from a windows machine, and "\
+        "can only build osx/linux conda packages from a unix machine. "\
         "Generated conda pkgs in ./output/. See ./README.md for more info."\
         "\n - 'python build_conda_package.py' for default build behavior"\
-        "\n - 'python build_conda_package.py -p 3.5 3.6 -o win-32 linux-64' "\
-        "for building both py3.5 and py3.6 for both win-32 and linux-64"\
+        "\n - 'python build_conda_package.py -p 3.5 3.6 -o osx-64 linux-32' "\
+        "for building both py3.5 and py3.6 for both osx-64 and linux-32"\
         "\n - 'python build_conda_package.py --all' for building for all "\
-        "platforms and all python versions"\
+        "platforms and all python versions supported by this system"\
         "\n - 'python build_conda_package.py --upload' for default build "\
         "behavior, plus upload any results to the anaconda cloud",
         formatter_class=argparse.RawTextHelpFormatter)
@@ -91,10 +84,11 @@ def _parse_cmd_line_args():
     parser.add_argument("--os", "-o", type=str, nargs="*",
         help="What OSes and architectures to target (linux-64, win-32, etc.)")
     parser.add_argument("--all", "-a", action="store_true",
-        help="Builds for all supported O.S. and python versions")
+        help="Builds for all supported O.S. and py supported by this system")
     parser.add_argument("--upload", "-u", action="store_true",
         help="Upload results to anaconda cloud (Note: anaconda-client must "\
-             "be configured on root conda enviroment)")
+             "be configured on root conda enviroment and 'anaconda login' "\
+             "must have been run)")
     parser.add_argument("--verbose", "-v", action="store_true",
         help="Print all DEBUG log msgs (i.e. print 'conda build' cmd output)")
     return parser.parse_args(sys.argv[1:]) #don't use filename as 1st arg
@@ -120,12 +114,10 @@ def _no_target_specified(args):
     """returns False if at least one of --all, --python, or --os passed in"""
     return (not args.all) and (not args.os) and (not args.python)
 
-def build_conda_pkg_with_default_meta_yaml():
-    """Use geosaurus/build/env/default_meta.yaml file as the meta.yaml file
-    the "conda build" process will use. Build for all supported python"""
-    _clear_output_folder()
-    _restore_default_meta_yml()
-    log.info("Using {} as meta.yaml file...".format(DEFAULT_META_YML_FILE))
+def build_conda_pkg_default(clear_output_folder = True):
+    """Build for all supported python, don't conda convert"""
+    if clear_output_folder:
+        _clear_output_folder()
     for python_version in DEFAULT_PYS:
         _run_conda_build_command(python_version = python_version,
                                  output_dir = BUILD_OUTPUT_DIR)
@@ -148,7 +140,14 @@ def _both_os_and_python_are_specified(args):
 
 def build_conda_packages_for_all_os_and_py():
     """Will generate conda packages for all supported os and pys"""
-    build_conda_packages(os_build_targets = SUPPORTED_OSES,
+    if _is_current_os_win():
+        os_build_targets = SUPPORTED_WIN
+    elif _is_current_os_unix():
+        os_build_targets = SUPPORTED_UNIX
+    else:
+        raise RuntimeError("{} is not a supported OS".format(os.name))
+
+    build_conda_packages(os_build_targets = os_build_targets,
                          python_versions = SUPPORTED_PYS)
 
 def build_conda_packages(os_build_targets,
@@ -156,7 +155,7 @@ def build_conda_packages(os_build_targets,
                          clear_output_folder = True):
     """Will build conda packages for specified oses, and pys. Places pkgs
     in output folder, will clear output folder if specified. Can only build
-    windows conda packages on windows machine.
+    windows conda packages on windows machine, unix packages on unix machines
 
     os_build_targets: a list of strings of target oses to build for. See
         SUPPORTED_OSES for all supported. Ex: ['win-64', 'linux-32']
@@ -168,20 +167,7 @@ def build_conda_packages(os_build_targets,
         _clear_output_folder()
     _check_edge_cases(os_build_targets,
                       python_versions)
-    unix_oses, win_oses = _split_oses_to_unix_and_win(os_build_targets)
 
-    if unix_oses:
-        _setup_meta_yaml_file_for("unix")
-        _run_and_convert(os_build_targets = unix_oses,
-                         python_versions = python_versions)
-    if win_oses:
-        _setup_meta_yaml_file_for("windows")
-        _run_and_convert(os_build_targets = win_oses,
-                         python_versions = python_versions)
-    
-    _restore_default_meta_yml()
-
-def _run_and_convert(os_build_targets, python_versions):
     for python_version in python_versions:
         with empty_temp_folder() as tmp_dir:
             _run_conda_build_command(python_version = python_version,
@@ -190,22 +176,18 @@ def _run_and_convert(os_build_targets, python_versions):
                                    os_build_targets = os_build_targets,
                                    output_dir = BUILD_OUTPUT_DIR)
 
-def _split_oses_to_unix_and_win(os_build_targets):
-    unix = []
-    win = []
-    for os in os_build_targets:
-        if _is_unix(os):
-            unix.append(os)
-        elif _is_windows(os):
-            win.append(os)
-    return unix, win
-
 def _is_windows(os_build_target):
     return os_build_target in SUPPORTED_WIN
 
 def _is_unix(os_build_target):
     return (os_build_target in SUPPORTED_LINUX) or\
            (os_build_target in SUPPORTED_OSX)
+
+def _is_current_os_windows():
+    return os.name == "nt"
+
+def _is_current_os_unix():
+    return os.name == "posix"
 
 def _clear_output_folder():
     items_to_ignore = [ ".gitignore" ]
@@ -219,12 +201,18 @@ def _clear_output_folder():
 
 def _check_edge_cases(os_build_targets,
                       python_versions):
-    """At the moment, you can only build for windows on a win machine"""
+    """check is os_build_targets and python_version are sane, assert building 
+    for windows on windows and unix on unix
+    """
     for os_build_target in os_build_targets:
-        if _is_windows(os_build_target) and os.name == "posix":
+        if _is_windows(os_build_target) and _is_current_os_unix():
             #if you are building for windows but your current os is unix
             raise RuntimeError("You're building for {}: Windows pkgs cannot "\
                                "be built on unix sys".format(os_build_targets))
+        if _is_unix(os_build_target) and _is_current_os_windows():
+            #if you are building for unix but your current os is windows
+            raise RuntimeError("You're building for {}: Unix pkgs cannot "\
+                               "be built on win sys".format(os_build_targets)) 
         if os_build_target not in SUPPORTED_OSES: 
             raise RuntimeError("{} not supported OS. Supported OSes = "\
                                "{}".format(os_build_target, SUPPORTED_OSES))
@@ -232,20 +220,6 @@ def _check_edge_cases(os_build_targets,
         if python_version not in SUPPORTED_PYS:
             raise RuntimeError("{} not supported py. Supported pys = "\
                                "{}".format(python_version, SUPPORTED_PYS))
-
-
-def _setup_meta_yaml_file_for(os_folder_name):
-    """the unix meta.yaml file differs from the windows meta.yaml file,
-    mainly due to shortcomings on the conda-convert command in regards
-    to O.S. specific depedencies
-    """
-    os_specific_meta_file = os.path.join(
-                                 BUILD_DIR,
-                                 "meta",
-                                 os_folder_name,
-                                 "meta.yaml")
-    shutil.copyfile(os_specific_meta_file, ACTIVE_META_YML_FILE)
-    log.info("using {} file for build.".format(os_specific_meta_file))
 
 def _run_conda_build_command(python_version,
                              output_dir):
@@ -264,15 +238,8 @@ def _convert_conda_package(conda_package, os_build_targets, output_dir):
         _run_conda_convert_command(conda_package = conda_package,
                                    os_build_target = os_build_target,
                                    output_dir = output_dir)
-        dir_containing_conda_package = os.path.dirname(conda_package)
-        _copy_repodata_files(src = dir_containing_conda_package,
-                             dst = os.path.join(output_dir, os_build_target))
-        noarch_dir_src = os.path.join(dir_containing_conda_package,
-                                  "..",
-                                  "noarch")
-        noarch_dir_dst = os.path.join(output_dir, "noarch")
-        if not os.path.isdir(noarch_dir_dst):
-            shutil.copytree(noarch_dir_src, noarch_dir_dst)
+        _run_conda_index_command(output_dir = output_dir,
+                                 os_build_target = os_build_target)
 
 def _run_conda_convert_command(conda_package, os_build_target, output_dir):
     output = _run_shell_cmd(BASE_CONVERT_CMD.format(
@@ -280,20 +247,30 @@ def _run_conda_convert_command(conda_package, os_build_target, output_dir):
                                 os_build_target = os_build_target,
                                 output_dir = output_dir))
     if "Skipping conversion" in output:
-        #If converting to current platform, cmd will silent fail and skip
+        #If converting to current platform, the command silent failed and
+        #we need to manually copy over the results
         target_folder = os.path.join(output_dir, os_build_target)
         os.makedirs(target_folder, exist_ok=True)
         shutil.copy(conda_package, target_folder)
         log.debug("conda convert didn't complete: manually copied {} to "
                   "{}".format(conda_package, target_folder))
-        
-def _copy_repodata_files(src, dst):
-    for root, dirs, files in os.walk(src):
-        for name in files:
-            if "repodata" in name:
-                found_repodata_file = os.path.join(root, name)
-                log.debug("moving repodata {}".format(found_repodata_file))
-                shutil.copy(found_repodata_file, dst)
+
+def _run_conda_index_command(output_dir, os_build_target):
+    """Generates repodata.json and other metadata files needed for local
+    file deployment installation (See 
+    https://conda.io/docs/user-guide/tasks/create-custom-channels.html)
+    """
+    _run_shell_cmd(BASE_INDEX_CMD.format(output_dir = output_dir,
+                                         os_build_target = os_build_target))
+
+def _copy_noarch_dir(conda_package, output_dir):
+    dir_containing_conda_package = os.path.dirname(conda_package)
+    noarch_dir_src = os.path.join(dir_containing_conda_package,
+                                  "..",
+                                  "noarch")
+    noarch_dir_dst = os.path.join(output_dir, "noarch")
+    if not os.path.isdir(noarch_dir_dst):
+        shutil.copytree(noarch_dir_src, noarch_dir_dst)
 
 def upload_any_conda_packages_in_output_folder():
     """runs the anaconda upload command on any generated .tar.bz2 files
@@ -301,7 +278,7 @@ def upload_any_conda_packages_in_output_folder():
     """
     log.info("Uploading any conda packages in {}. Make sure anaconda login "\
              "has been run to authenticate your anaconda cloud credentials "\
-             "(cryptic errors are thrown otherwise".format(BUILD_OUTPUT_DIR))
+             "(cryptic errors are thrown otherwise)".format(BUILD_OUTPUT_DIR))
     for root, dirs, files in os.walk(BUILD_OUTPUT_DIR):
         for name in files:
             if ".tar.bz2" in name:
@@ -333,7 +310,8 @@ def _restore_default_meta_yml():
 
 class empty_temp_folder:
     """Use with "with" syntax like "with empty_temp_folder() as tmp:"
-    Creates a temporary folder and deletes it after finished being used"""
+    Creates a temporary folder and deletes it after finished being used
+    """
     def __enter__(self):
         self.temp_folder = os.path.join(tempfile.gettempdir(),
                                    ".{}".format(hash(os.times())))
@@ -364,6 +342,5 @@ if __name__ == "__main__":
                  "packages in {}. Exiting....".format(BUILD_OUTPUT_DIR))
     except Exception as e:
         log.exception(e)
-        _restore_default_meta_yml()
         log.info("Program did not succesfully complete (unhandled exception)")
         sys.exit(1)
