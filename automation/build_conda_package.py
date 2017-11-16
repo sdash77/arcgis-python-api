e7d94319370394d91fb88be987cdd4e7b9c6aeed
DavidJVitale <DavidJVitale@users.noreply.github.com>
2017-11-16 15:38:45 -0800

    Initial recursive ftp uploading of conda packages to ftp://zion/

--- automation/build_conda_package.py
+++ automation/build_conda_package.py
@@ -12,14 +12,13 @@ sys.path.append(os.path.join(GEOSAURUS_ROOT_DIR))
 from build import build
 
 def build_conda_package(*args, **kwargs):
-    build_tag = kwargs["build_tag"]
     build.build_conda_packages_for_all_os_and_py()
-    _move_output_to_staging(build_tag)    
+    _move_output_to_staging()   
 
-def _move_output_to_staging(build_tag):
+def _move_output_to_staging():
     output_dir_of_conda_packages = os.path.join(BUILD_DIR, "output")
     shutil.copytree(output_dir_of_conda_packages,
-                    os.path.join(STAGING_DIR, 'conda_builds', build_tag))
+                    os.path.join(STAGING_DIR, 'conda_builds'))
     log.info("moved {} contents to {}...".format(output_dir_of_conda_packages,
                                                  STAGING_DIR))
 
--- automation/publish_results.py
+++ automation/publish_results.py
@@ -1,39 +1,49 @@
-import os
-import sys
-import shutil
+from ftplib import FTP
 import logging
 log = logging.getLogger()
 
 from __init__ import *
-#import python package from the BUILD_DIR dir
-sys.path.append(os.path.join(GEOSAURUS_ROOT_DIR))
-from build import build
+
+FTP_SITE = "zion"
+NUM_BUILDS_TO_KEEP = 100
 
 def publish_results(*args, **kwargs):
-    build_tag = kwargs["build_tag"]
-    _publish_to_fileshare(build_tag)
-    _publish_to_conda_cloud()
+    try:
+        ftp_username = kwargs["username"]
+        ftp_password = kwargs["password"]
+    except KeyError as e:
+        log.warn("FTP username/password not specified. Skipping publishing")
+        return
+    automation_type = kwargs["automation_type"]
+    build_number = kwargs["build_number"]
+    _publish_to_zion_ftp_site(username,
+                              password,
+                              automation_type,
+                              build_number)
 
-def _publish_to_fileshare(build_tag):
-    log.info("About to publish results to local fileshare...")
-    if os.name == 'posix':
-        raise RuntimeError("Conda building not supported on unix systems")
-    elif os.name == 'nt':
-        publish_dir_win = r"C:\Users\Public\conda_packages\{}".format(build_tag)
-        shutil.copytree(os.path.join(STAGING_DIR, "conda_builds"),
-                                     publish_dir_win)
-def _publish_to_conda_cloud():
-    log.info("About to publish results to anaconda cloud...")
-    _clear_arcgispyapibot_conda_packages()
-    build.upload_any_conda_packages_in_output_folder()
+def _publish_to_zion_ftp_site(username,
+                              password,
+                              automation_type,
+                              build_number):
+    ftp = FTP(host = FTP_SITE,
+              user = username,
+              passwd = password)
+    src_dir_path = os.path.join(STAGING_DIR, "conda_builds")
+    dst_dir_path = 'master/{}'.format(build_number)
+    ftp.mkd(dst_dir_path)
+    _upload_directory_recursive(ftp = ftp,
+                                src_dir_path = src_dir_path,
+                                dst_dir_path = dst_dir_path)
 
-def _clear_arcgispyapibot_conda_packages():
-    """https://anaconda.org/ArcGISPyAPIBot/arcgis contains http://zion/'s
-    outputted conda packages. We should be clearing them before uploading
-    any new ones.
-    This command will silently fail if 1) ArcGISPyAPIBot/arcgis  doesn't exit 
-    or 2) if the anaconda login username isn't ArcGISPyAPIBot"""
-    run_shell_command("anaconda remove ArcGISPyAPIBot/arcgis --force") 
-
-if __name__ == "__main__":
-    publish_results("NOT_SPECIFIED")
+def _upload_directory_recursive(ftp, src_dir_path, dst_dir_path):
+    for name in os.listdir(src_dir_path):
+        curr_src_path = os.path.join(src_dir_path, name)
+        curr_dst_path = "{}/{}".format(dst_dir_path, name) #ftp servers use '/'
+        if os.path.isfile(curr_src_path):
+            log.info("Uploading {} -> ftp://{}/{}".format(curr_src_path,
+                                                           FTP_SITE,
+                                                           curr_dst_path))
+            ftp.storbinary('STOR ' + curr_dst_path, open(curr_src_path, 'rb'))
+        elif os.path.isdir(curr_src_path):
+            ftp.mkd(curr_dst_path)
+            _upload_directory_recursive(ftp, curr_src_path, curr_dst_path)
