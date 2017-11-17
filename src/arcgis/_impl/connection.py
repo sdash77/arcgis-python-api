aad6bae22b7929633f6154430362f44c5ecf8ae7
Rohit Singh <rsingh@esri.com>
2017-11-17 13:11:44 -0800

    Handle error 403: SSL Required for #695

--- src/arcgis/_impl/connection.py
+++ src/arcgis/_impl/connection.py
@@ -858,6 +858,12 @@ class _ArcGISConnection(object):
                                 return self.get(path=newpath, params=params, ssl=ssl, compress=compress, try_json=try_json, is_retry=True)
                             elif errorcode == 498:
                                 raise RuntimeError('Invalid token')
+
+                            elif errorcode == 403:
+                                message = resp_json['error']['message'] if 'message' in resp_json['error'] else ''
+                                if message == "SSL Required":
+                                    return self.get(path=path, params=params, ssl=True, compress=compress, try_json=try_json, is_retry=True)
+
                             self._handle_json_error(resp_json['error'], errorcode)
                             return None
                 except AttributeError:
@@ -1129,6 +1135,13 @@ class _ArcGISConnection(object):
                                      is_retry=True)
                 elif errorcode == 498:
                     raise RuntimeError('Invalid token')
+
+                elif errorcode == 403:
+                    message = resp_json['error']['message'] if 'message' in resp_json['error'] else ''
+                    if message == "SSL Required":
+                        return self.post(path, postdata, files, ssl=True, compress=compress, token=token,
+                                         verify_cert=verify_cert, is_retry=True)
+
                 self._handle_json_error(resp_json['error'], errorcode)
                 return None
         except AttributeError:
