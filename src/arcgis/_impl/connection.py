95f5b05374aabc2cd3595b7d8760cd244fbf1292
Rohit Singh <rsingh@esri.com>
2017-11-18 06:26:10 -0800

    NTLM support on Python 3.6.1+ without pywin32 for #697

--- src/arcgis/_impl/connection.py
+++ src/arcgis/_impl/connection.py
@@ -970,8 +970,8 @@ class _ArcGISConnection(object):
                     handlers.append(auth_krb)
 
                 except Error as err:
-                    _log.error("pywin32 and winkerberos packages are required for IWA authentication.")
-                    _log.error("Please install them:\n\tconda install pywin32\n\tconda install winkerberos")
+                    _log.error("winkerberos packages is required for IWA authentication (NTLM and Kerberos).")
+                    _log.error("Please install it:\n\tconda install winkerberos")
                     _log.error(str(err))
             else:
                 _log.error('The GIS uses Integrated Windows Authentication which is currently only supported on the Windows platform')
--- src/setup.py
+++ src/setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.5',
+    version='1.3.0',
 
     description='ArcGIS Python API',
     long_description='The ArcGIS API for Python lets ArcGIS Online and ArcGIS Enterprise users, analysts, developers and administrators script and automate tasks ranging from performing big data analysis to content management and administration of their web GIS. The API integrates well with the Jupyter Notebook and the SciPy stack and enables academics, data scientists, and GIS analysts to share programs and reproducible research with others.',
