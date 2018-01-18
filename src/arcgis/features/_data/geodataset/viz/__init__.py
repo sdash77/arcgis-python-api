2654c07d18d5f93f3023175f2ef238c8e5fcdeb7
achapkowski <achapkowski@esri.com>
2018-01-18 07:24:24 -0500

    (code clean up) Cleaned up __all__ statement and removed print statements.

--- src/arcgis/features/_data/geodataset/viz/__init__.py
+++ src/arcgis/features/_data/geodataset/viz/__init__.py
@@ -1,10 +1,6 @@
 """
 Graphing Library
 """
-from arcgis.features._data.geodataset.viz.renderer import generate_renderer
-from arcgis.features._data.geodataset.viz.symbol import create_symbol, display_colormaps, show_styles
 from arcgis.features._data.geodataset.viz.mapping import plot
 
-__all__ = [ 'generate_renderer', 'create_symbol',
-            'display_colormaps', 'show_styles',
-            'plot']
+__all__ = ['plot']
