778ce798812f3c2b08094a706a1d180e5acb565c
achapkowski <achapkowski@esri.com>
2018-01-25 10:40:05 -0500

    Implemented group.search() added categories issue #629

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -1871,7 +1871,7 @@ class GroupManager(object):
         return None
 
     def search(self, query='', sort_field='title', sort_order='asc',
-               max_groups=1000, outside_org=False):
+               max_groups=1000, outside_org=False, categories=None):
         """
         Searches for portal groups.
 
@@ -1915,7 +1915,7 @@ class GroupManager(object):
            A list of groups matching the specified query.
         """
         grouplist = []
-        groups = self._portal.search_groups(query, sort_field, sort_order, max_groups, outside_org)
+        groups = self._portal.search_groups(query, sort_field, sort_order, max_groups, outside_org, categories)
         for group in groups:
             grouplist.append(Group(self._gis, group['id'], group))
         return grouplist
