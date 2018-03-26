fa8f18921e67455665912922fa7d97de7226ac6c
achapkowski <achapkowski@esri.com>
2018-03-26 07:11:37 -0400

    Added case where Case where build initial cache is False and user wants item to be returned.

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -6153,6 +6153,8 @@ class Item(dict):
                 ret = ms.manager.update_tiles(levels=lod, extent=full_extent)
             except Exception as tiles_ex:
                 raise Exception('Error unpacking tiles :' + str(tiles_ex))
+        elif not buildInitialCache and output_type.lower() in ['sceneservice']:
+            return Item(self._gis, ret[0]['serviceItemId'])
         else:
             serviceitem_id = self._check_publish_status(ret, folder)
         return Item(self._gis, serviceitem_id)
