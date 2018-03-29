sdash77/arcgis-python-api

commit 31fc3335e79ae46edd3f9de23ca4aecb262e51e1

Resolved error in MapService swizzling in web map. (#992)

src/arcgis/_impl/common/_clone.py
@@ -1642,8 +1642,9 @@ class _WebMapDefinition(_TextItemDefinition):
                         feature_collection['itemId'] = self._clone_mapping['Item IDs'][feature_collection['itemId']]
 
                 for map_service_layer in map_service_layers:
+                    map_service_url = map_service_layer['url']
                     for original_url, new_service in self._clone_mapping['Services'].items():
-                        if _compare_url(feature_service_url, original_url):
+                        if _compare_url(map_service_url, original_url):
                             map_service_layer['url'] = new_service['url']
                             map_service_layer['itemId'] = new_service['id']
                             break
@@ -2892,6 +2893,8 @@ def _compare_url(url1, url2):
     url1 - The first url
     url2 - The second url"""
 
+    url1 = url1.rstrip("/")
+    url2 = url2.rstrip("/")
     url_parse1 = urlparse(url1)
     url_parse2 = urlparse(url2)
     return "{0}{1}".format(url_parse1.netloc.lower(), url_parse1.path.lower()) == "{0}{1}".format(url_parse2.netloc.lower(), url_parse2.path.lower())

commit 547c915aea941307027c87a791e8796f6254e4b8

Resolved error introduced in PR #992 (#1007)
* Resolved error introduced in PR #992, sometimes url passed to compare function is null and need to handle it.

* Made requested changes.

src/arcgis/_impl/common/_clone.py
@@ -2894,6 +2894,9 @@ def _compare_url(url1, url2):
     url1 - The first url
     url2 - The second url"""
 
+    if url1 is None or url2 is None:
+       return False
+
     url1 = url1.rstrip("/")
     url2 = url2.rstrip("/")
     url_parse1 = urlparse(url1)