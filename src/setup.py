09e6bb372f07d386b06f7d2f97eb660abf641f21
DavidJVitale <DavidJVitale@users.noreply.github.com>
2018-04-25 14:10:31 -0700

    Added pypi_long_description.md file, the ability to read that file in in setup.py for the description, and the setuptools requirement for being able to build that sdist package with markdown

--- src/setup.py
+++ src/setup.py
@@ -118,6 +118,11 @@ class egg_info(_egg_info):
         self.execute(_post_install, (), msg="Running post-install...")
         super().run()
 
+#Read the description.md file
+description_md_file = open("pypi_long_description.md", "r")
+long_description = description_md_file.read()
+description_md_file.close()
+
 # Get the long description from the README file
 # with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 #     long_description = f.read()
@@ -131,7 +136,8 @@ kwargs = {
     "version":'1.4.1',
 
     "description":'ArcGIS API for Python',
-    "long_description":'ArcGIS API for Python is a Python library for working with maps and geospatial data, powered by web GIS. It provides simple and efficient tools for sophisticated vector and raster analysis, geocoding, map making, routing and directions, as well as for organizing and managing a GIS with users, groups and information items. In addition to working with your own data, the library enables access to ready to use maps and curated geographic data from Esri and other authoritative sources. It also integrates well with the scientific Python ecosystem and includes rich support for Pandas and Jupyter notebook.',
+    "long_description":long_description,
+    "long_description_content_type":"text/markdown",
 
     # The project's main homepage.
     "url":'https://developers.arcgis.com/python/',
