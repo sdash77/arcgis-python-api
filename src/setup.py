453935130fe69ceac7a9bde0c2f92ebbbe6d217f
achapkowski <achapkowski@esri.com>
2018-05-11 16:46:45 -0400

    made 'dill' optional for gp feature.

--- src/setup.py
+++ src/setup.py
@@ -219,12 +219,17 @@ kwargs = {
     "cmdclass":{
             'develop': develop,
             'install': install,
-            'egg_info': egg_info}
+            'egg_info': egg_info},
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
+    "extras_require":{
+        'gp': ['dill'],
+
+    },
+
     # extras_require={
     #     'dev': ['check-manifest'],
     #     'test': ['coverage'],
