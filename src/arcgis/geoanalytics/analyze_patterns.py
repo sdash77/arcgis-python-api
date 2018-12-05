f82787c723b5926cdb50d147958b8db19aeff0f6
achapkowski <achapkowski@esri.com>
2018-12-05 09:06:42 -0500

    - added first go of forest_based_regression

--- src/arcgis/geoanalytics/analyze_patterns.py
+++ src/arcgis/geoanalytics/analyze_patterns.py
@@ -17,6 +17,184 @@ _log=_logging.getLogger(__name__)
 
 _use_async=True
 
+def forest_based_regression(input_layer,
+                            prediction_type,
+                            features_to_predict,
+
+                            output_name=None,
+                            gis=None):
+    """
+    Creates models and generates predictions using an adaptation of Leo Breiman's random forest
+    algorithm, which is a supervised machine learning method. Predictions can be performed for both
+    categorical variables (classification) and continuous variables (regression). Explanatory
+    variables can take the form of fields in the attribute table of the training features. In
+    addition to validation of model performance based on the training data, predictions can be made
+    to another feature dataset.
+
+    The following are examples:
+
+        + Given data on occurrence of seagrass, as well as a number of environmental explanatory variables represented as both attributes which has been enriched using a multi-variable grid to calculate distances to factories upstream and major ports, future seagrass occurrence can be predicted based on future projections for those same environmental explanatory variables.
+        + Suppose you have crop yield data at hundreds of farms across the country along with other attributes at each of those farms (number of employees, acreage, and so on). Using these pieces of data, you can provide a set of features representing farms where you don't have crop yield (but you do have all of the other variables), and make a prediction about crop yield.
+        + Housing values can be predicted based on the prices of houses that have been sold in the current year. The sale price of homes sold along with information about the number of bedrooms, distance to schools, proximity to major highways, average income, and crime counts can be used to predict sale prices of similar homes.
+
+
+    **Forest Based Classification and Regression is available at ArcGIS Enterprise 10.7.**
+
+    ==========================   ===============================================================
+    **Argument**                 **Description**
+    --------------------------   ---------------------------------------------------------------
+    input_layer                  required FeatureSet, The table, point, line or polygon features
+                                 containing potential incidents.
+    --------------------------   ---------------------------------------------------------------
+    prediction_type              Specifies the operation mode of the tool. The tool can be run to train a model to only assess performance, or train a model and predict features. Prediction types are as follows:
+                                    + Train - This is the default. A model will be trained, but no predictions will be generated. Use this option to assess the accuracy of your model before generating predictions. This option will output model diagnostics in the messages window and a chart of variable importance.
+                                    + TrainAndPredict - Predictions or classifications will be generated for features. Explanatory variables must be provided for both the training features and the features to be predicted. The output of this option will be a feature service, model diagnostics, and an optional table of variable importance.
+    --------------------------   ---------------------------------------------------------------
+    features_to_predict          Optional dict. The variable from the `input_layer` parameter
+                                 containing the values to be used to train the model, and a
+                                 boolean denoting if it's categorical. This field contains known
+                                 (training) values of the variable that will be used to predict
+                                 at unknown locations.
+    --------------------------   ---------------------------------------------------------------
+    variable_predict
+    --------------------------   ---------------------------------------------------------------
+    explanatory_variable_match
+    --------------------------   ---------------------------------------------------------------
+    return_importance_table
+    --------------------------   ---------------------------------------------------------------
+    number_of_trees
+    --------------------------   ---------------------------------------------------------------
+    min_leaf_size
+    --------------------------   ---------------------------------------------------------------
+    max_tree_depth
+    --------------------------   ---------------------------------------------------------------
+    sample_size
+    --------------------------   ---------------------------------------------------------------
+    random_vars
+    --------------------------   ---------------------------------------------------------------
+    percentage_for_validation
+    --------------------------   ---------------------------------------------------------------
+    output_trained_name
+    --------------------------   ---------------------------------------------------------------
+    output_name                  optional string, The task will create a feature service of the
+                                 results. You define the name of the service.
+    --------------------------   ---------------------------------------------------------------
+    gis                          optional GIS, the GIS on which this tool runs. If not
+                                 specified, the active GIS is used.
+    ==========================   ===============================================================
+
+    :returns:
+       Output feature layer item
+
+
+    """
+    kwargs=locals()
+
+    gis=_arcgis.env.active_gis if gis is None else gis
+    url=gis.properties.helperServices.geoanalytics.url
+
+    params={}
+    for key, value in kwargs.items():
+        if value is not None:
+            params[key]=value
+
+    if output_name is None:
+        output_service_name='Forest Based Regression_' + _id_generator()
+        output_name=output_service_name.replace(' ', '_')
+    else:
+        output_service_name=output_name.replace(' ', '_')
+
+    output_service=_create_output_service(gis, output_name, output_service_name, 'Forest Based Classification And Regression')
+
+    _set_context(params)
+
+    param_db={
+        "input_layer": (_FeatureSet, "inFeatures"),
+        "prediction_type" : (str, "predictionType"),
+        "features_to_predict" : (_FeatureSet, "featuresToPredict"),
+        "variable_predict" : (_FeatureSet, "featuresToPredict"),
+        "explanatory_variables" : (list, "explanatoryVariables"),
+        "explanatory_variable_match" : (list, "explanatoryVariableMatching"),
+        "return_importance_table" : (bool, "returnVariableOfImportanceTable"),
+        "number_of_trees" : (int, "numberOfTrees"),
+        "min_leaf_size" : (int, "minimumLeafSize"),
+        "sample_size" : (int, "sampleSize"),
+        "random_vars" : (str, "randomVariables"),
+        "percentage_for_validation" : (int, "percentageForValidation"),
+        "outputTrainedName" : (str, "output_name"),
+        "context": (str, "context"),
+        "outputTrainedName": (_FeatureSet, "Output Features"),
+    }
+    return_values=[
+        {"name": "outputTrainedName", "display_name": "Output Features", "type": _FeatureSet},
+    ]
+
+    try:
+        _execute_gp_tool(gis, "ForestBasedClassificationAndRegression", params, param_db, return_values, _use_async, url, True)
+        return output_service
+    except:
+        output_service.delete()
+        raise
+
+    return
+
+
+def _generalized_linear_regression(input_layer,
+                                features_to_predict,
+                                dependent_variable,
+                                explanatory_variables,
+                                regression_family="Continuous",
+                                generate_coef_table=False,
+                                variable_matching=None,
+                                output_name=None,
+                                gis=None):
+    """
+    TODO: need the docstring
+    """
+    kwargs=locals()
+
+    gis=_arcgis.env.active_gis if gis is None else gis
+    url=gis.properties.helperServices.geoanalytics.url
+
+    params={}
+    for key, value in kwargs.items():
+        if value is not None:
+            params[key]=value
+
+    if output_name is None:
+        output_service_name='Find Point Clusters_' + _id_generator()
+        output_name=output_service_name.replace(' ', '_')
+    else:
+        output_service_name=output_name.replace(' ', '_')
+
+    output_service=_create_output_service(gis, output_name, output_service_name, 'Find Point Clusters')
+
+    _set_context(params)
+
+    param_db={
+        "input_layer": (_FeatureSet, "inputLayer"),
+        "output_name": (str, "outputName"),
+        "features_to_predict" : (_FeatureSet, "featuresToPredict"),
+        "dependent_variable" : (str, "dependentVariable"),
+        "explanatory_variables" : (str, "explanatoryVariables"),
+        "regression_family" : (str, "regressionFamily"),
+        "generate_coef_table" : (bool, "generateCoefficientTable"),
+        "variable_matching" : (str, "explanatoryVariableMatching"),
+        "context": (str, "context"),
+        "output": (_FeatureSet, "Output Features"),
+    }
+    return_values=[
+        {"name": "output", "display_name": "Output Features", "type": _FeatureSet},
+    ]
+
+    try:
+        _execute_gp_tool(gis, "FindPointClusters", params, param_db, return_values, _use_async, url, True)
+        return output_service
+    except:
+        output_service.delete()
+        raise
+
+    return
 
 def find_point_clusters(
     input_layer,
