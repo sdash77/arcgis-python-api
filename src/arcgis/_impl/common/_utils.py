6e602cd2a8003e23f11f8e9c20b508374aaf85e1
achapkowski <achapkowski@esri.com>
2019-01-07 06:32:34 -0500

    - Added Method to Upload Large Files Using add by parts.  This can be applied to any file over 5 MB.

--- src/arcgis/_impl/common/_utils.py
+++ src/arcgis/_impl/common/_utils.py
@@ -13,6 +13,21 @@ import logging
 import decimal
 import functools
 #----------------------------------------------------------------------
+def bytesto(size, to='m', bsize=1024):
+    """convert bytes to megabytes, etc.
+       sample code:
+           print('mb= ' + str(bytesto(314575262000000, 'm')))
+
+       sample output:
+           mb= 300002347.946
+    """
+
+    a = {'k' : 1, 'm': 2, 'g' : 3, 't' : 4, 'p' : 5, 'e' : 6 }
+    r = float(size)
+    for i in range(a[to]):
+        r = r / bsize
+    return(r)
+#----------------------------------------------------------------------
 def create_uid():
     if six.PY2:
         return uuid.uuid4().get_hex()
