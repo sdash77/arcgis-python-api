a4fbb61e2f86f894018a9e0c6c59fbf6dbefab3e
achapkowski <achapkowski@esri.com>
2019-02-04 13:05:41 -0500

    - fixed f-string issue for py 3.5

--- src/arcgis/learn/_data.py
+++ src/arcgis/learn/_data.py
@@ -41,7 +41,7 @@ def _bb_pad_collate(samples, pad_idx=0):
 
 
 def _get_bbox_lbls(imagefile, class_mapping):
-    xmlfile = imagefile.parents[1] / 'labels' / imagefile.name.replace(f'{imagefile.suffix}', '.xml')
+    xmlfile = imagefile.parents[1] / 'labels' / imagefile.name.replace('{ims}'.format(ims=imagefile.suffix), '.xml')
     tree = ET.parse(xmlfile)
     xmlroot = tree.getroot()
     bboxes  = []
@@ -59,9 +59,9 @@ def _get_bbox_lbls(imagefile, class_mapping):
 
 def prepare_data(path, class_mapping, chip_size=224, val_split_pct=0.1, batch_size=64, transforms=None, collate_fn=_bb_pad_collate, seed=42):
     """
-    Prepares a Fast.ai DataBunch from the exported Pascal VOC image chips 
-    exported by Export Training Data tool in ArcGIS Pro or Image Server.    
-    This DataBunch consists of training and validation DataLoaders with the 
+    Prepares a Fast.ai DataBunch from the exported Pascal VOC image chips
+    exported by Export Training Data tool in ArcGIS Pro or Image Server.
+    This DataBunch consists of training and validation DataLoaders with the
     specified transformations, chip size, batch size, split percentage.
 
     =====================   ===========================================
@@ -69,60 +69,60 @@ def prepare_data(path, class_mapping, chip_size=224, val_split_pct=0.1, batch_si
     ---------------------   -------------------------------------------
     path                    Required string. Path to data directory.
     ---------------------   -------------------------------------------
-    class_mapping           Required dictionary. Mapping from PascalVOC id to 
+    class_mapping           Required dictionary. Mapping from PascalVOC id to
                             its string label.
     ---------------------   -------------------------------------------
-    chip_size               Optional integer. Size of the image to train the 
+    chip_size               Optional integer. Size of the image to train the
                             model.
     ---------------------   -------------------------------------------
-    val_split_pct           Optional float. Percentage of training data to keep 
+    val_split_pct           Optional float. Percentage of training data to keep
                             as validation.
     ---------------------   -------------------------------------------
-    batch_size              Optional integer. Batch size for mini batch gradient 
-                            descent (Reduce it if getting CUDA Out of Memory 
+    batch_size              Optional integer. Batch size for mini batch gradient
+                            descent (Reduce it if getting CUDA Out of Memory
                             Errors).
-    ---------------------   -------------------------------------------    
-    transforms              Optional tuple. Fast.ai transforms for data 
-                            augmentation of training and validation datasets 
-                            respectively (We have set good defaults which work 
+    ---------------------   -------------------------------------------
+    transforms              Optional tuple. Fast.ai transforms for data
+                            augmentation of training and validation datasets
+                            respectively (We have set good defaults which work
                             for satellite imagery well).
     ---------------------   -------------------------------------------
     collate_fn              Optional function. Passed to PyTorch to collate data
                             into batches(usually default works).
     ---------------------   -------------------------------------------
-    seed                    Optional integer. Random seed for reproducible 
+    seed                    Optional integer. Random seed for reproducible
                             train-validation split.
-    =====================   ===========================================    
+    =====================   ===========================================
 
     :returns: fastai DataBunch object
     """
 
     if not HAS_FASTAI:
         _raise_fastai_import_error()
-    
+
     if type(path) is str:
         path = Path(path)
-        
+
     images = 'images'
-    
+
     get_y_func = partial(_get_bbox_lbls, class_mapping=class_mapping)
 
     src = (SSDObjectItemList.from_folder(path/images)
        .random_split_by_pct(val_split_pct, seed=seed)
        .label_from_func(get_y_func))
-    
+
     if transforms is None:
         ranges = (0,1)
         train_tfms = [crop(size=chip_size, p=1., row_pct=ranges, col_pct=ranges), dihedral_affine()]
         val_tfms = [crop(size=chip_size, p=1., row_pct=0.5, col_pct=0.5)]
-        transforms = (train_tfms, val_tfms)        
-        
+        transforms = (train_tfms, val_tfms)
+
     data = (src
         .transform(transforms, tfm_y=True)
         .databunch(bs=batch_size, collate_fn=collate_fn)
         .normalize(imagenet_stats)
        )
-    
+
     data.chip_size = chip_size
-    
+
     return data
