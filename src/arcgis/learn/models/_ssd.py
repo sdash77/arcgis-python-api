0392cf0a5c42e51d9101e97e43b0fb83a4509658
Divyansh Jha <divyanshj.16@gmail.com>
2019-04-16 00:44:18 +0530

    makes fit_one_cycle the default fit method (#1905)

--- src/arcgis/learn/models/_ssd.py
+++ src/arcgis/learn/models/_ssd.py
@@ -173,7 +173,7 @@ class SingleShotDetector(object):
         clear_output()
         self.learn.recorder.plot()
 
-    def fit(self, epochs=10, lr=slice(1e-4,3e-3)):
+    def fit(self, epochs=10, lr=slice(1e-4,3e-3), one_cycle=True):
         """
         Train the model for the specified number of epocs and using the
         specified learning rates
@@ -187,9 +187,17 @@ class SingleShotDetector(object):
         lr                      Required float or slice of floats. Learning rate
                                 to be used for training the model. Select from
                                 the `lr_find` plot.
+        ---------------------   -------------------------------------------
+        one_cycle               Optional boolean. Parameter to select 1cycle
+                                learning rate schedule. If set to `False` no 
+                                learning rate schedule is used.                                
         =====================   ===========================================
         """
-        self.learn.fit(epochs, lr)
+        if one_cycle:
+            self.learn.fit_one_cycle(epochs, lr)
+        else:
+            self.learn.fit(epochs, lr)
+
 
     def unfreeze(self):
         """
