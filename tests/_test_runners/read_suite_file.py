abc2e7ce17861b2ed18340247e36cac6a38a1733
DavidJVitale <DavidJVitale@users.noreply.github.com>
2019-05-28 14:01:08 -0700

    Removed reference to blacklist regex

--- tests/_test_runners/read_suite_file.py
+++ tests/_test_runners/read_suite_file.py
@@ -57,7 +57,7 @@ def _remove_blacklist_paths(suite):
     for tests_to_run_key in suite:
         if 'blacklist' not in suite[tests_to_run_key]['config']:
             continue
-        blacklist = suite[tests_to_run_key]['config']['blacklist_regexes']
+        blacklist = suite[tests_to_run_key]['config']['blacklist']
         for blacklist in blacklist:
             suite[tests_to_run_key]['paths'] = list(\
                 path for path in suite[tests_to_run_key]['paths'] \
