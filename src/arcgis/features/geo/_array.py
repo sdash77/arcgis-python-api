455f26edab54422696eb4ec148d99848562c97e1
achapkowski <achapkowski@esri.com>
2019-05-30 05:30:48 -0400

    - fix for property is_valid on GeoArray.

--- src/arcgis/features/geo/_array.py
+++ src/arcgis/features/geo/_array.py
@@ -196,7 +196,7 @@ class GeoArray(NumPyBackedExtensionArrayMixin):
     @property
     def is_valid(self):
         """Checks if the Geometry is Valid"""
-        return pd.Series([g.is_valid for g in self])
+        return pd.Series([g.is_valid() for g in self])
 
     #----------------------------------------------------------------------
     def _call_property(self, prop, as_ga=False):
@@ -275,10 +275,10 @@ class GeoArray(NumPyBackedExtensionArrayMixin):
     @property
     def is_multipart(self):
         return self._call_property("is_multipart")
-    #----------------------------------------------------------------------
-    @property
-    def is_valid(self):
-        return self._call_property("is_valid")
+    ##----------------------------------------------------------------------
+    #@property
+    #def is_valid(self):
+    #    return self._call_property("is_valid")
     #----------------------------------------------------------------------
     @property
     def JSON(self):
