6532071321a2901f44105e6821202692aaebe091
DavidJVitale <DavidJVitale@users.noreply.github.com>
2019-06-07 15:42:22 -0700

    path case sensitivity for sanity

--- tests/unit/sanity/test_arcgis_being_imported_correctly.py
+++ tests/unit/sanity/test_arcgis_being_imported_correctly.py
@@ -2,6 +2,6 @@ from utils._common import *
 
 def test_arcgis_being_imported_correctly():
     import arcgis
-    assert arcgis.__file__ == os.path.join(GEOSAURUS_SRC_ARCGIS_DIR,
-                                           "__init__.py")
-
+    expected = os.path.join(GEOSAURUS_SRC_ARCGIS_DIR, "__init__.py").lower()
+    actual = arcgis.__file__.lower()
+    assert actual == expected
