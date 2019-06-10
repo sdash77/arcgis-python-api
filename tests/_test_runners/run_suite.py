9d8db549c2724a60299249d3e3319bba44fbc755
DavidJVitale <DavidJVitale@users.noreply.github.com>
2019-06-10 14:48:19 -0700

    Changing to dev.yml so I can debug better

--- tests/_test_runners/run_suite.py
+++ tests/_test_runners/run_suite.py
@@ -9,13 +9,15 @@ from utils._common import *
 
 file_uri_output_dir = pathlib.Path(TESTS_DIR).as_uri()
 
-def run_suite(suite, output_dir, jenkins_job_url=file_uri_output_dir):
+def run_suite(suite, output_dir, 
+              jenkins_job_url=file_uri_output_dir,
+              skip_sanity = False):
     log.debug(f"running suite {json.dumps(suite)}")
     output_xml_results = []
 
-    # Always run the sanity tests located in geosaurus/tests/unit/sanity/
-    xml_output = run_sanity_tests(output_dir)
-    output_xml_results.append(xml_output)
+    if not skip_sanity:
+        xml_output = run_sanity_tests(output_dir)
+        output_xml_results.append(xml_output)
 
     if "unit_tests_to_run" in suite and \
        suite["unit_tests_to_run"]["paths"]:
