aabf086d264602a74dced32b7e42218d5d0c578d
DavidJVitale <DavidJVitale@users.noreply.github.com>
2019-06-12 15:55:57 -0700

    Final form of basic widget test to run...

--- tests/utils/mapview/override_css.py
+++ tests/utils/mapview/override_css.py
@@ -8,7 +8,10 @@ def override_css():
         f"Since importing <code>utils.mapview</code>, implementing CSS "\
         f"override to make all <code>MapView</code> instances the same "\
         f"width and height so screenshots are easier to compare. "\
-        f"<b>Make sure this message appears before drawing any maps. </b>"\
         f"CSS applying: <code>{css_override}</code><hr>"\
-        f'<style id="utils-mapview-override-css">{css_override}</style>'))
-
+        f'<style id="utils-mapview-override-css">{css_override}</style>'\
+        f"<h3>- Make sure this message appears before drawing any maps.</h3>"\
+        f"<h4>- Make sure window is fullscreen when taking screenshots</h4>"\
+        f"<h4>- Make sure there are <code>time.sleep(X)</code> calls in "\
+        f"individual cells surrounding <code>MapView.take_screenshot()</code>"\
+        f" calls.</h4>"))
