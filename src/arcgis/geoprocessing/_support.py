dc6a7aff41ae9bcf2be2e1bba89dd3d098ee85c5
ajc <achapkowski@esri.com>
2019-06-26 11:07:54 -0400

    - fixed issue with numpy.Int64 causes json.dumps to fail.

--- src/arcgis/geoprocessing/_support.py
+++ src/arcgis/geoprocessing/_support.py
@@ -263,7 +263,7 @@ def _execute_gp_tool(gis, task_name, params, param_db, return_values, use_async,
 
     # ---------------------in---------------------#
     for param_name, param_value in params.items():
-        #print(param_name + " = " + str(param_value))
+        print(param_name + " = " + str(param_value))
         if param_name in param_db:
             py_type, gp_param_name = param_db[param_name]
             if param_value is None:
@@ -285,7 +285,10 @@ def _execute_gp_tool(gis, task_name, params, param_db, return_values, use_async,
                     if type(param_value) == FeatureSet:
                         gp_params[gp_param_name] = param_value.to_dict()
                     elif  _is_geoenabled(param_value):
-                        gp_params[gp_param_name] = param_value.spatial.__feature_set__
+                        from arcgis._impl.common._utils import _date_handler
+                        import json
+                        gp_params[gp_param_name] = json.loads(json.dumps(param_value.spatial.__feature_set__, 
+                                                                         default=_date_handler))
                     elif type(param_value) == str:
 
                         try:
