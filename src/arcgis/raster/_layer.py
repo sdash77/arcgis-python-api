6a152a72a98e49918e29b9e7638a530899500ff6
Rhea Jackson <33869815+rhea-j@users.noreply.github.com>
2019-07-29 09:17:29 +0400

    Rhea9381/lyr json issue (#2235)
    * Resolve issue with _lyr_json
    
    * Resolve issue with _lyr_json

--- src/arcgis/raster/_layer.py
+++ src/arcgis/raster/_layer.py
@@ -485,6 +485,7 @@ class ImageryLayer(Layer):
             if self._mosaic_rule is not None:
                 options_dict["imageServiceParameters"]["mosaicRule"] = self._mosaic_rule
 
+
         if self._datastore_raster:
             options_dict["imageServiceParameters"]["raster"] =self._uri
             if isinstance(self._uri, bytes):
@@ -492,6 +493,7 @@ class ImageryLayer(Layer):
                     del options_dict["imageServiceParameters"]['renderingRule']
                 options_dict["imageServiceParameters"]["raster"] =self._fn
 
+        if options_dict['imageServiceParameters'] != {}:
             lyr_dict.update({
                 "options": json.dumps(options_dict)
             })
