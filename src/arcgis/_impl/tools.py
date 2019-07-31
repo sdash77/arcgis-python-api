c263939e39b14cb805cb71b7198e6fe9e7d646f5
ajc <achapkowski@esri.com>
2019-07-31 07:18:25 -0400

    - changes made per request on PR #2276 comments by @rohitgeo

--- src/arcgis/_impl/tools.py
+++ src/arcgis/_impl/tools.py
@@ -878,8 +878,6 @@ class _FeatureAnalysisTools(BaseAnalytics):
         if future:
             return gpjob
         ret = gpjob.result()
-        if output_name is not None and 'allocatedDemandLocationsLayer' in ret:
-            return ret['allocatedDemandLocationsLayer']
         return ret
     #----------------------------------------------------------------------
     def connect_origins_to_destinations(self,
@@ -986,8 +984,6 @@ class _FeatureAnalysisTools(BaseAnalytics):
         if future:
             return gpjob
         ret = gpjob.result()
-        if output_name is not None and 'routesLayer' in ret:
-            return ret['routesLayer']
         return ret
     #----------------------------------------------------------------------
     def create_drive_time_areas(self,
