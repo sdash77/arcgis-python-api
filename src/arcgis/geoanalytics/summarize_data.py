c365f42e5214ff725c7b6f9b78fc573af23bf5db
Priyanka Tuteja <39754605+priyankatuteja@users.noreply.github.com>
2019-08-12 21:36:24 +0530

    fix join tool (#2321)

--- src/arcgis/geoanalytics/summarize_data.py
+++ src/arcgis/geoanalytics/summarize_data.py
@@ -865,6 +865,12 @@ def join_features(target_layer,
         "context": (str, "context"),
         "output": (_FeatureSet, "Output Features"),
     }
+
+    return_values = [
+        {"name": "output", "display_name": "Output Features", "type": _FeatureSet},
+
+    ]
+    
     try:
         if future:
             gpjob = _execute_gp_tool(gis, "JoinFeatures", params, param_db, return_values, _use_async, url, True, future=future)
