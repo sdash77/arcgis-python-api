c479e7a32e29d2325a3e896cddc2ede69d9b78f1
David Vitale <18691334+DavidJVitale@users.noreply.github.com>
2019-09-06 10:14:12 -0700

    Hopefully makes our builds pass again (#2432)

--- src/setup.py
+++ src/setup.py
@@ -201,7 +201,6 @@ kwargs = {
             'arcgis/widgets/js/dist/extension.js',
             'arcgis/widgets/js/dist/arcgis-map-ipywidget.js',
             'arcgis/widgets/js/dist/arcgis-map-ipywidget.js.map',
-            'arcgis/gis/_impl/_decrypt_nbauth.cpython-36m-x86_64-linux-gnu.so'
             ]
         )
     ],
