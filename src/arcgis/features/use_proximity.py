56237d53b7446944c92f41f109624ba9952a7853
Priyanka Tuteja <39754605+priyankatuteja@users.noreply.github.com>
2019-09-19 13:00:50 +0530

    travel modes fix (#2501)

--- src/arcgis/features/use_proximity.py
+++ src/arcgis/features/use_proximity.py
@@ -1070,8 +1070,13 @@ def plan_routes(
     gis = _arcgis.env.active_gis if gis is None else gis
     if isinstance(travel_mode, str):
         route_service = network.RouteLayer(gis.properties.helperServices.route.url, gis=gis)
-        travel_mode = [i for i in route_service.retrieve_travel_modes()['supportedTravelModes'] if i['name'] == travel_mode][0]
-
+        travelmodes = route_service.retrieve_travel_modes()
+        for tm in travelmodes['supportedTravelModes']:
+            if tm['name'] == travel_mode:
+              tm = [i for i in route_service.retrieve_travel_modes()['supportedTravelModes'] if i['name'] == travel_mode][0]
+              travel_mode = tm
+            else: pass
+            
     return gis._tools.featureanalysis.plan_routes(
         stops_layer,
         route_count,
