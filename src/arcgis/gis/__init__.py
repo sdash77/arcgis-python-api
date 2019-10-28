c5e3eaa7c711ce939769df095a98539551a292f3
David Vitale <18691334+DavidJVitale@users.noreply.github.com>
2019-10-28 06:17:17 -0700

    Referer forced to be publicPortalUrl (#2790)

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -544,7 +544,7 @@ class GIS(object):
                 if "encryptedToken" in json_data:
                     from arcgis.gis._impl._decrypt_nbauth import get_token
                     self._utoken = get_token(nb_auth_file_path)
-                self._referer = json_data["referer"]
+                self._referer = json_data["publicPortalUrl"]
 
         # Catch errors and re-throw in with more human readable messages
         except json.JSONDecodeError as e:
