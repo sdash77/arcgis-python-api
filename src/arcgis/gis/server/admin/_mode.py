b781b65b77c043e2ff5995853fbb8cdaca4efe48
ajc <achapkowski@esri.com>
2019-11-25 07:52:00 -0500

    - Added deprecation warning to `update` method and moved logic to `update_mode`

--- src/arcgis/gis/server/admin/_mode.py
+++ src/arcgis/gis/server/admin/_mode.py
@@ -9,6 +9,7 @@ site are still available in READ_ONLY mode.
 from __future__ import absolute_import
 from __future__ import print_function
 from .._common import BaseServer
+from arcgis._impl.common._deprecate import deprecated
 
 ###########################################################################
 class Mode(BaseServer):
@@ -43,7 +44,44 @@ class Mode(BaseServer):
         if initialize:
             self._init(gis)
     #----------------------------------------------------------------------
+    @deprecated(deprecated_in="1.7.1", 
+                removed_in=None,
+                current_version="1.7.1",
+                details="Use `Mode.update_mode` instead.")    
     def update(self,
+               siteMode,
+               runAsync=False):
+        """
+        The update operation is used to move between the two types of site
+        modes. Switching to READ_ONLY mode will restart all your services
+        as the default behavior. Moving to EDITABLE mode will not restart
+        services.
+
+        ===============     ====================================================================
+        **Argument**        **Description**
+        ---------------     --------------------------------------------------------------------
+        siteMode:           Required string. The mode you will set your site to. Values:
+                            READ_ONLY or EDITABLE.
+        ---------------     --------------------------------------------------------------------
+        runAsync            Optional boolean. Determines if this operation must run asynchronously.
+        ===============     ====================================================================
+
+
+        :return: boolean
+
+        """
+        params = {"siteMode" : siteMode,
+                  "runAsync" : runAsync,
+                  "f" : "json"}
+        url = self._url + "/update"
+        res = self._con.post(path=url,
+                             postdata=params)
+        if 'status' in res:
+            return res['status'] == 'success'
+        return res
+
+    #----------------------------------------------------------------------
+    def update_mode(self,
                site_mode,
                run_async=False,
                allow_editing=True):
