53b399274674d90d7fee6b9ae6ec6200226c7290
Chunming Peng <cpeng@esri.com>
2019-12-13 18:08:58 -0800

    consider the situations when Item has no layers, or layers returned as None

--- src/arcgis/widgets/_mapview.py
+++ src/arcgis/widgets/_mapview.py
@@ -877,8 +877,16 @@ class MapView(widgets.DOMWidget):
         from pandas import DataFrame
 
         if isinstance(item, Item):
-            for layer in item.layers:
-                self._add_layer_to_widget(layer, options)
+            try:
+                getattr(item, 'layers')
+            except KeyError:
+                log.warning("No 'layers' in Item: will not be added to map")
+            else:
+                if item.layers is None:
+                    log.warning("Item.layers is a 'NoneType' object: nothing to be added to map")
+                else:
+                    for layer in item.layers:
+                        self._add_layer_to_widget(layer, options)
         elif isinstance(item, Layer):
             self._add_layer_to_webmap(item, options)
             # TODO: Expand this to separate out Layer types on Python side
