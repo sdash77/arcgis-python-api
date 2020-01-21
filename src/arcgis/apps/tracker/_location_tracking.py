ada6a8c11779f2947cf68c9d4fe3829f17922402
nira9586 <nilango@esri.com>
2020-01-21 17:49:28 -0500

    pass value into error messaging

--- src/arcgis/apps/tracker/_location_tracking.py
+++ src/arcgis/apps/tracker/_location_tracking.py
@@ -233,12 +233,12 @@ class LocationTrackingManager:
         self._validate_environment()
         if isinstance(value, str):
             if not value.isdigit():
-                raise LocationTrackingError("Invalid Retention Policy Setting: '{}' expected an integer greater than 0")
+                raise LocationTrackingError("Invalid Retention Policy Setting: '{}' expected an integer greater than 0".format(value))
         elif isinstance(value, int):
             if value < 0:
-                raise LocationTrackingError("Invalid Retention Policy Setting: '{}' expected an integer greater than 0")
+                raise LocationTrackingError("Invalid Retention Policy Setting: '{}' expected an integer greater than 0".format(value))
         else:
-            raise LocationTrackingError("Invalid Retention Policy Setting: '{}' expected an integer greater than 0")
+            raise LocationTrackingError("Invalid Retention Policy Setting: '{}' expected an integer greater than 0".format(value))
         self.tracks_layer.manager.update_definition({
             "tableMetadata": {
                 "dataRetentionStrategy": "{}".format(value)
