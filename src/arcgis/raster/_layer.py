2c108a0ddf53cd05b0a11a8318531f1147358fe0
rhea9381 <rjackson@esri.com>
2020-01-30 14:30:43 +0400

    Check for utc datetime object

--- src/arcgis/raster/_layer.py
+++ src/arcgis/raster/_layer.py
@@ -2302,6 +2302,12 @@ class ImageryLayer(Layer):
 
         if time is not None:
             if type(time) is list:
+                if isinstance(time[0], datetime.datetime) or isinstance(time[0], datetime.date):
+                    if time[0].tzname() is None or time[0].tzname() != "UTC":
+                        time[0] = time[0].astimezone(datetime.timezone.utc)
+                if isinstance(time[1], datetime.datetime) or isinstance(time[1], datetime.date):
+                    if time[1].tzname() is None or time[1].tzname() != "UTC":
+                        time[1] = time[1].astimezone(datetime.timezone.utc)
                 starttime = _date_handler(time[0])
                 endtime = _date_handler(time[1])
                 if starttime is None:
@@ -2614,6 +2620,12 @@ class ImageryLayer(Layer):
 
         if time is not None:
             if type(time) is list:
+                if isinstance(time[0], datetime.datetime) or isinstance(time[0], datetime.date):
+                    if time[0].tzname() is None or time[0].tzname() != "UTC":
+                        time[0] = time[0].astimezone(datetime.timezone.utc)
+                if isinstance(time[1], datetime.datetime) or isinstance(time[1], datetime.date):
+                    if time[1].tzname() is None or time[1].tzname() != "UTC":
+                        time[1] = time[1].astimezone(datetime.timezone.utc)
                 starttime = _date_handler(time[0])
                 endtime = _date_handler(time[1])
                 if starttime is None:
