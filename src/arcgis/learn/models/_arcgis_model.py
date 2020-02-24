8e211d8923cb34e8904f770ad8485116c3207097
Mayank Khanduja <mayankkhanduja8@gmail.com>
2020-02-24 11:18:38 +0530

    error fix when lr is defined by user (#3279)

--- src/arcgis/learn/models/_arcgis_model.py
+++ src/arcgis/learn/models/_arcgis_model.py
@@ -443,8 +443,11 @@ class ArcGISModel(object):
                 backbone = self._orig_backbone.__name__
 
         _emd_template = self._get_emd_params()
-
-        _emd_lr = slice('{0:1.4e}'.format(self._learning_rate.start), '{0:1.4e}'.format(self._learning_rate.stop))
+        
+        if isinstance(self._learning_rate, slice):
+            _emd_lr = slice('{0:1.4e}'.format(self._learning_rate.start), '{0:1.4e}'.format(self._learning_rate.stop))
+        else:
+            _emd_lr = '{0:1.4e}'.format(self._learning_rate)
 
         _emd_template["ModelFile"] = path.name
         _emd_template["ImageHeight"] = self._data.chip_size
