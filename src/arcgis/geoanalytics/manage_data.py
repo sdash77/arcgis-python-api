6692282a1d7eeafe5d7e03d0be9b59cce7a37699
priyankatuteja <priyanka.datascience@gmail.com>
2020-03-17 19:40:51 +0530

    run python script example pdated

--- src/arcgis/geoanalytics/manage_data.py
+++ src/arcgis/geoanalytics/manage_data.py
@@ -111,20 +111,18 @@ def run_python_script(code, layers=None, gis=None, context=None, future=False):
 
             # Usage Example: Execute calculate_density tool using run_python_script.
 
-            code = '''
-            import time 
-            r = geoanalytics.calculate_density(input_layer=layers[0],
-                            fields='Damage',
-                            weight='Uniform',
-                            bin_type='Square',
-                            bin_size=1,
-                            bin_size_unit='Meters',
-                            radius=2,
-                            radius_unit='Yards')        
-                            
-            r.write.format('webgis').save('RunPythonScriptTest_{0}'.format(time.time()))              
-            '''
-            run_python_script(code=code, layers=[lyr])
+            def density():
+                lyr  = layers[0]
+                r = geoanalytics.calculate_density(input_layer=lyr,
+                                                   fields='Damage',
+                                                   weight='Uniform',
+                                                   bin_type='Square',
+                                                   bin_size=1,
+                                                   bin_size_unit='Meters',
+                                                   radius=2,
+                                                   radius_unit='Yards')        
+                r.write.format('webgis').save('RunPythonScriptTest_{0}'.format(time.time()))              
+            run_python_script(code=density, layers=[hurricane_damage_lyr])
     """
     if layers is None:
         layers = []
