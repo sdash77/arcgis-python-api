f5ec1b8ffc0a7559a446860c9ce3278f2d9407c3
John Yaist <jyaist@esri.com>
2020-04-02 17:44:48 -0700

    initial examples for stand alone server connection

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -68,6 +68,9 @@ class GIS(object):
     A GIS is representative of a single ArcGIS Online organization or an ArcGIS Enterprise deployment. The GIS object
     provides helper objects to manage (search, create, retrieve) GIS resources such as content, users, and groups.
 
+       NOTE: For connecting to stand-alone GIS servers, see documentation for the Server class in the Server module
+       (https://developers.arcgis.com/python/api-reference/arcgis.gis.server.html#server)
+
     Additionally, the GIS object has properties to query its state, which is accessible using the properties attribute.
 
     The GIS provides a mapping widget that can be used in the Jupyter Notebook environment for visualizing GIS content
--- src/arcgis/gis/server/admin/administration.py
+++ src/arcgis/gis/server/admin/administration.py
@@ -46,7 +46,7 @@ class Server(BaseServer):
     baseurl                   Optional string. The root URL to a site.
                               Example: https://mysite.com/arcgis
     ---------------------     --------------------------------------------------------------------
-    tokenurl                  Optional string. Used when a site is federated or when the token
+    token_url                  Optional string. Used when a site is federated or when the token
                               URL differs from the site's baseurl.  If a site is federated, the
                               token URL will return as the Portal token and ArcGIS Server users
                               will not validate correctly.
@@ -86,6 +86,25 @@ class Server(BaseServer):
     =====================     ====================================================================
 
     """
+
+
+    ..code - block:: python
+
+    # Usage Example 1: Connecting to a stand alone unfederated server using Web Adaptor
+
+    server_base_url = "https://example.site.com"
+
+    server2 = Server(url=f"{server_base_url}/<web adaptor name>/admin",
+                     token_url=f"{server_base_url}/<web adaptor name>/tokens/generateToken",
+                     username="<admin_user>",
+                     password="<admin_passwrd>",
+                     verify_cert=False)
+
+    server2
+
+       <Server at https://dev0012279.esri.com/server/admin>
+
+
     _url = None
     _con = None
     _json_dict = None
