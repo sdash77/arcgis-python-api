Fix bugs in displaying precision and recall and loss function issue in pspnet and deeplab (#3593)
* fix warning for not having class balancing

* fix perclass metrics

* fix reduction error in pspnet and deeplab

* improve import statement

src/arcgis/learn/models/_deeplab.py
@@ -248,7 +248,7 @@ class DeepLab(ArcGISModel):
             model_accuracy = np.max(self.learn.recorder.metrics)             
         return float(model_accuracy)
 
-    def _deeplab_loss(self, outputs, targets):
+    def _deeplab_loss(self, outputs, targets, **kwargs):
         targets = targets.squeeze(1).detach()
 
         if self.class_balancing and self._data.class_weight is not None: