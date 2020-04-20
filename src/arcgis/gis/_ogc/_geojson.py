ce1d505260d8a6765118537f6076d6085e4937a2
ajc <achapkowski@esri.com>
2020-04-20 14:46:23 -0400

    - added GeoJSON Layer tests and GeoJSON Layer class - added small change to scale property

--- src/arcgis/gis/_ogc/_geojson.py
+++ src/arcgis/gis/_ogc/_geojson.py
@@ -22,7 +22,8 @@ class GeoJSONLayer(object):
         self._copyright = kwargs.pop("copyright", "")
         self._title = kwargs.pop("title", "GeoJSON Layer")
         self._id = kwargs.pop('id', uuid.uuid4().hex)
-        self._minScale, self._maxScale = kwargs.pop('scale', (0,0))
+        self._min_scale, self._max_scale = kwargs.pop('scale', (0,0))
+        self._opacity = kwargs.pop("opacity", 0)
         if 'renderer' in kwargs:
             r = kwargs.pop('renderer', None)
             if isinstance(r, dict):
