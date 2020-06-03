b4b817c734ba8ab0ef479b528170f8569c483949
Osheen Nayak <osh10490@esri.com>
2020-06-03 15:26:58 +0530

    change output_name from rn to retinanet

--- tests/integration/arcgis_learn/test_common.py
+++ tests/integration/arcgis_learn/test_common.py
@@ -64,7 +64,7 @@ def common_test(test_object, model_type, output_name, data_path, old_models, **p
     model_object.show_results()
 
     # Test for accuracy if nightly_test is run
-    if output_name in ['rn', 'ssd']:
+    if output_name in ['retinanet', 'ssd']:
         if os.environ['nightly_test'] == "1":
             test_object.assertGreater(model_object.average_precision_score(mean=True), .40)
 
@@ -213,11 +213,11 @@ class Test_Common(unittest.TestCase):
             )
 
     @unittest.skipIf(True, "Preconditions not met, skipping test")
-    def test_rn(self):
+    def test_retinanet(self):
         common_test(
             self,
             RetinaNet,
-            'rn',
+            'retinanet',
             self.obj_detection_data2,
             [self.model_rn_170],
             batch_size=2,
