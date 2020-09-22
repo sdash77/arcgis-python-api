3386f731e89ec478ae8bddb4db7b60bbe4718716
nira9586 <nilango@esri.com>
2020-09-22 08:37:06 -0400

    remove random

--- tests/integration/apps/test_workforce_AssignmentTypes.py
+++ tests/integration/apps/test_workforce_AssignmentTypes.py
@@ -7,7 +7,6 @@ from integration.dino_utils.dino_precondition_checks import PreconditionChecks
 from integration.dino_utils.dino_configs import DinoConfigs
 from configparser import ConfigParser
 import datetime
-import random
 
 #region PreCondition check
 test_skip = False
