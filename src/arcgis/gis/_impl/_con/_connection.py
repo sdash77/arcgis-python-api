b35091fb3afd0c8377242fd1e793b1b82ffe1509
Andrew <achapkowski@users.noreply.github.com>
2020-09-28 11:47:51 -0400

    resolution for issue BUG-000133897 ArcGIS Pro 2.6.1 returns SSL error when accessing Portal via Jupyter notebooks. (#4729)
    * - resolution for issue BUG-000133897 ArcGIS Pro 2.6.1 returns SSL error when accessing Portal via Jupyter notebooks.
    #4713
    resolves #4713
    
    * - added usage of certifi_win32
    
    * Added check for win32 on import and removed unneeded code comments
    
    * - added logic to skip import if `certifi-win32` does not install.
    
    * - updated yml and yaml files.
    
    * - used wrong word, `continue`, when I should have used `pass`

--- src/arcgis/gis/_impl/_con/_connection.py
+++ src/arcgis/gis/_impl/_con/_connection.py
@@ -11,8 +11,14 @@ try:
 except ImportError:
     HASARCPY = False
 
-import os
 import sys
+if sys.platform == 'win32':
+    try:
+        import certifi_win32
+    except ImportError:
+        pass
+
+import os
 import copy
 import json
 import uuid
@@ -237,7 +243,6 @@ class Connection(object):
         self._session.stream = True
         self._session.headers.update(self._header)
         self._session.proxies = proxies
-
         if self._referer is None and\
            (self._portal_connection and \
            str(self._portal_connection._auth).lower() == "home"):
@@ -809,7 +814,7 @@ class Connection(object):
         if self._cert_file:
             cert = (self._cert_file, self._key_file)
         else:
-            cert = None   
+            cert = None
         if json_encode:
             for k,v in params.items():
                 if isinstance(v, (dict, list, tuple, bool)):
@@ -827,7 +832,7 @@ class Connection(object):
             resp = self._session.put(url=url,
                                      data=params,
                                      cert=cert,
-                                     files=files)        
+                                     files=files)
         #
         return self._handle_response(resp=resp,
                                          out_path=out_path,
