88ad93ed181fdb6cbe2f373f0312f12659acf600
Sandeep Kumar <s.kumar@esri.com>
2020-10-15 12:59:33 +0530

    fastai version check

--- src/arcgis/learn/_utils/patches.py
+++ src/arcgis/learn/_utils/patches.py
@@ -1,12 +1,11 @@
 from typing import Collection
+import fastai
 import fastai.data_block
 from fastai.core import array
 
-## Fastai Pacthes Start ##
-def process(self, ds:Collection):
-    ds.items = array([self.process_one(item) for item in ds.items], dtype=object)
-fastai.data_block.PreProcessor.process = process
-## Fastai Pacthes END ##
-
+if fastai.__version__=='1.0.60':
+    def process(self, ds:Collection):
+        ds.items = array([self.process_one(item) for item in ds.items], dtype=object)
+    fastai.data_block.PreProcessor.process = process
 
 precondition = True
