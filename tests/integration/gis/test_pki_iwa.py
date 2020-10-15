3eab3cdf33da911e8971b0612a77442095332aa5
ajc <achapkowski@esri.com>
2020-10-15 08:41:56 -0400

    Updated test doc. PKI and IWA tests all pass with new URLs

--- tests/integration/gis/test_pki_iwa.py
+++ tests/integration/gis/test_pki_iwa.py
@@ -7,32 +7,32 @@ from arcgis.gis import GIS
 try:
     from utils import NOTEBOOK_TESTS_DIR
     fp = os.path.join(NOTEBOOK_TESTS_DIR, "dataset_test_123a.zip")
-    
+
 except:
     fp = r"./dataset_test_123a.zip"
 
 try:
     from utils import NOTEBOOK_TESTS_DIR
     PKI_ESRI_CERT = os.path.join(NOTEBOOK_TESTS_DIR, "gisproadv1.pfx")
-    
+
 except:
     PKI_ESRI_CERT = r"./gisproadv1.pfx"
-    
+
 try:
     from utils import NOTEBOOK_TESTS_DIR
     PKI_ORACLE_CERT = os.path.join(NOTEBOOK_TESTS_DIR, "EsriJDeveloper.pfx")
-    
+
 except:
     PKI_ORACLE_CERT = r"./EsriJDeveloper.pfx"
-        
+
 if os.path.isfile(PKI_ESRI_CERT) == False:
     SKIP_ORACLE = True
 else:
     SKIP_ORACLE = False
 
 
-PKI_JAVA = "https://portalpkijava.ags.esri.com/gis"
-PKI_WIN = "https://portalpkids.ags.esri.com/gis"
+PKI_JAVA = "https://rqawinjpki06pt.ags.esri.com/gis"
+PKI_WIN = "https://rqawinpki03pt.ags.esri.com/gis"
 
 PKI_ESRI_PW = "portalaccount1"
 
@@ -40,8 +40,8 @@ PKI_ESRI_PW = "portalaccount1"
 PKI_ORACLE = "https://wdcintelgx.dev.geocloud.com/portal"
 PKI_ORACLE_PW = "password"
 
-IWA_ESRI = "https://portaliwads.ags.esri.com/gis"
-IWA_DUEL_ESRI = "https://prtldualiwads.ags.esri.com/gis"
+IWA_ESRI = "https://rqawiniwa02pt.ags.esri.com/gis"
+IWA_DUEL_ESRI = "https://rqawinmiwa05pt.ags.esri.com/gis"
 #########################################################################################
 class TestIWAConnections(unittest.TestCase):
     """Tests IWA access to portal/server"""
@@ -65,28 +65,7 @@ class TestIWAConnections(unittest.TestCase):
                 i.delete()
             except:
                 pass
-        
-        item = gis.content.add({
-            "title" : "amazing_test_data_109",
-            "type" : "File Geodatabase",
-            "tags" : "erase me"
-        },
-                               data=fp)
-        pitem = item.publish()
-        assert pitem.layers[0].properties
-        pitem.delete()
-        item.delete()    
-    #########################################################################################        
-    def test_duel_iwa_publish_server(self):
-        """tests accessing a service via IWA"""
-        gis = GIS(url=IWA_DUEL_ESRI, verify_cert=False)
-        assert gis.users.me
-        for i in gis.content.search("amazing_test_data_109"):
-            try:
-                i.delete()
-            except:
-                pass
-        
+
         item = gis.content.add({
             "title" : "amazing_test_data_109",
             "type" : "File Geodatabase",
@@ -97,14 +76,35 @@ class TestIWAConnections(unittest.TestCase):
         assert pitem.layers[0].properties
         pitem.delete()
         item.delete()
-        
-        del gis        
-    
+    #########################################################################################
+    def test_duel_iwa_publish_server(self):
+        """tests accessing a service via IWA"""
+        gis = GIS(url=IWA_DUEL_ESRI, verify_cert=False)
+        assert gis.users.me
+        for i in gis.content.search("amazing_test_data_109"):
+            try:
+                i.delete()
+            except:
+                pass
+
+        item = gis.content.add({
+            "title" : "amazing_test_data_109",
+            "type" : "File Geodatabase",
+            "tags" : "erase me"
+        },
+                               data=fp)
+        pitem = item.publish()
+        assert pitem.layers[0].properties
+        pitem.delete()
+        item.delete()
+
+        del gis
+
 #########################################################################################
 class TestPKIConnections(unittest.TestCase):
     #########################################################################################
     def test_pki_login(self):
-        
+
         gis = GIS(url=PKI_WIN, cert_file=PKI_ESRI_CERT, password=PKI_ESRI_PW, verify_cert=False)
         assert gis.users.me.username.lower().find('gisproadv1') > -1
         del gis
@@ -116,7 +116,7 @@ class TestPKIConnections(unittest.TestCase):
     def test_oracle_webgate(self):
         """tests oracle's webgate pki like login"""
         try:
-            
+
             gis = GIS(url=PKI_ORACLE, cert_file=PKI_ORACLE_CERT, password=PKI_ORACLE_PW, verify_cert=False)
             assert gis.users.me.username.lower().find('esrideveloper01') > -1
             #########################################################################################
@@ -136,8 +136,8 @@ class TestPKIConnections(unittest.TestCase):
             assert pitem.layers[0].properties
             pitem.delete()
             item.delete()
-            
-            del gis        
+
+            del gis
         except:
             pass
     #########################################################################################
@@ -149,7 +149,7 @@ class TestPKIConnections(unittest.TestCase):
                 i.delete()
             except:
                 pass
-        
+
         item = gis.content.add({
             "title" : "amazing_test_data_109",
             "type" : "File Geodatabase",
@@ -160,12 +160,12 @@ class TestPKIConnections(unittest.TestCase):
         assert pitem.layers[0].properties
         pitem.delete()
         item.delete()
-        
+
         del gis
-        
+
     #########################################################################################
     def test_pki_access_server_JAVA(self):
-        """tests publishing then accessing the layers"""    
+        """tests publishing then accessing the layers"""
         gis = GIS(url=PKI_JAVA, cert_file=PKI_ESRI_CERT, password=PKI_ESRI_PW, verify_cert=False)
         for i in gis.content.search("amazing_test_data_109"):
             try:
@@ -186,4 +186,3 @@ class TestPKIConnections(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-        
