0879c75a129085466b868af3eb398f05cc2ad079
VIKASH RANJAN <vranjan@esri.com>
2020-11-09 19:11:59 +0530

    tensorboard fix for fasterrcnn (#5235)

--- src/arcgis/learn/_utils/tensorboard_utils.py
+++ src/arcgis/learn/_utils/tensorboard_utils.py
@@ -7,6 +7,7 @@ try:
     from fastai.vision.data import ImageList, ImageImageList
     from torch.utils.tensorboard import SummaryWriter
     from fastai.callbacks.tensorboard import *
+    from fastai.core import split_kwargs_by_func
     from PIL import Image
     from torchvision.transforms import ToTensor
 except:
@@ -222,7 +223,22 @@ class ArcGISTBCallback(LearnerTensorboardWriter, Learner, ImageImageList, ArcGIS
         if self._arcgis_model.learn.dl(ds_type).batch_size < n_items: n_items = self._arcgis_model.learn.dl(
             ds_type).batch_size
         self._arcgis_model.learn.model.eval()
-        preds = self._arcgis_model.learn.model(self._arcgis_model.model_conf.transform_input(xb))
+        transform_kwargs, kwargs = split_kwargs_by_func(kwargs, self._arcgis_model.model_conf.transform_input)
+        try:
+            preds = self._arcgis_model.learn.model(self._arcgis_model.model_conf.transform_input(xb, transform_kwargs))
+        except Exception as e:
+
+            if getattr(self._arcgis_model, "_is_fasterrcnn", False):
+                preds = []
+                for _ in range(xb.shape[0]):
+                    res={}
+                    res['boxes'] = torch.empty(0,4)
+                    res['scores'] = torch.tensor([])
+                    res['labels'] = torch.tensor([])
+                    preds.append(res)
+            else:
+                raise e
+
         x, y = to_cpu(xb), to_cpu(yb)
         norm = getattr(self._arcgis_model.learn.data, 'norm', False)
         if norm:
