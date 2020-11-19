756f2cfd193654237e0a4941357bd314167fd77a
Andrew Chapkowski <achapkowski@esri.com>
2020-11-19 14:39:34 -0500

    - fix for category schema

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -5971,7 +5971,8 @@ class CategorySchemaManager(object):
                   'items' : json.dumps(items)}
         if self._url.lower().find("/portals/") == -1:
             # If this SchemaManager is attached to a GroupManager
-            url = "{base}/updateCategories".format(base=self._url)
+            group = os.path.basename(self._url)
+            url = f"{self._gis_portal.resturl}content/groups/{group}/updateCategories"
         else:
             # else this SchemaManager is attached to a ContentManager
             url = "{base}content/updateItems".format(base=self._gis._portal.resturl)
