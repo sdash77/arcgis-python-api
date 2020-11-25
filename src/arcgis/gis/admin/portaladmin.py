b0ddd0977630c34ba91253893b3f5204cc87ee6c
John Yaist <jyaist@esri.com>
2020-11-25 09:38:32 -0800

    removed history method from PortalAdminManager

--- src/arcgis/gis/admin/portaladmin.py
+++ src/arcgis/gis/admin/portaladmin.py
@@ -313,36 +313,36 @@ class PortalAdminManager(BasePortalAdmin):
         return self._whm
 
     #----------------------------------------------------------------------
-    def history(self, start_date, num=100, save_folder=None):
-        """
-        Returns a CSV file containing the login history from a start_date to the present.
-
-        ================  ===============================================================================
-        **Argument**      **Description**
-        ----------------  -------------------------------------------------------------------------------
-        start_date        Required datetime.datetime object. The beginning date.
-        ----------------  -------------------------------------------------------------------------------
-        num               Optional Integer. The maximum number of records to return.
-        ----------------  -------------------------------------------------------------------------------
-        save_folder       Optional String. The save location of the CSV file.
-        ================  ===============================================================================
-
-        :returns: string
-
-        """
-        if self._gis.version >= [6.4]:
-            import tempfile, json
-            from arcgis._impl.common._utils import _date_handler
-            if save_folder is None:
-                save_folder = tempfile.gettempdir()
-            url = "{url}portals/self/history".format(url=self._gis._portal.resturl)
-            params = {
-                'f' : 'csv',
-                'num' : num,
-                'all' : True,
-                'fromDate' : json.dumps(start_date, default=_date_handler)
-            }
-            return self._gis._con.post(url, params,
-                                       file_name="history.csv",
-                                       out_folder=save_folder)
-        return None
+    # def history(self, start_date, num=100, save_folder=None):
+    #     """
+    #     Returns a CSV file containing the login history from a start_date to the present.
+    #
+    #     ================  ===============================================================================
+    #     **Argument**      **Description**
+    #     ----------------  -------------------------------------------------------------------------------
+    #     start_date        Required datetime.datetime object. The beginning date.
+    #     ----------------  -------------------------------------------------------------------------------
+    #     num               Optional Integer. The maximum number of records to return.
+    #     ----------------  -------------------------------------------------------------------------------
+    #     save_folder       Optional String. The save location of the CSV file.
+    #     ================  ===============================================================================
+    #
+    #     :returns: string
+    #
+    #     """
+    #     if self._gis.version >= [6.4]:
+    #         import tempfile, json
+    #         from arcgis._impl.common._utils import _date_handler
+    #         if save_folder is None:
+    #             save_folder = tempfile.gettempdir()
+    #         url = "{url}portals/self/history".format(url=self._gis._portal.resturl)
+    #         params = {
+    #             'f' : 'csv',
+    #             'num' : num,
+    #             'all' : True,
+    #             'fromDate' : json.dumps(start_date, default=_date_handler)
+    #         }
+    #         return self._gis._con.post(url, params,
+    #                                    file_name="history.csv",
+    #                                    out_folder=save_folder)
+    #     return None
