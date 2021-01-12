5e0fcb7634b8ea524aae0264fbef7409fefe684f
DavidJVitale <DavidJVitale@users.noreply.github.com>
2021-01-12 14:10:55 -0800

    Added failing test for layerType property in the webmap dictionary

--- tests/unit/test_arcgis/test_mapping/test_WebMap/test_add_layer.py
+++ tests/unit/test_arcgis/test_mapping/test_WebMap/test_add_layer.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 from utils.mocks import MockMapView, MockWebMap, MockGIS, MockFeatureLayer, PlaceholderItem
 from utils.mocks.placeholders.placeholder_featurecollection import PlaceholderFeatureLayerCollection
 from utils.mocks.placeholders.placeholder_featurelayer import PlaceholderFeatureLayer
+from utils.mocks.placeholders.placeholder_table import PlaceholderTable
 
 @patch("arcgis.widgets.MapView", MockMapView)
 def test_add_item_map_service():
@@ -34,32 +35,47 @@ def test_add_featurecollection_different_renderers():
     added to a WebMap
     """
     from arcgis.mapping import WebMap
-    from arcgis.gis import Layer
-
     mock_webmap = MockWebMap()
     # make sure recursive function gets called correctly
-    mock_webmap.add_layer = lambda layer, options: WebMap.add_layer(mock_webmap, layer, options) 
+    mock_webmap.add_layer = lambda layer, options: WebMap.add_layer(mock_webmap, layer, options)
+
+    placeholder_fl_1 = PlaceholderFeatureLayer("https://example.com")
+    placeholder_fl_2 = PlaceholderFeatureLayer("https://example.com")
     renderer_1 = { "type": "simple",
                    "symbol": {
                        "type": "esriPMS" }}
     renderer_2 = { "type": "simple",
                    "symbol" : {
                        "type": "esriSFS" }}
-    mock_fl_1 = PlaceholderFeatureLayer("https://example.com")
-    mock_fl_2 = PlaceholderFeatureLayer("https://example.com")
-    mock_fl_1.renderer = renderer_1
-    mock_fl_2.renderer = renderer_2
-    assert isinstance(mock_fl_1, Layer)
+    placeholder_fl_1.renderer = renderer_1
+    placeholder_fl_2.renderer = renderer_2
 
     placeholder_flc = PlaceholderFeatureLayerCollection("https://example.com")
     placeholder_flc.layers = [
-        mock_fl_1,
-        mock_fl_2
-    ]
+        placeholder_fl_1,
+        placeholder_fl_2 ]
 
     WebMap.add_layer(mock_webmap, placeholder_flc)
+
     assert len(mock_webmap._webmapdict['operationalLayers']) == 2
     assert mock_webmap._webmapdict['operationalLayers'][0].get('layerDefinition')\
         .get('drawingInfo').get('renderer') == renderer_1
     assert mock_webmap._webmapdict['operationalLayers'][1].get('layerDefinition')\
         .get('drawingInfo').get('renderer') == renderer_2
+
+@patch("arcgis.widgets.MapView", MockMapView)
+def test_add_table():
+    """FeatureCollections can have multiple layers with different renderers.
+    Check that each sublayer has the correct renderer attached to it when
+    added to a WebMap
+    """
+    from arcgis.mapping import WebMap
+    mock_webmap = MockWebMap()
+
+    placeholder_table = PlaceholderTable("https://example.com")
+
+    WebMap.add_layer(mock_webmap, placeholder_table)
+
+    assert len(mock_webmap._webmapdict['tables']) == 1
+    table = mock_webmap._webmapdict['tables'][0]
+    assert "layerType" not in table
