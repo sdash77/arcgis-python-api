0806d0c6e77d72d4e7ea79984a9777fdead4b307
Divyansh Jha <divyanshj.16@gmail.com>
2021-01-18 13:45:49 +0530

    raise proper error in from model pointcnn (#5594)

--- src/arcgis/learn/_data.py
+++ src/arcgis/learn/_data.py
@@ -96,17 +96,14 @@ imagery_type_lib = {
 
 
 def get_installation_command():
-    installation_steps = ("Install them using - 'conda install -c esri -c fastai -c pytorch arcgis=1.8.2 "
-                          "scikit-image=0.15.0 pillow=6.2.2 libtiff=4.0.10 fastai=1.0.60 pytorch=1.4.0 "
-                          "torchvision=0.5.0 scikit-learn=0.23.1 --no-pin'"
-                          "\n'conda install gdal=2.3.3'"
-                          "\n'pip install transformers==3.3.0'")
+    installation_steps = ('Install all of them using: "conda install -c esri arcgis_learn"')
     return installation_steps 
 
 
 def _raise_fastai_import_error(import_exception=import_exception):
     installation_steps = get_installation_command()
-    raise Exception(f"""{import_exception} \n\nThis module requires fastai, PyTorch, torchvision and scikit-image as its dependencies.\n{installation_steps}""")
+    raise Exception(f"{import_exception} \n\nThis module requires fastai, PyTorch, torchvision "
+                    f"as its dependencies.\n{installation_steps}")
 
 
 class _ImagenetCollater():
