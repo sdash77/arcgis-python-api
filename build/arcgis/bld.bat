d8634efff44f8202585c2cb604c1f2c24bd547fe
sandeep kumar <s.kumar@esri.com>
2021-03-05 11:32:43 +0530

    python3.7 win tracker (#5902)
    * python3.7 win tracker
    
    * conda build

--- build/arcgis/bld.bat
+++ build/arcgis/bld.bat
@@ -1,2 +1,8 @@
+set TRACKER_PATH=\\DEV0000935\Build\tracking-engine\win_py%PY_VER%
+ECHO Getting tracker artifacts from %TRACKER_PATH%
+if not exist arcgis\learn\_tracking mkdir arcgis\learn\_tracking
+copy %TRACKER_PATH%\TrackingEngine.dll arcgis\learn\_tracking\_track_processor.pyd /Y
+copy %TRACKER_PATH%\_track_processor.pyd arcgis\learn\_tracking\tracking_engine.dll /Y
+
 "%PYTHON%" setup.py install --conda-install-mode
 if errorlevel 1 exit 1
