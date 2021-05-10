52a8ef81c3d4c0b17d2f311777150d26879c2ece
ted-howard <thoward@esri.com>
2021-05-10 12:43:31 -0600

    account for response object the cannot be json decoded

--- src/arcgis/gis/_impl/_con/_connection.py
+++ src/arcgis/gis/_impl/_con/_connection.py
@@ -32,6 +32,7 @@ import requests
 from requests import Session
 from requests_toolbelt.downloadutils import stream
 from requests_toolbelt.multipart.encoder import MultipartEncoder
+from json import JSONDecodeError
 from ._helpers import _filename_from_headers, _filename_from_url
 from ._authguess import GuessAuth
 from arcgis._impl.common._mixins import PropertyMap
@@ -506,7 +507,13 @@ class Connection(object):
                 if 'error' in data:
                     raise Exception(data['error'])
             else:
-                data = resp.json()
+                try:
+                    data = resp.json()
+                except JSONDecodeError:
+                    if resp.text:
+                        raise Exception(resp.text)
+                    else:
+                        raise
             #if 'error' in data:
                 #raise Exception(data['error'])
             #return data
