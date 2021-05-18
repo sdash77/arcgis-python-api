c8eb442d0f6cf9b7fada262d8ee2bd83a8d7ce5e
Sameer Sethi <>
2021-05-18 16:20:41 -0700

    Velocity python notebook for Feeds, Real-Time Analytics, BigData Analytics and help docs
    - [x] Notebook support for the Feeds, Real-Time Analytics, BigData Analytics. This is initial implementation that would give us PE to enhance it and build up more story or add some additional markdown (notes) details.
    - [x] The Velocity API entry in help docs.

--- src/arcgis/realtime/velocity/__init__.py
+++ src/arcgis/realtime/velocity/__init__.py
@@ -4,3 +4,11 @@ ArcGIS Velocity is a real-time and big data processing and analysis capability o
 It enables you to ingest, visualize, analyze, store, and act upon data from Internet of Things (IoT) sensors.
 """
 from .velocity import Velocity
+
+from .velocity import FeedsManager
+from .velocity import RealTimeAnalyticsManager
+from .velocity import BigDataAnalyticsManager
+
+from .feeds_manager import Feed
+from .realtime_analytics_manager import RealTimeAnalytics
+from .bigdata_analytics_manager import BigDataAnalytics
