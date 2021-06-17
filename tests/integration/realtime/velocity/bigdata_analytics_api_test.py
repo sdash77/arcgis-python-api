ce547b921d564eddd4013d16623bea8ea8d39ffc
Sameer Sethi <ssethi@esri.com>
2021-06-17 08:49:30 -0700

    ArcGIS Velocity python api (#6457)
    * #8753 - Add velocity inside GIS
    
    * #8753 - Add velocity inside GIS
    
    - [x] Add velocity property inside GIS
    
    * #8754, #8755, #8756 - Implement Feed, Real-Time Analytics, Big Data Analytics getter methods (publish) API
    
    - [x] Implement API getter methods (publish) for:
    
    - Feeds:
    - [x] Items
    - [x] get(id)
    - [x] start()
    - [x] stop()
    - [x] status()
    - [x] metrics()
    
     - Real-Time Analytics:
    - [x] tasks
    - [x] get(id)
    - [x] start()
    - [x] stop()
    - [x] status()
    - [x] metrics()
    
    - Big Data Analytics:
    - [x] Items
    - [x] get(id)
    - [x] start()
    - [x] stop()
    - [x] status()
    - [ ] metrics()
    
    * Abstract method for metrics.
    
    * Refactored `tasks` to `items` for Real time and Big data
    
    * Updated unit tests to assert response values
    
    * Velocity python notebook for Feeds, Real-Time Analytics, BigData Analytics and help docs
    
    - [x] Notebook support for the Feeds, Real-Time Analytics, BigData Analytics. This is initial implementation that would give us PE to enhance it and build up more story or add some additional markdown (notes) details.
    - [x] The Velocity API entry in help docs.
    
    * Updated the return type for Stop and Delete in Feed, Real-Time and Big-Data
    
    Adjusted the return type of following based on the Python API documentations and other implementations.
    
    - [x] Stop(): Return: True if the Feed was successfully stopped.
           :return: boolean
    - [x] Delete():  Return:  A boolean containing True (for success) or
             False (for failure) a dictionary with details is returned.
    
    * Applying "black" linting to velocity module per feedback.
    
    * Fixed double addition of Velocity property due to branch sync up.
    
    * Addressed PR feedback. Updated notebook and unit tests.
    
    - Added @property for metrics and status
    - Shortened variable references
    - Removed unused method of serialized_object
    - Updated Jupyter notebook and unit tests
    
    * Cleaned up unused references of `url`
    
    - [x] Cleaned up unused references of `url` that would not be required.
    
    * Addressed feedback in utils
    
    - [x] Updated the default parameter for 'payload' to None and set its default value within scope of funciton.
    - [x] Updated parameter name from `type` to `task_type` as per feedback `type` is protected keyword in python.
    
    * Removed unused print_test method.
    
    - [x] Removed unused print_test method which is not required any more.
    
    * Updated to feeds, realtime_analytics, bigdata_analytics properites per feedback
    
    - [x] As per PR feedback and usage pattern in Python GIS class updating the properties to following: feeds, realtime_analytics, bigdata_analytics
    - [x] Also updated the unit tests and jupyter notebook to sync these changes.
    
    * Update velocity.py
    
    Updated docstrings
    
    Co-authored-by: Sameer Sethi <>
    Co-authored-by: Rohit Singh <rsingh@esri.com>

---
+++ tests/integration/realtime/velocity/bigdata_analytics_api_test.py
@@ -0,0 +1,176 @@
+import unittest
+
+from arcgis.gis import GIS
+from arcgis.realtime.velocity.bigdata_analytics_manager import BigDataAnalytics
+
+try:
+    # Use your ArcGIS enterprise url and credentials to run the test
+    url = ""
+    username = ""
+    password = ""
+    gis = GIS(url, username, password)
+    SKIP_TESTS = False
+    # Skip task start/stop/delete tests by default. Set as false to run
+    SKIP_SOME_TESTS = True
+except:
+    SKIP_TESTS = True
+
+
+@unittest.skipIf(SKIP_TESTS, reason="GIS connection failed")
+class TestBigDataAnalyticsMethods(unittest.TestCase):
+    velocity = gis.velocity
+    bigdata_analytics = velocity.bigdata_analytics
+    bigdata_analytics_item = bigdata_analytics.get(
+        "7a49c634c09f4e558f842db06c76a346"
+    )
+
+    # ----------------------------------------------------------------------
+    @unittest.skipIf(SKIP_SOME_TESTS, "test_get_all_bigdata_analytics skipping")
+    def test_get_all_bigdata_analytics(self):
+        print("\n ---- test_get_all_bigdata_analytics ----")
+
+        try:
+            items = self.bigdata_analytics.items
+            for item in items:
+                assert isinstance(item, BigDataAnalytics)
+
+        except AssertionError as assertErrorException:
+            self.SKIP_TESTS = True
+            raise assertErrorException
+
+        except unittest.SkipTest as skipException:
+            raise skipException
+
+        except Exception as testException:
+            self.fail("Error during test: " + testException.__str__())
+
+    # ----------------------------------------------------------------------
+    def test_get_bigdata_analytics(self):
+        print("\n ---- test_get_bigdata_analytics ----")
+
+        try:
+            response = self.bigdata_analytics.get(
+                "7a49c634c09f4e558f842db06c76a346"
+            )
+            assert isinstance(response, BigDataAnalytics)
+
+        except AssertionError as assertErrorException:
+            self.SKIP_TESTS = True
+            raise assertErrorException
+
+        except unittest.SkipTest as skipException:
+            raise skipException
+
+        except Exception as testException:
+            self.fail("Error during test: " + testException.__str__())
+
+    # ----------------------------------------------------------------------
+    @unittest.skipIf(SKIP_SOME_TESTS, "test_start_bigdata_analytics skipping")
+    def test_start_bigdata_analytics(self):
+        print("\n ---- test_start_bigdata_analytics ----")
+
+        try:
+            response = self.bigdata_analytics_item.start()
+            assert response and response.get("status") == "success"
+
+        except AssertionError as assertErrorException:
+            self.SKIP_TESTS = True
+            raise assertErrorException
+
+        except unittest.SkipTest as skipException:
+            raise skipException
+
+        except Exception as testException:
+            self.fail("Error during test: " + testException.__str__())
+
+    # ----------------------------------------------------------------------
+    @unittest.skipIf(SKIP_SOME_TESTS, "test_stop_bigdata_analytics skipping")
+    def test_stop_bigdata_analytics(self):
+        print("\n ---- test_stop_bigdata_analytics ----")
+
+        try:
+            response = self.bigdata_analytics_item.stop()
+            assert response and response.get("status") == "success"
+
+        except AssertionError as assertErrorException:
+            self.SKIP_TESTS = True
+            raise assertErrorException
+
+        except unittest.SkipTest as skipException:
+            raise skipException
+
+        except Exception as testException:
+            self.fail("Error during test: " + testException.__str__())
+
+    # ----------------------------------------------------------------------
+    def test_bigdata_analytics_status(self):
+        print("\n ---- test_bigdata_analytics_status ----")
+
+        try:
+            response = self.bigdata_analytics_item.status
+            assert response and response.get("status")
+
+        except AssertionError as assertErrorException:
+            self.SKIP_TESTS = True
+            raise assertErrorException
+
+        except unittest.SkipTest as skipException:
+            raise skipException
+
+        except Exception as testException:
+            self.fail("Error during test: " + testException.__str__())
+
+    # ----------------------------------------------------------------------
+    @unittest.skipIf(SKIP_SOME_TESTS, "test_bigdata_analytics_metrics skipping")
+    def test_bigdata_analytics_metrics(self):
+        print("\n ---- test_bigdata_analytics_metrics ----")
+
+        try:
+            response = self.bigdata_analytics_item.metrics
+            assert response and response.get("itemId")
+
+        except AssertionError as assertErrorException:
+            self.SKIP_TESTS = True
+            raise assertErrorException
+
+        except unittest.SkipTest as skipException:
+            raise skipException
+
+        except Exception as testException:
+            self.fail("Error during test: " + testException.__str__())
+
+    # ----------------------------------------------------------------------
+    @unittest.skipIf(SKIP_SOME_TESTS, "test_delete_bigdata_analytics skipping")
+    def test_delete_bigdata_analytics(self):
+        print("\n ---- test_delete_bigdata_analytics ----")
+        try:
+            bigdata_analytics_to_delete = self.bigdata_analytics.get(
+                "f34d3a7c899f4c178c9a0ff9dfc24bf9"
+            )
+            try:
+                response = bigdata_analytics_to_delete.delete()
+                assert response and response.get("id")
+
+            except AssertionError as assertErrorException:
+                self.SKIP_TESTS = True
+                raise assertErrorException
+
+            except unittest.SkipTest as skipException:
+                raise skipException
+
+            except Exception as testException:
+                self.fail("Error during test: " + testException.__str__())
+
+        except AssertionError as assertErrorException:
+            self.SKIP_TESTS = True
+            raise assertErrorException
+
+        except unittest.SkipTest as skipException:
+            raise skipException
+
+        except Exception as testException:
+            self.fail("Error during test: " + testException.__str__())
+
+
+if __name__ == "__main__":
+    unittest.main()
