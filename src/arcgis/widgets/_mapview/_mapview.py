e5e14f2e010575ce354e176e0758cc71c6d514d1
jyaistMap <jyaist@esri.com>
2021-07-07 15:54:40 -0700

    add allowedOrigins code snippet

--- src/arcgis/widgets/_mapview/_mapview.py
+++ src/arcgis/widgets/_mapview/_mapview.py
@@ -252,10 +252,17 @@ class MapView(widgets.DOMWidget):
         configure your portal/organization to allow your host and port; or else
         you will run into ``CORs`` issues.
 
-        This can be accomplished by signing into your portal/organization in a
-        browser, then navigating to:
-
-        `Organization` > `Settings` > `Security` > `Allow origins` > `Add` > http://localhost:8888 (replace with the host/port you are running on)
+        This can be accomplished programmatically by signing into your organization 
+        and running this code:
+        
+        .. code-block:: python
+        
+            from arcgis.gis import GIS
+            gis = GIS(profile="your_admin_profile")
+            
+            more_origins = {"allowedOrigins":"http://localhost:8888"} #replace with your port
+            
+            gis_update_properties(more_origins)
 
     """
 
