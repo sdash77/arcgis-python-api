eb1db08abba7a249f9b5fbb1de6831a84d07f7dd
Andrew Chapkowski <achapkowski@esri.com>
2021-07-08 11:54:30 -0400

    - fixed ttl issue on classes.

--- src/setup.py
+++ src/setup.py
@@ -47,6 +47,7 @@ if conda_install_mode:
     dependencies = []
 else:
     dependencies = [
+        "cachetools",
         "six",
         "ipywidgets >=7",
         "widgetsnbextension >=3",
@@ -67,7 +68,6 @@ else:
         'requests-negotiate-sspi;platform_system=="Windows"',
         'requests-kerberos;platform_system=="Windows"',
         'winkerberos;platform_system=="Windows"',
-        #'arcgis_auth'
     ]
 
 
@@ -270,9 +270,7 @@ kwargs = {
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    "extras_require": {
-        "gp": ["dill"],
-    },
+    "extras_require": {"gp": ["dill"],},
     # extras_require={
     #     'dev': ['check-manifest'],
     #     'test': ['coverage'],
