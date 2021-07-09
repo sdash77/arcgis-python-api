cc6167fa5711d4b660176515ae8a80223fdd8fae
jyaistMap <jyaist@esri.com>
2021-07-09 10:36:55 -0700

    fix broken hyperlink in parameter table

--- src/arcgis/features/layer.py
+++ src/arcgis/features/layer.py
@@ -2571,10 +2571,10 @@ class FeatureLayer(Layer):
                                 reference. When specifying transformations, you need to think about which datum
                                 transformation best projects the layer (not the feature service) to the `outSR` and
                                 `sourceSpatialReference` property in the layer properties. For a list of valid datum
-                                transformation ID values ad well-known text strings, see `Coordinate systems and
-                                transformations <https://developers.arcgis.com/net/latest/wpf/guide/coordinate-systems-and-transformations.htm>`_.
-                                For more information on datum transformations, please see the transformation
-                                parameter in the `Project operation <https://developers.arcgis.com/rest/services-reference/project.htm>`_.
+                                transformation ID values ad well-known text strings, see `Geographic and Vertical
+                                Transformation Tables  <https://desktop.arcgis.com/en/arcmap/latest/map/projections/pdf/geographic_transformations.pdf>`_.
+                                For more information on datum transformations please see the transformation 
+                                parameter in the `Project operation <https://developers.arcgis.com/rest/services-reference/project.htm>`_ documentation.
 
                                 **Examples**
 
