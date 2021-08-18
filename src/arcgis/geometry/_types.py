5b2e53b6c9672874452275cfd6deb4f67b70ace6
nanaeaubry <naubry@esri.com>
2021-08-18 14:32:35 +0200

    implemented geomet for WKB and WKT in _types

--- src/arcgis/geometry/_types.py
+++ src/arcgis/geometry/_types.py
@@ -3,6 +3,7 @@ New Geometries Classes
 """
 import copy
 import json
+from arcpy.arcobjects.geometries import Multipoint, Polyline
 import ujson as _ujson
 
 try:
@@ -204,6 +205,9 @@ class GeometryFactory(type):
             _HASARCPY = False
         if _HASARCPY:
             return _ujson.loads(arcpy.FromWKB(iterable).JSON)
+        else:
+            from geomet import wkb
+            return wkb.loads(iterable)
         return {}
 
     @staticmethod
@@ -324,7 +328,7 @@ class Geometry(BaseGeometry):
                 return arcpy.PointGeometry(self.as_arcpy).__geo_interface__
             else:
                 return self.as_arcpy.__geo_interface__
-        else:
+        elif _HASSHAPELY:
             if isinstance(self, Point):
                 return {"type": "Point", "coordinates": (self.x, self.y)}
             elif isinstance(self, Polygon):
@@ -345,10 +349,19 @@ class Geometry(BaseGeometry):
                     "type": "Multipoint",
                     "coordinates": [(pt[0], pt[1]) for pt in self["points"]],
                 }
-
             from arcgis._impl.common._arcgis2geojson import arcgis2geojson
-
             return arcgis2geojson(arcgis=self)
+        else: 
+            from geomet import esri
+            if isinstance(self, Point):
+                return esri._to_gj_point(self)
+            elif isinstance(self, Polygon):
+                return esri._to_gj_polygon(self)
+            elif isinstance(self, Polyline):
+                return esri._to_gj_polyline(self)
+            elif isinstance(self, Multipoint):
+                return esri._to_gj_multipoint(self)
+
 
     def __hash__(self):
         return hash(frozenset(self.items()))
@@ -927,7 +940,7 @@ class Geometry(BaseGeometry):
     @property
     def WKT(self):
         """
-        The ``WKY`` method retrieves the ``well-known text`` (``WKT``) representation for OGC geometry.
+        The ``WKT`` method retrieves the ``well-known text`` (``WKT``) representation for OGC geometry.
         It provides a portable representation of a geometry value as a text
         string.
 
@@ -952,8 +965,10 @@ class Geometry(BaseGeometry):
                 return self.as_shapely.wkt
             except:
                 return self._wkt(fmt="%.16f")
-
-        return self._wkt(fmt="%.16f")
+        else:
+            from geomet import wkt, esri
+            geojson_item = self.__geo_interface__
+            return wkt.dumps(geojson_item)
 
     # ----------------------------------------------------------------------
     @property
@@ -983,7 +998,11 @@ class Geometry(BaseGeometry):
                 return self.as_shapely.wkb
             except:
                 return None
-        return None
+        else:
+            #geomet conversion
+            from geomet import wkb
+            geojson_item = self.__geo_interface__
+            return wkb.dumps(geojson_item, big_endian=False)
 
     # ----------------------------------------------------------------------
     @property
@@ -2614,7 +2633,7 @@ class Geometry(BaseGeometry):
                         in_srid, out_srid
                     )
                 )
-
+            # in geomet there is transform in tools and then call from_geomet to make it back to what it was
             g = transform(project, self.as_shapely)
             return Geometry.from_shapely(g, spatial_reference=spatial_reference)
 
