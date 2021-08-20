80505fa738ba854b53630940389d751e0cccf8a4
Andrew Chapkowski <achapkowski@esri.com>
2021-08-20 10:03:05 -0400

    format fix.

--- src/arcgis/geometry/_types.py
+++ src/arcgis/geometry/_types.py
@@ -843,7 +843,7 @@ class Geometry(BaseGeometry):
             import arcpy
         if HASARCPY and isinstance(self.as_arcpy, arcpy.Geometry):
             return getattr(self.as_arcpy, "JSON", None)
-        elif 'coordinates' in self:
+        elif "coordinates" in self:
             from geomet import esri
 
             return json.dumps(esri.dumps(dict(self)))
