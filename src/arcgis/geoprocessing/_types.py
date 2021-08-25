d8b4a1d16106a5cbf88c4e21a003593ce57a3a39
Andrew Chapkowski <achapkowski@esri.com>
2021-08-25 06:59:48 -0400

    - added case where when AGOL is returning a file, user token is not appended.

--- src/arcgis/geoprocessing/_types.py
+++ src/arcgis/geoprocessing/_types.py
@@ -114,12 +114,21 @@ class DataFile(object):
         if not save_path:
             save_path = tempfile.gettempdir()
         if data_path:
-            return self._con.get(
-                path=data_path,
-                out_folder=save_path,
-                try_json=False,
-                token=self._token,
-            )
+            if self._con.product == "AGOL":
+                return self._con.get(
+                    path=data_path,
+                    out_folder=save_path,
+                    try_json=False,
+                    add_token=False,
+                    token=self._token,
+                )
+            else:
+                return self._con.get(
+                    path=data_path,
+                    out_folder=save_path,
+                    try_json=False,
+                    token=self._token,
+                )
 
 
 class RasterData(object):
