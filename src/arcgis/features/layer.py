83426ee7cb0369d61280d2b1f39da5b9d1643354
Andrew Chapkowski <achapkowski@esri.com>
2021-08-31 07:09:02 -0400

    added async applyEdits for feature layers. Added check for if async is supported in Feature Layer properties.

--- src/arcgis/features/layer.py
+++ src/arcgis/features/layer.py
@@ -1923,7 +1923,10 @@ class FeatureLayer(Layer):
             params["sqlType"] = sql_type
         sql_type = sql_type.lower()
         url = self._url + "/validateSQL"
-        return self._con.post(path=url, postdata=params,)
+        return self._con.post(
+            path=url,
+            postdata=params,
+        )
 
     # ----------------------------------------------------------------------
     def query_related_records(
@@ -2248,8 +2251,10 @@ class FeatureLayer(Layer):
             params["upsertMatchingField"] = upsert_matching_field
         if not skip_inserts is None:
             params["skipInserts"] = skip_inserts
-        upload_formats = """sqlite,shapefile,filegdb,featureCollection,geojson,csv,excel""".split(
-            ","
+        upload_formats = (
+            """sqlite,shapefile,filegdb,featureCollection,geojson,csv,excel""".split(
+                ","
+            )
         )
         if upload_format not in upload_formats:
             raise ValueError("Invalid upload format: %s." % upload_format)
@@ -2604,7 +2609,17 @@ class FeatureLayer(Layer):
             HAS_PANDAS = True
         except:
             HAS_PANDAS = False
-
+        if (
+            future
+            and "advancedQueryCapabilities" in self.properties
+            and "supportsAsyncApplyEdits"
+            in self.properties["advancedQueryCapabilities"]
+        ):
+            future = self.properties["advancedQueryCapabilities"][
+                "supportsAsyncApplyEdits"
+            ]
+        else:
+            future = False
         if adds is None:
             adds = []
         if updates is None:
@@ -2895,13 +2910,19 @@ class FeatureLayer(Layer):
         ):
             params["async"] = True
             executor = concurrent.futures.ThreadPoolExecutor(1)
-            res = self._con.post(path=url, postdata=params,)
+            res = self._con.post(
+                path=url,
+                postdata=params,
+            )
             future = executor.submit(
                 self._status_via_url, *(self._con, res["statusUrl"], {"f": "json"})
             )
             executor.shutdown(False)
             return future
-        return self._con.post(path=url, postdata=params,)
+        return self._con.post(
+            path=url,
+            postdata=params,
+        )
 
     # ----------------------------------------------------------------------
     def _query(self, url, params, raw=False, **kwargs):
@@ -2912,7 +2933,10 @@ class FeatureLayer(Layer):
                     path=url, postdata=params, add_token=kwargs.get("add_token", True)
                 )
             else:
-                result = self._con.post(path=url, postdata=params,)
+                result = self._con.post(
+                    path=url,
+                    postdata=params,
+                )
         except Exception as queryException:
             error_list = [
                 "Error performing query operation",
