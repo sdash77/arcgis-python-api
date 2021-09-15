fd2477d329e693f9ace9f722a4532d7303244270
Joel McCune <knu2xs@gmail.com>
2021-09-15 16:25:53 -0700

    Getting enrich link working

--- src/arcgis/geoenrichment/enrichment.py
+++ src/arcgis/geoenrichment/enrichment.py
@@ -1091,7 +1091,7 @@ def enrich(
 
     .. note::
 
-        Please refer to the `Enrich REST endpoint documentation<https://developers.arcgis.com/rest/geoenrichment/api-reference/enrich.htm#ESRI_SECTION2_6A987CF67F914FA39B61BE14BE115F27>`_
+        Please refer to the `Enrich REST endpoint documentation <https://developers.arcgis.com/rest/geoenrichment/api-reference/enrich.htm#ESRI_SECTION2_6A987CF67F914FA39B61BE14BE115F27>`_
         for the format of `intersection_geographies` parameter.
 
     .. note::
