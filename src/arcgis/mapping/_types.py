9d40ec9b368d38daf22e01eda90e5787388777d1
nanaeaubry <naubry@esri.com>
2021-10-18 09:01:06 +0200

    format

--- src/arcgis/mapping/_types.py
+++ src/arcgis/mapping/_types.py
@@ -5386,7 +5386,10 @@ class MapImageLayer(arcgis.gis.Layer):
         if len(kwargs) > 0:
             for k, v in kwargs.items():
                 params[k] = v
-        res = self._con.post(path=url, postdata=params,)
+        res = self._con.post(
+            path=url,
+            postdata=params,
+        )
         return res
 
     # ----------------------------------------------------------------------
@@ -5434,7 +5437,11 @@ class MapImageLayer(arcgis.gis.Layer):
             "layers": layers,
             "layerOptions": options,
         }
-        return self._con.get(kmlURL, params, out_folder=save_location,)
+        return self._con.get(
+            kmlURL,
+            params,
+            out_folder=save_location,
+        )
 
     # ----------------------------------------------------------------------
     def export_map(
