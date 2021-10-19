60ab1aef1de351455d2eb9a4e915ef5f4feb393f
Andrew Chapkowski <achapkowski@esri.com>
2021-10-19 14:17:03 -0400

    resolves issues with queryDomains

--- src/arcgis/features/layer.py
+++ src/arcgis/features/layer.py
@@ -1956,10 +1956,7 @@ class FeatureLayer(Layer):
             params["sqlType"] = sql_type
         sql_type = sql_type.lower()
         url = self._url + "/validateSQL"
-        return self._con.post(
-            path=url,
-            postdata=params,
-        )
+        return self._con.post(path=url, postdata=params,)
 
     # ----------------------------------------------------------------------
     def query_related_records(
@@ -2298,10 +2295,8 @@ class FeatureLayer(Layer):
             params["upsertMatchingField"] = upsert_matching_field
         if not skip_inserts is None:
             params["skipInserts"] = skip_inserts
-        upload_formats = (
-            """sqlite,shapefile,filegdb,featureCollection,geojson,csv,excel""".split(
-                ","
-            )
+        upload_formats = """sqlite,shapefile,filegdb,featureCollection,geojson,csv,excel""".split(
+            ","
         )
         if upload_format not in upload_formats:
             raise ValueError("Invalid upload format: %s." % upload_format)
@@ -2977,28 +2972,19 @@ class FeatureLayer(Layer):
         ):
             params["async"] = True
             executor = concurrent.futures.ThreadPoolExecutor(1)
-            res = self._con.post(
-                path=url,
-                postdata=params,
-            )
+            res = self._con.post(path=url, postdata=params,)
             future = executor.submit(
                 self._status_via_url, *(self._con, res["statusUrl"], {"f": "json"})
             )
             executor.shutdown(False)
             return future
-        return self._con.post(
-            path=url,
-            postdata=params,
-        )
+        return self._con.post(path=url, postdata=params,)
 
     # ----------------------------------------------------------------------
     def _query(self, url, params, raw=False, **kwargs):
         """returns results of query"""
         try:
-            result = self._con.post(
-                path=url,
-                postdata=params,
-            )
+            result = self._con.post(path=url, postdata=params,)
         except Exception as queryException:
             error_list = [
                 "Error performing query operation",
@@ -3861,15 +3847,20 @@ class FeatureLayerCollection(_GISResource):
             List of dictionaries
 
         """
-        if not isinstance(layers, (tuple, list)):
-            raise ValueError("The layer variable must be a list.")
-        url = "{base}/queryDomains".format(base=self._url)
-        params = {"f": "json"}
-        params["layers"] = layers
-        res = self._con.post(url, params)
-        if "domains" in res:
-            return res["domains"]
-        return res
+        if (
+            "supportsQueryDomains" in self.properties
+            and self.properties['supportsQueryDomains']
+        ):
+            if not isinstance(layers, (tuple, list)):
+                raise ValueError("The layer variable must be a list.")
+            url = "{base}/queryDomains".format(base=self._url)
+            params = {"f": "json"}
+            params["layers"] = layers
+            res = self._con.post(url, params)
+            if "domains" in res:
+                return res["domains"]
+            return res
+        return []
 
     # ----------------------------------------------------------------------
     def extract_changes(
