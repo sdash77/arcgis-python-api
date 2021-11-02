63dea8a3688097b94348ad8bc4cca3612e24c14f
Andrew Chapkowski <achapkowski@esri.com>
2021-11-02 07:42:44 -0400

    Ensures table.query() finds the OBJECTID when not present in the Table.properties.

--- src/arcgis/features/layer.py
+++ src/arcgis/features/layer.py
@@ -1956,7 +1956,10 @@ class FeatureLayer(Layer):
             params["sqlType"] = sql_type
         sql_type = sql_type.lower()
         url = self._url + "/validateSQL"
-        return self._con.post(path=url, postdata=params,)
+        return self._con.post(
+            path=url,
+            postdata=params,
+        )
 
     # ----------------------------------------------------------------------
     def query_related_records(
@@ -2295,8 +2298,10 @@ class FeatureLayer(Layer):
             params["upsertMatchingField"] = upsert_matching_field
         if not skip_inserts is None:
             params["skipInserts"] = skip_inserts
-        upload_formats = """sqlite,shapefile,filegdb,featureCollection,geojson,csv,excel""".split(
-            ","
+        upload_formats = (
+            """sqlite,shapefile,filegdb,featureCollection,geojson,csv,excel""".split(
+                ","
+            )
         )
         if upload_format not in upload_formats:
             raise ValueError("Invalid upload format: %s." % upload_format)
@@ -2972,19 +2977,28 @@ class FeatureLayer(Layer):
         ):
             params["async"] = True
             executor = concurrent.futures.ThreadPoolExecutor(1)
-            res = self._con.post(path=url, postdata=params,)
+            res = self._con.post(
+                path=url,
+                postdata=params,
+            )
             future = executor.submit(
                 self._status_via_url, *(self._con, res["statusUrl"], {"f": "json"})
             )
             executor.shutdown(False)
             return future
-        return self._con.post(path=url, postdata=params,)
+        return self._con.post(
+            path=url,
+            postdata=params,
+        )
 
     # ----------------------------------------------------------------------
     def _query(self, url, params, raw=False, **kwargs):
         """returns results of query"""
         try:
-            result = self._con.post(path=url, postdata=params,)
+            result = self._con.post(
+                path=url,
+                postdata=params,
+            )
         except Exception as queryException:
             error_list = [
                 "Error performing query operation",
@@ -3528,7 +3542,7 @@ class Table(FeatureLayer):
 
         params["returnCountOnly"] = True
         if where == "1=1":
-            if 'objectIdField' in self.properties:
+            if "objectIdField" in self.properties:
                 params["where"] = f"{self.properties.objectIdField} > 0"
             else:
                 fields = [
