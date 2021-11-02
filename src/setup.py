579caa7a47d7914f9bd739c0e99b2e2a2ea83ccf
nanaeaubry <naubry@esri.com>
2021-11-02 11:20:31 +0100

    fix typing in doc

--- src/setup.py
+++ src/setup.py
@@ -73,6 +73,7 @@ else:
         'requests-kerberos;platform_system=="Windows"',
         'winkerberos;platform_system=="Windows"',
         "requests-gssapi",
+        "sphinx-autodoc-typehints"
     ]
 
 
