2bd86f855206ad6cebd7128936fb4c820ef08196
nanaeaubry <naubry@esri.com>
2021-11-04 10:44:59 +0100

    format with black

--- src/arcgis/features/managers.py
+++ src/arcgis/features/managers.py
@@ -1754,7 +1754,9 @@ class FeatureLayerCollectionManager(_GISResource):
                         add_def["layers"].append(
                             {
                                 "adminLayerInfo": {
-                                    "popupInfo": data["layers"][0]["popupInfo"] if "layers" in data else None,
+                                    "popupInfo": data["layers"][0]["popupInfo"]
+                                    if "layers" in data
+                                    else None,
                                     "viewLayerDefinition": {
                                         "sourceServiceName": os.path.basename(
                                             os.path.dirname(fs.url)
