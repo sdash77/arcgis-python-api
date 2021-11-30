5da7b8116fba550fa9da980b45fa2a08b5ee9f67
Andrew <achapkowski@users.noreply.github.com>
2021-11-30 09:20:09 -0500

    Update src/arcgis/geoanalytics/use_proximity.py
    Co-authored-by: Sarah Ambrose <sambrose@esri.com>

--- src/arcgis/geoanalytics/use_proximity.py
+++ src/arcgis/geoanalytics/use_proximity.py
@@ -490,7 +490,7 @@ def create_buffers(
     """
     if field and distance:
         raise ValueError(
-            "A distance and field cannot be specified.  Choose one and resubmit."
+            "Both a distance and field cannot be specified.  Choose one and resubmit."
         )
     if field is None and distance is None:
         distance = 1
