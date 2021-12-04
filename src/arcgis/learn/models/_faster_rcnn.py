d71f8bbe8163f89eb3e35754ffd0bcc455442b38
jyaistMap <jyaist@esri.com>
2021-12-03 17:31:17 -0800

    uncomment torch.jit for local doc build

--- src/arcgis/learn/models/_faster_rcnn.py
+++ src/arcgis/learn/models/_faster_rcnn.py
@@ -403,7 +403,7 @@ def postprocess_transform(self, result, image_shapes, original_image_sizes):
 
 if HAS_FASTAI:
 
-    #    @torch.jit.unused
+    @torch.jit.unused
     def eager_outputs_modified(self, losses, detections):
 
         train_val = getattr(self, "train_val", False)
