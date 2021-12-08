87d03399aa977c0b1ee847bf7190d696700e00bd
Andrew Chapkowski <achapkowski@esri.com>
2021-12-08 06:22:47 -0500

    format fix.

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -9736,15 +9736,15 @@ class User(dict):
         ================  ==========================================================
 
         :return: str
-        
+
         .. code-block:: python
-        
+
            # Usage example: Setting login page
-           
+
            >>> user1 = gis.users.get("org_data_viewer")
-           
+
            >>> user1.landing_page = "map"
-           
+
         """
         value = self.user_settings.get("landingPage", {}).get("url", "")
         lu = {
