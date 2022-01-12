efd123790407c9bc4b62a9eda3fe392d669f37f9
Andrew Chapkowski <achapkowski@esri.com>
2022-01-12 09:28:20 -0500

    Fix black formatting

--- src/arcgis/learn/_utils/common.py
+++ src/arcgis/learn/_utils/common.py
@@ -24,8 +24,13 @@ try:
         from fastai.vision import Image, imagenet_stats, pil2tensor
         import PIL
     elif _LAMBDA_TEXT_CLASSIFICATION:
-        class Image: pass
-        class ImageList: pass
+
+        class Image:
+            pass
+
+        class ImageList:
+            pass
+
     from fastai.data_block import get_files
     import torch
     import numpy as np
@@ -34,8 +39,12 @@ try:
     HAS_FASTAI = True
 except Exception:
     import_exception = traceback.format_exc()
-    class Image: pass
-    class ImageList: pass
+
+    class Image:
+        pass
+
+    class ImageList:
+        pass
 
 
 def read_image(path, resize_to: int = None, keep_raw=False):
