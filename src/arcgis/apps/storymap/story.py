46a69204276197fac46485565ddd7c54e06c63b4
NANAE AUBRY <nanaeaubry@gmail.com>
2022-01-18 10:17:17 -0800

    edit typeKeyword for duplicate item

--- src/arcgis/apps/storymap/story.py
+++ src/arcgis/apps/storymap/story.py
@@ -966,7 +966,7 @@ class StoryMap(object):
             ]
             # Setting the keywords in a set will remove duplicates
             p = {
-                "keywords": list(set(keywords + new_keywords)),
+                "typeKeywords": list(set(keywords + new_keywords)),
                 "text": json.dumps(self._properties),
             }
             if title:
@@ -997,12 +997,11 @@ class StoryMap(object):
                     # Update the date in new keywords
                     previously_published = True
                     keywords.remove(keyword)
-                elif (
-                    "smstatuspublished"
-                    or "smstatusdraft"
-                    or "smdraftresourceid"
-                    or "smeditorapp"
-                ) in keyword:
+                elif ("smstatuspublished" in keyword or
+                    "smstatusdraft" in keyword or
+                    "smdraftresourceid" in keyword or
+                    "smeditorapp" in keyword or
+                    "Copy Item" in keyword):
                     # Remove old keywords and will be replaced in new keywords
                     keywords.remove(keyword)
             if previously_published is True:
@@ -1026,7 +1025,7 @@ class StoryMap(object):
                     "smdraftresourceid:" + draft,
                 ]
             # Pass through set first to remove duplicates
-            p = {"keywords": list(set(keywords + new_keywords))}
+            p = {"typeKeywords": list(set(keywords + new_keywords))}
             if title:
                 p["title"] = title
             if tags:
@@ -1082,13 +1081,16 @@ class StoryMap(object):
 
         # enterprise has no copy_item
         if item._portal.is_arcgisonline is False:
-            return self._gis.content.clone_items(items=[item])
+            clone = self._gis.content.clone_items(items=[item])
         else:
-            return item.copy_item(
+            clone = item.copy_item(
                 title="(Copy) " + self._item.title if title is None else title,
                 include_resources=True,
                 include_private=True,
             )
+        # save to update keywords
+        clone_story = StoryMap(clone.id)
+        return clone_story.save()
 
     # ----------------------------------------------------------------------
     def _delete(self, node_id, resource_id=None):
