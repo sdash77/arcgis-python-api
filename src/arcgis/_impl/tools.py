149b9e1615a9feddaa2a350d8b678a0b9b085ab4
Rhea Jackson <rjackson@esri.com>
2022-01-19 03:54:47 -0800

    fix token issue for feature input for RA

--- src/arcgis/_impl/tools.py
+++ src/arcgis/_impl/tools.py
@@ -211,6 +211,18 @@ class BaseAnalytics(object):
         if isinstance(input_layer, arcgis.gis.Item):
             if input_layer.type.lower() == "feature service":
                 input_param = input_layer.layers[0]._lyr_dict
+                try:
+                    input_layer_url = input_param.get("url", "")
+                    if (
+                        isinstance(input_param, dict)
+                        and "serviceToken" not in input_param.keys()
+                    ):
+                        token = input_layer.layers[0]._gis._con._create_token(
+                            input_layer_url
+                        )
+                        input_param.update({"serviceToken": token})
+                except:
+                    pass
             elif input_layer.type.lower() == "feature collection":
                 fcdict = input_layer.get_data()
                 fc = arcgis.features.FeatureCollection(fcdict["layers"][0])
@@ -232,6 +244,16 @@ class BaseAnalytics(object):
                 input_param = input_layer.layer
         elif isinstance(input_layer, arcgis.gis.Layer):
             input_param = input_layer._lyr_dict
+            try:
+                input_layer_url = input_param.get("url", "")
+                if (
+                    isinstance(input_param, dict)
+                    and "serviceToken" not in input_param.keys()
+                ):
+                    token = input_layer._gis._con._create_token(input_layer_url)
+                    input_param.update({"serviceToken": token})
+            except:
+                pass
 
         elif isinstance(
             input_layer, tuple
@@ -5941,6 +5963,18 @@ class _RasterAnalysisTools(BaseAnalytics):
             else:
                 if "layers" in input_layer:
                     input_param = input_layer.layers[0]._lyr_dict
+                    try:
+                        if (
+                            isinstance(input_param, dict)
+                            and "url" in input_param.keys()
+                        ):
+                            url = input_param["url"]
+                            if "token" not in url:
+                                token = input_layer._gis._con._create_token(url)
+                                url = input_param["url"] + "?token=" + token
+                                input_param.update({"url": url})
+                    except:
+                        pass
                 else:
                     raise TypeError("No layers in input layer Item")
 
@@ -5971,9 +6005,11 @@ class _RasterAnalysisTools(BaseAnalytics):
 
                 try:
                     if isinstance(input_param, dict) and "url" in input_param.keys():
-                        token = input_layer._gis._con._create_token(url)
-                        url = input_param["url"] + "?token=" + token
-                        input_param.update({"url": url})
+                        url = input_param["url"]
+                        if "token" not in url:
+                            token = input_layer._gis._con._create_token(url)
+                            url = input_param["url"] + "?token=" + token
+                            input_param.update({"url": url})
                 except:
                     pass
 
