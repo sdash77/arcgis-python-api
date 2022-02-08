c1b7f148f08a56a1791038f6f7ea670f3a0d7fa2
Andrew Chapkowski <achapkowski@esri.com>
2022-02-08 09:06:19 -0500

    black format fix.

--- src/setup.py
+++ src/setup.py
@@ -300,7 +300,9 @@ kwargs = {
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    "extras_require": {"gp": ["dill"],},
+    "extras_require": {
+        "gp": ["dill"],
+    },
     # extras_require={
     #     'dev': ['check-manifest'],
     #     'test': ['coverage'],
