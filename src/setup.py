22296625034a018810d5c035d4a50eaa5825d319
Andrew Chapkowski <achapkowski@esri.com>
2022-02-24 08:11:33 -0500

    setup.py fix

--- src/setup.py
+++ src/setup.py
@@ -202,21 +202,25 @@ data_files = [
 if not "darwin" in sys.platform:
     _get_rel_site_packages_dir() + "arcgis/gis/_impl"
     data_files += [
-        (
-            _get_rel_site_packages_dir() + "arcgis/gis/_impl",
-            [
-                "arcgis/gis/_impl/_decrypt_nbauth.cp37-win_amd64.pyd",
-                "arcgis/gis/_impl/_decrypt_nbauth.cp38-win_amd64.pyd",
-                "arcgis/gis/_impl/_decrypt_nbauth.cp39-win_amd64.pyd",
-                "arcgis/gis/_impl/_decrypt_nbauth.cpython-37m-x86_64-linux-gnu.so",
-                "arcgis/gis/_impl/_decrypt_nbauth.cpython-38-x86_64-linux-gnu.so",
-                "arcgis/gis/_impl/_decrypt_nbauth.cpython-39-x86_64-linux-gnu.so",
-            ],
-        )
+        # (
+        #    _get_rel_site_packages_dir() + "arcgis/gis/_impl",
+        #    [
+        "arcgis/gis/_impl/_decrypt_nbauth.cp37-win_amd64.pyd",
+        "arcgis/gis/_impl/_decrypt_nbauth.cp38-win_amd64.pyd",
+        "arcgis/gis/_impl/_decrypt_nbauth.cp39-win_amd64.pyd",
+        "arcgis/gis/_impl/_decrypt_nbauth.cpython-37m-x86_64-linux-gnu.so",
+        "arcgis/gis/_impl/_decrypt_nbauth.cpython-38-x86_64-linux-gnu.so",
+        "arcgis/gis/_impl/_decrypt_nbauth.cpython-39-x86_64-linux-gnu.so",
+        #    ],
+        # )
     ]
     data_files += [
-        "arcgis/graph/_arcgisknowledge.pyd",
-        "arcgis/graph/_arcgisknowledge.so",
+        "arcgis/graph/_decoder/for_python37/_arcgisknowledge.pyd",
+        "arcgis/graph/_decoder/for_python38/_arcgisknowledge.pyd",
+        "arcgis/graph/_decoder/for_python39/_arcgisknowledge.pyd",
+        "arcgis/graph/_decoder/for_python39/_arcgisknowledge.so",
+        "arcgis/graph/_decoder/for_python38/_arcgisknowledge.so",
+        "arcgis/graph/_decoder/for_python37/_arcgisknowledge.so",
     ]
 
 
