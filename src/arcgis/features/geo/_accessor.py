fa182044c5c45e0fcd7320aded9cdb481552ab31
Andrew Chapkowski <achapkowski@esri.com>
2022-03-25 10:49:57 -0400

    update tests - fix issue with spatial.name

--- src/arcgis/features/geo/_accessor.py
+++ src/arcgis/features/geo/_accessor.py
@@ -1464,11 +1464,11 @@ class GeoAccessor(object):
         """
         if self._name is None:
             try:
-                cols = [c.lower() for c in self._data.columns.tolist()]
                 if any(self._data.dtypes == "geometry"):
                     name = self._data.dtypes[self._data.dtypes == "geometry"].index[0]
                     self.set_geometry(name)
-                elif "shape" in cols:
+                elif "shape" in [str(c).lower() for c in self._data.columns.tolist()]:
+                    cols = [str(c).lower() for c in self._data.columns.tolist()]
                     idx = cols.index("shape")
                     self.set_geometry(self._data.columns[idx])
             except:
