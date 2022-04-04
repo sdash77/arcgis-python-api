33ff119f31f7c8bb2102e76896782ad0fc6d43cd
Andrew Chapkowski <achapkowski@esri.com>
2022-04-04 06:06:31 -0400

    fix black format.

--- src/arcgis/learn/models/_pix2pix_hd_utils.py
+++ src/arcgis/learn/models/_pix2pix_hd_utils.py
@@ -228,7 +228,7 @@ class LocalEnhancer(nn.Module):
         self.n_local_enhancers = n_local_enhancers
 
         ###### global generator model #####
-        ngf_global = ngf * (2 ** n_local_enhancers)
+        ngf_global = ngf * (2**n_local_enhancers)
         model_global = GlobalGenerator(
             input_nc,
             output_nc,
@@ -335,7 +335,7 @@ class GlobalGenerator(nn.Module):
         ]
         ### downsample
         for i in range(n_downsampling):
-            mult = 2 ** i
+            mult = 2**i
             model += [
                 nn.Conv2d(
                     ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1
@@ -345,7 +345,7 @@ class GlobalGenerator(nn.Module):
             ]
 
         ### resnet blocks
-        mult = 2 ** n_downsampling
+        mult = 2**n_downsampling
         for i in range(n_blocks):
             model += [
                 ResnetBlock(
@@ -445,7 +445,7 @@ class Encoder(nn.Module):
         ]
         ### downsample
         for i in range(n_downsampling):
-            mult = 2 ** i
+            mult = 2**i
             model += [
                 nn.Conv2d(
                     ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1
