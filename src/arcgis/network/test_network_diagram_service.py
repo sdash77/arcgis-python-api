d0cc731d887dbb6152497e34eb97f3654083c3c0
NANAE AUBRY <nanaeaubry@gmail.com>
2022-04-19 13:08:44 -0700

    add network diagram test

---
+++ src/arcgis/network/test_network_diagram_service.py
@@ -0,0 +1,41 @@
+import logging
+import sys
+import unittest
+from arcgis.features._network_diagram import NetworkDiagramManager, Diagram
+from arcgis.gis import GIS
+from arcgis.auth.tools._util import detect_proxy
+from arcgis.gis.server.catalog import ServicesDirectory
+
+__logger__ = logging.getLogger()
+
+def enable_verbose_logging(root):
+    """Enables all messages to be shown to stdout"""
+    root.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    # formatter = logging.Formatter(' -  -  - ')
+    # handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+
+PROXIES = detect_proxy(True)  # Handles Fiddler when True
+enable_verbose_logging(__logger__)
+url = "https://rextapilnxsvr01.esri.com/server"
+username = "siteadmin"
+password = "esri.agp2"
+
+class TestNetworkDiagramManager(unittest.TestCase):
+    """Tests the Network Diagram Service"""
+
+    def add_product(self):
+        sd = ServicesDirectory(
+            url=url,
+            username=username,
+            password=password,
+            verify_cert=False,
+            proxy=PROXIES,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
