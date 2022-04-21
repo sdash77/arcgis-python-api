fd8031ed167a77f621683a769973955c1c57ad96
NANAE AUBRY <nanaeaubry@gmail.com>
2022-04-21 14:44:26 -0700

    add column to license reports

--- src/arcgis/gis/admin/_license.py
+++ src/arcgis/gis/admin/_license.py
@@ -1,6 +1,9 @@
 """
 Entry point to working with licensing on Portal or ArcGIS Online
 """
+import datetime
+import numpy
+from arcgis.gis.admin.agoladmin import AGOLAdminManager
 from .._impl._con import Connection
 from ..._impl.common._mixins import PropertyMap
 from ...gis import GIS, User, Item
@@ -415,9 +418,9 @@ class License(object):
     ===============     ====================================================
     **Argument**        **Description**
     ---------------     ----------------------------------------------------
-    gis                 required GIS, the gis connection object
+    gis                 Required GIS, the gis connection object
     ---------------     ----------------------------------------------------
-    info                required dictionary, the information provided by
+    info                Required dictionary, the information provided by
                         the organization's site containing the provision
                         and listing information.
     ===============     ====================================================
@@ -472,16 +475,27 @@ class License(object):
         import pandas as pd
 
         data = []
-        columns = ["Entitlement", "Total", "Assigned", "Remaining"]
-        if "provision" in self.properties:
+        columns = ["Entitlement", "Total", "Assigned", "Remaining", "Users"]
+        if (
+            "provision" in self.properties
+            and "orgEntitlements" in self.properties["provision"]
+        ):
             for k, v in self.properties["provision"]["orgEntitlements"][
                 "entitlements"
             ].items():
                 counter = 0
+                user_list = []
                 for u in self.all():
                     if k in u["entitlements"]:
                         counter += 1
-                row = [k, v["num"], counter, v["num"] - counter]
+                        if u["lastLogin"] != -1:
+                            last_used = datetime.datetime.fromtimestamp(
+                                u["lastLogin"] / 1000
+                            ).strftime("%B %d, %Y")
+                        else:
+                            last_used = "unknown"
+                        user_list.append({"user": u["username"], "lastUsed": last_used})
+                row = [k, v["num"], counter, v["num"] - counter, user_list]
                 data.append(row)
                 del k, v
         return pd.DataFrame(data=data, columns=columns)
@@ -541,7 +555,7 @@ class License(object):
         ===============     ====================================================
         **Argument**        **Description**
         ---------------     ----------------------------------------------------
-        user                required string, the name of the user you want to
+        user                Required string, the name of the user you want to
                             examine the entitlements for.
         ===============     ====================================================
 
@@ -574,12 +588,12 @@ class License(object):
     # ----------------------------------------------------------------------
     def user_entitlement(self, username: str):
         """
-        checks if a user has the entitlement assigned to them
+        Checks if a user has the entitlement assigned to them
 
         ===============     ====================================================
         **Argument**        **Description**
         ---------------     ----------------------------------------------------
-        username            required string, the name of the user you want to
+        username            Required string, the name of the user you want to
                             examine the entitlements for.
         ===============     ====================================================
 
