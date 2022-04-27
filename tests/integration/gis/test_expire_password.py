931fb9869235d42a7cd3dce7499af2a34d997eb4
Atma Mani <>
2022-04-27 09:56:03 -0700

    updates 2.0.1 tests - item content status, profile names, passwords for certain accounts, missing content for vector tile management tests

--- tests/integration/gis/test_expire_password.py
+++ tests/integration/gis/test_expire_password.py
@@ -4,8 +4,7 @@ sys.path.insert(0, r"C:\SVN\geosaurus_master_issue_7210\src")
 import unittest
 from arcgis.gis import GIS, User, UserManager
 
-PROFILES = ["your_enterprise_profile", "your_online_profile"]
-
+PROFILES = ["your_ent_admin_profile", "your_online_admin_profile"]
 
 class TestUserExpirePassword(unittest.TestCase):
     """
