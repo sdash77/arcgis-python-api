ff8de270f3e322c2069495ba52d51b0219bc0524
John Yaist <jyaist@esri.com>
2022-04-29 12:21:33 -0700

    move code to setup to avoid redundancy

--- tests/integration/mapping/test_VectorTileLayer.py
+++ tests/integration/mapping/test_VectorTileLayer.py
@@ -6,7 +6,6 @@ import unittest
 import os
 from arcgis.mapping._types import VectorTileLayer
 
-vector_tile_layers = []
 
 class TestVectorTileLayerClass_online(unittest.TestCase):
     """
@@ -19,57 +18,46 @@ class TestVectorTileLayerClass_online(unittest.TestCase):
         cls.gis = GIS(profile="your_online_admin_profile")
         
         cls.vtl_item = cls.gis.content.get(cls.vtl_id)
-        vector_tile_layers.append(cls.vtl_item)
+        cls.tl = VectorTileLayer.fromitem(cls.vtl_item)
         
     def test_properties(self):
-        for vtitem in vector_tile_layers:
-            tl = VectorTileLayer.fromitem(vtitem)
-            styles = tl.styles
-            assert isinstance(styles, dict)
+        styles = self.tl.styles
+        assert isinstance(styles, dict)
 
-            info = tl.info
-            assert info
-            assert isinstance(info, dict)
+        info = self.tl.info
+        assert info
+        assert isinstance(info, dict)
 
     def test_tile_fonts(self):              
-        for vtitem in vector_tile_layers:
-            tl = VectorTileLayer.fromitem(vtitem)
-            fonts = tl.tile_fonts(fontstack="Arial Bold", stack_range="0-255")
-            assert fonts
+        fonts = self.tl.tile_fonts(fontstack="Arial Bold", stack_range="0-255")
+        assert fonts
 
     def test_vector_tile(self):
-        for vtitem in vector_tile_layers:
-            tl = VectorTileLayer.fromitem(vtitem)
-            vt = tl.vector_tile(level="1", row="1", column="1")
-            assert vt
+        vt = self.tl.vector_tile(level="1", row="1", column="1")
+        assert vt
 
-    def test_tile_sprint(self):
-        for vtitem in vector_tile_layers:
-            tl = VectorTileLayer(vtitem)
-            sprite = tl.tile_sprite()
-            assert sprite
-            assert isinstance(sprite, dict)
+    def test_tile_sprite(self):
+        sprite = self.tl.tile_sprite()
+        assert sprite
+        assert isinstance(sprite, dict)
 
     def test_export_tiles(self):
-        for vtitem in vector_tile_layers:
-            tl = VectorTileLayer.fromitem(vtitem)
-            exported = tl.export_tiles(
-                levels="0-2",
-                export_extent={
-                    "xmin": -109.55,
-                    "ymin": 25.76,
-                    "xmax": -86.39,
-                    "ymax": 49.94,
-                    "spatialReference": {"wkid": 102100, "latestWkid": 3857},
-                },
-                max_export_tile_count=500,
-            )
-            assert exported
-            os.remove(exported[0])
+        exported = self.tl.export_tiles(
+            levels="0-2",
+            export_extent={
+                "xmin": -109.55,
+                "ymin": 25.76,
+                "xmax": -86.39,
+                "ymax": 49.94,
+                "spatialReference": {"wkid": 102100, "latestWkid": 3857},
+            },
+            max_export_tile_count=500,
+        )
+        assert exported
+        os.remove(exported[0])
     
     @classmethod
     def tearDownClass(cls):
-        vector_tile_layers.pop(0)
         print("\n=============================================================")
         
 class TestVectorTileLayerClass_enterprise(unittest.TestCase):
@@ -83,57 +71,46 @@ class TestVectorTileLayerClass_enterprise(unittest.TestCase):
         cls.gis = GIS(profile="your_ent_admin_profile", verify_cert=False, trust_env=True)
 
         cls.vtl_item = cls.gis.content.get(cls.vtl_id)
-        vector_tile_layers.append(cls.vtl_item)
+        cls.tl = VectorTileLayer.fromitem(cls.vtl_item)
         
     def test_properties(self):
-        for vtitem in vector_tile_layers:
-            tl = VectorTileLayer.fromitem(vtitem)
-            styles = tl.styles
-            assert isinstance(styles, dict)
+        styles = self.tl.styles
+        assert isinstance(styles, dict)
 
-            info = tl.info
-            assert info
-            assert isinstance(info, dict)
+        info = self.tl.info
+        assert info
+        assert isinstance(info, dict)
 
     def test_tile_fonts(self):       
-        for vtitem in vector_tile_layers:
-            tl = VectorTileLayer.fromitem(vtitem)
-            fonts = tl.tile_fonts(fontstack="Arial Bold", stack_range="0-255")
-            assert fonts
+        fonts = self.tl.tile_fonts(fontstack="Arial Bold", stack_range="0-255")
+        assert fonts
 
     def test_vector_tile(self):       
-        for vtitem in vector_tile_layers:
-            tl = VectorTileLayer.fromitem(vtitem)
-            vt = tl.vector_tile(level="1", row="1", column="1")
-            assert vt
+        vt = self.tl.vector_tile(level="1", row="1", column="1")
+        assert vt
 
-    def test_tile_sprint(self):
-        for vtitem in vector_tile_layers:
-            tl = VectorTileLayer(vtitem)
-            sprite = tl.tile_sprite()
-            assert sprite
-            assert isinstance(sprite, dict)
+    def test_tile_sprite(self):
+        sprite = self.tl.tile_sprite()
+        assert sprite
+        assert isinstance(sprite, dict)
 
     def test_export_tiles(self):           
-        for vtitem in vector_tile_layers:
-            tl = VectorTileLayer.fromitem(vtitem)
-            exported = tl.export_tiles(
-                levels="0-2",
-                export_extent={
-                    "xmin": -109.55,
-                    "ymin": 25.76,
-                    "xmax": -86.39,
-                    "ymax": 49.94,
-                    "spatialReference": {"wkid": 102100, "latestWkid": 3857},
-                },
-                max_export_tile_count=500,
-            )
-            assert exported
-            os.remove(exported[0])
+        exported = self.tl.export_tiles(
+            levels="0-2",
+            export_extent={
+                "xmin": -109.55,
+                "ymin": 25.76,
+                "xmax": -86.39,
+                "ymax": 49.94,
+                "spatialReference": {"wkid": 102100, "latestWkid": 3857},
+            },
+            max_export_tile_count=500,
+        )
+        assert exported
+        os.remove(exported[0])
 
     @classmethod
     def tearDownClass(cls):
-        vector_tile_layers.pop(0)
         print("\n=============================================================")
 
 if __name__ == "__main__":
