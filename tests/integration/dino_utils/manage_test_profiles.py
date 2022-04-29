055d5b3fa41ee7bbd4fd85b333f39c65f74fcdeb
Atma <amani@esri.com>
2022-04-29 12:00:49 -0700

    minor - making path os agnostic

--- tests/integration/dino_utils/manage_test_profiles.py
+++ tests/integration/dino_utils/manage_test_profiles.py
@@ -1,9 +1,10 @@
 import json
 from arcgis.gis import login_profiles
+import os
 
 
 # Check if profile exists, else create it
-def create_test_profiles(profile_json_path="../../test_profiles.json"):
+def create_test_profiles(profile_json_path=os.path.join("..", "..", "test_profiles.json")):
 
     with open(profile_json_path, 'r') as file_handle:
         profile_dict = json.load(file_handle)
