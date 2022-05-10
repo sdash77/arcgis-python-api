f9d71aa03a3d8d7739233ba8d39bdc4e494d24d5
Ryan Cosby <RCosby@esri.com>
2022-05-10 11:05:50 -0700

    Add check for ._token in event that property is set

--- src/arcgis/_impl/common/_clone.py
+++ src/arcgis/_impl/common/_clone.py
@@ -193,7 +193,8 @@ class _DeepCloner:
             admin_layer_info = None
 
             has_admin_info = (
-                layer._con.token
+                layer._token
+                or layer._con.token
                 and layer.manager
                 and layer.manager.properties
                 and "adminLayerInfo" in layer.manager.properties
