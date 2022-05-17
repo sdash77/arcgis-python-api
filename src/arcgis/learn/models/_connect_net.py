ee81c5e8fe4fe65fb71e7b120e86cb1cb2c5fe27
Vaibhav Raj <vbhv14@users.noreply.github.com>
2022-05-17 16:07:14 +0530

    commit 1 (#8415)

--- src/arcgis/learn/models/_connect_net.py
+++ src/arcgis/learn/models/_connect_net.py
@@ -7,51 +7,53 @@ class ConnectNet(MultiTaskRoadExtractor):
     and Multispectral Imagery.
     Implementation based on https://doi.org/10.1109/CVPR.2019.01063 .
 
-    =====================   ===========================================
+    =====================   =====================================================
     **Argument**            **Description**
-    ---------------------   -------------------------------------------
+    ---------------------   -----------------------------------------------------
     data                    Required fastai Databunch. Returned data object from
                             ``prepare_data`` function.
-    ---------------------   -------------------------------------------
+    ---------------------   -----------------------------------------------------
     backbone                Optional String. Backbone CNN model to be used for
                             creating the base. If hourglass is chosen as
-                            the mtl_model (Architecture),then this parameter is
-                            ignored as hourglass uses a special customised
+                            the `mtl_model` (Architecture), then this parameter 
+                            is ignored as hourglass uses a special customised
                             architecture.
-                            This parameter is used with `linknet` model.
+                            This parameter is to be used with
+                            `linknet` architecture.
                             Default: 'resnet34'
 
-                            Supported backbones: 'resnet18', 'resnet34',
-                            'resnet50', 'resnet101', 'resnet152'
-    ---------------------   -------------------------------------------
-    mtl_model               Optional String. It is used to create model
-                            from linknet or hourglass based neural architectures.
-                            Supported: 'linknet', 'hourglass'.
-                            Default: 'hourglass'
-    ---------------------   -------------------------------------------
+                            Use `supported_backbones` property to get the list
+                            of all the supported backbones.
+    ---------------------   -----------------------------------------------------
     pretrained_path         Optional String. Path where a compatible pre-trained
                             model is saved. Accepts a Deep Learning Package
                             (DLPK) or Esri Model Definition(EMD) file.
-    =====================   ===========================================
+    =====================   =====================================================
 
     **kwargs**
 
     =============================   =============================================
     **Argument**                    **Description**
     -----------------------------   ---------------------------------------------
+    mtl_model                       Optional String. It is used to create model
+                                    from linknet or 
+                                    hourglass based neural architectures.
+                                    Supported: 'linknet', 'hourglass'.
+                                    Default: 'hourglass'
+    -----------------------------   ---------------------------------------------
     gaussian_thresh                 Optional float. Sets the gaussian threshold
-                                    which allows to set the required width of the
-                                    linear feature.
+                                    which allows to set the required width of 
+                                    the linear feature.
                                     Range: 0.0 to 1.0
-                                    Default:0.76
+                                    Default: 0.76
     -----------------------------   ---------------------------------------------
     orient_bin_size                 Optional Int. Sets the bin size for
                                     orientation angles.
-                                    Default:20
+                                    Default: 20
     -----------------------------   ---------------------------------------------
     orient_theta                    Optional Int. Sets the width of orientation
                                     mask.
-                                    Default:8
+                                    Default: 8
     =============================   =============================================
 
     :return: `ConnectNet` Object
--- src/arcgis/learn/models/_multi_task_road_extractor.py
+++ src/arcgis/learn/models/_multi_task_road_extractor.py
@@ -79,50 +79,52 @@ class MultiTaskRoadExtractor(ArcGISModel):
     and Multispectral Imagery.
     Implementation based on https://doi.org/10.1109/CVPR.2019.01063 .
 
-    =====================   ===========================================
+    =====================   =====================================================
     **Argument**            **Description**
-    ---------------------   -------------------------------------------
+    ---------------------   -----------------------------------------------------
     data                    Required fastai Databunch. Returned data object from
                             ``prepare_data`` function.
-    ---------------------   -------------------------------------------
-    backbone                Optional String. Backbone convolutional neural network
-                            model used for feature extraction. If hourglass is chosen as
-                            the mtl_model (Architecture),then this parameter is
-                            ignored as hourglass uses a special customised
+    ---------------------   -----------------------------------------------------
+    backbone                Optional String. Backbone CNN model to be used for
+                            creating the base. If hourglass is chosen as
+                            the `mtl_model` (Architecture), then this parameter 
+                            is ignored as hourglass uses a special customised
                             architecture.
-                            This parameter is used with `linknet` model.
+                            This parameter is to be used with
+                            `linknet` architecture.
                             Default: 'resnet34'
 
-                            Supported backbones: ResNet family and specified Timm
-                            models from :func:`~arcgis.learn.MultiTaskRoadExtractor.backbones`.
-    ---------------------   -------------------------------------------
+                            Use `supported_backbones` property to get the list
+                            of all the supported backbones.
+    ---------------------   -----------------------------------------------------
     pretrained_path         Optional String. Path where a compatible pre-trained
                             model is saved. Accepts a Deep Learning Package
                             (DLPK) or Esri Model Definition(EMD) file.
-    =====================   ===========================================
+    =====================   =====================================================
 
     **kwargs**
 
     =============================   =============================================
     **Argument**                    **Description**
-    ---------------------   -------------------------------------------
-    mtl_model               Optional String. It is used to create model
-                            from linknet or hourglass based neural architectures.
-                            Supported: 'linknet', 'hourglass'.
-                            Default: 'hourglass'
+    -----------------------------   ---------------------------------------------
+    mtl_model                       Optional String. It is used to create model
+                                    from linknet or 
+                                    hourglass based neural architectures.
+                                    Supported: 'linknet', 'hourglass'.
+                                    Default: 'hourglass'
     -----------------------------   ---------------------------------------------
     gaussian_thresh                 Optional float. Sets the gaussian threshold
                                     which allows to set the required road width.
                                     Range: 0.0 to 1.0
-                                    Default:0.76
+                                    Default: 0.76
     -----------------------------   ---------------------------------------------
     orient_bin_size                 Optional Int. Sets the bin size for
                                     orientation angles.
-                                    Default:20
+                                    Default: 20
     -----------------------------   ---------------------------------------------
     orient_theta                    Optional Int. Sets the width of orientation
                                     mask.
-                                    Default:8
+                                    Default: 8
     =============================   =============================================
 
     :return: `MultiTaskRoadExtractor` Object
