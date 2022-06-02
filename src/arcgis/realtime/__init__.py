4dd6f28fda6bcdecc00d419cf020a621153bc1c0
jyaistMap <jyaist@esri.com>
2022-06-02 13:33:44 -0700

    update table formatting for method and add cross-references

--- src/arcgis/realtime/__init__.py
+++ src/arcgis/realtime/__init__.py
@@ -6,7 +6,7 @@ This module contains the following:
     + :class:`~arcgis.realtime.StreamLayer` provides types and functions for receiving real-time data feeds and sensor data streamed from
       the GIS to perform continuous processing and analysis. It includes support for stream layers that allow Python scripts
       to subscribe to the streamed feature data or broadcast updates or alerts.
-    + :class:`~arcgis.realtime.Velocity` and its submodules provide API functions to automate the ArcGIS Velocity REST API.
+    + The :class:`~arcgis.realtime.Velocity`  class and the various submodules provide API functions to automate the ArcGIS Velocity REST API.
 
 """
 
