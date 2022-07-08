69c2cac1787265e487478c6898a6cdb58d35fd92
Parth Poply <34216657+ppoply@users.noreply.github.com>
2022-07-08 17:16:03 +0400

    context param fix

--- src/arcgis/_impl/tools.py
+++ src/arcgis/_impl/tools.py
@@ -10451,6 +10451,20 @@ class _RasterAnalysisTools(BaseAnalytics):
         use_input_rasters_by_ref = None
         upload_properties = None
 
+        from arcgis.raster import RasterCollection
+        from arcgis.raster._layer import _LocalRasterCollection
+
+        if isinstance(input_rasters, RasterCollection):
+
+            if input_rasters._ras_coll_engine == _LocalRasterCollection:
+                raster_list = [ras["Raster"].catalog_path for ras in input_rasters]
+                input_rasters = raster_list
+                context = {"byref": True}
+            else:
+                raise RuntimeError(
+                    "This type of RasterCollection input is not supported for create_image_collection()"
+                )
+
         if context is not None:
             if "image_collection_properties" in context:
                 image_collection_properties = context["image_collection_properties"]
@@ -10502,20 +10516,6 @@ class _RasterAnalysisTools(BaseAnalytics):
                 else:
                     context = {}
                     context["outSR"] = out_sr
-
-        from arcgis.raster import RasterCollection
-        from arcgis.raster._layer import _LocalRasterCollection
-
-        if isinstance(input_rasters, RasterCollection):
-
-            if input_rasters._ras_coll_engine == _LocalRasterCollection:
-                raster_list = [ras["Raster"].catalog_path for ras in input_rasters]
-                input_rasters = raster_list
-                context = {"byref": True}
-            else:
-                raise RuntimeError(
-                    "This type of RasterCollection input is not supported for create_image_collection()"
-                )
 
         context_param = {}
         _set_raster_context(context_param, context)
