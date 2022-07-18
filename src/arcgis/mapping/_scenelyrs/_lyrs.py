1916c206ab5b928902bf18444c7ce4861afc5922
NANAE AUBRY <nanaeaubry@gmail.com>
2022-07-18 13:04:59 -0700

    typo fix and edit managers

--- src/arcgis/mapping/_scenelyrs/_lyrs.py
+++ src/arcgis/mapping/_scenelyrs/_lyrs.py
@@ -19,13 +19,13 @@ class SceneLayerManager(_GISResource):
         self._sl = scene_lyr
 
     # ----------------------------------------------------------------------
-    def refresh(self, service_definition: bool = True):
+    def refresh(self):
         """
         The ``refresh`` operation refreshes a service, which clears the web
         server cache for the service.
         """
         url = self._url + "SceneServer/refresh"
-        params = {"f": "json", "serviceDefinition": service_definition}
+        params = {"f": "json"}
 
         res = self._con.post(url, params)
 
@@ -99,16 +99,9 @@ class SceneLayerManager(_GISResource):
         return self._con.post(url, params)
 
     # ----------------------------------------------------------------------
-    def import_tiles(
-        self,
-        item: Union[str, Item],
-        levels: Optional[Union[str, list]] = None,
-        extent: Optional[Union[str, dict]] = None,
-        merge: bool = False,
-        replace: bool = False,
-    ):
+    def import_item(self, item: Union[str, Item]):
         """
-        The ``import_tiles`` method imports tiles from an :class:`~arcgis.gis.Item` object.
+        The ``import`` method imports from an :class:`~arcgis.gis.Item` object.
 
         ===============     ====================================================
         **Argument**        **Description**
@@ -116,63 +109,15 @@ class SceneLayerManager(_GISResource):
         item                Required ItemId or :class:`~arcgis.gis.Item` object. The TPK file's item id.
                             This TPK file contains to-be-extracted bundle files
                             which are then merged into an existing cache service.
-        ---------------     ----------------------------------------------------
-        levels              Optional String / List of integers, The level of details
-                            to update. Example: "1,2,10,20" or [1,2,10,20]
-        ---------------     ----------------------------------------------------
-        extent              Optional String / Dict. The area to update as Xmin, YMin, XMax, YMax
-                            example: "-100,-50,200,500" or
-                            {'xmin':100, 'ymin':200, 'xmax':105, 'ymax':205}
-        ---------------     ----------------------------------------------------
-        merge               Optional Boolean. Default is false and applicable to
-                            compact cache storage format. It controls whether
-                            the bundle files from the TPK file are merged with
-                            the one in the existing cached service. Otherwise,
-                            the bundle files are overwritten.
-        ---------------     ----------------------------------------------------
-        replace             Optional Boolean. Default is false, applicable to
-                            compact cache storage format and used when
-                            merge=true. It controls whether the new tiles will
-                            replace the existing ones when merging bundles.
         ===============     ====================================================
 
         :return:
             A dictionary
 
-        .. code-block:: python
-
-            # USAGE EXAMPLE
-
-            >>> from arcgis.gis import GIS
-            >>> from arcgis.mapping import SceneLayer
-
-            # connect to your GIS and get the web map item
-            >>> gis = GIS(url, username, password)
-            >>> scene_item = gis.content.get('abcd_item-id')
-            >>> scene_layer = SceneLayer(scene_item.sourceUrl, gis)
-            >>> sl_manager = scene_layer.manager
-            >>> imported_tiles = sl_manager.import_tiles(item = "<item_id>",
-                                                          levels = "11-20",
-                                                          extent = {"xmin":6224324.092137296,
-                                                                    "ymin":487347.5253569535,
-                                                                    "xmax":11473407.698535524,
-                                                                    "ymax":4239488.369818687,
-                                                                    "spatialReference":{"wkid":102100}
-                                                                    },
-                                                          merge = True,
-                                                        replace = True
-                                                          )
-            >>> type(imported_tiles)
-            <Dictionary>
-
         """
         params = {
             "f": "json",
             "sourceItemId": None,
-            "extent": extent,
-            "levels": levels,
-            "mergeBundle": merge,
-            "replaceTiles": replace,
         }
         if isinstance(item, str):
             params["sourceItemId"] = item
@@ -180,76 +125,24 @@ class SceneLayerManager(_GISResource):
             params["sourceItemId"] = item.itemid
         else:
             raise ValueError("The `item` must be a string or Item")
-        url = self._url + "/importTiles"
+        url = self._url + "/import"
         res = self._con.post(url, params)
         return res
 
     # ----------------------------------------------------------------------
-    def update_tiles(
-        self,
-        levels: Optional[Union[str, list]] = None,
-        extent: Optional[Union[str, dict]] = None,
-    ):
+    def update(self):
         """
-        The ``update_tiles`` method starts tile generation for ArcGIS Online. The levels of detail
+        The ``update`` method starts tile generation for ArcGIS Online. The levels of detail
         and the extent are needed to determine the area where tiles need
         to be rebuilt.
-
-        .. note::
-            The ``update_tiles`` operation is for ArcGIS Online only.
-
-        ===============     ====================================================
-        **Argument**        **Description**
-        ---------------     ----------------------------------------------------
-        levels              Optional String / List of integers, The level of details
-                            to update. Example: "1,2,10,20" or [1,2,10,20]
-        ---------------     ----------------------------------------------------
-        extent              Optional String / Dict. The area to update as Xmin, YMin, XMax, YMax
-                            example: "-100,-50,200,500" or
-                            {'xmin':100, 'ymin':200, 'xmax':105, 'ymax':205}
-        ===============     ====================================================
 
         :return:
            Dictionary. If the product is not ArcGIS Online tile service, the
            result will be None.
-
-        .. code-block:: python
-
-            # USAGE EXAMPLE
-
-            >>> from arcgis.gis import GIS
-            >>> from arcgis.mapping import SceneLayer
-
-            # connect to your GIS and get the web map item
-            >>> gis = GIS(url, username, password)
-            >>> scene_item = gis.content.get('abcd_item-id')
-            >>> scene_layer = SceneLayer(scene_item.sourceUrl, gis)
-            >>> sl_manager = scene_layer.manager
-            >>> update_tiles = sl_manager.update_tiles(levels = "11-20",
-                                                        extent = {"xmin":6224324.092137296,
-                                                                    "ymin":487347.5253569535,
-                                                                    "xmax":11473407.698535524,
-                                                                    "ymax":4239488.369818687,
-                                                                    "spatialReference":{"wkid":102100}
-                                                                    }
-                                                        )
-            >>> type(update_tiles)
-            <Dictionary>
         """
         if self._gis._portal.is_arcgisonline:
-            url = "%s/updateTiles" % self._url
+            url = "%s/update" % self._url
             params = {"f": "json"}
-            if levels:
-                if isinstance(levels, list):
-                    levels = ",".join(str(e) for e in levels)
-                params["levels"] = levels
-            if extent:
-                if isinstance(extent, dict):
-                    extent2 = "{},{},{},{}".format(
-                        extent["xmin"], extent["ymin"], extent["xmax"], extent["ymax"]
-                    )
-                    extent = extent2
-                params["extent"] = extent
             return self._con.post(url, params)
         return None
 
@@ -279,121 +172,35 @@ class SceneLayerManager(_GISResource):
         return self._con.post(url, params)
 
     # ----------------------------------------------------------------------
-    def edit_tile_service(
-        self,
-        service_definition: str,
-        min_scale: Optional[float],
-        max_scale: Optional[float],
-        source_item_id: str,
-        export_tiles_allowed: bool = False,
-        max_export_tile_count: float = 100000,
-    ):
+    def edit_item(self, item: Union[str, Item]):
         """
-        The ``edit_tile_service`` operation updates a Tile Service's properties.
-
-        =====================       ======================================================
-        **Argument**                **Description**
-        ---------------------       ------------------------------------------------------
-        service_definition          Required String. Updates a service definition.
-        ---------------------       ------------------------------------------------------
-        min_scale                   Required float. Sets the services minimum scale for caching.
-        ---------------------       ------------------------------------------------------
-        max_scale                   Required float. Sets the services maximum scale for caching.
-        ---------------------       ------------------------------------------------------
-        source_item_id              Required String. The Source Item ID is the GeoWarehouse Item ID of the scene service
-        ---------------------       ------------------------------------------------------
-        export_tiles_allowed        Required boolean. ``exports_tiles_allowed`` sets the value to let users export tiles
-        ---------------------       ------------------------------------------------------
-        max_export_tile_count       Optional float. ``max_export_tile_count``sets the maximum amount of tiles to be exported from a single call.
-
-                                    .. note::
-                                        The default value is 100000.
-        =====================       ======================================================
-
-        .. code-block:: python
-
-            # USAGE EXAMPLE
-
-            >>> from arcgis.mapping import SceneLayerManager
-            >>> from arcgis.gis import GIS
-
-            # connect to your GIS and get the web map item
-            >>> gis = GIS(url, username, password)
-
-            >>> SceneLayerManager.edit_tile_service(service_definition = "updated service definition",
-                                                        min_scale = 50,
-                                                        max_scale = 100,
-                                                        source_item_id = "geowarehouse_item_id",
-                                                        export_tiles_allowed = True,
-                                                        max_Export_Tile_Count = 10000
-                                                        )
-        """
-        params = {
-            "f": "json",
-        }
-        if not service_definition is None:
-            params["serviceDefinition"] = service_definition
-        if not min_scale is None:
-            params["minScale"] = float(min_scale)
-        if not max_scale is None:
-            params["maxScale"] = float(max_scale)
-        if not source_item_id is None:
-            params["sourceItemId"] = source_item_id
-        if not export_tiles_allowed is None:
-            params["exportTilesAllowed"] = export_tiles_allowed
-        if not max_export_tile_count is None:
-            params["maxExportTileCount"] = int(max_export_tile_count)
-        url = self._url + "/edit"
-        return self._con.post(url, params)
-
-    # ----------------------------------------------------------------------
-    def delete_tiles(self, levels: str, extent: Optional[Union[str, dict]] = None):
-        """
-        The ``delete_tiles`` method deletes tiles from the current cache.
+        The ``edit_item`` method edits from an :class:`~arcgis.gis.Item` object.
 
         ===============     ====================================================
         **Argument**        **Description**
         ---------------     ----------------------------------------------------
-        levels              Required string, The level to delete.
-                            Example, 0-5,10,11-20 or 1,2,3 or 0-5
-        ---------------     ----------------------------------------------------
-        extent              Optional dictionary,  If specified, the tiles within
-                            this extent will be deleted or will be deleted based
-                            on the service's full extent.
+        item                Required ItemId or :class:`~arcgis.gis.Item` object. The TPK file's item id.
+                            This TPK file contains to-be-extracted bundle files
+                            which are then merged into an existing cache service.
         ===============     ====================================================
 
         :return:
-           A dictionary
+            A dictionary
 
-        .. code-block:: python
-
-            # USAGE EXAMPLE
-
-            >>> from arcgis.mapping import SceneLayerManager
-            >>> from arcgis.gis import GIS
-
-            # connect to your GIS and get the web map item
-            >>> gis = GIS(url, username, password)
-
-            >>> deleted_tiles = SceneLayerManager.delete_tiles(levels = "11-20",
-                                                  extent = {"xmin":6224324.092137296,
-                                                            "ymin":487347.5253569535,
-                                                            "xmax":11473407.698535524,
-                                                            "ymax":4239488.369818687,
-                                                            "spatialReference":{"wkid":102100}
-                                                            }
-                                                  )
-            >>> type(deleted_tiles)
-            <Dictionary>
         """
         params = {
             "f": "json",
-            "levels": levels,
+            "sourceItemId": None,
         }
-        if extent:
-            params["extent"] = extent
-        url = self._url + "/deleteTiles"
-        return self._con.post(url, params)
+        if isinstance(item, str):
+            params["sourceItemId"] = item
+        elif isinstance(item, Item):
+            params["sourceItemId"] = item.itemid
+        else:
+            raise ValueError("The `item` must be a string or Item")
+        url = self._url + "/edit"
+        res = self._con.post(url, params)
+        return res
 
 
 ###########################################################################
@@ -470,7 +277,7 @@ class EnterpriseSceneLayerManager(_GISResource):
 ###########################################################################
 class Object3DLayer(Layer):
     """
-    The ``Object3DLayer`` rresents a Web scene 3D Object layer.
+    The ``Object3DLayer`` represents a Web scene 3D Object layer.
 
     .. note::
         Web scene layers are cached web layers that are optimized for displaying a large amount of 2D and 3D features.
