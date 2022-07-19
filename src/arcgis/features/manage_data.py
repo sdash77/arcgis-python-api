a043fd58ace11166e762f4ffec940de78d705af7
NANAE AUBRY <nanaeaubry@gmail.com>
2022-07-19 08:49:36 -0700

    Update docstrings for FA tools, add new overwrite

--- src/arcgis/features/manage_data.py
+++ src/arcgis/features/manage_data.py
@@ -206,7 +206,7 @@ def dissolve_boundaries(
 
                                              - ``extent`` - a bounding box that defines the analysis area. Only those features in the input_layer that intersect the bounding box will be analyzed.
                                              - ``outSR`` - the output features will be projected into the output spatial reference referred to by the `wkid`.
-                                             - ``overwrite`` - if True, then the feature layer in output_name will be overwritten with new feature layer. Available for ArcGIS Online or Enterprise 10.9.1+
+                                             - ``overwrite`` - if True, then the feature layer in output_name will be overwritten with new feature layer. Available for ArcGIS Online or Enterprise 11+
 
                                                 .. code-block:: python
 
