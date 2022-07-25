f99618a6eb8abd58cede4e725de1edc0851b2479
NANAE AUBRY <nanaeaubry@gmail.com>
2022-07-25 15:09:09 -0700

    Add overwrite for to_featurelayer from SeDF

--- src/arcgis/features/geo/_accessor.py
+++ src/arcgis/features/geo/_accessor.py
@@ -5,7 +5,6 @@ import logging
 import pandas as pd
 from collections.abc import Iterable
 
-from arcgis.features.layer import FeatureLayerCollection
 from ._internals import register_dataframe_accessor, register_series_accessor
 from ._array import GeoType
 from ._io.fileops import (
@@ -24,6 +23,7 @@ datetime = LazyLoader("datetime")
 np = LazyLoader("numpy")
 tempfile = LazyLoader("tempfile")
 warnings = LazyLoader("warnings")
+features = LazyLoader("arcgis.features")
 _geometry = LazyLoader("arcgis.geometry")
 _mixins = LazyLoader("arcgis._impl.common._mixins")
 _isd = LazyLoader("arcgis._impl.common._isd")
@@ -2559,8 +2559,7 @@ class GeoAccessor(object):
         folder=None,
         sanitize_columns=False,
         service_name=None,
-        overwrite=False,
-        layer=None,
+        **kwargs
     ):
         """
         The ``to_featurelayer`` method publishes a spatial dataframe to a new
@@ -2587,16 +2586,25 @@ class GeoAccessor(object):
         service_name                    Optional String. The name for the service that will be added to the Item.
                                         Name cannot be used already and cannot contain special characters, spaces,
                                         or a numerical value as the first letter.
+        ===========================     ====================================================================
+
+        When publishing a Spatial Dataframe, additional options can be given:
+
+        ===========================     ====================================================================
+        **Optional Arguments**          **Description**
         ---------------------------     --------------------------------------------------------------------
-        overwrite                       Optional boolean. If True, the specified layer in the `layer` parameter
+        overwrite                       Optional boolean. If True, the specified layer in the `service` parameter
                                         will be overwritten.
         ---------------------------     --------------------------------------------------------------------
-        layer                           Dictionary that is required if `overwrite = True`. Dictionary with two
+        service                         Dictionary that is required if `overwrite = True`. Dictionary with two
                                         keys: "FeatureServiceId" and "layers".
-                                        "FeatureServiceId" value is a string of the feature service id that the layer
+                                        "featureServiceId" value is a string of the feature service id that the layer
                                         belongs to.
-                                        "layers" value is an integer depicting the index value of the layer to
+                                        "layer" value is an integer depicting the index value of the layer to
                                         overwrite.
+
+                                        Example:
+                                        {"featureServiceId" : "9311d21a9a2047d19c0faaebd6f2cca6", "layer": 0}
         ===========================     ====================================================================
 
         :return:
@@ -2629,58 +2637,19 @@ class GeoAccessor(object):
                 raise ValueError(
                     "This service name is unavailable for Feature Service."
                 )
-        if overwrite is False:
-            result = content.import_data(
-                self._data,
-                folder=folder,
-                title=title,
-                tags=tags,
-                sanitize_columns=sanitize_columns,
-                service_name=service_name,
-            )
-            self._data.columns = origin_columns
-            self._data.index = origin_index
-            return result
-        else:
-            # check that overwrite parameters are included
-            if layer is None:
-                raise ValueError(
-                    "Need to provide a dictionary for the layer parameter. Indicate the feature service id and the layer index to overwrite."
-                )
-            if "FeatureServiceId" not in layer and "layer" not in layer:
-                raise ValueError(
-                    "Ensure that the keys: 'FeatureServiceId' and 'layer' are present in the layer parameter."
-                )
 
-            # check that feature service exists and has layers before beginning
-            fs_item = content.get(layer["FeatureServiceId"])
-            if fs_item is None:
-                raise ValueError(
-                    "The Feature Service is not reachable or does not exist. Please ensure this is the correct id."
-                )
-            if fs_item.layers is None:
-                raise ValueError(
-                    "The Feature Service provided has no layers to overwrite"
-                )
-
-            # 1. Call Update on the Feature Service and Enable
-            flc = FeatureLayerCollection(fs_item)
-            if flc.manager.properties["preserveLayerIds"] is False:
-                flc.manager.update_definition({"preserveLayerIds": True})
-                revert = True
-
-            # 2. Call Delete from definition to remove the Feature Layer:
-            flc.manager.delete_from_definition({"layers": [{"id": layer["layer"]}]})
-
-            # 3. Add the new layer by calling add to definition
-            #     a. Set ID to the removed index position in part 2
-            #     {"layers" : [{"id" : <index position>, .... other properties... }]}
-
-            # 4. Append New Data
-
-            # 5. Reset the preserveLayerIds to original state if needed.
-            if revert is True:
-                flc.manager.update_definition({"preserveLayerIds": False})
+        result = content.import_data(
+            self._data,
+            folder=folder,
+            title=title,
+            tags=tags,
+            sanitize_columns=sanitize_columns,
+            service_name=service_name,
+            **kwargs,
+        )
+        self._data.columns = origin_columns
+        self._data.index = origin_index
+        return result
 
     # ----------------------------------------------------------------------
     @staticmethod
--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -6983,6 +6983,16 @@ class ContentManager(object):
                                modified in order to allow for successful publishing.
         ---------------------  --------------------------------------------------------------------------
         service_name           Optional String. The name for the service that will be added to the Item.
+        ---------------------  --------------------------------------------------------------------------
+        overwrite              Optional boolean. If True, the specified feature layer for the specified
+                               feature service will be overwritten.
+        ---------------------  --------------------------------------------------------------------------
+        service                Dictionary that is required if `overwrite = True`. Dictionary with two
+                               keys: "FeatureServiceId" and "layers".
+                               "featureServiceId" value is a string of the feature service id that the layer
+                               belongs to.
+                               "layer" value is an integer depicting the index value of the layer to
+                               overwrite.
         =====================  ==========================================================================
 
 
@@ -6998,7 +7008,11 @@ class ContentManager(object):
             warnings.warn(
                 "`item_id` is not allowed at this version of Portal, please use Enterprise 10.8.1+"
             )
-        from arcgis.features import FeatureCollection, FeatureSet
+        from arcgis.features import (
+            FeatureCollection,
+            FeatureSet,
+            FeatureLayerCollection,
+        )
 
         from arcgis._impl.common._utils import zipws
 
@@ -7070,6 +7084,8 @@ class ContentManager(object):
                     folder=folder,
                 )
                 shutil.rmtree(temp_dir, ignore_errors=True)
+
+                # Publish as new feature layer
                 publish_parameters = {
                     "hasStaticData": True,
                     "name": os.path.splitext(item["name"])[0],
@@ -7078,9 +7094,58 @@ class ContentManager(object):
                 }
                 if target_sr is not None:
                     publish_parameters["targetSR"] = {"wkid": target_sr}
-                return item.publish(
+
+                new_item = item.publish(
                     publish_parameters=publish_parameters, item_id=item_id
                 )
+                # If overwrite specified, perform overwrite
+                overwrite = kwargs.pop("overwrite", None)
+                if overwrite:
+                    # Get user defined parameters
+                    fs_dict = kwargs.pop("service", None)
+                    if fs_dict is None:
+                        raise ValueError(
+                            "If overwite is True, then the feature service id and layer index need to be specified in the `service` parameter."
+                        )
+                    fs_id = fs_dict["featureServiceId"]
+                    fl_index = fs_dict["layer"]
+
+                    # Create the feature layer manager from the kwargs
+                    fs_item = self._gis.content.get(fs_id)
+                    flc = FeatureLayerCollection.fromitem(fs_item)
+                    flc_manager = flc.manager
+                    old_props = flc.manager.properties.layers[fl_index]
+
+                    # Get properties from the newly created feature layer, assign correct id
+                    new_fl = new_item.layers[0]
+                    new_props = new_fl.properties
+                    new_props.update(old_props)
+                    new_props["id"] = fl_index
+
+                    # Perform edit on the flc
+                    # Step 1: Preserve layer ids
+                    revert = False
+                    if (
+                        "preserveLayerIds" not in flc_manager.properties
+                        or flc_manager.properties["preserveLayerIds"] is not True
+                    ):
+                        flc_manager.update_definition({"preserveLayerIds": True})
+                        revert = True
+                    # Step 2: Delete layer from definition
+                    flc_manager.delete_from_definition({"layers": [{"id": fl_index}]})
+                    # Step 3: Add new layer to definition
+                    flc_manager.add_to_definition({"layers": [dict(new_props)]})
+                    # Step 4: Cleanup
+                    if revert:
+                        flc_manager.update_definition({"preserveLayerIds": False})
+                    new_item.delete()
+                    # Step 5: Append the data
+                    fs_item.layers[fl_index].append(
+                        item_id=item.id, upload_format="filegdb"
+                    )
+                    return self._gis.content.get(fs_id)
+                else:
+                    return new_item
             elif has_pyshp:
                 import random
                 import string
@@ -7101,6 +7166,49 @@ class ContentManager(object):
                     folder=folder,
                 )
                 shutil.rmtree(temp_dir, ignore_errors=True)
+                # If overwrite specified, perform overwrite on existing Feature Layer
+                overwrite = kwargs.pop("overwrite", None)
+                if overwrite:
+                    # Get user defined parameters
+                    fs_dict = kwargs.pop("service", None)
+                    if fs_dict is None:
+                        raise ValueError(
+                            "If overwite is True, then the feature service id and layer index need to be specified in the `service` parameter."
+                        )
+                    fs_id = fs_dict["featureServiceId"]
+                    fl_index = fs_dict["layer"]
+
+                    # Create the feature layer manager from the kwargs
+                    fs_item = self._gis.content.get(fs_id)
+                    flc = FeatureLayerCollection.fromitem(fs_item)
+                    flc_manager = flc.manager
+
+                    # Analyze the shapefile item to get definition
+                    publish_parameters_orig = flc_manager.properties["layers"][fl_index]
+                    publish_parameters = self._gis.content.analyze(
+                        item=item, file_type="shapefile"
+                    )["publishParameters"]["layers"][0]
+                    # Update to get all correct parameters to add to definition
+                    publish_parameters.update(publish_parameters_orig)
+                    publish_parameters["id"] = fl_index
+
+                    # Perform edit on the feature layer collection using the manager
+                    revert = False
+                    if flc_manager.properties["preserveLayerIds"] is not True:
+                        flc_manager.update_definition({"preserveLayerIds": True})
+                        revert = True
+                    flc_manager.delete_from_definition({"layers": [{"id": fl_index}]})
+                    flc_manager.add_to_definition({"layers": [publish_parameters]})
+                    if revert:
+                        flc_manager.update_definition({"preserveLayerIds": False})
+
+                    # Append the features
+                    fs_item.layers[fl_index].append(
+                        item_id=item.id, upload_format="shapefile"
+                    )
+
+                    return self._gis.content.get(fs_id)
+                # Publish as new Feature Layer
                 publish_parameters = {
                     "hasStaticData": True,
                     "name": os.path.splitext(item["name"])[0],
