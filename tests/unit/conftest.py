81133c12194ac39c3aec53c4d7a205d181025b6e
Shuang Song <103439749+cariashuang0417@users.noreply.github.com>
2022-08-05 15:42:42 -0400

    add unittest class and main function for all unit tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -6,5 +6,5 @@ THIS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 TESTS_DIR = os.path.join(THIS_DIR, "..")
 
 
-def pytest_configure():
+def test_configure():
     sys.path.append(TESTS_DIR)
--- tests/unit/test_arcgis/test_widgets/test_MapView/test_portal_url.py
+++ tests/unit/test_arcgis/test_widgets/test_MapView/test_portal_url.py
@@ -1,31 +1,36 @@
+import unittest
 from unittest.mock import patch, MagicMock
 
 from utils.mocks import MockMapView
 from arcgis.widgets import MapView
 
 
-def test_portal_url_format():
-    _test_portal_url(
-        mock_rest_url="https://pythonapi.playground.esri.com/portal/sharing/rest/",
-        expected="https://pythonapi.playground.esri.com/portal/",
-    )
+class TestPortalUrl(unittest.TestCase):
+
+    def test_portal_url_format(self):
+        self._test_portal_url(
+            mock_rest_url="https://pythonapi.playground.esri.com/portal/sharing/rest/",
+            expected="https://pythonapi.playground.esri.com/portal/",
+        )
+
+    def test_agol_standard(self):
+        self._test_portal_url(
+            mock_rest_url="https://www.arcgis.com/sharing/rest/",
+            expected="https://www.arcgis.com/",
+        )
+
+    def test_agol_subdomain(self):
+        self._test_portal_url(
+            mock_rest_url="https://geosaurus.maps.arcgis.com/sharing/rest/",
+            expected="https://geosaurus.maps.arcgis.com/",
+        )
+
+    def _test_portal_url(self, mock_rest_url, expected):
+        mock_mapview = MockMapView()
+        mock_mapview.gis._portal.resturl = mock_rest_url
+        assert expected == MapView._get_portal_url(mock_mapview)
 
 
-def test_agol_standard():
-    _test_portal_url(
-        mock_rest_url="https://www.arcgis.com/sharing/rest/",
-        expected="https://www.arcgis.com/",
-    )
+if __name__ == "__main__":
 
-
-def test_agol_subdomain():
-    _test_portal_url(
-        mock_rest_url="https://geosaurus.maps.arcgis.com/sharing/rest/",
-        expected="https://geosaurus.maps.arcgis.com/",
-    )
-
-
-def _test_portal_url(mock_rest_url, expected):
-    mock_mapview = MockMapView()
-    mock_mapview.gis._portal.resturl = mock_rest_url
-    assert expected == MapView._get_portal_url(mock_mapview)
+    unittest.main()
