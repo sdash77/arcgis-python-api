fe15b4179894f12f3aca1189e9a5f575f5cb1b79
Shuang Song <103439749+cariashuang0417@users.noreply.github.com>
2022-08-25 17:34:06 -0400

    change pytest to unittest for geoenrichment tests (except business analysis part)

--- tests/integration/geoenrichment/test_introspection.py
+++ tests/integration/geoenrichment/test_introspection.py
@@ -1,3 +1,5 @@
+import unittest
+
 from arcgis.gis import GIS
 from arcgis.geoenrichment import get_countries, Country
 import pandas as pd
@@ -35,33 +37,41 @@ def get_country_levels_test(cntry: Country, expectation: object) -> None:
         assert len(res.index)
 
 
-# local
-@skip_if_no_local
-def test_get_countries_local(gis_pro):
-    get_countries_test(gis_pro, does_not_raise())
+class TestIntrospection(unittest.TestCase):
+
+    # local
+    @skip_if_no_local
+    def test_get_countries_local(self):
+        gis_pro_inst = gis_pro()
+        get_countries_test(gis_pro_inst, does_not_raise())
+
+    @skip_if_no_local
+    def test_get_enrich_variables_local(self):
+        usa_local_inst = usa_local()
+        get_enrich_variables_test(usa_local_inst, does_not_raise())
+
+    @skip_if_no_local
+    def test_get_country_levels_local(self):
+        usa_local_inst = usa_local()
+        get_country_levels_test(usa_local_inst, does_not_raise())
+
+    # ArcGIS Online
+    @skip_if_no_agol
+    def test_get_countries_agol(self):
+        gis_agol_inst = gis_agol()
+        get_countries_test(gis_agol_inst, does_not_raise())
+
+    @skip_if_no_agol
+    def test_get_enrich_variables_agol(self):
+        usa_agol_inst = usa_agol()
+        get_enrich_variables_test(usa_agol_inst, does_not_raise())
+
+    @skip_if_no_agol
+    def test_get_country_levels_agol(self):
+        usa_agol_inst = usa_agol()
+        get_country_levels_test(usa_agol_inst, does_not_raise())
 
 
-@skip_if_no_local
-def test_get_enrich_variables_local(usa_local):
-    get_enrich_variables_test(usa_local, does_not_raise())
+if __name__ == "__main__":
 
-
-@skip_if_no_local
-def test_get_country_levels_local(usa_local):
-    get_country_levels_test(usa_local, does_not_raise())
-
-
-# ArcGIS Online
-@skip_if_no_agol
-def test_get_countries_agol(gis_agol):
-    get_countries_test(gis_agol, does_not_raise())
-
-
-@skip_if_no_agol
-def test_get_enrich_variables_agol(usa_agol):
-    get_enrich_variables_test(usa_agol, does_not_raise())
-
-
-@skip_if_no_agol
-def test_get_country_levels_agol(usa_agol):
-    get_country_levels_test(usa_agol, does_not_raise())
+    unittest.main()
