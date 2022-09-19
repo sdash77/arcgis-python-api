ee194d0067d70c81931aceb09a303f12f343b857
Rohit Thakur <1297rohit@gmail.com>
2022-09-19 16:15:21 +0530

    Class imbalance for Imagery and Tabular Models (#8834)
    * tabular data class imbalance
    
    * added warning for PASCAL_VOC_rectangles and KITTI_rectangles
    
    * added warning for RCNN_Masks
    
    * added warning for Classified_Tiles, Labeled_Tiles and Imagenet
    
    * moved logic to common function inside utils
    
    * added check imbalance logic inside try catch
    
    * code fix
    
    * bug fixes and code refactoring

--- src/arcgis/learn/_utils/utils.py
+++ src/arcgis/learn/_utils/utils.py
@@ -1,3 +1,6 @@
+import warnings
+
+
 def extract_zipfile(filepath, filename, remove=False):
     """Function to extract the contents of a zip file
     Args:
@@ -26,3 +29,25 @@ def arcpy_localization_helper(msg, id, msg_type="ERROR", param=None):
                 pass
     except:
         return msg
+
+
+def check_imbalance(total_sample, unique_sample, class_imbalance_pct, stratify):
+    """Function to check class imabalance in the data
+    Args:
+        total_sample: Total number of samples combining all the class(es).
+        unique_sample: Class(es) in the data.
+        class_imbalance_pct: Percentage of data for each class to consider for imbalance
+    """
+    imabalanced_class_list = []
+    for sample in unique_sample:
+        if (total_sample == sample).sum() < len(total_sample) * class_imbalance_pct:
+            imabalanced_class_list.append(sample)
+
+    if stratify == True and len(imabalanced_class_list) > 0:
+        warnings.warn(
+            f'We see a class imbalance in the dataset. The class(es) {",".join(imabalanced_class_list)} does not have enough data points in your dataset.'
+        )
+    elif stratify == False and len(imabalanced_class_list) > 0:
+        warnings.warn(
+            f'We see a class imbalance in the dataset. The class(es) {",".join(imabalanced_class_list)} does not have enough data points in your dataset. Although, class imbalance cannot be overcome easily, adding the parameter stratify = True will to a certain extent help get over this problem.'
+        )
