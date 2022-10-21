9275df6f80609821ee8d57f8a2755f83fe82cc15
NANAE AUBRY <nanaeaubry@gmail.com>
2022-10-21 12:14:38 -0700

    format with black

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -7497,18 +7497,6 @@ class ContentManager(object):
             title = kwargs.pop("title", uuid4().hex)
             tags = kwargs.pop("tags", "CSV")
 
-            if kwargs.get("geocode_url", None):
-                geocode_url = kwargs.get("geocode_url")
-            else:
-                locators = [
-                    gc["url"]
-                    for gc in self._gis.properties.helperServices.geocode
-                    if gc.get("batch", False)
-                ]
-                if len(locators) == 0:
-                    raise Exception("No batch geocoding service found.")
-                geocode_url = locators[0]
-
             # Step 1: Add the csv as an item
             temp_file = tempfile.gettempdir() + "\\%s%s.csv" % (
                 random.choice(string.ascii_lowercase),
@@ -7540,6 +7528,18 @@ class ContentManager(object):
             published_item = csv_item.publish(publish_parameters)
             return published_item
         elif isinstance(df, pd.DataFrame) and "location_type" in kwargs:
+            if kwargs.get("geocode_url", None):
+                geocode_url = kwargs.get("geocode_url")
+            else:
+                locators = [
+                    gc["url"]
+                    for gc in self._gis.properties.helperServices.geocode
+                    if gc.get("batch", False)
+                ]
+                if len(locators) == 0:
+                    raise Exception("No batch geocoding service found.")
+                geocode_url = locators[0]
+
             path = "content/features/analyze"
 
             postdata = {
