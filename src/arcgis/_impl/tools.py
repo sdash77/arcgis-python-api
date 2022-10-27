6e5a87a5d456cd41f6311f7c2e0187557e18f44b
NANAE AUBRY <nanaeaubry@gmail.com>
2022-10-27 11:35:19 -0700

    fix doc typo

--- src/arcgis/_impl/tools.py
+++ src/arcgis/_impl/tools.py
@@ -6158,7 +6158,7 @@ class _FeatureAnalysisTools(BaseAnalytics):
 
                                 - ``extent`` - a bounding box that defines the analysis area. Only those features in the input_layer that intersect the bounding box will be analyzed.
                                 - ``outSR`` - the output features will be projected into the output spatial reference referred to by the `wkid`.
-                                - ``overwrite`` - if True, then the feature layer in output_name will be overwritten with new feature layer. Available for ArcGIS Online Only and Enterprise 11+.
+                                - ``overwrite`` - if True, then the feature layer in output_name will be overwritten with new feature layer. Available for ArcGIS Online and ArcGIS Enterprise 11+.
 
                                     .. code-block:: python
 
