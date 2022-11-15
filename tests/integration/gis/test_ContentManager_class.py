3593a3450f93cdb6228c0d8532ee898e5f858c53
NANAE AUBRY <nanaeaubry@gmail.com>
2022-11-15 15:03:17 -0800

    format with black

--- tests/integration/gis/test_ContentManager_class.py
+++ tests/integration/gis/test_ContentManager_class.py
@@ -88,7 +88,7 @@ class Test_ContentManager_portal_builtin(unittest.TestCase):
         if not r1:
             cls.class_skip = True
 
-        cls.gis = GIS(profile="your_online_profile")
+        cls.gis = GIS(cls.portal_url, cls.portal_username, cls.portal_password)
         if cls.gis is None:
             cls.class_skip = True
 
