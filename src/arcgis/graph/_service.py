cfa8b3903aa18b0b0b97b4364fb3ef4988c23416
Andrew <achapkowski@esri.com>
2022-12-12 14:42:42 -0500

    fix black format issue.

--- src/arcgis/graph/_service.py
+++ src/arcgis/graph/_service.py
@@ -90,7 +90,7 @@ class KnowledgeGraph:
         ================    ===============================================================
 
         .. note::
-            Check the `service definition for the Knowledge Graph service <https://developers.arcgis.com/rest/services-reference/enterprise/kgs-hosted-server.htm>`_  
+            Check the `service definition for the Knowledge Graph service <https://developers.arcgis.com/rest/services-reference/enterprise/kgs-hosted-server.htm>`_
             for valid values of category. Not all services support both.
 
         .. code-block:: python
