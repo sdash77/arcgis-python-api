e6868c585d3facae3691e62977519f2c5dd5f862
Andrew <achapkowski@esri.com>
2023-01-11 11:00:19 -0500

    format fix

--- src/arcgis/features/managers.py
+++ src/arcgis/features/managers.py
@@ -81,7 +81,10 @@ class WebHookScheduleInfo:
         return {
             "name": self.name,
             "startAt": int(self.start_at.timestamp() * 1000),
-            "recurrenceInfo": {"frequency": self.frequency, "interval": self.interval},
+            "recurrenceInfo": {
+                "frequency": self.frequency,
+                "interval": self.interval,
+            },
         }
 
 
@@ -110,7 +113,9 @@ class AttachmentManager(object):
     """
 
     def __init__(
-        self, layer: features.FeatureLayer, version: str | _version.Version = None
+        self,
+        layer: features.FeatureLayer,
+        version: str | _version.Version = None,
     ):
         self._layer = layer
         if isinstance(version, str):
@@ -704,7 +709,11 @@ class AttachmentManager(object):
         )
 
     def update(
-        self, oid: str, attachment_id: str, file_path: str, return_moment: bool = False
+        self,
+        oid: str,
+        attachment_id: str,
+        file_path: str,
+        return_moment: bool = False,
     ) -> bool:
         """
         Updates an existing attachment with a new file
@@ -1793,7 +1802,7 @@ class FeatureLayerCollectionManager(_GISResource):
         :return:
             :class:`~arcgis.features.managers.WebHookServiceManager`
         """
-        if self._gis.version >= [8, 2] and self._gis._portal.is_arcgisonline:
+        if self._gis.version >= [8, 2]:
             if self._wh is None:
                 self._wh = WebHookServiceManager(
                     url=self._url + "/WebHooks", fc=self._fs, gis=self._gis
