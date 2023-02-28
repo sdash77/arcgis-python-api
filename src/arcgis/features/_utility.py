6d09101b8f51eaaa1a4fb1b6079c1b6bcb67be5a
NANAE AUBRY <nanaeaubry@gmail.com>
2023-02-28 09:47:24 -0800

    update to version 2.2.0

--- src/arcgis/features/_utility.py
+++ src/arcgis/features/_utility.py
@@ -516,7 +516,7 @@ class UtilityNetworkManager(object):
         return self._con.post(url, params)
 
     # ----------------------------------------------------------------------
-    @deprecated(deprecated_in="2.1.0", removed_in=None, current_version="2.1.0")
+    @deprecated(deprecated_in="2.1.0", removed_in=None, current_version="2.2.0")
     def query_overrides(
         self,
         attribute_ids: Optional[list[str]] = None,
@@ -776,7 +776,7 @@ class UtilityNetworkManager(object):
         return self._con.post(url, params)
 
     # ----------------------------------------------------------------------
-    @deprecated(deprecated_in="2.1.0", removed_in=None, current_version="2.1.0")
+    @deprecated(deprecated_in="2.1.0", removed_in=None, current_version="2.2.0")
     def apply_overrides(
         self,
         adds: Optional[Union[list, dict[str, Any]]] = None,
