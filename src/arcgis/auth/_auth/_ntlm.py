d958de60154136da8d7cd9872b76e03a7ddedd8f
Andrew <achapkowski@esri.com>
2023-03-31 13:14:50 -0400

    reformat code.

--- src/arcgis/auth/_auth/_ntlm.py
+++ src/arcgis/auth/_auth/_ntlm.py
@@ -45,9 +45,7 @@ class EsriHttpNtlmAuth(AuthBase):
     Supports pass-the-hash.
     """
 
-    def __init__(
-        self, username, password, session=None, send_cbt=True, **kwargs
-    ):
+    def __init__(self, username, password, session=None, send_cbt=True, **kwargs):
         """Create an authentication handler for NTLM over HTTP.
 
         :param str username: Username in 'domain\\username' format
@@ -81,7 +79,9 @@ class EsriHttpNtlmAuth(AuthBase):
         if parsed.port:
             server_url = f'{parsed.scheme}://{parsed.netloc}:{parsed.port}/{parsed.path[1:].split("/")[0]}'
         else:
-            server_url = f'{parsed.scheme}://{parsed.netloc}/{parsed.path[1:].split("/")[0]}'
+            server_url = (
+                f'{parsed.scheme}://{parsed.netloc}/{parsed.path[1:].split("/")[0]}'
+            )
         token_url: str = None
         if server_url in self._server_log:
             token_url: str = self._server_log[server_url]
@@ -94,9 +94,7 @@ class EsriHttpNtlmAuth(AuthBase):
                 headers={"referer": self._referer},
                 proxies=self._proxy,
             ).json()
-            self._server_log[parsed.netloc] = resp["authInfo"][
-                "tokenServicesUrl"
-            ]
+            self._server_log[parsed.netloc] = resp["authInfo"]["tokenServicesUrl"]
             token_url: str = self._server_log[parsed.netloc]
 
         if token_url:
@@ -142,8 +140,7 @@ class EsriHttpNtlmAuth(AuthBase):
         cbt = None
         if server_certificate_hash:
             cbt = spnego.channel_bindings.GssChannelBindings(
-                application_data=b"tls-server-end-point:"
-                + server_certificate_hash
+                application_data=b"tls-server-end-point:" + server_certificate_hash
             )
 
         """Attempt to authenticate using HTTP NTLM challenge/response."""
@@ -257,9 +254,7 @@ class EsriHttpNtlmAuth(AuthBase):
                 )
         elif r.status_code == 407:
             # If we didn't have server auth, do proxy auth.
-            proxy_authenticate = r.headers.get(
-                "proxy-authenticate", ""
-            ).lower()
+            proxy_authenticate = r.headers.get("proxy-authenticate", "").lower()
             auth_type = _auth_type_from_header(proxy_authenticate)
             if auth_type is not None:
                 return self.retry_using_http_NTLM_auth(
