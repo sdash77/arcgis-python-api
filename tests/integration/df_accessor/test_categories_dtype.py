9370892e4e18ed77ed6d4b1fc1acdc80b0f8244b
Andrew <achapkowski@esri.com>
2023-04-07 11:29:58 -0400

    added test for feature set and feature collections

--- tests/integration/df_accessor/test_categories_dtype.py
+++ tests/integration/df_accessor/test_categories_dtype.py
@@ -57,6 +57,42 @@ class TestCategoriesSeDF(unittest.TestCase):
             sdf.spatial.to_featureclass(out_fc)
             assert arcpy.Exists(out_fc)
 
+    def test_categories_to_featureset_featureclass(self):
+        fc = r"\\qalab_server\pydata\v109\geosaurus\df_accessor_test\world30.shp"
+        if arcpy.Exists(fc):
+            sdf = pd.DataFrame.spatial.from_featureclass(fc)
+
+            sdf["cats_str"] = [
+                "a" if i % 2 == 0 else "b" for i in range(len(sdf))
+            ]
+            sdf["cats_ints"] = [
+                1 if i % 2 == 0 else 2 for i in range(len(sdf))
+            ]
+            sdf["cats_ints"] = sdf["cats_ints"].astype("category")
+            sdf["cats_str"] = sdf["cats_str"].astype("category")
+
+            fs = sdf.spatial.to_featureset()
+            assert fs
+            assert fs.features[0]
+            assert fs.features[0].attributes['cats_str']
+
+    def test_categories_to_featurecollection(self):
+        fc = r"\\qalab_server\pydata\v109\geosaurus\df_accessor_test\world30.shp"
+        if arcpy.Exists(fc):
+            sdf = pd.DataFrame.spatial.from_featureclass(fc)
+
+            sdf["cats_str"] = [
+                "a" if i % 2 == 0 else "b" for i in range(len(sdf))
+            ]
+            sdf["cats_ints"] = [
+                1 if i % 2 == 0 else 2 for i in range(len(sdf))
+            ]
+            sdf["cats_ints"] = sdf["cats_ints"].astype("category")
+            sdf["cats_str"] = sdf["cats_str"].astype("category")
+
+            fc = sdf.spatial.to_feature_collection()
+            assert fc
+
     def test_categories_to_table(self):
         df = pd.DataFrame({"value": np.random.randint(0, 100, 20)})
         labels = ["{0} - {1}".format(i, i + 9) for i in range(0, 100, 10)]
@@ -72,6 +108,20 @@ class TestCategoriesSeDF(unittest.TestCase):
         df.spatial.to_table(table)
 
         assert arcpy.Exists(table)
+
+    def test_categories_to_featureset_table(self):
+        df = pd.DataFrame({"value": np.random.randint(0, 100, 20)})
+        labels = ["{0} - {1}".format(i, i + 9) for i in range(0, 100, 10)]
+        df["group"] = pd.cut(
+            df.value, range(0, 105, 10), right=False, labels=labels
+        )
+        df["cats_ints"] = [
+            1 if i % 2 == 0 else 2 for i in range(len(df['group']))
+        ]
+        df["cats_ints"] = df["cats_ints"].astype("category")
+        fs = df.spatial.to_featureset()
+        assert fs
+        assert fs.features[0]
 
 
 if __name__ == "__main__":
