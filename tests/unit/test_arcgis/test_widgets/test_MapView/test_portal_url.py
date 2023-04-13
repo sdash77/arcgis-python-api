2c82cdc650fb0743fecc26b8c595ade1e413b9ec
NANAE AUBRY <nanaeaubry@gmail.com>
2023-04-13 15:07:55 -0700

    Fix url smoke test

--- tests/unit/test_arcgis/test_widgets/test_MapView/test_portal_url.py
+++ tests/unit/test_arcgis/test_widgets/test_MapView/test_portal_url.py
@@ -10,19 +10,19 @@ class TestPortalUrl(unittest.TestCase):
     def test_portal_url_format(self):
         self._test_portal_url(
             mock_rest_url="https://pythonapi.playground.esri.com/portal/sharing/rest/",
-            expected="https://pythonapi.playground.esri.com/portal/",
+            expected="https://pythonapi.playground.esri.com/portal",
         )
 
     def test_agol_standard(self):
         self._test_portal_url(
             mock_rest_url="https://www.arcgis.com/sharing/rest/",
-            expected="https://www.arcgis.com/",
+            expected="https://www.arcgis.com",
         )
 
     def test_agol_subdomain(self):
         self._test_portal_url(
             mock_rest_url="https://geosaurus.maps.arcgis.com/sharing/rest/",
-            expected="https://geosaurus.maps.arcgis.com/",
+            expected="https://geosaurus.maps.arcgis.com",
         )
 
     def _test_portal_url(self, mock_rest_url, expected):
