087c11897d13c7d6361d26dc556db958d51c3de8
NANAE AUBRY <nanaeaubry@gmail.com>
2023-04-14 07:47:57 -0700

    update mapview url tests

--- tests/unit/test_arcgis/test_widgets/test_MapView/test_portal_url.py
+++ tests/unit/test_arcgis/test_widgets/test_MapView/test_portal_url.py
@@ -1,34 +1,36 @@
-import unittest
-from unittest.mock import patch, MagicMock
+import sys
 
-from utils.mocks import MockMapView
-from arcgis.widgets import MapView
+sys.path.insert(0, r"C:\ipython_workfolder\geosaurus\src")
+sys.path.insert(1, r"C:\ipython_workfolder\geosaurus\tests")
+
+import unittest
+from arcgis.gis import GIS
 
 
 class TestPortalUrl(unittest.TestCase):
 
     def test_portal_url_format(self):
         self._test_portal_url(
-            mock_rest_url="https://pythonapi.playground.esri.com/portal/sharing/rest/",
+            profile="your_enterprise_profile",
             expected="https://pythonapi.playground.esri.com/portal",
         )
 
     def test_agol_standard(self):
         self._test_portal_url(
-            mock_rest_url="https://www.arcgis.com/sharing/rest/",
+            profile=None,
             expected="https://www.arcgis.com",
         )
 
     def test_agol_subdomain(self):
         self._test_portal_url(
-            mock_rest_url="https://geosaurus.maps.arcgis.com/sharing/rest/",
+            profile="your_online_profile",
             expected="https://geosaurus.maps.arcgis.com",
         )
 
-    def _test_portal_url(self, mock_rest_url, expected):
-        mock_mapview = MockMapView()
-        mock_mapview.gis._portal.resturl = mock_rest_url
-        assert expected == MapView._get_portal_url(mock_mapview)
+    def _test_portal_url(self, profile, expected):
+        gis = GIS(profile=profile)
+        mv = gis.map()
+        assert expected == mv._get_portal_url()
 
 
 if __name__ == "__main__":
