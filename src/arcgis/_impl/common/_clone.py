01b2019b0202cb2a0a0f9079789c16bad6f05fab
Andrew <achapkowski@esri.com>
2023-04-25 08:07:23 -0400

    fixes portal_item being passed as null which prevents cloning of experience builder from 10.9 to 10.9.1 and preserving the Item ID.

--- src/arcgis/_impl/common/_clone.py
+++ src/arcgis/_impl/common/_clone.py
@@ -1669,10 +1669,10 @@ class _DeepCloner:
                 dict(item),
                 data=None,
                 thumbnail=None,
-                portal_item=item,
                 folder=self.folder,
                 search_existing=self._search_existing_items,
                 owner=self.owner,
+                portal_item=item,
                 preserve_item_id=self._preserve_item_id,
             )
 
@@ -2497,7 +2497,8 @@ class _FeatureServiceDefinition(_TextItemDefinition):
         """Get the features for the given feature layer of a feature service. Returns a list of json features.
         Keyword arguments:
         feature_layer - The feature layer to return the features for
-        spatial_reference -  The spatial reference to return the features in"""
+        spatial_reference -  The spatial reference to return the features in
+        """
         if spatial_reference is None:
             spatial_reference = {"wkid": 102100}
 
@@ -2533,7 +2534,8 @@ class _FeatureServiceDefinition(_TextItemDefinition):
         layers - Dictionary containing the id of the layer and its corresponding arcgis.lyr.FeatureLayer
         relationships - Dictionary containing the id of the layer and its relationship definitions
         layer_field_mapping - field mapping if the case or name of field changed from the original service
-        spatial_reference -  The spatial reference to create the features in"""
+        spatial_reference -  The spatial reference to create the features in
+        """
 
         # Get the features if they haven't already been queried
         features = self.features
@@ -6836,7 +6838,8 @@ def _zip_dir(path, zip_file, include_root=True):
     Keyword arguments:
     path - The folder containing the files and subfolders to zip
     zip_file - The zip file that will store the compressed files
-    include_root -  Indicates if the root folder should be included in the zip"""
+    include_root -  Indicates if the root folder should be included in the zip
+    """
 
     rel_path = ""
     if include_root:
