96ff5ada8266b21f00668eb2e1b5f87f1231b19b
NANAE AUBRY <nanaeaubry@gmail.com>
2023-05-09 12:43:54 +0200

    fix documentation and run black

--- src/arcgis/geoenrichment/enrichment.py
+++ src/arcgis/geoenrichment/enrichment.py
@@ -1240,13 +1240,15 @@ def create_report(
                            Example
 
                            Creating a new output in a Portal for ArcGIS Instance:
-
-                           | return_type = {'user' : 'testUser',
-                           |               'folder' : 'FolderName',
-                           |               'title' : 'Report Title',
-                           |               'item_properties' : '<properties>',
-                           |               'url' : '``https://hostname.domain.com/webadaptor``',
-                           |               'token' : 'token', 'referrer' : 'referrer'}
+                                return_type = {
+                                    "user": "testUser",
+                                    "folder": "FolderName",
+                                    "title": "Report Title",
+                                    "item_properties": {...},
+                                    "url": "https://hostname.domain.com/webadaptor",
+                                    "token": "...",
+                                    "referrer": "..."
+                                }
     ------------------     --------------------------------------------------------------------
     use_data               Optional dictionary. This parameter explicitly specify the country
                            or dataset to query. When all input features specified in the
@@ -1507,40 +1509,46 @@ def enrich(
     study_areas                   Required list, dictionary, :class:`~arcgis.features.FeatureSet`
                                   or SpatiallyEnabledDataFrame containing the input areas to be enriched.
 
-                                  study_areas can be a SpatiallyEnabledDataFrame, :class:`~arcgis.features.FeatureSet` or a
+                                  `study_areas` can be a Spatially Enabled DataFrame, :class:`~arcgis.features.FeatureSet` or a
                                   lists of the following types:
 
-                                  * addresses, points of interest, place names or other
-                                  supported locations as strings.
+                                  * addresses, points of interest, place names or other supported locations as strings.
 
-                                  * dicts such as [{"address":{"Address":"380 New York St.",
-                                  "Admin1":"Redlands","Admin2":"CA","Postal":"92373",
-                                  "CountryCode":"USA"}}] for multiple field addresses
+                                  * dicts for multiple field addresses such as:
+                                    [{
+                                        "address":{
+                                            "Address":"380 New York St.",
+                                            "Admin1":"Redlands",
+                                            "Admin2":"CA",
+                                            "Postal":"92373",
+                                            "CountryCode":"USA"}
+                                    }]
 
                                   * :class:`~arcgis.geometry.Geometry` instances
 
                                   * BufferStudyArea instances. By default, one-mile ring
-                                  buffers are created around the points to collect and append
-                                  enrichment data. You can use BufferStudyArea to change the ring
-                                  buffer size or create drive-time service areas around the points.
+                                    buffers are created around the points to collect and append
+                                    enrichment data. You can use BufferStudyArea to change the ring
+                                    buffer size or create drive-time service areas around the points.
 
                                   * NamedArea instances to support standard geography. They are
-                                  obtained using Country.subgeographies()/search(). When
-                                  the NamedArea instances should be combined together (union), a list
-                                  of such NamedArea instances should constitute a study area in the
-                                  list of requested study areas. Otherwise, pass in the result of subgeographies
-                                  as a dictionary.
+                                    obtained using Country.subgeographies()/search(). When
+                                    the NamedArea instances should be combined together (union), a list
+                                    of such NamedArea instances should constitute a study area in the
+                                    list of requested study areas. Otherwise, pass in the result of subgeographies
+                                    as a dictionary.
 
-                                    .. code-block:: python
-                                        usa = Country("USA")
-                                        ca_counties = usa.subgeographies.states['California'].counties
+                                        .. code-block:: python
 
-                                        # Pass as a dictionary
-                                        counties_df = enrich(study_areas=ca_counties, data_collections=['Age'])
-                                        counties_df
+                                            usa = Country("USA")
+                                            ca_counties = usa.subgeographies.states['California'].counties
 
-                                        # Pass as a list
-                                        counties_df = enrich(study_areas=list(ca_counties.values()), data_collections=['Age'])
+                                            # Pass as a dictionary
+                                            counties_df = enrich(study_areas=ca_counties, data_collections=['Age'])
+                                            counties_df
+
+                                            # Pass as a list
+                                            counties_df = enrich(study_areas=list(ca_counties.values()), data_collections=['Age'])
     -------------------------     --------------------------------------------------------------------
     data_collections              Optional list. A Data Collection is a preassembled list of
                                   attributes that will be used to enrich the input features.
