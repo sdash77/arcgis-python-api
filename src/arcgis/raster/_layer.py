3c64e1a6c665bfd904631eb6fca016248a2694da
kreeti chandran <kree9547@esri.com>
2023-05-15 14:34:40 +0400

    doc updates to temporal profile

--- src/arcgis/raster/_layer.py
+++ src/arcgis/raster/_layer.py
@@ -6427,8 +6427,8 @@ class ImageryLayer(Layer):
         variables: list[str] = [],
         bands: list[int] = [0],
         time_extent: Optional[list[datetime.datetime]] = None,
-        dimension: Optional[list[float]] = None,
-        dimension_values: list = [],
+        dimension: Optional[list[str]] = None,
+        dimension_values: Optional[list[float]] = [],
         show_values: bool = False,
         trend_type: Optional[str] = None,
         trend_order: Optional[int] = None,
@@ -6465,19 +6465,19 @@ class ImageryLayer(Layer):
                                                  If not specified the time field is obtained from the timeInfo of
                                                  the image service.
         ------------------------------------     --------------------------------------------------------------------
-        variables                                Required list of variable names.
+        variables                                Required list of strings. The variables that will be used for plotting temporal profile.
                                                  For non multidimensional data, the variable would be name of the Sensor.
                                                  To plot the graph against all sensors specify - "ALL_SENSORS"
         ------------------------------------     --------------------------------------------------------------------
-        bands                                    Optional list of band indices. By default takes the
-                                                 first band (band index - 0).
+        bands                                    Optional list of integers. Band indices to be used for plotting temporal profile. 
+                                                 By default takes the first band (band index - 0).
                                                  For a multiband data, you can compare the time change of different
                                                  bands over different locations.
         ------------------------------------     --------------------------------------------------------------------
-        time_extent                              Optional list of date time object. This represents the time extent
+        time_extent                              Optional list of datetime objects. This represents the time extent.
         ------------------------------------     --------------------------------------------------------------------
-        dimension                                Optional list of dimension names. This option works specifically on
-                                                 multidimensional data containing a time dimension and other dimensions.
+        dimension                                Optional list of strings. The dimension names that will be used for plotting temporal profile. 
+                                                 This option works specifically on multidimensional data containing a time dimension and other dimensions.
 
                                                  The temporal profile is created based on the specific values in other
                                                  dimensions, such as depth at the corresponding time value. For example,
@@ -6485,7 +6485,7 @@ class ImageryLayer(Layer):
                                                  dimension below the earth's surface, resulting in a temporal profile
                                                  at 0.1, 0.2, and 0.3 meters below the ground.
         ------------------------------------     --------------------------------------------------------------------
-        dimension_values                         Optional list of dimension values. This parameter can be used to specify
+        dimension_values                         Optional list of floats. This parameter can be used to specify
                                                  the values of dimension parameter other than the time dimension (dimension
                                                  name specified using dimension parameter)
         ------------------------------------     --------------------------------------------------------------------
@@ -7007,7 +7007,7 @@ class ImageryLayer(Layer):
         ====================================     ====================================================================
         **Parameter**                             **Description**
         ------------------------------------     --------------------------------------------------------------------
-        dimension                                Required string. Dimension name. Use this parameter to set the field that 
+        dimension                                Required string. The dimension name that will be used for plotting dimension profile. Use this parameter to set the field that 
                                                  represents the dimension field in the image service.
         ------------------------------------     --------------------------------------------------------------------
         time                                     Required datetime object. The time slice that will be used for plotting dimension profile.
