d2afff72e8ac7d53657ae638095f0bf51e688f75
Rahul Deo <52111992+r-deo@users.noreply.github.com>
2023-05-26 14:36:21 +0530

    Lstm fix (#9636)
    * New code for multivariate data
    
    * Removed print statement and refactored the code
    
    * black formatting
    
    * Change in docstrings as per modified changes
    
    * Fixed minor bug in show result
    
    * Changed to fix bug 9555 and other modification in index calculation
    
    * Formatting as per black 23.1
    
    * Refactored the code
    
    * Changes for multistep
    
    * Added the code for multi-step prediction.
    
    * Added changes for pandas native datatype
    
    * Removed the print statement and changed the graph generation during model save
    
    * New changes to handle categorical data
    
    * Added Support for categorical and Number of prediction issue handling
    
    * Changed LSTM module to add batching support and multivariate support
    
    * Changed the default assignment of transform
    
    * Removed ml_data changes
    
    * add codes from multistep branch
    
    * Fixed normalize and preprocessing bug
    
    * Fixed data conversion bug in _ml_data
    
    * Changed LSTM module
    
    * Changed show_results to accommodate categorical value
    
    * Added tsdev code changes
    
    * Added changes to remove warning
    
    * docstring changes
    
    * Changes to preserve the order of columns in show_result
    
    * Changes for emd param and categorical variable infer
    
    * typo correction
    
    * docstring changes
    
    * warning statement changes
    
    * warning statement changes
    
    * Fixed dropout warning coming from Pytorch
    
    ---------
    
    Co-authored-by: Rahul Deo <rdeo@esri.com>

--- src/arcgis/learn/_utils/tabular_data.py
+++ src/arcgis/learn/_utils/tabular_data.py
@@ -2436,3 +2436,4 @@ def global_interpretation(model, plot_type="bar", method="KernelRegressor"):
             explainer.expected_value[0], shap_values, df, matplotlib=True
         )
     return
+
--- src/arcgis/learn/models/_tsmodel_archs/_LSTM.py
+++ src/arcgis/learn/models/_tsmodel_archs/_LSTM.py
@@ -3,34 +3,45 @@ import torch.nn as nn
 
 
 class _TSLSTM(nn.Module):
-    def __init__(self, input_size=1, output_size=1, device=None, hidden_layer_size=100):
-        super().__init__()
-        self.hidden_layer_size = hidden_layer_size
-
-        self.lstm = nn.LSTM(input_size, hidden_layer_size)
-
-        self.linear = nn.Linear(hidden_layer_size, output_size)
+    def __init__(self, input_size=1, output_size=1, device=None, hidden_layer_size=100, num_layers=1, dropout=0.2):
+        super(_TSLSTM, self).__init__()
         self.device = device
-        self.hidden_cell = (
-            torch.zeros(1, 1, self.hidden_layer_size).to(device),
-            torch.zeros(1, 1, self.hidden_layer_size).to(device),
-        )
+        self.input = input_size
+        self.hidden = hidden_layer_size
+        self.num_layers = num_layers
+        if self.num_layers > 1:
+            self.lstm = nn.LSTM(
+                input_size=self.input,
+                hidden_size=self.hidden,
+                num_layers=self.num_layers,
+                batch_first=True,
+                bidirectional=True,
+                dropout=dropout
+            )
+        else:
+            self.lstm = nn.LSTM(
+                input_size=self.input,
+                hidden_size=self.hidden,
+                num_layers=self.num_layers,
+                batch_first=True,
+                bidirectional=True,
+            )
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(2 * self.hidden, output_size)
 
     def forward(self, input_seq):
-        outputs = []
-        for batch in input_seq:
-            batch = batch.squeeze()
-            self.hidden_cell = (
-                torch.zeros(1, 1, self.hidden_layer_size).to(self.device),
-                torch.zeros(1, 1, self.hidden_layer_size).to(self.device),
-            )
-
-            lstm_out, self.hidden_cell = self.lstm(
-                batch.view(len(batch), 1, -1).to(self.device), self.hidden_cell
-            )
-            predictions = self.linear(lstm_out.view(len(batch), -1))
-            outputs += [predictions[-1]]
-
-        outputs = torch.stack(outputs, 1)
-
+        input_seq = input_seq.permute(0, 2, 1)
+        batch_size = input_seq.size(0)
+        x = input_seq.to(torch.float)
+        h0 = torch.zeros(2 * self.num_layers, batch_size, self.hidden).to(
+            self.device
+        )
+        c0 = torch.zeros(2 * self.num_layers, batch_size, self.hidden).to(
+            self.device
+        )
+        out, (h0, c0) = self.lstm(x, (h0, c0))
+        h_comb = torch.cat(
+            [h0[: self.num_layers], h0[self.num_layers:]], dim=-1
+        )[-1]
+        outputs = self.linear(h_comb)
         return outputs.to(self.device)
