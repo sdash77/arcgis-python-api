50cfadd497a8cedcbf5c2955320b5112944d5974
NANAE AUBRY <nanaeaubry@gmail.com>
2023-06-05 09:57:57 +0200

    format with black

--- src/arcgis/_impl/tools.py
+++ src/arcgis/_impl/tools.py
@@ -8490,7 +8490,10 @@ class _OrthoMappingTools:
                         {"itemId": output_ortho_image_result.itemid}
                     )
                 else:
-                    (output_ortho_image, output_service,) = self._set_output_raster(
+                    (
+                        output_ortho_image,
+                        output_service,
+                    ) = self._set_output_raster(
                         output_name=output_ortho_image,
                         task=task,
                         output_properties=kwargs,
@@ -9852,7 +9855,10 @@ class _RasterAnalysisTools(BaseAnalytics):
                     input_barrier_raster_or_features
                 )
 
-        (output_distance_raster, output_distance_service,) = self._set_output_raster(
+        (
+            output_distance_raster,
+            output_distance_service,
+        ) = self._set_output_raster(
             output_name=output_distance_name,
             task=task,
             output_properties=kwargs,
@@ -10007,7 +10013,10 @@ class _RasterAnalysisTools(BaseAnalytics):
         if "context" in context_param.keys():
             context = context_param["context"]
 
-        (output_distance_raster, output_distance_service,) = self._set_output_raster(
+        (
+            output_distance_raster,
+            output_distance_service,
+        ) = self._set_output_raster(
             output_name=output_name, task=task, output_properties=kwargs
         )
 
@@ -10602,7 +10611,10 @@ class _RasterAnalysisTools(BaseAnalytics):
                         {"itemId": image_collection_result.itemid}
                     )
                 else:
-                    (image_collection, output_service,) = self._set_output_raster(
+                    (
+                        image_collection,
+                        output_service,
+                    ) = self._set_output_raster(
                         output_name=image_collection,
                         task=task,
                         output_properties=kwargs,
@@ -10639,7 +10651,11 @@ class _RasterAnalysisTools(BaseAnalytics):
 
         md_data_info = []
         if (isinstance(raster_type_name, str)) and raster_type_name == "mosaic_dataset":
-            (input_rasters, raster_type, md_data_info,) = self._build_param_dictionary(
+            (
+                input_rasters,
+                raster_type,
+                md_data_info,
+            ) = self._build_param_dictionary(
                 input_rasters=input_rasters,
                 raster_type_name=raster_type_name,
                 raster_type_params=raster_type_params,
@@ -11929,7 +11945,10 @@ class _RasterAnalysisTools(BaseAnalytics):
 
         output_drop_raster = None
         if output_drop_name is not None:
-            (output_drop_raster, output_drop_service,) = self._set_output_raster(
+            (
+                output_drop_raster,
+                output_drop_service,
+            ) = self._set_output_raster(
                 output_name=output_drop_name,
                 task=task,
                 output_properties=kwargs,
