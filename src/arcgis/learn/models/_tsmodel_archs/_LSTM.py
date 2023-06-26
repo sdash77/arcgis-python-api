048baa45832ed146be20acb598bf2ae26377f3cf
Rahul Deo <52111992+r-deo@users.noreply.github.com>
2023-06-26 11:25:11 +0530

    Added the support for vanilla LSTM.  (#10056)
    * Added the support for vanilla LSTM. Updated the docstring.
    
    * Fixed the performance related looping
    
    * Speed issue
    
    * Removed the new code. Rather a docstring change would serve the purpose
    
    * changed the code to handle the implicit conversion to datetime
    
    * Added implicit conversion in show_results.
    
    ---------
    
    Co-authored-by: Rahul Deo <rdeo@esri.com>

--- src/arcgis/learn/models/_tsmodel_archs/_LSTM.py
+++ src/arcgis/learn/models/_tsmodel_archs/_LSTM.py
@@ -11,19 +11,23 @@ class _TSLSTM(nn.Module):
         hidden_layer_size=100,
         num_layers=1,
         dropout=0.2,
+        bidirectional=False,
     ):
         super(_TSLSTM, self).__init__()
         self.device = device
         self.input = input_size
         self.hidden = hidden_layer_size
         self.num_layers = num_layers
+        self.multiplier = 1
+        if bidirectional:
+            self.multiplier = 2
         if self.num_layers > 1:
             self.lstm = nn.LSTM(
                 input_size=self.input,
                 hidden_size=self.hidden,
                 num_layers=self.num_layers,
                 batch_first=True,
-                bidirectional=True,
+                bidirectional=bidirectional,
                 dropout=dropout,
             )
         else:
@@ -32,18 +36,27 @@ class _TSLSTM(nn.Module):
                 hidden_size=self.hidden,
                 num_layers=self.num_layers,
                 batch_first=True,
-                bidirectional=True,
+                bidirectional=bidirectional,
             )
         self.dropout = nn.Dropout(dropout)
-        self.linear = nn.Linear(2 * self.hidden, output_size)
+        self.linear = nn.Linear(self.multiplier * self.hidden, output_size)
 
     def forward(self, input_seq):
         input_seq = input_seq.permute(0, 2, 1)
         batch_size = input_seq.size(0)
         x = input_seq.to(torch.float)
-        h0 = torch.zeros(2 * self.num_layers, batch_size, self.hidden).to(self.device)
-        c0 = torch.zeros(2 * self.num_layers, batch_size, self.hidden).to(self.device)
+        h0 = torch.zeros(self.multiplier * self.num_layers, batch_size, self.hidden).to(
+            self.device
+        )
+        c0 = torch.zeros(self.multiplier * self.num_layers, batch_size, self.hidden).to(
+            self.device
+        )
         out, (h0, c0) = self.lstm(x, (h0, c0))
-        h_comb = torch.cat([h0[: self.num_layers], h0[self.num_layers :]], dim=-1)[-1]
+        if self.multiplier == 1:
+            h_comb = h0[-1]
+        else:
+            h_comb = torch.cat([h0[: self.num_layers], h0[self.num_layers :]], dim=-1)[
+                -1
+            ]
         outputs = self.linear(h_comb)
         return outputs.to(self.device)
