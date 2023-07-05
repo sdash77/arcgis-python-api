e077a53b5472fc84e1ce7c23f45895b559bfe08e
Andrew <achapkowski@esri.com>
2023-07-05 06:46:21 -0400

    fix black issues update link url

--- src/arcgis/_impl/common/_deprecate.py
+++ src/arcgis/_impl/common/_deprecate.py
@@ -184,9 +184,7 @@ def deprecated(deprecated_in=None, removed_in=None, current_version=None, detail
             # of the parts.
             parts = {
                 "deprecated_in": " %s" % deprecated_in if deprecated_in else "",
-                "removed_in": "\n   Removed in: %s." % removed_in
-                if removed_in
-                else "",
+                "removed_in": "\n   Removed in: %s." % removed_in if removed_in else "",
                 "details": " %s" % details if details else "",
             }
 
