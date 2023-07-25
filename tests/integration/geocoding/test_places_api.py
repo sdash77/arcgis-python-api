5b93c3afd77780de6b03f1cbf6c73f8b033375d2
Andrew <achapkowski@esri.com>
2023-07-25 06:17:01 -0400

    comment out sys.path.insert.

--- tests/integration/geocoding/test_places_api.py
+++ tests/integration/geocoding/test_places_api.py
@@ -2,7 +2,7 @@ import sys
 
 #
 #  Update the Path to set the test area
-sys.path.insert(0, r"C:\SVN\geosaurus_issue_10201\src")
+# sys.path.insert(0, r"C:\SVN\geosaurus_issue_10201\src")
 import logging
 import unittest
 from arcgis.auth.tools._util import detect_proxy
