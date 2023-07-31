cd8561bafae2ee90295fbf0e3ca35ec4bd49f554
Andrew <achapkowski@esri.com>
2023-07-31 14:45:27 -0400

    adds name check for create service and create view

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -7080,6 +7080,11 @@ class ContentManager(object):
             # Usage Example
             >>> gis.content.create_service("Hurricane Collection")
         """
+        regex = r"^[-a-zA-Z0-9_]*$"
+        if len(re.findall(regex, name)) == 0:
+            raise ValueError(
+                "The service `name` cannot contain any spaces or special characters except underscores."
+            )
         if capabilities is None:
             if service_type == "imageService":
                 capabilities = "Image,Catalog,Metadata,Download,Pixels"
