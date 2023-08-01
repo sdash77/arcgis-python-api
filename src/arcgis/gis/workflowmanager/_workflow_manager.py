64f56df9d171ce3a21282a4543b78a0ac35069dd
Kevin Bedel <kbedel@esri.com>
2023-08-01 16:58:14 -0600

    Add support for passphrase to import/export

--- src/arcgis/gis/workflowmanager/_workflow_manager.py
+++ src/arcgis/gis/workflowmanager/_workflow_manager.py
@@ -245,6 +245,7 @@ class WorkflowManagerAdmin:
         job_template_ids: Optional[str] = None,
         diagram_ids: Optional[str] = None,
         include_other_configs: bool = True,
+        passphrase: Optional[str] = None,
     ):
         """
         Exports a new Workflow Manager configuration (.wmc) file based on the indicated item. This configuration file
@@ -267,6 +268,10 @@ class WorkflowManagerAdmin:
         ---------------------  ---------------------------------------------------------
         include_other_configs  Optional. If false other configurations are not exported including templates,
                                User defined settings, shared searches, shared queries, email settings etc.
+        ---------------------  ---------------------------------------------------------
+        passphrase             Optional. If exporting encrypted user defined settings, define a passphrase.
+                               If no passphrase is specified, the keys for encrypted user defined settings will be
+                               exported without their values.
         =====================  =========================================================
 
         :return:
@@ -278,6 +283,8 @@ class WorkflowManagerAdmin:
             params["jobTemplateIds"] = job_template_ids
         if diagram_ids is not None:
             params["diagramIds"] = diagram_ids
+        if passphrase is not None:
+            params["passphrase"] = passphrase
 
         url = "{base}/admin/{id}/export".format(base=self._url, id=item.id)
         return_obj = self._gis._con.post(
@@ -289,7 +296,7 @@ class WorkflowManagerAdmin:
             self._gis._con._handle_json_error(return_obj["error"], 0)
         return return_obj
 
-    def import_item(self, item: Item, config_file):
+    def import_item(self, item: Item, config_file, passphrase: Optional[str] = None):
         """
         Imports a new Workflow Manager configuration from the selected .wmc file. Configurations from Workflow
         items with a server that is on a more recent version will not import due to incompatability. This will
@@ -305,6 +312,10 @@ class WorkflowManagerAdmin:
         item                Required Item. The Workflow Manager Item that to import the configuration to.
         ------------------  ---------------------------------------------------------
         config_file         Required. The file path to the workflow manager configuration file.
+        ------------------  ---------------------------------------------------------
+        passphrase          Optional. If importing encrypted user defined settings, specify the same passphrase
+                            used when exporting the configuration file. If no passphrase is specified, the keys for
+                            encrypted user defined settings will be imported without their values.
         ==================  =========================================================
 
         :return:
@@ -313,13 +324,17 @@ class WorkflowManagerAdmin:
         """
 
         url = "{base}/admin/{id}/import".format(base=self._url, id=item.id)
+        data = {}
+        if passphrase is not None:
+            data["passphrase"] = passphrase
 
         return_obj = self._gis._con.post(
             url,
             files={"file": config_file},
+            params=data,
             try_json=False,
             json_encode=False,
-            post_json=True,
+            post_json=False,
         )
         return_obj = json.loads(return_obj)
 
