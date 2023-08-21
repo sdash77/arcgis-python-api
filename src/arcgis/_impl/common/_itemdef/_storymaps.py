9b2b88588c3f3113aa8e132b00b1e2f85aa1c5fc
nparavicini7 <nparavicini@esri.com>
2023-08-21 08:43:07 -0700

    ExB and StoryMap cloning fixes
    Fixed text formatting getting lost in experience clones, fixed drafts being corrupted in storymap clones

--- src/arcgis/_impl/common/_itemdef/_storymaps.py
+++ src/arcgis/_impl/common/_itemdef/_storymaps.py
@@ -3,7 +3,6 @@ import uuid
 import copy
 import shutil
 import tempfile
-import json
 import logging
 from arcgis._impl.common._clone import CloneNode, _deep_get
 from arcgis._impl.common._clone import (
@@ -307,10 +306,16 @@ class _StoryMapDefinition(CloneNode):
                 new_item.resources.add(self.resources, archive=True)
             for resource in new_item.resources.list():
                 if ".json" in resource["resource"]:
-                    res = json.dumps(new_item.resources.get(resource["resource"]))
+                    res = new_item.resources.get(resource["resource"])
                     for k, v in webmap_mapper.items():
                         res = res.replace(k, v)
-                    new_item.resources.update(file_name=resource["resource"], text=res)
+                    tfile = tempfile.NamedTemporaryFile(mode="w+", suffix=".json")
+                    json.dump(res, tfile)
+                    tfile.seek(0)
+                    new_item.resources.update(
+                        file_name=resource["resource"],
+                        file=tfile.name,
+                    )
             if new_item.url:
                 new_item.update(
                     {"url": new_item.url.replace(self.portal_item.id, new_item.id)}
