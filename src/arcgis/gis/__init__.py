781a2fef9bbfe2e51f25eee56f7f8d626a9914a4
NANAE AUBRY <nanaeaubry@gmail.com>
2023-08-22 23:34:22 +0200

    add for enterprise membership access

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -5882,7 +5882,10 @@ class GroupManager(object):
         if hidden_members in [True, False]:
             params["hiddenMembers"] = hidden_members
         if membership_access in ["org", "collaboration", None, "none"]:
-            if membership_access is None:
+            if self._gis._is_agol is False:
+                # Only value for Enterprise is org
+                params["membershipAccess"] = "org"
+            elif self._gis._is_agol and membership_access is None:
                 membership_access = "none"
             params["membershipAccess"] = membership_access
         if autojoin in [True, False]:
