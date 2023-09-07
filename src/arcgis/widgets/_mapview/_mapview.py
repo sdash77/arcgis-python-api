4c921e0b2a31970cfaabd89e57b84f91e434ba34
NANAE AUBRY <nanaeaubry@gmail.com>
2023-09-07 15:09:34 +0200

    fix feature collection error when opacity given

--- src/arcgis/widgets/_mapview/_mapview.py
+++ src/arcgis/widgets/_mapview/_mapview.py
@@ -1596,6 +1596,7 @@ class MapView(widgets.DOMWidget):
                 _lyr["options"] = lyr_options
             else:
                 _lyr["options"] = options
+
             _lyr["_hashFromPython"] = self._get_hash(item)
             self._add_notype_layer(item, _lyr, True)
         elif isinstance(item, pd.DataFrame):
@@ -1708,6 +1709,8 @@ class MapView(widgets.DOMWidget):
             wm_properties= {"title": "Test Update", "tags":["update_layer"], "snippet":"Updated a layer and now save"}
             map1.save(wm_properties)
         """
+        if isinstance(layer, dict):
+            layer = json.loads(json.dumps(layer))
         # Update the webmap part
         self.webmap.update_layer(layer)
 
