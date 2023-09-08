179d21bb8b29fa2b286386dded81dffe7aa67863
Rahul Deo <52111992+r-deo@users.noreply.github.com>
2023-09-08 13:43:49 +0530

    Bugfix random split #10445 (#10495)
    * Fixed the random split issue
    
    * Fixed the issue due to unwanted column as object dtype

--- src/arcgis/learn/_utils/tabular_data.py
+++ src/arcgis/learn/_utils/tabular_data.py
@@ -91,7 +91,6 @@ class TabularDataObject(object):
         batch_size=64,
         index_field=None,
         column_transforms_mapping=None,
-        random_split=True,
         **kwargs,
     ):
         if not HAS_FASTAI:
@@ -99,6 +98,10 @@ class TabularDataObject(object):
 
         feature_variables = feature_variables if feature_variables else []
         raster_variables = raster_variables if raster_variables else []
+
+        random_split = True
+        if kwargs.get("random_split") == False:
+            random_split = False
 
         tabular_data = cls()
         (
