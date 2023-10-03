3a0befe4dab26d006c79c2302a0b98a1054f11d2
Rhea Jackson <rjackson@esri.com>
2023-10-03 04:39:22 -0700

    Remove raster info dict from kernel density method

--- src/arcgis/raster/functions/gbl.py
+++ src/arcgis/raster/functions/gbl.py
@@ -2083,17 +2083,6 @@ def kernel_density(
             "OutputRasterParameterName": "out_raster",
             "in_features": input_features,
             "population_field": population_field,
-            "RasterInfo": {
-                "blockWidth": 2048,
-                "blockHeight": 256,
-                "bandCount": 1,
-                "pixelType": 9,
-                "firstPyramidLevel": 1,
-                "maximumPyramidLevel": 30,
-                "pixelSizeX": 0,
-                "pixelSizeY": 0,
-                "type": "RasterInfo",
-            },
         },
     }
 
@@ -4608,17 +4597,6 @@ def optimal_path_as_raster(
             "PrimaryInputParameterName": "in_destination_data",
             "OutputRasterParameterName": "out_path_accumulation_raster",
             "in_destination_data": input_destination_data,
-            "RasterInfo": {
-                "blockWidth": 2048,
-                "blockHeight": 256,
-                "bandCount": 1,
-                "pixelType": 8,
-                "firstPyramidLevel": 1,
-                "maximumPyramidLevel": 30,
-                "pixelSizeX": 1,
-                "pixelSizeY": 1,
-                "type": "RasterInfo",
-            },
         },
     }
 
