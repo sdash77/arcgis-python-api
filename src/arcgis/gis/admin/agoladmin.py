27dc10cc067a59db6e2b55abde6ed498ae6356ab
Andrew <achapkowski@esri.com>
2023-10-26 12:22:33 -0400

    adds the admin operation to list all items in the organization

--- src/arcgis/gis/admin/agoladmin.py
+++ src/arcgis/gis/admin/agoladmin.py
@@ -158,6 +158,54 @@ class AGOLAdminManager(object):
         return self._collaborations
 
     # ----------------------------------------------------------------------
+    def content(
+        self,
+        item_type: "ItemTypeEnum" | None = None,
+        sort_field: str = "created",
+        order: str = "asc",
+    ):
+        """
+        The portal content operation allows an administrator to return a
+        list of all items in the organization. Only available to
+        administrators with a privilege to view all items in the
+        organization.
+
+        ===========================     ====================================================================
+        **Parameter**                    **Description**
+        ---------------------------     --------------------------------------------------------------------
+        item_type                       Optional ItemTypeEnum. The item type to filter by.
+        ---------------------------     --------------------------------------------------------------------
+        sort_field                      Optional String. Field to sort by.
+        ---------------------------     --------------------------------------------------------------------
+        order                           Optional String. The sort order of the return data.
+        ===========================     ====================================================================
+
+        """
+        params: dict = {
+            "sortField": sort_field,
+            "sortOrder": order,
+            "f": "json",
+            "start": 1,
+            "num": 100,
+        }
+        if item_type:
+            params["type"] = item_type.value
+        url: str = f"{self._gis._portal.resturl}content/portals/{self._gis.properties.get('id')}"
+        session = self._gis._con._session
+        resp = session.get(url=url, params=params)
+        resp.raise_for_status()
+        data: dict = resp.json()
+
+        while data["items"]:
+            for i in data["items"]:
+                yield Item(gis=self._gis, itemid=i["id"], itemdict=i)
+            if data.get("nextStart") == -1:
+                break
+            params["start"] = data["nextStart"]
+            resp = session.get(url=url, params=params)
+            data: dict = resp.json()
+
+    # ----------------------------------------------------------------------
     @property
     def category_schema(self):
         """
