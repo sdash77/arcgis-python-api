13a056f12231fd556e2a101c741dcec73ae4df74
Jay Roebuck <jroebuck@esri.com>
2023-11-03 09:50:14 -0400

    fix lazy load per review

--- src/arcgis/mapping/forms.py
+++ src/arcgis/mapping/forms.py
@@ -10,7 +10,7 @@ from arcgis.auth.tools import LazyLoader
 from arcgis.gis import Item
 from arcgis.features import FeatureLayer
 import copy
-Map = LazyLoader("arcgiswidgets.widgets.map_widget.Map")
+map_widget = LazyLoader("arcgiswidgets.widgets.map_widget")
 
 
 class FormCollection:
@@ -77,7 +77,7 @@ class FormCollection:
             return self.forms[self._index - 1]
 
     def _refresh_forms(self, parent):
-        if isinstance(parent, Map):
+        if isinstance(parent, map_widget.Map):
             self.forms = self._get_forms_from_webmap(parent)
         elif isinstance(parent, Item):
             if parent.type != "Feature Layer Collection":
@@ -366,7 +366,7 @@ class FormInfo:
             else:
                 item_data["layers"][self._layer_data["id"]].pop("formInfo", None)
             self._parent.update(data=item_data)
-        if isinstance(self._parent, Map):
+        if isinstance(self._parent, map_widget.Map):
             if self.exists():
                 self._original_layer["formInfo"] = self.to_dict()
             else:
