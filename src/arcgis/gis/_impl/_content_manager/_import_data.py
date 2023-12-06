eadabaff4e9e4e767169995dfa099284e7eec304
Andrew <achapkowski@esri.com>
2023-12-06 08:47:19 -0500

    fix pandas test for unittest.

--- src/arcgis/gis/_impl/_content_manager/_import_data.py
+++ src/arcgis/gis/_impl/_content_manager/_import_data.py
@@ -2,7 +2,6 @@ import random
 from uuid import uuid4
 import string
 import os
-import pandas as pd
 import tempfile
 import shutil
 from arcgis._impl.common._utils import _date_handler
@@ -15,7 +14,7 @@ _tool_utils = LazyLoader("arcgis.features.geo._tools._utils")
 _common_utils = LazyLoader("arcgis._impl.common._utils")
 features = LazyLoader("arcgis.features")
 json = LazyLoader("json")
-
+pd = LazyLoader("pandas")
 try:
     from arcgis.features.geo import _is_geoenabled
 except:
