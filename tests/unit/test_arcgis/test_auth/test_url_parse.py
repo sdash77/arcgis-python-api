5c716f22aba0ef76e62cdde5ab4df86938521ac4
Jay Roebuck <jroebuck@esri.com>
2024-01-04 10:51:04 -0500

    enhance url parse test

--- tests/unit/test_arcgis/test_auth/test_url_parse.py
+++ tests/unit/test_arcgis/test_auth/test_url_parse.py
@@ -3,16 +3,20 @@ from arcgis.auth.tools import parse_url
 from urllib.parse import ParseResult
 
 
-class TestParseURL(unittest.TestCase):
+class TestParseUrl(unittest.TestCase):
     """tests the parse_url"""
 
     def test_parse_url(self):
         """tests the parse url process"""
         parsed = parse_url(url="https://www.arcgis.com")
         assert parsed
-        assert isinstance(
-            parsed, ParseResult
-        )
+        assert isinstance(parsed, ParseResult)
+        assert parsed.scheme == "https"
+        assert parsed.netloc == "www.arcgis.com"
+        assert not parsed.path
+        assert not parsed.params
+        assert not parsed.query
+        assert not parsed.fragment
 
 
 if __name__ == "__main__":
