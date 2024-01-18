4f36ed3e075f2ffc48de1db96919ac50c9aab8c0
Andrew <achapkowski@users.noreply.github.com>
2024-01-18 10:57:52 -0500

    Update src/arcgis/features/managers.py
    Co-authored-by: Jay Roebuck <jroebuck@esri.com>

--- src/arcgis/features/managers.py
+++ src/arcgis/features/managers.py
@@ -2346,8 +2346,8 @@ class FeatureLayerCollectionManager(_GISResource):
         import os
         from . import FeatureLayerCollection
 
-        regex: str = r"[$&+,:;=?@#|'<>.^*()%!-]"
-        if len(re.findall(regex, name)) > 0:
+        invalid_char_regex: str = r"[$&+,:;=?@#|'<>.^*()%!-]"
+        if len(re.findall(invalid_char_regex, name)) > 0:
             raise ValueError(
                 "The service `name` cannot contain any spaces or special characters except underscores."
             )
