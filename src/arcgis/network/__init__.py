04334e79c3e58da499ba230692ab9a127749db52
Andrew <achapkowski@esri.com>
2024-02-01 14:31:24 -0500

    solvertye moved up.

--- src/arcgis/network/__init__.py
+++ src/arcgis/network/__init__.py
@@ -13,7 +13,7 @@ from ._layer import (
     NAJob,
     ODCostMatrixLayer,
 )
-from ._utils import publish_routing_services
+from ._utils import publish_routing_services, SolverType
 from arcgis.auth.tools import LazyLoader
 
 analysis = LazyLoader("arcgis.network.analysis")
