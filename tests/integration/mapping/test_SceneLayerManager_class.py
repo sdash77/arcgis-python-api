94a0fe5162dc67610e9b2f0a4bb696f4bab7b846
NANAE AUBRY <nanaeaubry@gmail.com>
2024-02-01 10:43:05 +0100

    update tests

--- tests/integration/mapping/test_SceneLayerManager_class.py
+++ tests/integration/mapping/test_SceneLayerManager_class.py
@@ -1,7 +1,7 @@
 import sys
 from unittest.case import SkipTest
 
-sys.path.insert(0, r"C:\\ipython_workfolder\\geosaurus\\src")
+sys.path.insert(0, r"C:\\workspace\\geosaurus\\src")
 import unittest
 import os
 from arcgis.gis import GIS
@@ -36,7 +36,6 @@ class TestSceneLayerManager(unittest.TestCase):
         status = manager.status()
         assert status
 
-
     def test_jobs(self):
         """
         Test various job functions
@@ -45,6 +44,9 @@ class TestSceneLayerManager(unittest.TestCase):
         assert jobs
         assert isinstance(jobs, dict)
 
+        if not jobs["jobs"]:
+            print("No jobs available for the scene layer.")
+            return
         # get the job id for a job
         job_id = jobs["jobs"][0]["id"]
         # get stats for a job
@@ -81,9 +83,7 @@ class TestSceneLayerManager(unittest.TestCase):
         source_item_id = scene_layer_item.related_items(
             rel_type="Service2Data", direction="forward"
         )[0]["id"]
-        res = manager.edit_item(
-            item = source_item_id
-        )
+        res = manager.edit(item=source_item_id)
         assert res
         assert res["status"] == "success"
 
@@ -95,6 +95,7 @@ class TestSceneLayerManager(unittest.TestCase):
         if res:
             # could be none
             assert res
+
 
 if __name__ == "__main__":
     unittest.main()
