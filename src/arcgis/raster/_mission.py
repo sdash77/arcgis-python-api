ab633b57c493f316c47518a33fb47ba8dd512865
Rhea Jackson <rjackson@esri.com>
2024-02-07 04:08:33 -0800

    Adding reset method to mission object

--- src/arcgis/raster/_mission.py
+++ src/arcgis/raster/_mission.py
@@ -135,6 +135,16 @@ class Mission:
                 self._collection = image_collection_item
                 return image_collection_item
 
+    def reset(self):
+        """
+        The ``reset`` method resets the mission to its original state
+
+        :return: None
+        """
+        from arcgis.raster.orthomapping import reset_image_collection
+
+        return reset_image_collection(self, gis=self._gis)
+
     def add_image(
         self,
         input_rasters: list,
