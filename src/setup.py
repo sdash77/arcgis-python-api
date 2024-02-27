1ebbcd96bebd50c1a54bf527f2f13fe51a373b2e
Jay Roebuck <jroebuck@esri.com>
2024-02-27 11:13:46 -0500

    pin urllib3 to v2

--- src/setup.py
+++ src/setup.py
@@ -64,7 +64,7 @@ if conda_install_mode:
 else:
     dependencies = [
         "pillow",
-        "urllib3>=1.21.1,<3",
+        "urllib3>=2,<3",
         "cachetools",
         "lxml",
         "notebook",
