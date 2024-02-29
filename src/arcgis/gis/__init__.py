d2818b942246702213d16ef36a9ec9ef94faf68b
Andrew <achapkowski@esri.com>
2024-02-29 06:03:58 -0500

    adds an additional datastore metric test: test_agol_datastore_metrics.py updates the User Report logic.

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -60,6 +60,7 @@ from cachetools import cached, TTLCache
 
 from arcgis.auth.tools import LazyLoader
 from arcgis.auth import EsriSession
+import datetime as _dt
 
 arcgis_env = LazyLoader("arcgis.env")
 arcgis = LazyLoader("arcgis")
@@ -11304,9 +11305,10 @@ class User(dict):
     def report(
         self,
         report_type: str,
-        start_time: Optional[datetime],
         *,
-        duration: Optional[str] = "weekly",
+        start_time: _dt.datetime | None = None,
+        duration: str = "weekly",
+        time_aggregate: str | None = None,
     ) -> Item:
         """
 
@@ -11326,11 +11328,13 @@ class User(dict):
                           generate. The allowed arguments are:
 
                           * *credits*
-                          * *content*
-                          * *users*
+                          * *content* (does not honor start_time)
+                          * *users* (does not honor start_time)
                           * *activity*
+                          * *serviceUsages*
+                          * *itemUsages*
         ----------------  --------------------------------------------------------
-        start_time        Required Datetime. The time from which the report
+        start_time        Optional Datetime. The time from which the report
                           generates information.
 
                           * If *duration* is *weekly*, the day component must
@@ -11354,10 +11358,18 @@ class User(dict):
                           * *monthly*
                           * *weekly*
                           * *daily* - only available if *report_type* is *activity*
+                          * *yearly*
+                          * *quarterly*
 
                           .. note::
                               Argument is required when setting *report_type*
                               argument to *activity* or *credits*.
+
+                          .. note::
+                              The yearly value is only available when reportSubType is set to itemUsages.
+        ----------------  --------------------------------------------------------
+        time_aggregate    Optional String.  When the `report_type` is `itemUsages`, the records can be aggregated on
+                          specific time groups.  These are `day`, `week`, or `month`.
         ================  ========================================================
 
 
@@ -11393,8 +11405,23 @@ class User(dict):
 
         import datetime as _dt
 
-        assert report_type in ["users", "credits", "activity", "content"]
-        assert duration in ["monthly", "weekly", "daily"]
+        assert report_type in [
+            "users",
+            "credits",
+            "activity",
+            "content",
+            "serviceUsages",
+            "itemUsages",
+        ]
+        assert duration in [
+            "monthly",
+            "weekly",
+            "daily",
+            "quarterly",
+            "yearly",
+        ]
+        if report_type == "itemUsages" and time_aggregate:
+            assert time_aggregate in ["day", "week", "month"]
 
         def weeknumber(dayname):
             if dayname == "Monday":
@@ -11443,15 +11470,23 @@ class User(dict):
             start_time = now + _dt.timedelta(days=dow)
             start_time = int(start_time.timestamp() * 1000)
         elif start_time is None and duration in ["daily"]:
-            start_time = _dt.datetime.now(_dt.timezone.utc)
+            start_time = int(_dt.datetime.now(_dt.timezone.utc).totimestamp() * 1000)
         params = {
             "f": "json",
             "reportType": "org",
             "reportSubType": report_type,
             "timeDuration": duration,
-            "startTime": start_time,
+            "startTime": start_time or "",
+            "timeAggregate": "" or time_aggregate,
         }
 
+        if report_type in ["content", "users"] and start_time:
+            del params["startTime"]
+            _log.warning(
+                "`start_time` is not honored with report type of content and users."
+            )
+        if report_type != "itemUsages":
+            del params["timeAggregate"]
         url = "%s/sharing/rest/community/users/%s/report" % (
             self._gis._url,
             self._user_id,
