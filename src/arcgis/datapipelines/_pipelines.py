cb227e5031e62b69e04f1d072be541321df6548c
maxp8108 <mpayson@esri.com>
2024-03-06 12:15:34 -0500

    Fix tests

--- src/arcgis/datapipelines/_pipelines.py
+++ src/arcgis/datapipelines/_pipelines.py
@@ -164,9 +164,7 @@ class PipelineRun:
                 # Unknown results, return the whole object
                 parsed["results"] = result["results"]
 
-        parsed["messages"] = (
-            [self._parse_message(m) for m in result.get("messages", [])],
-        )
+        parsed["messages"] = [self._parse_message(m) for m in result.get("messages", [])]
 
         return parsed
 
