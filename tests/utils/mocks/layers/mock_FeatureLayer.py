7d8c2c29c08bc7ba83d38550ed9514f5f819bd79
NANAE AUBRY <nanaeaubry@gmail.com>
2024-03-08 21:02:19 +0100

    ran black

--- tests/utils/mocks/layers/mock_FeatureLayer.py
+++ tests/utils/mocks/layers/mock_FeatureLayer.py
@@ -4,6 +4,7 @@ from utils.mocks.mock_arcgisconnection import MockArcGISConnection
 
 from arcgis._impl.common._mixins import PropertyMap
 
+
 class MockFeatureLayer(MagicMock):
     _dynamic_layer = None
     _token = "da78c9fb55bb5e44e44c6f88ee7d4ee4"
@@ -11,19 +12,19 @@ class MockFeatureLayer(MagicMock):
     _url = url
     _con = MockArcGISConnection()
     properties = PropertyMap(
-    {   
-        "supportsAppend": True,
-        "supportedAppendFormats": [
-            "csv",
-            "excel",
-            "featureCollection",
-            "featureService",
-            "filegdb",
-            "geoPackage",
-            "geojson",
-            "jsonl",
-            "shapefile",
-            "sqlite",
-        ],
-    }
-)
+        {
+            "supportsAppend": True,
+            "supportedAppendFormats": [
+                "csv",
+                "excel",
+                "featureCollection",
+                "featureService",
+                "filegdb",
+                "geoPackage",
+                "geojson",
+                "jsonl",
+                "shapefile",
+                "sqlite",
+            ],
+        }
+    )
