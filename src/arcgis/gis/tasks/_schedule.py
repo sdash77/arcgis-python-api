c86e5396dc09a7aa81a4f0e022689315954663e0
John Yaist <jyaist@esri.com>
2024-03-11 15:52:56 -0700

    add params definition for other task types

--- src/arcgis/gis/tasks/_schedule.py
+++ src/arcgis/gis/tasks/_schedule.py
@@ -592,6 +592,8 @@ class TaskManager(object):
         if parameters:
             if params["type"] == "ExecuteNotebook":
                 params["parameters"] = json.dumps({"notebookParameters": parameters, "saveInjectedParameters": True})
+            else:
+                params["parameters"] = json.dumps(parameters)
         res = self._gis._con.post(url, params)
 
         if "success" in res and res["success"]:
