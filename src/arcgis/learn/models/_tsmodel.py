98ee59d42432b4f22382e6d750305ae76c8ecc27
Mayank Khanduja <mayankkhanduja8@gmail.com>
2024-03-18 11:22:04 +0530

    timeseries predict fix for pandas 2.x (#11250)
    Co-authored-by: Mayank Khanduja <may10489@esri.com>
    Co-authored-by: Priyanka Tuteja <39754605+priyankatuteja@users.noreply.github.com>

--- src/arcgis/learn/models/_tsmodel.py
+++ src/arcgis/learn/models/_tsmodel.py
@@ -1122,7 +1122,7 @@ class TimeSeriesModel(ArcGISModel):
                 if isinstance(transform, LabelEncoder):
                     transformed_data = transform.transform(
                         np.array(
-                            transformed_data,
+                            transformed_data.to_numpy(na_value=np.nan),
                             dtype=type(processed_dataframe[col][0]),
                         )
                     )
@@ -1130,8 +1130,8 @@ class TimeSeriesModel(ArcGISModel):
                 else:
                     transformed_data = transform.transform(
                         np.array(
-                            transformed_data,
-                            dtype=type(processed_dataframe[col][0]),
+                            transformed_data.to_numpy(na_value=np.nan),
+                            dtype=type(processed_dataframe[col][0])
                         ).reshape(-1, 1)
                     )
 
