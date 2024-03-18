Fixed the missing attention mask issue in batch mode inference. (#11259)
* Fixed the missing attention mask issue in batch mode inference.

* adding condition for text models

* black formatting

* suggested fix

* suggested fix

* refactoring

---------

Co-authored-by: 1297rohit <1297rohit@gmail.com>
Co-authored-by: Priyanka Tuteja <39754605+priyankatuteja@users.noreply.github.com>
Co-authored-by: Rohit Singh <rsingh@esri.com>

src/arcgis/learn/text/_transformer_text_classifier.py
@@ -310,10 +310,12 @@ class TransformerForTextClassification(ArcGISTransformer):
             text_batch, max_length=self._max_seq_len, padding=True, truncation=True
         )
         batch_token_ids = encodings["input_ids"]
+        mask = encodings["attention_mask"]
         results_list = []
         sequence = torch.tensor(batch_token_ids).to(device)
+        mask = torch.tensor(mask).to(device)
         with torch.no_grad():
-            logits = self._transformer(sequence)[0]
+            logits = self._transformer(sequence, attention_mask=mask)[0]
 
         if is_multilabel_problem:
             results = torch.sigmoid(logits)