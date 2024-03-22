45567e4cbf63f0fff6c0779a9fc790b612cfda79
Andrew <achapkowski@esri.com>
2024-03-22 07:23:43 -0400

    fix code formatting

--- src/arcgis/gis/tasks/_schedule.py
+++ src/arcgis/gis/tasks/_schedule.py
@@ -607,7 +607,9 @@ class TaskManager(object):
             )
         if parameters:
             if params["type"] == "ExecuteNotebook":
-                params["parameters"] = json.dumps({"notebookParameters": parameters, "saveInjectedParameters": True})
+                params["parameters"] = json.dumps(
+                    {"notebookParameters": parameters, "saveInjectedParameters": True}
+                )
             else:
                 params["parameters"] = json.dumps(parameters)
         res = self._gis._con.post(url, params)
