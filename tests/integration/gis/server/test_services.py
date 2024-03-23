bff1868d26ce1040753e207b7857c487e182ae4f
John Yaist <jyaist@esri.com>
2024-03-23 15:04:24 -0700

    comment out generic reference to local src and tests directories

--- tests/integration/gis/server/test_services.py
+++ tests/integration/gis/server/test_services.py
@@ -1,7 +1,8 @@
 import sys
 
-sys.path.insert(0, r"C:\Job\repos\geosaurus\src")
-sys.path.insert(1, r"C:\Job\repos\geosaurus\tests")
+#sys.path.insert(0, r"<path to local repo>\src")
+#sys.path.insert(1, r"<path to local repo>\tests")
+
 import unittest
 from utils.decorators import profiles, integration_test
 from utils._logging import enable_verbose_logging
