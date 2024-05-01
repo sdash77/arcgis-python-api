5c6c494fba253e70f588ec606c780d44fb7d4bfa
Andrew <achapkowski@esri.com>
2024-05-01 14:05:52 -0400

    fixes verification issues when verify_cert=False

--- src/arcgis/auth/tools/_adapter.py
+++ src/arcgis/auth/tools/_adapter.py
@@ -15,6 +15,17 @@ except ImportError:
 from requests.adapters import HTTPAdapter
 from .certificate import _handle_cert_context
 
+from requests.utils import (
+    DEFAULT_CA_BUNDLE_PATH,
+    extract_zipped_paths,
+    get_auth_from_url,
+    get_encoding_from_headers,
+    prepend_scheme_if_needed,
+    select_proxy,
+    urldefragauth,
+)
+from requests.compat import basestring, urlparse
+
 __all__ = ["PKIAdapter", "TruststoreAdapter", "EsriHostHeaderSSLAdapter"]
 
 
@@ -145,7 +156,7 @@ class EsriHostHeaderSSLAdapter(HTTPAdapter):
             # an assert_hostname from a previous request may have been left
             connection_pool_kwargs.pop("assert_hostname", None)
 
-        return super(HostHeaderSSLAdapter, self).send(request, **kwargs)
+        return super(EsriHostHeaderSSLAdapter, self).send(request, **kwargs)
 
 
 ###########################################################################
@@ -163,3 +174,62 @@ class TruststoreAdapter(HTTPAdapter):
     def init_poolmanager(self, connections, maxsize, block=False):
         ctx = truststore.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
         return super().init_poolmanager(connections, maxsize, block, ssl_context=ctx)
+
+    # ---------------------------------------------------------------------
+    def cert_verify(self, conn, url, verify, cert):
+        """Verify a SSL certificate. This method should not be called from user
+        code, and is only exposed for use when subclassing the
+        :class:`HTTPAdapter <requests.adapters.HTTPAdapter>`.
+
+        :param conn: The urllib3 connection object associated with the cert.
+        :param url: The requested URL.
+        :param verify: Either a boolean, in which case it controls whether we verify
+            the server's TLS certificate, or a string, in which case it must be a path
+            to a CA bundle to use
+        :param cert: The SSL certificate to verify.
+        """
+        if url.lower().startswith("https") and verify:
+
+            cert_loc = None
+
+            # Allow self-specified cert location.
+            if verify is not True:
+                cert_loc = verify
+
+            if not cert_loc:
+                cert_loc = extract_zipped_paths(DEFAULT_CA_BUNDLE_PATH)
+
+            if not cert_loc or not os.path.exists(cert_loc):
+                raise OSError(
+                    f"Could not find a suitable TLS CA certificate bundle, "
+                    f"invalid path: {cert_loc}"
+                )
+
+            conn.cert_reqs = "CERT_REQUIRED"
+
+            if not os.path.isdir(cert_loc):
+                conn.ca_certs = cert_loc
+            else:
+                conn.ca_cert_dir = cert_loc
+        else:
+            conn.cert_reqs = "CERT_NONE"
+            conn.ca_certs = None
+            conn.ca_cert_dir = None
+            conn.conn_kw["ssl_context"].check_hostname = verify
+
+        if cert:
+            if not isinstance(cert, basestring):
+                conn.cert_file = cert[0]
+                conn.key_file = cert[1]
+            else:
+                conn.cert_file = cert
+                conn.key_file = None
+            if conn.cert_file and not os.path.exists(conn.cert_file):
+                raise OSError(
+                    f"Could not find the TLS certificate file, "
+                    f"invalid path: {conn.cert_file}"
+                )
+            if conn.key_file and not os.path.exists(conn.key_file):
+                raise OSError(
+                    f"Could not find the TLS key file, invalid path: {conn.key_file}"
+                )
