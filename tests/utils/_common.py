a9a13c98e87068261063c71ed2dfe98a9d87b445
Jay Roebuck <jroebuck@esri.com>
2024-06-10 13:55:53 -0400

    fixing unit tests

--- tests/utils/_common.py
+++ tests/utils/_common.py
@@ -87,6 +87,9 @@ def should_smoketest_arcgis_learn():
     test_arcgis_learn = os.environ.get("TEST_ARCGIS_LEARN", "true")
     return test_arcgis_learn.lower() in ["true", "yes", "1"]
 
+def should_allow_testing_against_packaged():
+    return os.environ.get("ALLOW_INSTALLED_ARCGIS", "false").lower() in ["true", "yes", "1"]
+
 def run_unittest_on(
     paths,
     output_xml_path,
