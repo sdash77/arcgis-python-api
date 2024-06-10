66b5ee5a6a1f0f6e6f6dd3f677dead85fbb9f8b9
NANAE AUBRY <nanaeaubry@gmail.com>
2024-06-10 16:41:44 +0200

    fix learn issue

--- tests/utils/_common.py
+++ tests/utils/_common.py
@@ -83,6 +83,9 @@ def setup_env():
     ]
     run_shell_command(" ".join(pip_install_cmd))
 
+def should_smoketest_arcgis_learn():
+    test_arcgis_learn = os.environ.get("TEST_ARCGIS_LEARN", "true")
+    return test_arcgis_learn.lower() in ["true", "yes", "1"]
 
 def run_unittest_on(
     paths,
