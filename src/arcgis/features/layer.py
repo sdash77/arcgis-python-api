3c376c21e42fb5687b1cd349ef8bc130ec91d145
Ken Galliher <kgalliher092@live.com>
2024-06-14 11:54:08 -0700

    Add full create replica response

--- src/arcgis/features/layer.py
+++ src/arcgis/features/layer.py
@@ -5487,13 +5487,14 @@ class FeatureLayerCollection(_GISResource):
                 dl_url = res["responseUrl"]
 
             if dl_url is not None:
-                return self._con.get(
+                download_url = self._con.get(
                     path=dl_url,
                     file_name=dl_url.split("/")[-1],
                     out_folder=out_path,
                     try_json=False,
                 )
-
+                res["download_url"] = download_url
+                return res
             else:
                 return res
         elif res is not None:
