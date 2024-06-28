9c4e9c537e299f3c9f299f308eeef4d5786394ae
Jay Roebuck <jroebuck@esri.com>
2024-06-28 12:24:44 -0400

    +copy

--- build/arcgis/bld.bat
+++ build/arcgis/bld.bat
@@ -1,6 +1,6 @@
 cd src
 
-"%PYTHON%" ..\build\manage_binaries.py --conda --python %PY_VER%
+"%PYTHON%" ..\build\manage_binaries.py copy --conda --python %PY_VER%
 if errorlevel 1 exit 1
 "%PYTHON%" setup.py install --conda-install-mode
 if errorlevel 1 exit 1
--- build/arcgis/build.sh
+++ build/arcgis/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd src
 
-$PYTHON ../build/manage_binaries.py --conda --python $PY_VER
+$PYTHON ../build/manage_binaries.py copy --conda --python $PY_VER
 $PYTHON setup.py install --conda-install-mode
