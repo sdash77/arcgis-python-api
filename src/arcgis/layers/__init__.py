d5fbb9be646e9f0565b3b48684ef682fb3b09eba
NANAE AUBRY <nanaeaubry@gmail.com>
2024-06-28 12:06:49 +0200

    fix imports

--- src/arcgis/layers/__init__.py
+++ src/arcgis/layers/__init__.py
@@ -4,9 +4,6 @@ This module also includes mapping layers like MapImageLayer, SceneLayer and Vect
 """
 
 from ._vtl._vector_tile_layers import (
-    MapImageLayer,
-    MapImageLayerManager,
-    EnterpriseMapImageLayerManager,
     VectorTileLayer,
     VectorTileLayerManager,
     EnterpriseVectorTileLayerManager,
@@ -29,7 +26,17 @@ from arcgis.layers._msl import (
     MapFeatureLayer,
     MapTable,
     MapRasterLayer,
+    MapImageLayer,
+    MapImageLayerManager,
+    EnterpriseMapImageLayerManager,
 )
+from ._ogc._wms import WMSLayer
+from ._ogc.wmts import WMTSLayer
+from ._ogc._csv import CSVLayer
+from ._ogc._georss import GeoRSSLayer
+from ._ogc._kml import KMLLayer
+from ._ogc._geojson import GeoJSONLayer
+from ._ogc._service import OGCCollection, OGCFeatureService
 
 __all__ = [
     "MapImageLayer",
@@ -44,4 +51,23 @@ __all__ = [
     "SceneLayer",
     "SceneLayerManager",
     "EnterpriseSceneLayerManager",
+    "Object3DLayer",
+    "IntegratedMeshLayer",
+    "Tiles3DLayer",
+    "Point3DLayer",
+    "PointCloudLayer",
+    "BuildingLayer",
+    "VoxelLayer",
+    "MapServiceLayer",
+    "MapFeatureLayer",
+    "MapTable",
+    "MapRasterLayer",
+    "WMSLayer",
+    "WMTSLayer",
+    "CSVLayer",
+    "GeoRSSLayer",
+    "KMLLayer",
+    "GeoJSONLayer",
+    "OGCCollection",
+    "OGCFeatureService",
 ]
--- src/arcgis/layers/_msl/__init__.py
+++ src/arcgis/layers/_msl/__init__.py
@@ -1,1 +1,9 @@
-from .map_layers import MapServiceLayer, MapFeatureLayer, MapTable, MapRasterLayer
+from .map_layers import (
+    MapServiceLayer,
+    MapFeatureLayer,
+    MapTable,
+    MapRasterLayer,
+    MapImageLayer,
+    MapImageLayerManager,
+    EnterpriseMapImageLayerManager,
+)
