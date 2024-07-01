f36ba6ff1da0017fd098dd74d3a0e8905a3964c4
Andrew <achapkowski@esri.com>
2024-07-01 14:08:58 -0400

    resolves #11741 resolves #11730 Adds missing service types for factory and fixes geocoding issue

--- src/arcgis/layers/_service_factory/_layerfactory.py
+++ src/arcgis/layers/_service_factory/_layerfactory.py
@@ -7,7 +7,12 @@ from __future__ import absolute_import
 import os
 from urllib.parse import urlparse
 from arcgis.gis import GIS
-from arcgis.features.layer import FeatureLayer, FeatureLayerCollection
+from arcgis.features.layer import (
+    FeatureLayer,
+    FeatureLayerCollection,
+    OrientedImageryLayer,
+    Table,
+)
 from arcgis.geocoding import Geocoder
 from arcgis.geoprocessing._tool import Toolbox
 from arcgis.geoprocessing import import_toolbox as _import_toolbox
@@ -21,6 +26,89 @@ from arcgis.schematics import SchematicLayers
 from arcgis.layers._scenelyrs import SceneLayer
 from ...gis._impl._con import Connection
 from ...gis.server._service._geodataservice import GeoData
+
+
+###########################################################################
+class _FeatureServiceLayerFactory(type):
+    """
+    Factory that generates the Scene Layers
+
+    ==================     ====================================================================
+    **Parameter**           **Description**
+    ------------------     --------------------------------------------------------------------
+    url                    Required string, specify the url ending in /SceneServer/
+    ------------------     --------------------------------------------------------------------
+    gis                    Optional :class:`~arcgis.gis.GIS`  object. If not specified, the active GIS connection is
+                           used.
+    ==================     ====================================================================
+
+    .. code-block:: python
+
+        # USAGE EXAMPLE 1: Instantiating a SceneLayer object
+
+        from arcgis.layers import SceneLayer
+        s_layer = SceneLayer(url='https://your_portal.com/arcgis/rest/services/service_name/SceneServer/')
+
+        type(s_layer)
+        >> arcgis.layers._types.PointCloudLayer
+
+        print(s_layer.properties.layers[0].name)
+        >> 'your layer name'
+    """
+
+    def __call__(cls, url, gis=None):
+        lyr = Layer(url=url, gis=gis)
+        props = lyr.properties
+        if props["type"] in ["Feature Layer", "Catalog Layer"]:
+            return FeatureLayer(url, gis=gis)
+        elif props["type"] == "Oriented Imagery Layer":
+            return OrientedImageryLayer(url, gis=gis)
+        elif props["type"] == "Table":
+            return Table(url, gis=gis)
+
+        return lyr
+
+
+###########################################################################
+class FeatureServiceLayer(Layer, metaclass=_FeatureServiceLayerFactory):
+    """
+    The ``SceneLayer`` class represents a Web scene layer.
+
+    .. note::
+        Web scene layers are cached web layers that are optimized for displaying a large amount of 2D and 3D features.
+
+    .. note::
+        Web scene layers can be used to represent 3D points, point clouds, 3D objects and
+        integrated mesh layers.
+
+    ==================     ====================================================================
+    **Parameter**           **Description**
+    ------------------     --------------------------------------------------------------------
+    url                    Required string, specify the url ending in /SceneServer/
+    ------------------     --------------------------------------------------------------------
+    gis                    Optional :class:`~arcgis.gis.GIS`  object. If not specified, the active GIS connection is
+                           used.
+    ==================     ====================================================================
+
+    .. code-block:: python
+
+        # USAGE EXAMPLE 1: Instantiating a SceneLayer object
+
+        from arcgis.layers import SceneLayer
+        s_layer = SceneLayer(url='https://your_portal.com/arcgis/rest/services/service_name/SceneServer/')
+
+        type(s_layer)
+        >> arcgis.layers._types.PointCloudLayer
+
+        print(s_layer.properties.layers[0].name)
+        >> 'your layer name'
+    """
+
+    def __init__(self, url: str, gis=None):
+        """
+        Constructs a SceneLayer given a web scene layer URL
+        """
+        super(SceneLayer, self).__init__(url, gis)
 
 
 class ServiceFactory(type):
@@ -73,13 +161,15 @@ class ServiceFactory(type):
                 return MapImageLayer(url=url, gis=server)
         elif base_name.lower() == "featureserver":
             if hasLayer:
-                return FeatureLayer(url=url, gis=server)
+                return FeatureServiceLayer(url=url, gis=server)
             else:
                 return FeatureLayerCollection(url=url, gis=server)
         elif base_name.lower() == "imageserver":
             return ImageryLayer(url=url, gis=server)
         elif base_name.lower() == "gpserver":
-            from arcgis.geoprocessing import import_toolbox as _import_toolbox
+            from arcgis.geoprocessing import (
+                import_toolbox as _import_toolbox,
+            )
 
             res = _import_toolbox(url, server)
             return res
@@ -103,6 +193,22 @@ class ServiceFactory(type):
             return SchematicLayers(url=url, gis=server)
         elif base_name.lower() == "vectortileserver":
             return VectorTileLayer(url=url, gis=server)
+        elif base_name.find(".geojson") > -1:
+            from .._ogc import GeoJSONLayer
+
+            return GeoJSONLayer(url=url, gis=connection)
+        elif base_name.find(".csv") > -1:
+            from .._ogc import CSVLayer
+
+            return CSVLayer(url_or_item=url, gis=connection)
+        elif base_name.find(".kml") > -1 or base_name.find(".kmz") > -1:
+            from .._ogc import KMLLayer
+
+            return KMLLayer(url=url, gis=connection)
+        elif base_name.lower() == "ogcfeatureserver":
+            from .._ogc._service import OGCFeatureService
+
+            return OGCFeatureService(url, gis=connection)
         else:
             return Layer(url=url, gis=server)
         return type.__call__(cls, url, connection, item, initialize)
