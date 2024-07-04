b8bb95a9e9e02cfbafb4de9e34ae2bffaf3c55a7
Soumya Kanta Das <67886231+sdash77@users.noreply.github.com>
2024-07-04 15:52:00 +0530

    update_showresults_4

--- src/arcgis/learn/models/_climax.py
+++ src/arcgis/learn/models/_climax.py
@@ -228,7 +228,7 @@ class ClimaX(ArcGISModel):
     def _supported_datasets():
         return ["Export Tiles"]
 
-    def show_results(self, rows=20, **kwargs):
+    def show_results(self, rows=20, variable="", **kwargs):
         """
         Displays the results of a trained model on a part of the validation set.
 
@@ -246,7 +246,7 @@ class ClimaX(ArcGISModel):
         **kwargs**
 
         """
-        show_results(self, rows, variable="", **kwargs)
+        show_results(self, rows, variable, **kwargs)
 
     def compute_metrics(self):
         """
