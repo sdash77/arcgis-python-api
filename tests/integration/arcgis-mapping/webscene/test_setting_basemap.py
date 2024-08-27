18c2033d6d4956cf3b690dfe235184bd38cd8dcd
John Yaist <jyaist@esri.com>
2024-08-27 11:45:36 -0700

    remove errant spacing in __main__

--- tests/integration/arcgis-mapping/webscene/test_setting_basemap.py
+++ tests/integration/arcgis-mapping/webscene/test_setting_basemap.py
@@ -67,4 +67,4 @@ class TestAddLayersToMap(unittest.TestCase):
         assert self.wm.extent["spatialReference"]["wkid"] != current_basemap_sr
 
 if __name__ == "__main__":
-     unittest.main()
+    unittest.main()
