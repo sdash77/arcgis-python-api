91bceed0aebab006ab92882038a7184d93e6e514
Andrew <achapkowski@users.noreply.github.com>
2024-08-27 11:30:59 -0400

    Update src/arcgis/_impl/_async/jobs.py
    Co-authored-by: Jay Roebuck <jroebuck@esri.com>

--- src/arcgis/_impl/_async/jobs.py
+++ src/arcgis/_impl/_async/jobs.py
@@ -208,7 +208,7 @@ class NotebookJob(Job):
 
         :return: boolean
         """
-        return self._job_status.get("status", None) in [
+        return self._job_status.get("status") in [
             "COMPLETED",
             "CANCELLED",
         ]
