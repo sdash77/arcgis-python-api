f9d32aadd0df6dce0ecd69b168e067ea3f374bb1
NANAE AUBRY <nanaeaubry@gmail.com>
2024-09-20 11:46:18 +0200

    update error message

--- src/arcgis/gis/_impl/_content_manager/folder/core.py
+++ src/arcgis/gis/_impl/_content_manager/folder/core.py
@@ -707,7 +707,7 @@ class Folder:
         }
         if item_properties.get("overwrite", False):
             logger.warning(
-                "The property `overwrite` is not supported and will be ignored."
+                "The property `overwrite` is deprecated and support will be removed two releases after 2.4.0."
             )
         text: str = text or item_properties.pop("text", None)
 
