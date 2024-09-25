479db6d3ddc0f53943f30ef0befe35ba7e63cfb1
Andrew <achapkowski@esri.com>
2024-09-25 06:30:09 -0400

    fix import issues.

--- src/arcgis/learn/__init__.py
+++ src/arcgis/learn/__init__.py
@@ -11,8 +11,12 @@ import json as _json
 import arcgis as _arcgis
 from arcgis.raster._layer import ImageryLayer as _ImageryLayer
 from arcgis.raster._util import _set_context, _id_generator
-from ._scannedmapdigitizer import ScannedMapDigitizer
-from .models._timm_utils import load_timm_bckbn_pretrained
+
+try:
+    from ._scannedmapdigitizer import ScannedMapDigitizer
+    from .models._timm_utils import load_timm_bckbn_pretrained
+except ImportError as ie:
+    print(ie)
 
 # from timm.models import helpers
 
@@ -69,9 +73,12 @@ if not _LAMBDA_TEXT_CLASSIFICATION:
     from ._object_tracker import ObjectTracker
 
     from ._utils.pointcloud_data import Transform3d
-from ._data import prepare_data, prepare_tabulardata, prepare_textdata
-from ._process_df import process_df, add_datepart
-from ._utils.evaluate_batchsize import estimate_batch_size
+try:
+    from ._data import prepare_data, prepare_tabulardata, prepare_textdata
+    from ._process_df import process_df, add_datepart
+    from ._utils.evaluate_batchsize import estimate_batch_size
+except:
+    print("here")
 
 
 _point_cloud_classification_model_list = ["PointCNN", "RandLANet", "SQNSeg"]
@@ -1638,7 +1645,10 @@ class Model:
             model = _json.loads(self._model)
             if "url" in model.keys():
                 return "<Model:%s>" % self._model
-            return "<Model Title:%s owner:%s>" % (self.item.title, self.item.owner)
+            return "<Model Title:%s owner:%s>" % (
+                self.item.title,
+                self.item.owner,
+            )
 
         else:
             try:
@@ -1907,5 +1917,10 @@ def export_point_dataset(
     from ._utils.pointcloud_data import prepare_las_data
 
     prepare_las_data(
-        data_path, block_size, max_points, output_path, extra_features, **kwargs
+        data_path,
+        block_size,
+        max_points,
+        output_path,
+        extra_features,
+        **kwargs,
     )
