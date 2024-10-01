c617605e8ce56fad7f56a0c3753576745085e71e
NANAE AUBRY <nanaeaubry@gmail.com>
2024-10-01 12:04:21 +0200

    remove group layer test for table

--- tests/integration/arcgis-mapping/webmap/test_tables.py
+++ tests/integration/arcgis-mapping/webmap/test_tables.py
@@ -14,7 +14,9 @@ class TestTablesMap(unittest.TestCase):
         webmap = Map(gis=self.gis)
         assert webmap
 
-        table_item = self.gis.content.get("9d9a0b8128f649029873e79900fa2885")
+        table_item = self.gis.content.advanced_search(
+            '(car)  (typekeywords:Table)  -type:"Code Attachment" -type:"Featured Items" -type:"Symbol Set" -type:"Color Set" -type:"Windows Viewer Add In" -type:"Windows Viewer Configuration" -type:"Map Area" -typekeywords:"MapAreaPackage"'
+        )["results"][0]
         
         #Test adding from tables property
         webmap.content.add(table_item.tables[0])
@@ -26,23 +28,14 @@ class TestTablesMap(unittest.TestCase):
         webmap.content.add(table_item)
         assert len(webmap.content.tables) == 2
         assert len(webmap.content.layers) == 0
-
-        #Test adding as a group
-        layer_item = self.gis.content.get("bba3d0a070d34cea9a303c24a71d5190")
-        items = [table_item.tables[0], layer_item.layers[0]]
-        webmap.content.add(items)
-        assert len(webmap.content.tables) == 2
-        assert len(webmap.content.layers) == 1
-        
-        assert isinstance(webmap.content.layers[0], GroupLayer)
-        group = webmap.content.layers[0]
-        assert len(group.layers) == 2
         
     def test_table_popup(self):
         webmap = Map(gis=self.gis)
         assert webmap
 
-        table_item = self.gis.content.get("9d9a0b8128f649029873e79900fa2885")
+        table_item = self.gis.content.advanced_search(
+            '(car)  (typekeywords:Table)  -type:"Code Attachment" -type:"Featured Items" -type:"Symbol Set" -type:"Color Set" -type:"Windows Viewer Add In" -type:"Windows Viewer Configuration" -type:"Map Area" -typekeywords:"MapAreaPackage"'
+        )["results"][0]
         
         #Test adding from tables property
         webmap.content.add(table_item.tables[0])
