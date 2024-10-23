40f62e89362f11932a48d5be58c5c9aeaac26c34
Jay Roebuck <jroebuck@esri.com>
2024-10-23 13:17:22 -0400

    update test harness and test class to more fully demonstrate
    you may now run `python unit_template.py` and achieve passing tests!

--- tests/unit_template.py
+++ tests/unit_template.py
@@ -1,11 +1,14 @@
 import unittest
+
+# import mock utils if needed from tests/utils, or MagicMock from unittest
+# from utils.mocks import *  
 from unittest.mock import MagicMock
-from utils.mocks import *  # import mock utils if needed from tests/utils, or MagicMock from unittest
 
 
-# These are test classes for class and functions from tests/sample_class.py
+# These are test classes for class and functions below
 # Tests can be varied for different modules, all based on the classes and methods in arcgis src
 # Please include as much as possible test cases to improve unit test coverage :)
+# Test class should be named as Test<ClassName> and should inherit from unittest.TestCase
 class TestFoo(unittest.TestCase):
     """
     Unittest class for unit testing ArcGIS Python API classes
@@ -15,8 +18,8 @@ class TestFoo(unittest.TestCase):
     def setUp(self):
         """optional method to set up test data, run before each test"""
         # can set up test input, test object, and mock object here
-        self.gis = MagicMock()
-        self.obj = Foo(self.gis)
+        gis = MagicMock()
+        self.obj = Foo(gis)
         pass
 
     def tearDown(self):
@@ -24,54 +27,46 @@ class TestFoo(unittest.TestCase):
         # can set up test output deletion here
         pass
 
-    def test_foo_method(self, data):
+    # all tests should be named as test_<method_name> or test_<method_name>_<case>
+    # tests should accept self (representing the test case harness) as only parameter
+    def test_foo_returns_valid_payload(self):
         """method to test method of a class"""
+        data = "test_data"
         # use unittest assertions here
-        result = self.obj.foo_method(data)
+        result = self.obj.foo(data)
+        # use built-in assert statements on the unittest class
+        self.assertIsInstance(result, dict)
+        # use assert keyword
         assert result
+        # optionally include error message
+        assert "data" in result, "data key not found in result"
 
-    def test_foo_private_method(self, data):
-        """method to test private method of a class"""
-        result = self.obj._private_method()
+    def test_convert_foo_raises_error(self):
+        """method to test private method of a class, demonstrating a raised exception"""
+        with self.assertRaises(NotImplementedError):
+            self.obj._convert_foo()
+
+    # test utils / static methods
+    def test_util_validate_succeeds_even_number_true(self):
+        result = Foo.util_is_valid(10)
         assert result
+    
+    def test_util_validate_succeeds_odd_number_false(self):
+        result = Foo.util_is_valid(11)
+        assert not result
+    
+    def test_util_validate_fails_string_false(self):
+        result = Foo.util_is_valid("test")
+        assert not result
 
-    def test_foo_method_raise_exception(self, error_raising_data):
-        """method to test exception raising cases"""
-        with self.assertRaises(Exception) as e:
-            result = self.obj.foo_method(error_raising_data)
-            assert result
-            assert "<error_message>" in e
+    def test_util_double(self):
+        result = Foo.util_double(10)
+        self.assertEqual(result, 20)
 
-
-class TestUtil(unittest.TestCase):
-    """
-    Unittest class for unit testing ArcGIS Python API utils
-    Run tests that do not need gis connection, as well as static methods in classes
-    """
-    def setUp(self):
-        """optional method to set up test data, run before each test"""
-        # can set up test input, test object, and mock object here
-        self.obj = Foo()
-
-    def tearDown(self):
-        """optional method to tear down test fixture, run after each test"""
-        # can set up test output deletion here
-        pass
-
-    def test_util_function_a(self, data):
-        result = self.obj.util_function_a(data)
-        assert result
-
-    def test_util_function_b(self, data):
-        result = self.obj.util_function_b(data)
-        assert result
-
-
-if __name__ == "__main__":
-    unittest.main()
 
 #########################################################
 # sample class stub for demonstrating unit test template
+# for sample purposes only, not part of the test harness
 #########################################################
 class Foo:
     """
@@ -82,21 +77,35 @@ class Foo:
     def __init__(self, gis):
         self.gis = gis
 
-    def foo_method(self, data):
+    def foo(self, data):
         # a sample method in Foo class
-        pass
+        # just return a valid dict payload
+        return {"data": data}
 
-    def _foo_private_method(self):
+    def _convert_foo(self):
         # a sample private method in Foo class
-        pass
+        # raise an exception to demonstrate a test that verifies exception
+        raise NotImplementedError("_convert_foo not yet implemented")
 
 
     # sample util functions
-    data = None
+    # static methods should manipulate inputs and return pure outputs
+    # they should not depend on class instance or class variables
+    # testing these should be the most straight forward and lowest hanging fruit for unit testing
+    # but this will require a TDD approach and a change in the way the code is written
     @staticmethod
-    def util_function_a(data):
-        pass
+    def util_double(number: int) -> int:
+        return number * 2
     
     @staticmethod
-    def util_function_b(data):
-        pass
+    def util_is_valid(data) -> bool:
+        """returns True if data is a number and is even, else False"""
+        return isinstance(data, int) and data % 2 == 0
+#########################################################
+# end of sample class stub
+#########################################################
+
+# if running this file directly, run the tests
+# please include this stub with your test
+if __name__ == "__main__":
+    unittest.main()
