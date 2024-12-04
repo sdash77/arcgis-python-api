d3a621ab404d0d88128152bd02459b98c064fd30
Andrew <achapkowski@esri.com>
2024-12-04 13:29:42 -0500

    add tests.

---
+++ tests/integration/gis/content/test_user_content.py
@@ -0,0 +1,25 @@
+#######################################################################
+import unittest
+from utils.decorators import profiles, integration_test
+from utils._logging import enable_verbose_logging
+from arcgis.gis import User, GIS, Item
+enable_verbose_logging()
+
+
+@profiles.enterprise_and_agol
+@integration_test
+class TestUserContentMethods(unittest.TestCase):
+    def test_item_folders(self):
+        gis: GIS = self.gis
+        users: list[User] = [user for user in gis.users.search("*") if user.role == 'org_admin']
+        if len(users) > 0:
+            user: User = users[-1]
+            folders = list(user.folders)
+            if len(folders) > 0:
+                folder= folders[0]
+                assert folder
+                assert isinstance(list(user.items(folder, 1)), list)
+    
+
+if __name__ == "__main__":
+    unittest.main()
