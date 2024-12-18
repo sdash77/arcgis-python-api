42fcba80eb2217d666743cb46db199324ef87a06
NANAE AUBRY <nanaeaubry@gmail.com>
2024-12-18 17:30:54 +0100

    blacken

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -16676,7 +16676,7 @@ class Item(dict):
 
         # New parameter that affects arcgis Online and Enterprise 11.4+
         # Applied to geojson, csv, excel
-        if (self._gis.is_arcgisonline or self._gis.version >= [2024,2]) and (
+        if (self._gis.is_arcgisonline or self._gis.version >= [2024, 2]) and (
             fileType in ["excel", "csv", "geojson"]
         ):
             publish_parameters["fieldTypesVersion"] = "V2"
