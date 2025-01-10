6f6f056063b25c8f8273ed3f281487e04a4c9611
Shuang Song <103439749+cariashuang0417@users.noreply.github.com>
2025-01-10 16:33:44 -0500

    add decorator for workflow manager

--- tests/utils/decorators.py
+++ tests/utils/decorators.py
@@ -368,6 +368,10 @@ class profiles:
         "utility_network",
         "your_utility_network_profile",
     )
+    _workflow_manager_profile_parameters = (
+        "workflow_manager",
+        "your_workflow_manager_profile",
+    )
 
     def _get_profile_parameterized_class(*args):
         """Returns a parameterized class for the profile parameters from provided args"""
@@ -497,6 +501,13 @@ class profiles:
         """Run tests for utility network profile"""
         return cls._get_profile_parameterized_class(
             cls._utility_network_profile_parameters
+        )
+
+    @classproperty
+    def workflow_manager(cls):
+        """Run tests for utility network profile"""
+        return cls._get_profile_parameterized_class(
+            cls._workflow_manager_profile_parameters
         )
     # endregion
 
