6215f9bcd1b1a9ed690afb267a24d799892139fd
Jay Roebuck <jroebuck@esri.com>
2025-01-13 19:02:04 -0500

    remove sys.path.insert in test

--- tests/integration/network/test_snap_to_lines.py
+++ tests/integration/network/test_snap_to_lines.py
@@ -1,10 +1,3 @@
-import sys
-
-sys.path.insert(0, r"C:\SVN\geosaurus_issue_11494\src")
-sys.path.insert(1, r"C:\SVN\geosaurus_issue_11494\tests")
-
-
-#######################################################################
 import unittest
 from utils.decorators import profiles, integration_test
 from utils._logging import enable_verbose_logging
