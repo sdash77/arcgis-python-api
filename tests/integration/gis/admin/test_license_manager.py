ed1ade5c3fd4179d83774f36f637c29895d7689f
Andrew <achapkowski@esri.com>
2025-01-15 07:47:29 -0500

    remove skips.

--- tests/integration/gis/admin/test_license_manager.py
+++ tests/integration/gis/admin/test_license_manager.py
@@ -1,8 +1,3 @@
-import sys
-
-sys.path.insert(0, r"C:\SVN\geosaurus_issue_12518\tests")
-sys.path.insert(1, r"C:\SVN\geosaurus_issue_12518\src")
-import unittest
 from utils.decorators import profiles, integration_test
 from arcgis.gis.admin import AGOLAdminManager, PortalAdminManager
 
@@ -19,35 +14,29 @@ class TestLicenseClass(unittest.TestCase):
                 url=f"{self.gis.url}/sharing/rest/", gis=self.gis
             )
 
-    @unittest.skip("said so")
     def test_offline_report(self):
         licenses = self.admin.license.all()
         for lic in licenses:
             assert lic.offline_report
 
-    @unittest.skip("said so")
     def test_get_license(self):
         licenses = self.admin.license.all()
         for lic in licenses:
             assert lic
 
-    @unittest.skip("said so")
     def test_properties(self):
         assert self.admin.license.properties
 
-    @unittest.skip("said so")
     def test_report(self):
         licenses = self.admin.license.all()
         for lic in licenses:
             assert lic.report
 
-    @unittest.skip("said so")
     def test_check(self):
         licenses = self.admin.license.all()
         for lic in licenses:
             assert isinstance(lic.check(self.gis._username), list)
 
-    @unittest.skip("said so")
     def test_user_entitlement(self):
         licenses = self.admin.license.all()
         for lic in licenses:
