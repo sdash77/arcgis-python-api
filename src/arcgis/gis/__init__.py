4ba394b26e117b9d30136fef8b26cc9396107b67
Andrew <achapkowski@users.noreply.github.com>
2025-01-29 09:28:07 -0500

    Update src/arcgis/gis/__init__.py
    Co-authored-by: Jay Roebuck <jroebuck@esri.com>

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -562,7 +562,7 @@ class GIS(object):
                     from getpass import getpass
 
                     password = getpass("Enter PFX password: ")
-                from arcgis.auth.tools._adapter import pfx_to_pem
+                from arcgis.auth.tools import pfx_to_pem
 
                 cert_file, key_file = pfx_to_pem(cert_file, password)
             else:
