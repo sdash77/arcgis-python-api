5d035feaa7bc24a363838c6c00779e10658fa04a
nparavicini7 <nparavicini@esri.com>
2025-01-31 11:05:03 -0800

    parser improvement

--- src/arcgis/apps/itemgraph/_get_dependencies.py
+++ src/arcgis/apps/itemgraph/_get_dependencies.py
@@ -328,6 +328,7 @@ def _parse_dashboard(item):
     for widget in widgets:
         if widget.get("type") == "mapWidget":
             deps.append(widget.get("itemId"))
+            continue
         try:
             for dataset in widget.get("datasets", []):
                 if dataset.get("type") == "serviceDataset":
