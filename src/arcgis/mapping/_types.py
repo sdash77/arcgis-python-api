ffb0c29e3a5a047f892cd8194315d87666b3c8e4
Nanae Aubry <naubry@esri.com>
2025-02-03 14:49:22 +0100

    Update src/arcgis/mapping/_types.py
    Co-authored-by: Jay Roebuck <jroebuck@esri.com>

--- src/arcgis/mapping/_types.py
+++ src/arcgis/mapping/_types.py
@@ -965,8 +965,7 @@ class OfflineMapAreaManager(object):
 
         # region build input parameters - for CreateMapArea tool
         if folder:
-            folder_gen = self._gis.users.me.folders
-            user_folders = [f for f in folder_gen]
+            user_folders = list(self._gis.users.me.folders)
             if user_folders:
                 matching_folder_ids = [
                     f["id"] for f in user_folders if f["title"] == folder
