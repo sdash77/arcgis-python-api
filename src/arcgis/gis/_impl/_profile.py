7c81984cfe29d854f155c96478e2bae9df91d926
Jay Roebuck <jroebuck@esri.com>
2025-02-04 14:14:54 -0500

    update url for standard enterprise
    - uses profile value as default value for standard enterprise credentials
    - adds _retrieve_dict to ProfileManager for better experience in consuming retrieved values

--- src/arcgis/gis/_impl/_profile.py
+++ src/arcgis/gis/_impl/_profile.py
@@ -1034,31 +1034,43 @@ class ProfileManager(object):
             )
 
     # ----------------------------------------------------------------------
-    def _retrieve(self, profile):
-        """gets the login information"""
-        url, username, password, key_file, cert_file, client_id = (
-            None,
-            None,
-            None,
-            None,
-            None,
-            None,
-        )
+    def _retrieve_dict(self, profile):
+        """gets the login information as a dictionary"""
+        result = {
+            "url": None,
+            "username": None,
+            "password": None,
+            "key_file": None,
+            "cert_file": None,
+            "client_id": None,
+        }
         if profile.lower() in [p.lower() for p in self.list()]:
             cfg_file_path = self._cfg_file_path
             config = configparser.ConfigParser()
             if os.path.isfile(cfg_file_path):
                 config.read(cfg_file_path)
             if config.has_option(profile, "url"):
-                url = config[profile]["url"]
+                result["url"] = config[profile]["url"]
             if config.has_option(profile, "username"):
-                username = config[profile]["username"]
+                result["username"] = config[profile]["username"]
             if config.has_option(profile, "key_file"):
-                key_file = config[profile]["key_file"]
+                result["key_file"] = config[profile]["key_file"]
             if config.has_option(profile, "cert_file"):
-                cert_file = config[profile]["cert_file"]
+                result["cert_file"] = config[profile]["cert_file"]
             if config.has_option(profile, "client_id"):
-                client_id = config[profile]["client_id"]
+                result["client_id"] = config[profile]["client_id"]
 
-            password = self._securely_get_password(profile)
-        return url, username, password, key_file, cert_file, client_id
+            result["password"] = self._securely_get_password(profile)
+        return result
+
+    def _retrieve(self, profile):
+        """gets the login information"""
+        result = self._retrieve_dict(profile)
+        return (
+            result["url"],
+            result["username"],
+            result["password"],
+            result["key_file"],
+            result["cert_file"],
+            result["client_id"],
+        )
