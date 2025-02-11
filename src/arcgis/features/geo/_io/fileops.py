845bf8fffc79fee9b6431cf578aaaf2abf645608
nparavicini7 <nparavicini@esri.com>
2025-02-10 20:48:49 -0800

    adjusted error condition

--- src/arcgis/features/geo/_io/fileops.py
+++ src/arcgis/features/geo/_io/fileops.py
@@ -1598,15 +1598,15 @@ def _gdal_to_sedf(file_path):
         geom = feature.geometry()
         if geom is not None:
             # Export geometry to JSON and parse with ujson
-            try:
-                geom_json = _ujson.loads(geom.ExportToJson())
-                esri_geom = Geometry(geom_json)
-                esri_geom.spatialReference = Geometry({"wkid": sr_code})
-                row.append(esri_geom)
-            except:
+            gj = geom.ExportToJson()
+            if not gj:
                 raise RuntimeError(
                     f"Unable to read geometry of type {geom.GetGeometryName()} with gdal."
                 )
+            geom_json = _ujson.loads(gj)
+            esri_geom = Geometry(geom_json)
+            esri_geom.spatialReference = Geometry({"wkid": sr_code})
+            row.append(esri_geom)
         else:
             row.append(None)
 
