e68fdddbba02d1c2e5f1a8a4be28278193d69244
Jay Roebuck <jroebuck@esri.com>
2025-02-19 22:43:25 -0500

    fix for conda init purgatory

--- .devcontainer/init_conda_env.sh
+++ .devcontainer/init_conda_env.sh
@@ -8,8 +8,9 @@ conda env create --file environment.yml --yes
 echo "conda activate geosaurus_dev_env" >> ~/.bashrc
 echo "conda activate geosaurus_dev_env" >> ~/.zshrc
 
-# reload terminal config
-. ~/.bashrc
+# force init of conda shell in this script
+# (conda init requires the shell to be restarted to take effect)
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 
 # install src package
-python -m pip install ./src --no-deps
+conda activate geosaurus_dev_env && python -m pip install ./src --no-deps
