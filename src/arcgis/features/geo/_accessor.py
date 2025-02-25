6896266fbabe269455473a2119ac4b5f1d4148c3
Andrew <achapkowski@users.noreply.github.com>
2025-02-25 10:32:41 -0500

    Update src/arcgis/features/geo/_accessor.py
    Co-authored-by: Jay Roebuck <jroebuck@esri.com>

--- src/arcgis/features/geo/_accessor.py
+++ src/arcgis/features/geo/_accessor.py
@@ -19,13 +19,8 @@ from ._io.fileops import (
     _gdal_to_fc,
 )
 
-try:
-    from pandas.io.json import ujson_dumps as json_dumps
-    from pandas.io.json import ujson_loads as json_loads
-except Exception as ex:
-    from pandas.io.json import dumps as json_dumps
-    from pandas.io.json import loads as json_loads
-
+json_dumps = pd.io.json.ujson_dumps if hasattr(pd.io.json, "ujson_dumps") else pd.io.json.dumps
+json_loads = pd.io.json.ujson_loads if hasattr(pd.io.json, "ujson_loads") else pd.io.json.loads
 from arcgis.auth.tools import LazyLoader
 
 os = LazyLoader("os")
