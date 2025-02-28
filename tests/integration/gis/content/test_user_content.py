fffe08fb9bab86b006490a4f498912b982776d3e
John Yaist <jyaist@esri.com>
2025-02-27 16:27:32 -0800

    update resource path, add verification of new assigned user, update with suggestions

--- tests/integration/gis/content/test_user_content.py
+++ tests/integration/gis/content/test_user_content.py
@@ -4,7 +4,7 @@ import time
 import os
 from types import GeneratorType
 
-from integration.config import QALAB_ROOT_PATH
+from integration.config import QALAB_ROOT_PATH, get_resource_path
 from utils.decorators import profiles, integration_test
 from utils._logging import enable_verbose_logging
 from utils.data_utils import publish_test_item, cleanup_published_items
@@ -12,23 +12,24 @@ from arcgis.gis import GIS, User, Item, Group, Folder, ItemTypeEnum
 
 enable_verbose_logging()
 
+
 @profiles.admin_enterprise_and_agol
 @integration_test
 class TestUserContentMethods(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        
-        cls.uid = int(time.time())
-        cls.qalab_base_path = QALAB_ROOT_PATH
-        cls.qalab_cls_path = os.path.join(cls.qalab_base_path, "gis_mod_Item_cls")
-        cls.shapefile_data = "major_cities_shp.zip"
+
+        uid = int(time.time())
+        source_data_path = get_resource_path(
+            "staging_data/USA_Major_Cities.zip",
+            unique_copy=True,
+        )
 
         cls.published_item = publish_test_item(
-            gis=cls.gis,
-            layer_name=f"major_cities_{cls.uid}",
-            source_data_path=os.path.join(cls.qalab_cls_path, cls.shapefile_data),
+            cls.gis,
+            layer_name=f"ntgrtn_tst_user_content_{uid}",
+            source_data_path=source_data_path,
             item_type=ItemTypeEnum.SHAPEFILE,
-            prep_for_editing=False,
         )
 
         cls.user_list: list[User] = [
@@ -39,11 +40,15 @@ class TestUserContentMethods(unittest.TestCase):
 
     def test_user_folders(self):
         gis: GIS = self.gis
-        if len(self.user_list) == 0:
+        if not self.user_list:
             self.skipTest("No valid users, skipping")
-
         user: User = self.user_list[-1]
         self.published_item.reassign_to(user)
+        self.assertNotEqual(
+            self.published_item.owner,
+            gis.users.me.username,
+            "Item owner should be different than initial owner.",
+        )
         folder_gen = user.folders
         assert isinstance(user.folders, GeneratorType)
         folder = next(folder_gen)
@@ -51,7 +56,13 @@ class TestUserContentMethods(unittest.TestCase):
         assert folder.name == "Root Folder"
         assert len(list(folder.list(item_type=ItemTypeEnum.SHAPEFILE))) >= 1
         assert (
-            len([i for i in user.items(folder) if i.title.startswith("major_cities_")])
+            len(
+                [
+                    i
+                    for i in user.items(folder)
+                    if i.title.startswith("ntgrtn_tst_user_content_")
+                ]
+            )
             >= 2
         )
         pfolder = next(folder_gen)
@@ -60,9 +71,9 @@ class TestUserContentMethods(unittest.TestCase):
         del folder_gen
 
     def test_user_groups(self):
-        for user in self.user_list:
-            if len(user.groups) > 0:
-                break
+        user = next((user for user in self.user_list if len(user.groups) > 0), None)
+        if not user:
+            self.skipTest("No user who with groups configured.")
         assert isinstance(user.groups[0], Group)
 
     def test_user_items(self):
@@ -76,6 +87,7 @@ class TestUserContentMethods(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cleanup_published_items(items=[cls.published_item])
+
 
 if __name__ == "__main__":
     unittest.main()
