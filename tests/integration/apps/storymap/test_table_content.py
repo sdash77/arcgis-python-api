60f9ef96aac4cdc533e8ad39c53550739eadb241
NANAE AUBRY <nanaeaubry@gmail.com>
2025-03-04 08:51:39 -0800

    Fix table storymaps not accepting string

--- tests/integration/apps/storymap/test_table_content.py
+++ tests/integration/apps/storymap/test_table_content.py
@@ -23,9 +23,9 @@ class TestTableContent(unittest.TestCase):
 
         cells = table.content
         cells.loc["0"] = [
-            {"value": Text("Hello")},
+            {"value": "Hello"},
             {"value": Text("World")},
-            {"value": Text("!")},
+            {"value": "!"},
         ]
         cells.loc["1"] = [
             {"value": Text("Hello")},
@@ -40,7 +40,9 @@ class TestTableContent(unittest.TestCase):
 
         table.content = cells
 
+        story.save()
         item = gis.content.get(story._itemid)
+        assert item
         assert story.delete_story()
 
 
