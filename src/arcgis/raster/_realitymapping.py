1226e1427e1202292ca14bbb2666e1fa56fb6784
DhruvDuseja <dduseja@esri.com>
2025-03-12 00:05:16 +0400

    reformat file

--- src/arcgis/raster/_realitymapping.py
+++ src/arcgis/raster/_realitymapping.py
@@ -2235,7 +2235,7 @@ class RMProject:
                     definition=definition,
                     sensor_type=sensor_type,
                     scenario_type=scenario_type,
-                    gis=gis
+                    gis=gis,
                 )
             except:
                 raise RuntimeError("Creation of realitymapping project failed.")
