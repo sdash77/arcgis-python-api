b371d36718e9d2f95d7af0dd1a5846052d08857b
John Yaist <jyaist@esri.com>
2025-03-19 11:34:33 -0700

    remove kwargs from portalpy

--- src/arcgis/gis/_impl/_portalpy.py
+++ src/arcgis/gis/_impl/_portalpy.py
@@ -2387,8 +2387,7 @@ class Portal(object):
         leaving_disallowed: bool = False,
         hidden_members: bool = False,
         membership_access: Optional[str] = None,
-        autojoin: bool = False,
-        **kwargs
+        autojoin: bool = False
     ):
         """Updates a group.
 
