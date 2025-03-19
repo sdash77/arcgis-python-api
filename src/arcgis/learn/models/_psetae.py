a4388c35e8b4e03d70e30c109cc03e05bee38b5f
Soumya Kanta Das <67886231+sdash77@users.noreply.github.com>
2025-03-19 14:07:59 +0530

    add backbone=None

--- src/arcgis/learn/models/_psetae.py
+++ src/arcgis/learn/models/_psetae.py
@@ -105,6 +105,7 @@ class PSETAE(ArcGISModel):
         if pretrained_path is not None:
             self.load(pretrained_path)
         self._code = imagets_classifier_prf
+        self._backbone = None
 
         def __str__(self):
             return self.__repr__()
