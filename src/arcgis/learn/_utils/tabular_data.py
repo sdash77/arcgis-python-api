ef2f57bcc5de69d5b273482e402347351c1b2d77
Pankaj <Pankaj@DELDEVD031.esri.com>
2025-03-19 15:07:19 +0530

    fixed for decesion tree and random forrest

--- src/arcgis/learn/_utils/tabular_data.py
+++ src/arcgis/learn/_utils/tabular_data.py
@@ -2510,9 +2510,17 @@ def show_local_interpretation(
                 matplotlib=True,
             )
         else:
-            shap.force_plot(
-                explainer.expected_value, shap_values, processed_df, matplotlib=True
-            )
+            if isinstance(explainer.expected_value, float):
+                shap.force_plot(
+                    explainer.expected_value, shap_values, processed_df, matplotlib=True
+                )
+            else:
+                shap.plots.force(
+                    explainer.expected_value[0],
+                    shap_values[0][:, 0],
+                    processed_df,
+                    matplotlib=True,
+                )
     elif method == "KernelRegressor":
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", UserWarning)
