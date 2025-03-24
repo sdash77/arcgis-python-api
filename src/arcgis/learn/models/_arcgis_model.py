e5f10d46593c2332bec00241b730904961752c56
VIKASH RANJAN <ranjanvikash26@gmail.com>
2025-03-24 12:57:10 +0530

    clean code arcgis model

--- src/arcgis/learn/models/_arcgis_model.py
+++ src/arcgis/learn/models/_arcgis_model.py
@@ -62,7 +62,6 @@ try:
         transformer_backbone_downstream,
         wavelengths_required_models,
     )
-    from ._dofa_utils import dofa_backbone, dofa_backbones_downstream
     from ._wavelengths import wavelength_dict
 
     # EarlyStoppingCallback should run as one
@@ -459,14 +458,10 @@ def change_tail_transformer(model, data):
 
 
 def _change_tail(model, data, tail_weights_type=None, **kwargs):
-    # print(model.backbone._is_vitdet)
-    if hasattr(model, "_is_dofa"):
-        return model
     if (
         hasattr(model, "backbone")
         and (
             getattr(model.backbone, "_is_prithvi", False)
-            or getattr(model.backbone, "_is_dofa", False)
             or getattr(model.backbone, "_is_vitdet", False)
         )
     ) or getattr(model, "_is_vitdet", False):
@@ -666,42 +661,6 @@ def get_backbone_func(backbone, data, **kwargs):
                 num_classes=kwargs.get("num_classes", data.c),
             )
             backbone.__name__ = backbone_name
-        elif backbone in dofa_backbones_downstream:
-            backbone_name = backbone
-            wavelengths = kwargs.get("wavelengths", None)
-            band_names = None
-            if wavelengths is None:
-                if data._emd.get("InputRastersProps", None) is not None:
-                    band_names = data._emd.get("InputRastersProps").get("BandNames")
-                elif data._emd.get("AllTilesStats", None) is not None:
-                    band_names = [
-                        x.get("BandName") for x in data._emd.get("AllTilesStats")
-                    ]
-                else:
-                    raise Exception(
-                        '\nDOFA and CLAY models require a list of central wavelengths corresponding to each data band (in micrometers).\nPlease provide a value (list of floats) for the "wavelengths" keyword argument.',
-                    )
-                wavelengths = get_wavelengths_from_bandnames(band_names)
-                assert len(wavelengths) == len(data._extract_bands)
-                band_names = band_names
-            else:
-                if len(wavelengths) != len(data._extract_bands):
-                    raise Exception(
-                        'The number of wavelengths provided in the "wavelengths" keyword argument does not match the number of bands \nin the input data. Please provide a wavelength for each band in the data.',
-                    )
-
-            backbone = partial(
-                dofa_backbone,
-                backbone_name=backbone,
-                img_size=int(kwargs.get("chip_size", data.chip_size)),
-                pretrained=True,
-                wavelengths=wavelengths,
-                is_clf=kwargs.get("is_clf", False),
-                num_classes=kwargs.get("num_classes", data.c),
-                band_names=band_names,
-            )
-            backbone.__name__ = backbone_name
-
     else:
         backbone = backbone
     return backbone
@@ -1425,19 +1384,6 @@ class ArcGISModel(object):
         else:
             for _key in model_params:
                 _emd_template["ModelParameters"][_key] = model_params[_key]
-
-        if (
-            model_params.get("backbone", None) is not None
-            and model_params["backbone"] in dofa_backbones_downstream
-        ):
-            if self._model_kwargs.get("wavelengths", None) is not None:
-                _emd_template["ModelParameters"]["wavelengths"] = self._model_kwargs[
-                    "wavelengths"
-                ]
-            else:
-                _emd_template["ModelParameters"]["wavelengths"] = (
-                    get_wavelengths_from_bandnames(self._data._band_names)
-                )
 
         if compute_metrics:
             if self._model_metrics_cache == None:
