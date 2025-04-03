a628e3c1ec714af5f1cc3e92246f262ad1104fe5
nparavicini7 <nparavicini@esri.com>
2025-04-03 09:24:22 -0700

    added docstring

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -2037,6 +2037,13 @@ class OfflineContentManager(object):
                                be deleted if any error occurs during the process.
                              * If *False*, any item that fails to import will be skipped and the
                                process will continue. Default is *False*.
+        ----------------     ----------------------------------------------------------------------
+        item_mapping         Optional dictionary. A dictionary of item ids to be remapped to
+                             item ids already existent in the import org. The keys are the item ids 
+                             of the dependencies in the offline package and the values are item 
+                             ids of the intended replacements in the import org. This is useful 
+                             when a specific dependency has already been uploaded and there is no 
+                             need for duplication.
         ================     ======================================================================
 
         :return:
