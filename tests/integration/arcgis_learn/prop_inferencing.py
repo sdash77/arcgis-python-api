ff5f408442b7c0b2e2cb121cbd1a98428f7cc25a
Sanoj Prasad <san14339@esri.com>
2025-04-21 11:05:47 +0530

    removed commented code

--- tests/integration/arcgis_learn/prop_inferencing.py
+++ tests/integration/arcgis_learn/prop_inferencing.py
@@ -1,26 +1,38 @@
+import os
+
+input_data_path_ms = r"D:\files_sanoj\inferencing_test_resources\input_data\ms"
+input_data_path_rgb = r"D:\files_sanoj\inferencing_test_resources\input_data\rgb"
+output_gdb_folder = r"D:\files_sanoj\inferencing_test_resources"
+saved_models_path = r"D:\files_sanoj\inferencing_test_resources\models"
+output_gdb = "outputs_latest.gdb"
+
 data_inferencing = {
-    'ssd': {
-        "name": "singleshotdetector",
-        "inference_function": "DetectObjectsUsingDeepLearning",
-        "image_path": r"C:\files_sanoj\inference_testing\raster\5b1b6fb2-5024-4681-a175-9b667174f48c.tif",
-        "model_path": r"C:\files_sanoj\inference_testing\model\palms_houses_e30.dlpk"
+    'mtre_hourglass': {
+        "name": "mtre_hourglass",
+        "inference_function": "ClassifyPixelsUsingDeepLearning",
+        "input_path_rgb": os.path.join(input_data_path_rgb, "mtre.tif"),
+        "input_path_ms": os.path.join(input_data_path_ms, "mtre.tif"),
+        "model_path_rgb": os.path.join(saved_models_path, "rgb_hourglass.dlpk"),
+        "model_path_ms": os.path.join(saved_models_path, "ms_hourglass.dlpk"),
+        "output_filename_rgb": "segmentation_rgb_mtre_hourglass",
+        "output_filename_ms": "segmentation_ms_mtre_hourglass",
+        "should_test": True,
+        "padding": 32,
+        "batch_size": 4,
+        "threshold": 0.5,
     },
-    'fasterrcnn': {
-        "name": "singleshotdetector",
-        "inference_function": "DetectObjectsUsingDeepLearning",
-        "image_path": r"C:\files_sanoj\inference_testing\raster\5b1b6fb2-5024-4681-a175-9b667174f48c.tif",
-        "model_path": r"C:\files_sanoj\inference_testing\model\palms_houses_e30.dlpk"
-    },
-    'retinanet': {
-        "name": "singleshotdetector",
-        "inference_function": "DetectObjectsUsingDeepLearning",
-        "image_path": r"C:\files_sanoj\inference_testing\raster\5b1b6fb2-5024-4681-a175-9b667174f48c.tif",
-        "model_path": r"C:\files_sanoj\inference_testing\model\palms_houses_e30.dlpk"
-    },
-    'deeplab': {
-        "name": "singleshotdetector",
-        "inference_function": "classifypixelusingdeeplearning",
-        "image_path": r"C:\files_sanoj\inference_testing\raster\5b1b6fb2-5024-4681-a175-9b667174f48c.tif",
-        "model_path": r"C:\files_sanoj\inference_testing\model\palms_houses_e30.dlpk"
+    'mtre_linknet': {
+        "name": "mtre_linknet",
+        "inference_function": "ClassifyPixelsUsingDeepLearning",
+        "input_path_rgb": os.path.join(input_data_path_rgb, "mtre.tif"),
+        "input_path_ms": os.path.join(input_data_path_ms, "mtre.tif"),
+        "model_path_rgb": os.path.join(saved_models_path, "rgb_linknet.dlpk"),
+        "model_path_ms": os.path.join(saved_models_path, "ms_linknet.dlpk"),
+        "output_filename_rgb": "segmentation_rgb_mtre_linknet",
+        "output_filename_ms": "segmentation_ms_mtre_linknet",
+        "should_test": True,
+        "padding": 32,
+        "batch_size": 4,
+        "threshold": 0.5,
     },
 }
--- tests/integration/arcgis_learn/test_inferencing.py
+++ tests/integration/arcgis_learn/test_inferencing.py
@@ -1,61 +1,123 @@
 import unittest
 import arcpy
-from prop_inferencing import data_inferencing
+import os
+import stat
+import shutil
+from prop_inferencing import (
+    data_inferencing,
+    input_data_path_ms,
+    input_data_path_rgb,
+    output_gdb_folder,
+    saved_models_path,
+    output_gdb,
+)
 from parameterized import parameterized
 
-data_path = r"C:\files_sanoj\inference_testing\raster\5b1b6fb2-5024-4681-a175-9b667174f48c.tif"
-output_folder = r"C:\files_sanoj\inference_testing\detection_output\test.gdb\detected_objects_2"
-model_path = r"C:\files_sanoj\inference_testing\model\palms_houses_e30.dlpk"
-module_skip = True
+file_gdb_path = os.path.join(output_gdb_folder, output_gdb)
 
-
-def update_detection_param():
+def pixelClassification_params_rgb():
+    is_ms = False
     parameter = []
     for key, val in data_inferencing.items():
-        if val["inference_function"] == "DetectObjectsUsingDeepLearning":
-            parameter.append(key)
+        if (val["should_test"] and val["inference_function"] == "ClassifyPixelsUsingDeepLearning"):
+            parameter.append(
+                (
+                    key,
+                    val["input_path_rgb"],
+                    val["model_path_rgb"],
+                    os.path.join(output_gdb_folder, output_gdb, val["output_filename_rgb"]),
+                    val["padding"],
+                    val["batch_size"],
+                    val["threshold"],
+                    os.path.join(output_gdb_folder, output_gdb),
+                )
+            )
     return parameter
 
-def update_pixelClassification_param():
+
+def pixelClassification_params_ms():
+    is_ms = True
     parameter = []
     for key, val in data_inferencing.items():
-        if val["inference_function"] == "classifypixelusingdeeplearning":
-            parameter.append(key)
+        if (val["should_test"] and val["inference_function"] == "ClassifyPixelsUsingDeepLearning" and val["input_path_ms"] != False):
+            parameter.append(
+                (
+                    key+"_ms",
+                    val["input_path_ms"],
+                    val["model_path_ms"],
+                    os.path.join(output_gdb_folder, output_gdb, val["output_filename_ms"]),
+                    val["padding"],
+                    val["batch_size"],
+                    val["threshold"],
+                    os.path.join(output_gdb_folder, output_gdb),
+                )
+            )
     return parameter
 
-def update_classifyObject_param():
-    return []
+def pixelClassificationInferencing(name, input_image_path, model, output_file_path, padding, batch_size, threshold, scratch_workspace):
+    print("Running Inferencing for:", name)
+    with arcpy.EnvManager(scratchWorkspace=scratch_workspace):
+        out_classified_raster = arcpy.ia.ClassifyPixelsUsingDeepLearning(
+            in_raster=input_image_path,
+            in_model_definition=model,
+            arguments=f"padding {padding};batch_size {batch_size};return_probability_raster False;threshold {threshold};test_time_augmentation False;merge_policy max;tile_size 128",
+            processing_mode="PROCESS_AS_MOSAICKED_IMAGE",
+            out_classified_folder=None,
+            out_featureclass=None,
+            overwrite_attachments="NO_OVERWRITE",
+            use_pixelspace="NO_PIXELSPACE"
+        )
+        out_classified_raster.save(output_file_path)
 
 
 class TestInferencing(unittest.TestCase):
-    # def test_objectdetection(self):
-    #     with arcpy.EnvManager(extent='-19519319.9244659 -2402912.15167235 -19518974.483775 -2402641.64113133 PROJCS["unnamed_ellipse_Mercator_2SP",GEOGCS["GCS_unnamed_ellipse",DATUM["D_unknown",SPHEROID["Unknown",6378137.0,0.0]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator_2SP"],PARAMETER["false_easting",0.0],PARAMETER["false_northing",0.0],PARAMETER["central_meridian",0.0],PARAMETER["standard_parallel_1",0.0],UNIT["Meter",1.0]]', scratchWorkspace=r""):
-    #         arcpy.ia.DetectObjectsUsingDeepLearning(
-    #     in_raster=data_path,
-    #     out_detected_objects=output_folder,
-    #     in_model_definition=model_path,
-    #     arguments="padding 0;threshold 0.5;nms_overlap 0.1;batch_size 64;exclude_pad_detections True;test_time_augmentation False;tta_scales 1",
-    #     run_nms="NMS",
-    #     confidence_score_field="Confidence",
-    #     class_value_field="Class",
-    #     max_overlap_ratio=0.1,
-    #     processing_mode="PROCESS_AS_MOSAICKED_IMAGE",
-    #     use_pixelspace="NO_PIXELSPACE",
-    #     in_objects_of_interest="Palm;House"
-    # )
 
-    @unittest.skipIf(module_skip, "Preconditions not met, skipping test")
-    @parameterized.expand(update_detection_param, skip_on_empty=True)
-    def test_objectDetection(self, name):
-        print("I am object detection model", name)
+    @parameterized.expand(pixelClassification_params_rgb)
+    def test_pixelClassification(
+        self,
+        name,
+        input_image_path,
+        model,
+        output_file_path,
+        padding,
+        batch_size,
+        threshold,
+        scratch_workspace,
+    ):
+        pixelClassificationInferencing(name, input_image_path, model, output_file_path, padding, batch_size, threshold, scratch_workspace)
 
-    @unittest.skipIf(module_skip, "Preconditions not met, skipping test")
-    @parameterized.expand(update_pixelClassification_param, skip_on_empty=True)
-    def test_pixelClassification(self, name):
-        print("I am pixel classification model", name)
 
-    @unittest.skipIf(module_skip, "Preconditions not met, skipping test")
-    @parameterized.expand(update_classifyObject_param, skip_on_empty=True)
-    def test_objectClassification(self):
-        print("I am object classification model")
+    @parameterized.expand(pixelClassification_params_ms)
+    def test_pixelClassification_ms(
+        self,
+        name,
+        input_image_path,
+        model,
+        output_file_path,
+        padding,
+        batch_size,
+        threshold,
+        scratch_workspace,
+    ):
+        pixelClassificationInferencing(name, input_image_path, model, output_file_path, padding, batch_size, threshold, scratch_workspace)
 
+
+    @classmethod
+    def setUpClass(cls):
+        if os.path.exists(file_gdb_path):
+            print("The geodatabase already exists at: ", file_gdb_path)
+            print("Deleting existing file geodatabase")
+            shutil.rmtree(file_gdb_path)
+            print("Deleted old geodatabase successfully")
+            print("creating new file geodatabase")
+            arcpy.CreateFileGDB_management(output_gdb_folder, output_gdb)
+            print("File geodatabase created successfully")
+        else:
+             print("creating new file geodatabase")
+             arcpy.CreateFileGDB_management(output_gdb_folder, output_gdb)
+             print("File geodatabase created successfully")
+            
+    
+    @classmethod
+    def tearDownClass(cls):
+        print("Test cases completed successfully")
