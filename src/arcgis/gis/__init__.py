09f35fe41e3fc02e9e14c97a0086da7e166a2ab6
nparavicini7 <nparavicini@esri.com>
2025-05-13 12:41:06 -0400

    Update __init__.py

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -567,7 +567,7 @@ class GIS(object):
                 )
         self.resturl = _create_base_url(url)
         self._url = url.replace("http://", "https://")
-        self.url = self._url = self._url.rstrip("/")
+        self._url = self._url.rstrip("/")
         self._username = username
         self._password = password
         self._key_file = key_file
