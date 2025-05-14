96dee5488ae4b5bae417490c1ada789f208fa9de
Jay Roebuck <jroebuck@esri.com>
2025-05-14 09:27:22 -0400

    move build scripts to meta.yaml

--- build/arcgis/bld.bat
+++
@@ -1,4 +0,0 @@
-"%PYTHON%" .\build\manage_binaries.py copy --conda --python %PY_VER%
-if errorlevel 1 exit 1
-"%PYTHON%" -m pip install .\src --no-deps --no-build-isolation
-if errorlevel 1 exit 1
--- build/arcgis/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-$PYTHON ./build/manage_binaries.py copy --conda --python $PY_VER
-$PYTHON -m pip install ./src --no-deps --no-build-isolation
