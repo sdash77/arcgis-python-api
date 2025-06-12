3412fb783abeb676147c8ca3aaff30f8d1594c55
VIKASH RANJAN <vranjan@esri.com>
2025-06-12 16:26:02 +0530

    Optimized performance of RGB imagery with more than 8-bit depth. (#13183)
    * optimized rgb imagery with more than 8 bit depth
    
    * renamed variable _is_not_8bitrgb to _is_non8bit_rgb
    
    * renamed emd variable
    
    ---------
    
    Co-authored-by: Priyanka Tuteja <39754605+priyankatuteja@users.noreply.github.com>
    Co-authored-by: Rohit Singh <rsingh@esri.com>

--- src/arcgis/learn/_data.py
+++ src/arcgis/learn/_data.py
@@ -1876,6 +1876,7 @@ def prepare_data(
 
     _infered = False
     sensor_name = "ms"
+    _is_non8bit_rgb = False
     if (
         has_esri_files
         and "InputRastersProps" in emd
@@ -1909,6 +1910,8 @@ def prepare_data(
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
                 ds = gdal.Open(_im_path)
+            if ds.RasterCount == 3 and ds.GetRasterBand(1).DataType != gdal.GDT_Byte:
+                _is_non8bit_rgb = True
             if ds.RasterCount != 3 or ds.GetRasterBand(1).DataType != gdal.GDT_Byte:
                 imagery_type = sensor_name
             _infered = True
@@ -3263,6 +3266,7 @@ def prepare_data(
         data._do_normalize = True
         if kwargs.get("do_normalize", None) is not None:
             data._do_normalize = kwargs.get("do_normalize", True)
+        # multispectral normalization
         if data._do_normalize:
             data = data.normalize(
                 stats=(data._scaled_mean_values, data._scaled_std_values),
@@ -3581,6 +3585,11 @@ def prepare_data(
         if [data._bands[i] for i in data._extract_bands] == ["r", "g", "b"]:
             _train_tail = False
         data._train_tail = kwargs.get("train_tail", _train_tail)
+
+    if _is_non8bit_rgb:
+        data._is_multispectral = False
+        data._train_tail = False
+    data._is_non8bit_rgb = _is_non8bit_rgb
 
     if not_label_count[0]:
         logger = logging.getLogger()
