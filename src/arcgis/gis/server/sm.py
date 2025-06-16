54e68011d6f33910a45b6e76ad10829af383caa4
nanaeaubry <nanaeaubry@gmail.com>
2025-06-16 14:18:04 +0200

    fix getting notebook server

--- src/arcgis/gis/server/sm.py
+++ src/arcgis/gis/server/sm.py
@@ -105,7 +105,7 @@ class ServerManager(object):
             admin_url = server["adminUrl"]
             public_url = server["url"]
             try:
-                if server["serverFunction"] == "NotebookServer":
+                if "notebookserver" in server["serverFunction"].lower():
                     try:
                         from arcgis.gis.nb import NotebookServer
 
@@ -118,7 +118,7 @@ class ServerManager(object):
                         nbs = NotebookServer(url=public_url, gis=self._gis)
                         nbs.info
                         self._server_list.append(nbs)
-                elif server["serverFunction"] == "MissionServer":
+                elif "missionserver" in server["serverFunction"].lower():
                     from arcgis.gis.mission import MissionServer
 
                     try:
