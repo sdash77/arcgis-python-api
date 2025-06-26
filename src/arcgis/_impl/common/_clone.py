b3814c959a5ce9c629eaa0b10336fc39b6f17236
nparavicini7 <nparavicini@esri.com>
2025-06-26 16:56:05 -0700

    black

--- src/arcgis/_impl/common/_clone.py
+++ src/arcgis/_impl/common/_clone.py
@@ -5732,7 +5732,9 @@ class _FormDefinition(_ItemDefinition):
             zip_file.close()
 
             # Upload the zip to the item
-            rand_suffix = '_'.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+            rand_suffix = "_".join(
+                random.choices(string.ascii_uppercase + string.digits, k=5)
+            )
             new_form = shutil.copy2(
                 form_zip,
                 os.path.join(temp_dir, new_item["id"] + rand_suffix + ".zip"),
