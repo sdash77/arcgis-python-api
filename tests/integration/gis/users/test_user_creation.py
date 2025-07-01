373ef98a94a7b35c2669d7156c988544887997e6
andrew <achapkowski@esri.com>
2025-07-01 13:22:51 -0400

    fixed test.

--- tests/integration/gis/users/test_user_creation.py
+++ tests/integration/gis/users/test_user_creation.py
@@ -3,7 +3,7 @@ import uuid
 import os
 from arcgis.gis import User
 from utils.decorators import integration_test, profiles
-from config import get_resource_path
+from integration.config import get_resource_path
 
 
 @integration_test
