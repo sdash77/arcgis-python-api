efcb39fc360623f44e6b8f50dab9cce872b31bd0
andrew <achapkowski@esri.com>
2025-07-02 06:20:27 -0400

    fix code format issue.

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -4025,7 +4025,7 @@ class UserManager(object):
                 "level",
                 "email_text",
             }
-            if self._gis._is_kubernetes == False: 
+            if self._gis._is_kubernetes == False:
                 allowed_keys = {
                     "username",
                     "password",
