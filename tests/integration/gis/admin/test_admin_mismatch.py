ca522c813d99b2147a1b9ac557c6fd5ad955124f
andrew <achapkowski@esri.com>
2025-07-11 06:20:27 -0400

    fixed test.

--- tests/integration/gis/admin/test_admin_mismatch.py
+++ tests/integration/gis/admin/test_admin_mismatch.py
@@ -2,25 +2,18 @@ import sys
 import unittest
 from utils.decorators import profiles, integration_test
 from utils._logging import enable_verbose_logging
-from functools import lru_cache
-enable_verbose_logging()
 from arcgis.gis import GIS
 
-@lru_cache(maxsize=128)
-def _version_checker():
-    gis = GIS(profile='your_ent_admin_profile')
-    version = tuple(gis.version)
-    return version
-    
+enable_verbose_logging()
+
 @integration_test
+@profiles.admin_enterprise
 class TestMisMatchEnterprise(unittest.TestCase):
+
     
-    @classmethod
-    def setUpClass(cls):
-        cls.gis = GIS(profile='your_ent_admin_profile', verify_cert=False, trust_env=True)
-    
-    @unittest.skipIf(list(_version_checker()) < [2025, 1], "enterprise does not support `about`")
-    def test_about(self):
+    def test_mismatch(self):
+        if self.gis.version < [2025, 1]:
+            self.skipTest("enterprise does not support `mismatch`")
         val = self.gis.admin.system.indexer.mismatch
         assert isinstance(val, dict)
         assert "status" in val
