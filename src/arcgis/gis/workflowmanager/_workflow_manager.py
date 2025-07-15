d79a0e7b4fa6ca006b86ce54f36411ba045e4585
sam11235 <slabrador@esri.com>
2025-07-15 13:43:47 -0700

    Updating python doc in regard to #9364

--- src/arcgis/gis/workflowmanager/_workflow_manager.py
+++ src/arcgis/gis/workflowmanager/_workflow_manager.py
@@ -315,7 +315,7 @@ class WorkflowManagerAdmin:
         ---------------------  ---------------------------------------------------------
         passphrase             Optional. If exporting encrypted user defined settings, define a passphrase.
                                If no passphrase is specified, the keys for encrypted user defined settings will be
-                               exported without their values.
+                               exported without their values. Starting at 12.0, this property is deprecated.
         ---------------------  ---------------------------------------------------------
         run_async              Optional. A boolean indicating whether to run export item asynchronously. If set to true,
                                export_item will return a :class:`~arcgis.gis.workflowmanager.ItemExecution`. The download
@@ -486,7 +486,7 @@ class WorkflowManagerAdmin:
         -----------------------  ---------------------------------------------------------
         passphrase               Optional. If importing encrypted user defined settings, specify the same passphrase
                                  used when exporting the configuration file. If no passphrase is specified, the keys for
-                                 encrypted user defined settings will be imported without their values.
+                                 encrypted user defined settings will be imported without their values. This 
         -----------------------  ---------------------------------------------------------
         run_async                Optional. A boolean indicating whether to run import item asynchronously. If set to true,
                                  import_item will return a :class:`~arcgis.gis.workflowmanager.ItemExecution`
