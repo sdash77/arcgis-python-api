04198049c1c3de099833a354176067121fcb2d39
John Yaist <jyaist@esri.com>
2025-08-19 13:13:10 -0700

    update profile decorator

--- tests/integration/gis/item/test_item_publishing.py
+++ tests/integration/gis/item/test_item_publishing.py
@@ -26,7 +26,7 @@ def setUpModule():
     warnings.filterwarnings("ignore")
 
 
-@profiles.admin_enterprise
+@profiles.all
 @integration_test
 class Test_Item_publish_file_types(unittest.TestCase):
     @classmethod
