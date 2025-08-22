7b23b30e6b647896afa499c0345dc85636a6ded2
nparavicini7 <nparavicini@esri.com>
2025-08-22 12:06:12 -0400

    black

--- src/arcgis/features/geo/_accessor.py
+++ src/arcgis/features/geo/_accessor.py
@@ -2278,11 +2278,15 @@ class GeoAccessor(object):
                     d2 = os.path.split(dir_name)[0]
                     os.makedirs(d2, exist_ok=True)
                 elif file_name.endswith(".gdb"):
-                    service_name = kwargs.pop("service_name", "a" + uuid.uuid4().hex[0:5])
+                    service_name = kwargs.pop(
+                        "service_name", "a" + uuid.uuid4().hex[0:5]
+                    )
                     gdb_path = location
                     os.makedirs(dir_name, exist_ok=True)
                 else:
-                    service_name = kwargs.pop("service_name", "a" + uuid.uuid4().hex[0:5])
+                    service_name = kwargs.pop(
+                        "service_name", "a" + uuid.uuid4().hex[0:5]
+                    )
                     service_gdb = service_name + ".gdb"
                     gdb_path = os.path.join(location, service_gdb)
                     os.makedirs(gdb_path, exist_ok=True)
