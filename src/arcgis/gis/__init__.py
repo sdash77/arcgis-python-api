5ad83b76210abc33ec26f633f30be11e12c2181e
John Yaist <jyaist@esri.com>
2025-09-12 16:49:01 -0700

    run black formatting

--- src/arcgis/gis/__init__.py
+++ src/arcgis/gis/__init__.py
@@ -17124,12 +17124,12 @@ class Item(dict):
         # Handle Vector Tile Service
         if self.type == "Vector Tile Service":
             params["name"] = self.title.replace(" ", "_")
-            
+
         # Handle Map Service (hosted tile layers)
         if self.type == "Map Service" and "Hosted Service" in self.typeKeywords:
             params["name"] = self.title.replace(" ", "_")
             params["stype"] = "tiles"
-            
+
         # Date Range Handling
         if isinstance(date_range, (tuple, list)) and len(date_range) == 2:
             sd = date_range[0]
