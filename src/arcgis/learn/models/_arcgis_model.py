deb7025ae99fe6095ca2c90332511a6f25b21280
VIKASH RANJAN <ranjanvikash26@gmail.com>
2025-09-16 16:01:47 +0530

    fixed lr_find

--- src/arcgis/learn/models/_arcgis_model.py
+++ src/arcgis/learn/models/_arcgis_model.py
@@ -862,8 +862,9 @@ class ArcGISModel(object):
         # take slice to handule layer_gropus to take multiple lrs
         start_lr = kwargs.get("start_lr", 1e-6)
         end_lr = kwargs.get("end_lr", 0.1)
-        start_lr = slice(start_lr / 10, start_lr)
-        end_lr = slice(end_lr / 10, end_lr)
+        if self._slice_lr is True and len(self.learn.layer_groups) > 1:
+            start_lr = slice(start_lr / 10, start_lr)
+            end_lr = slice(end_lr / 10, end_lr)
         num_it = min(150, len(self.learn.data.train_dl))
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", UserWarning)
