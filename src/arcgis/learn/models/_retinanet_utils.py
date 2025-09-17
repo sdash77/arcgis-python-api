c72e6d1acd34c5f7fd55c9835a5f89dc9dea5c1e
VIKASH RANJAN <ranjanvikash26@gmail.com>
2025-09-17 12:26:22 +0530

    fixed detection models

--- src/arcgis/learn/models/_retinanet_utils.py
+++ src/arcgis/learn/models/_retinanet_utils.py
@@ -659,7 +659,7 @@ def compute_ap_score(tps, p_scores, clas, n_gts, n_classes):
             aps.append(compute_ap(precision, recall))
         else:
             aps.append(0.0)
-    return aps.item()
+    return aps
 
 
 def compute_ap(precision, recall):
@@ -670,7 +670,7 @@ def compute_ap(precision, recall):
         precision[i - 1] = np.maximum(precision[i - 1], precision[i])
     idx = np.where(recall[1:] != recall[:-1])[0]
     ap = np.sum((recall[idx + 1] - recall[idx]) * precision[idx + 1])
-    return ap
+    return ap.item()
 
 
 try:
